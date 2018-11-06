547b86518ec1fc4db9cdf3f514310fa80513f6c2
Tiziano Santoro <tzn@google.com>
2018-11-06 17:36:00 +0000

    Make hello world example work

--- oak/server/oak_server.cc
+++ oak/server/oak_server.cc
@@ -13,22 +13,21 @@ namespace grpc_server {
 
 // From https://github.com/WebAssembly/wabt/blob/master/src/tools/wasm-interp.cc .
 
-static wabt::Features s_features;
-static std::unique_ptr<wabt::FileStream> s_log_stream;
-static std::unique_ptr<wabt::FileStream> s_stdout_stream;
+static std::unique_ptr<wabt::FileStream> s_log_stream = wabt::FileStream::CreateStdout();
+static std::unique_ptr<wabt::FileStream> s_stdout_stream = wabt::FileStream::CreateStdout();
 
 static wabt::interp::Result PrintCallback(const wabt::interp::HostFunc* func,
                                           const wabt::interp::FuncSignature* sig,
                                           const wabt::interp::TypedValues& args,
                                           wabt::interp::TypedValues& results) {
-  LOG(INFO) << "Called host";
+  LOG(INFO) << "Called host function: " << func->module_name << "." << func->field_name;
   return wabt::interp::Result::Ok;
 }
 
 static wabt::Index UnknownFuncHandler(wabt::interp::Environment* env,
                                       wabt::interp::HostModule* host_module, wabt::string_view name,
                                       wabt::Index sig_index) {
-  LOG(INFO) << "Unknown func export: " << name.to_string();
+  LOG(WARNING) << "Unknown func export: " << name.to_string() << " (sig=" << sig_index << ")";
   std::pair<wabt::interp::HostFunc*, wabt::Index> pair =
       host_module->AppendFuncExport(name, sig_index, PrintCallback);
   return pair.second;
@@ -36,6 +35,28 @@ static wabt::Index UnknownFuncHandler(wabt::interp::Environment* env,
 
 static void InitEnvironment(wabt::interp::Environment* env) {
   wabt::interp::HostModule* go_module = env->AppendHostModule("go");
+
+  go_module->AppendFuncExport("debug", 1, PrintCallback);
+
+  go_module->AppendFuncExport("runtime.wasmExit", 1, PrintCallback);
+  go_module->AppendFuncExport("runtime.wasmWrite", 1, PrintCallback);
+  go_module->AppendFuncExport("runtime.scheduleCallback", 1, PrintCallback);
+  go_module->AppendFuncExport("runtime.clearScheduledCallback", 1, PrintCallback);
+  go_module->AppendFuncExport("runtime.getRandomData", 1, PrintCallback);
+  go_module->AppendFuncExport("runtime.nanotime", 1, PrintCallback);
+  go_module->AppendFuncExport("runtime.walltime", 1, PrintCallback);
+
+  go_module->AppendFuncExport("syscall/js.stringVal", 1, PrintCallback);
+  go_module->AppendFuncExport("syscall/js.valueGet", 1, PrintCallback);
+  go_module->AppendFuncExport("syscall/js.valueSet", 1, PrintCallback);
+  go_module->AppendFuncExport("syscall/js.valueIndex", 1, PrintCallback);
+  go_module->AppendFuncExport("syscall/js.valueSetIndex", 1, PrintCallback);
+  go_module->AppendFuncExport("syscall/js.valueCall", 1, PrintCallback);
+  go_module->AppendFuncExport("syscall/js.valueNew", 1, PrintCallback);
+  go_module->AppendFuncExport("syscall/js.valueLength", 1, PrintCallback);
+  go_module->AppendFuncExport("syscall/js.valuePrepareString", 1, PrintCallback);
+  go_module->AppendFuncExport("syscall/js.valueLoadString", 1, PrintCallback);
+
   go_module->on_unknown_func_export = UnknownFuncHandler;
 
   wabt::interp::HostModule* oak_module = env->AppendHostModule("oak");
@@ -49,22 +70,21 @@ static wabt::Result ReadModule(std::string module_bytes, wabt::interp::Environme
 
   *out_module = nullptr;
 
+  wabt::Features s_features;
   const bool kReadDebugNames = true;
   const bool kStopOnFirstError = true;
   const bool kFailOnCustomSectionError = true;
-  wabt::ReadBinaryOptions options(s_features, s_log_stream.get(), kReadDebugNames,
-                                  kStopOnFirstError, kFailOnCustomSectionError);
+  wabt::Stream* log_stream = nullptr;
+  wabt::ReadBinaryOptions options(s_features, log_stream, kReadDebugNames, kStopOnFirstError,
+                                  kFailOnCustomSectionError);
 
-  LOG(INFO) << "xxx";
   result = wabt::ReadBinaryInterp(env, module_bytes.data(), module_bytes.size(), options, errors,
                                   out_module);
-  LOG(INFO) << "yyy";
 
   if (Succeeded(result)) {
     // env->DisassembleModule(s_stdout_stream.get(), *out_module);
   }
 
-  LOG(INFO) << "Read module";
   return result;
 }
 
@@ -75,38 +95,48 @@ OakServer::OakServer() : Service() {}
                                               ::oak::InitiateComputationResponse* response) {
   LOG(INFO) << "Initate Computation: " << request->DebugString();
 
-  s_stdout_stream = wabt::FileStream::CreateStdout();
-
   wabt::Result result;
   wabt::interp::Environment env;
   InitEnvironment(&env);
+  LOG(INFO) << "Func count: " << env.GetFuncCount();
 
   wabt::Errors errors;
   wabt::interp::DefinedModule* module = nullptr;
+
+  LOG(INFO) << "Reading module";
   result = ReadModule(request->business_logic(), &env, &errors, &module);
   if (wabt::Succeeded(result)) {
-    LOG(INFO) << "Success";
+    LOG(INFO) << "Read module";
+    wabt::interp::Thread::Options thread_options;
+
+    // wabt::Stream* trace_stream = s_stdout_stream.get();
+    wabt::Stream* trace_stream = nullptr;
+    wabt::interp::Executor executor(&env, trace_stream, thread_options);
+    LOG(INFO) << "Executing module";
+
+    // module->start_func_index = 1;
+    // wabt::interp::ExecResult exec_result = executor.RunStartFunction(module);
+
+    wabt::interp::TypedValues args;
+
+    wabt::interp::TypedValue zero(wabt::Type::I32);
+    zero.set_i32(0);
+    args.push_back(zero);
+    args.push_back(zero);
+
+    wabt::interp::ExecResult exec_result = executor.RunExportByName(module, "run", args);
+
+    if (exec_result.result == wabt::interp::Result::Ok) {
+      LOG(INFO) << "Executed module";
+    } else {
+      LOG(WARNING) << "Could not execute module";
+      wabt::interp::WriteResult(s_stdout_stream.get(), "error", exec_result.result);
+      // TODO: Print error.
+    }
   } else {
-    LOG(INFO) << "Failure: " << result;
-    LOG(INFO) << "Errors: " << wabt::FormatErrorsToString(errors, wabt::Location::Type::Binary);
+    LOG(WARNING) << "Could not read module: " << result;
+    LOG(WARNING) << "Errors: " << wabt::FormatErrorsToString(errors, wabt::Location::Type::Binary);
   }
-
-  // int token_cnt = 3;
-  // char *tokens[] = {(char *)"mul", (char *)"11", (char *)"22"};
-
-  // int res = 0;
-  LOG(INFO) << "Invoking function";
-  // res = invoke(m, tokens[0], token_cnt - 1, tokens + 1);
-  LOG(INFO) << "Function invoked";
-  // if (res) {
-  // char *value = value_repr(&m->stack[m->sp]);
-  // LOG(INFO) << "value: " << value;
-  // response->set_value(value);
-  //} else {
-  // fprintf(stderr, "error");
-  // LOG(INFO) << "error";
-  // response->set_value("error");
-  //}
 
   return ::grpc::Status::OK;
 }
