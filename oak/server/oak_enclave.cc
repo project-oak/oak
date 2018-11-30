b0ffe7826fdbebe6fb3f4e85c4c92f2fa491dcb3
Tiziano Santoro <tzn@google.com>
2018-11-30 11:53:22 +0000

    Fix copyright authors

--- oak/server/oak_enclave.cc
+++ oak/server/oak_enclave.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Project Oak authors
+ * Copyright 2018 The Project Oak Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
