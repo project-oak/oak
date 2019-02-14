a24cdfc53ffb9e8e73d9606086eb09ca54f2feb2
Tiziano Santoro <tzn@google.com>
2019-02-14 11:08:43 +0000

    Implement running average example
    Change-Id: I1fd6af2729bdc1c8e347ed3083c1e55907c17af2

---
+++ examples/running_average/client/running_average.cc
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2019 The Project Oak Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "absl/memory/memory.h"
+#include "asylo/util/logging.h"
+#include "gflags/gflags.h"
+#include "include/grpcpp/grpcpp.h"
+
+#include "examples/utils/utils.h"
+#include "oak/client/node_client.h"
+#include "oak/client/scheduler_client.h"
+
+DEFINE_string(scheduler_address, "127.0.0.1:8888", "Address of the Oak Scheduler to connect to");
+DEFINE_string(module, "", "File containing the compiled WebAssembly module");
+
+int main(int argc, char** argv) {
+  ::google::ParseCommandLineFlags(&argc, &argv, /*remove_flags=*/true);
+
+  // Connect to the Oak Scheduler.
+  std::unique_ptr<::oak::SchedulerClient> scheduler_client =
+      ::absl::make_unique<::oak::SchedulerClient>(
+          ::grpc::CreateChannel(FLAGS_scheduler_address, ::grpc::InsecureChannelCredentials()));
+
+  // Load the Oak Module to execute. This needs to be compiled from Rust to WebAssembly separately.
+  std::string module_bytes = ::oak::utils::read_file(FLAGS_module);
+  ::oak::CreateNodeResponse create_node_response = scheduler_client->CreateNode(module_bytes);
+
+  std::stringstream addr;
+  addr << "127.0.0.1:" << create_node_response.port();
+  LOG(INFO) << "Connecting to Oak Node: " << addr.str();
+
+  // Connect to the newly created Oak Node from different clients.
+  std::unique_ptr<::oak::NodeClient> node_client_0 = ::absl::make_unique<::oak::NodeClient>(
+      ::grpc::CreateChannel(addr.str(), ::asylo::EnclaveChannelCredentials(
+                                            ::asylo::BidirectionalNullCredentialsOptions())));
+
+  std::unique_ptr<::oak::NodeClient> node_client_1 = ::absl::make_unique<::oak::NodeClient>(
+      ::grpc::CreateChannel(addr.str(), ::asylo::EnclaveChannelCredentials(
+                                            ::asylo::BidirectionalNullCredentialsOptions())));
+
+  // Perform multiple invocations of the same Oak Node from different clients.
+  {
+    std::string response = node_client_0->Invoke("100");
+    LOG(INFO) << "response: " << response;
+  }
+  {
+    std::string response = node_client_1->Invoke("200");
+    LOG(INFO) << "response: " << response;
+  }
+  {
+    std::string response = node_client_0->Invoke("40");
+    LOG(INFO) << "response: " << response;
+  }
+  {
+    std::string response = node_client_1->Invoke("60");
+    LOG(INFO) << "response: " << response;
+  }
+
+  return 0;
+}
