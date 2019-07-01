b90faf2546d89cbec5aca831305f5d9e4015dcd6
David Drysdale <drysdale@google.com>
2019-07-01 15:17:52 +0100

    Split BufferChannel into halves
    The existing BufferChannel is left over as a bi-directional channel
    implementation - there are two buffers, one for each direction.
    
    Now that channels are unidirectional, this is split into two distinct
    classes:
     - ReadBufferChannelHalf, which reads from a provided buffer.
     - WriteBufferChannelHalf, which writes to the end of a provided
       vector of data.

--- oak/server/buffer_channel.h
+++ oak/server/buffer_channel.h
@@ -22,38 +22,49 @@
 
 namespace oak {
 
-// A channel implementation that forwards read and writes to underlying local buffers, keeping track
-// of read and write cursors.
-//
-// The channel owns the output buffer (std::vector), but does not own the input buffer (absl::Span).
-class BufferChannel final : public ChannelHalf {
+// A channel implementation that only has a receive half, which reads from a fixed
+// buffer provided at construction.  The channel does not own the provided buffer,
+// so the caller must ensure its lifetime is longer than that of the channel.
+class ReadBufferChannelHalf final : public ChannelHalf {
  public:
-  BufferChannel(absl::Span<const char> data_input, std::vector<char>* data_output)
-      : data_input_(data_input), data_output_(data_output){};
+  ReadBufferChannelHalf(absl::Span<const char> data) : data_(data) {}
 
   absl::Span<const char> Read(uint32_t size) override {
     LOG(INFO) << "Reading from channel: " << size << " bytes";
-    absl::Span<const char> data = data_input_.subspan(0, size);
-    data_input_.remove_prefix(data.size());
+    absl::Span<const char> data = data_.subspan(0, size);
+    data_.remove_prefix(data.size());
     return data;
-  };
-
-  uint32_t Write(absl::Span<const char> data) override {
-    LOG(INFO) << "Writing to channel: " << data.size() << " bytes";
-    if (data_output_ == nullptr) {
-      LOG(WARNING) << "Channel is read-only, discarding output";
-      return 0;
-    }
-    data_output_->insert(data_output_->end(), data.cbegin(), data.cend());
-    return data.size();
-  };
+  }
 
  private:
   // This is a view of the data which advances each time the Read method is called.
-  absl::Span<const char> data_input_;
+  absl::Span<const char> data_;
+};
 
-  // Data is inserted each time the Write method is called.
-  std::vector<char>* data_output_;
+// A channel implementation that only has a send half, which writes to an output
+// buffer.  The channel does not own the provided buffer, so the caller must ensure
+// its lifetime is longer than that of the channel.
+class WriteBufferChannelHalf final : public ChannelHalf {
+ public:
+  WriteBufferChannelHalf(std::vector<char>* data) : data_(data) {
+    if (data_ == nullptr) {
+      LOG(ERROR) << "Channel has no output buffer, all written data will be discarded";
+    }
+  }
+
+  uint32_t Write(absl::Span<const char> data) override {
+    LOG(INFO) << "Writing to channel: " << data.size() << " bytes";
+    if (data_ == nullptr) {
+      LOG(WARNING) << "Channel has no output buffer, discarding written data";
+      return 0;
+    }
+    data_->insert(data_->end(), data.cbegin(), data.cend());
+    return data.size();
+  }
+
+ private:
+  // Data is inserted into owner's vector each time the Write method is called.
+  std::vector<char>* data_;
 };
 
 }  // namespace oak
