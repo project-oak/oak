80fef39aa2b50b9095c89f41c71bf3ed485d66d6
iovi <iovi@google.com>
2019-07-12 15:29:47 +0100

    Move rust examples to a different folder
    This allows us to have modules written in different languages.
    For now, we just move this to a subfolder, a future commit will add the
    cpp version. Everything should work as before for now.

--- examples/hello_world/module/rust/build.rs
+++ examples/hello_world/module/rust/build.rs
@@ -3,8 +3,8 @@ extern crate protoc_rust_grpc;
 fn main() {
     protoc_rust_grpc::run(protoc_rust_grpc::Args {
         out_dir: "src/proto",
-        input: &["proto/running_average.proto"],
-        includes: &[],
+        input: &["../../proto/hello_world.proto"],
+        includes: &["../../proto"],
         rust_protobuf: true, // also generate protobuf messages, not just services
         ..Default::default()
     })
