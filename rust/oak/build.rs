6ed55231b2877305e807ddfc1a0a73bec692c541
Michael K. Sanders <mks@google.com>
2019-08-21 13:14:21 +0100

    Fix proto import path

--- rust/oak/build.rs
+++ rust/oak/build.rs
@@ -4,12 +4,12 @@ fn main() {
     protoc_rust::run(protoc_rust::Args {
         out_dir: "src/proto",
         input: &[
-            "../../third_party/google/rpc/status.proto",
+            "../../google/rpc/status.proto",
             "../../oak/proto/grpc_encap.proto",
             "../../oak/proto/oak_api.proto",
             "../../oak/proto/storage.proto",
         ],
-        includes: &["../../oak/proto", "../../third_party"],
+        includes: &["../../oak/proto", "../.."],
         customize: protoc_rust::Customize::default(),
     })
     .expect("protoc");
