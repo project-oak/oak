5de3c39ee9918283939d38f35fbaa8982e9348ca
David Drysdale <drysdale@google.com>
2019-09-11 13:22:54 +0100

    Example multi-node application
    The multinode application has two nodes, a "frontend" and a "backend".
    The frontend receives gRPC requests from the gRPC pseudo-Node, and
    forwards the content to the backend.  The backend transmutes the content
    and returns it to the frontend, which replies over gRPCs.
    
    This example application deliberately does things a little differently
    than other examples, so we can be re-assured that conventions/defaults
    are not actually hard requirements.
    
     - The frontend and backend communicate over the raw channel without
       using gRPC/protobuf encodings (they use serde_json instead).
     - The application configuration uses non-default names for nodes and
       ports.
     - The backend node does not use the oak_derive crate, nor does it
       use the SDK's event_loop() helper.

---
+++ examples/multinode/module/rust/src/lib.rs
@@ -0,0 +1,109 @@
+//
+// Copyright 2019 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#[macro_use]
+extern crate log;
+extern crate oak;
+extern crate oak_log;
+extern crate protobuf;
+extern crate serde;
+
+mod proto;
+
+use oak::{GrpcResult, OakNode};
+use proto::multinode::{ExampleRequest, ExampleResponse};
+use proto::multinode_grpc::{dispatch, ExampleServiceNode};
+use protobuf::ProtobufEnum;
+use serde::{Deserialize, Serialize};
+use std::io::Write;
+
+struct FrontendNode {
+    grpc_in: oak::Handle,
+    grpc_out: oak::Handle,
+    backend_out: oak::SendChannelHalf,
+    read_handles: Vec<oak::Handle>,
+    backend_in: oak::ReceiveChannelHalf,
+}
+
+#[no_mangle]
+pub extern "C" fn oak_main() -> i32 {
+    match std::panic::catch_unwind(|| {
+        let node = FrontendNode::new();
+        let grpc_in = node.grpc_in;
+        let grpc_out = node.grpc_out;
+        oak::grpc_event_loop(node, grpc_in, grpc_out)
+    }) {
+        Ok(rc) => rc,
+        Err(_) => oak::proto::oak_api::OakStatus::ERR_INTERNAL.value(),
+    }
+}
+
+impl oak::OakNode for FrontendNode {
+    fn new() -> Self {
+        oak_log::init(log::Level::Debug, oak::channel_find("logging_port")).unwrap();
+        let backend_in = oak::channel_find("from_backend");
+        FrontendNode {
+            grpc_in: oak::channel_find("gRPC_input"),
+            grpc_out: oak::channel_find("gRPC_output"),
+            backend_out: oak::SendChannelHalf::new(oak::channel_find("to_backend")),
+            read_handles: vec![backend_in],
+            backend_in: oak::ReceiveChannelHalf::new(backend_in),
+        }
+    }
+    fn invoke(&mut self, method: &str, req: &[u8], out: &mut oak::SendChannelHalf) {
+        dispatch(self, method, req, out)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct InternalMessage {
+    msg: String,
+}
+
+impl ExampleServiceNode for FrontendNode {
+    fn example_method(&mut self, req: ExampleRequest) -> GrpcResult<ExampleResponse> {
+        info!("Say hello to {}", req.greeting);
+
+        // Ask the backend to transmute the greeting.
+        let internal_req = InternalMessage { msg: req.greeting };
+        let serialized_req = serde_json::to_string(&internal_req).unwrap();
+        info!("send serialized message to backend: {}", serialized_req);
+        self.backend_out
+            .write_all(&serialized_req.into_bytes())
+            .unwrap();
+
+        // Block until there is a response from the backend available.
+        match oak::wait_on_channels(&self.read_handles) {
+            Ok(_ready_handles) => (),
+            Err(err) => {
+                let mut status = oak::proto::status::Status::new();
+                status.set_code(13); // INTERNAL
+                status.set_message(format!("Wait failure {}", err.value()));
+                return Err(status);
+            }
+        }
+
+        let mut buffer = Vec::<u8>::with_capacity(256);
+        self.backend_in.read_message(&mut buffer).unwrap();
+        let serialized_rsp = String::from_utf8(buffer).unwrap();
+        let internal_rsp: InternalMessage = serde_json::from_str(&serialized_rsp).unwrap();
+        info!("received backend response: {:?}", internal_rsp);
+
+        let mut res = ExampleResponse::new();
+        res.reply = format!("HELLO {}!", internal_rsp.msg);
+        Ok(res)
+    }
+}
---
+++ examples/multinode/module2/rust/src/lib.rs
@@ -0,0 +1,74 @@
+//
+// Copyright 2019 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#[macro_use]
+extern crate log;
+extern crate oak;
+extern crate oak_log;
+extern crate protobuf;
+extern crate serde;
+
+use protobuf::ProtobufEnum;
+use serde::{Deserialize, Serialize};
+use std::io::Write;
+
+// TODO: use common struct definition between modules
+#[derive(Serialize, Deserialize, Debug)]
+struct InternalMessage {
+    msg: String,
+}
+
+#[no_mangle]
+pub extern "C" fn oak_main() -> i32 {
+    match std::panic::catch_unwind(|| {
+        oak_log::init(log::Level::Debug, oak::channel_find("be_logging_port")).unwrap();
+        oak::set_panic_hook();
+        let in_handle = oak::channel_find("from_frontend");
+        let out_handle = oak::channel_find("to_frontend");
+        info!("backend node: in={}, out={}", in_handle, out_handle);
+
+        let handles = vec![in_handle];
+        let mut in_channel = oak::ReceiveChannelHalf::new(in_handle);
+        let mut out_channel = oak::SendChannelHalf::new(out_handle);
+        loop {
+            match oak::wait_on_channels(&handles) {
+                Ok(_ready_handles) => (),
+                Err(err) => return err.value(),
+            }
+            let mut buf = Vec::<u8>::with_capacity(1024);
+            in_channel.read_message(&mut buf).unwrap();
+            if buf.is_empty() {
+                info!("no pending message; poll again");
+                continue;
+            }
+
+            let serialized_req = String::from_utf8(buf).unwrap();
+            let internal_req: InternalMessage = serde_json::from_str(&serialized_req).unwrap();
+            info!("received frontend request: {:?}", internal_req);
+
+            let internal_rsp = InternalMessage {
+                msg: internal_req.msg + "xxx",
+            };
+
+            let serialized_rsp = serde_json::to_string(&internal_rsp).unwrap();
+            info!("send serialized message to frontend: {}", serialized_rsp);
+            out_channel.write_all(&serialized_rsp.into_bytes()).unwrap();
+        }
+    }) {
+        Ok(rc) => rc,
+        Err(_) => oak::proto::oak_api::OakStatus::ERR_INTERNAL.value(),
+    }
+}
