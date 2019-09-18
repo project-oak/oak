3974f595ecf170e2421cff43904cea0ca63b3b80
Michael "Kernel" Sanders <mks@google.com>
2019-09-18 14:18:41 +0100

    Add storage encryption (#225)
    * Eschew abbreviation
    
    * Add storage encryption
    
    * Address review comments
    
    * Address review comments

--- oak/server/storage/storage_node.cc
+++ oak/server/storage/storage_node.cc
@@ -1,4 +1,3 @@
-
 /*
  * Copyright 2019 The Project Oak Authors
  *
@@ -18,6 +17,8 @@
 #include "oak/server/storage/storage_node.h"
 
 #include "absl/memory/memory.h"
+#include "absl/strings/escaping.h"
+#include "asylo/util/cleansing_types.h"
 #include "asylo/util/logging.h"
 #include "grpcpp/create_channel.h"
 #include "oak/common/app_config.h"
@@ -25,35 +26,116 @@
 
 namespace oak {
 
+constexpr size_t kMaxMessageSize = 1 << 16;
+
 static std::string GetStorageId(const std::string& storage_name) {
   // TODO: Generate name-based UUID.
   return storage_name;
 }
 
+static asylo::CleansingVector<uint8_t> GetStorageEncryptionKey(const std::string& storage_name) {
+  // TODO: Request encryption key from escrow service.
+  std::string encryption_key =
+      absl::HexStringToBytes("c0dedeadc0dedeadc0dedeadc0dedeadc0dedeadc0dedeadc0dedeadc0dedead");
+  return asylo::CleansingVector<uint8_t>(encryption_key.begin(), encryption_key.end());
+}
+
 StorageNode::StorageNode(const std::string& name, const std::string& storage_address)
-    : NodeThread(name) {
+    : NodeThread(name),
+      fixed_nonce_generator_(new oak::FixedNonceGenerator()),
+      datum_name_cryptor_(kMaxMessageSize, fixed_nonce_generator_),
+      datum_value_cryptor_(kMaxMessageSize, new asylo::AesGcmSivNonceGenerator()) {
   storage_service_ = oak::Storage::NewStub(
       grpc::CreateChannel(storage_address, grpc::InsecureChannelCredentials()));
 }
 
+const asylo::StatusOr<std::string> StorageNode::EncryptDatum(const std::string& datum,
+                                                             DatumType datum_type) {
+  // TODO: Replace "foo" with identifier for the encryption key.
+  asylo::CleansingVector<uint8_t> key = GetStorageEncryptionKey("foo");
+  asylo::CleansingString additional_authenticated_data;
+  asylo::CleansingString nonce;
+  asylo::CleansingString datum_encrypted;
+  asylo::AesGcmSivCryptor* cryptor = nullptr;
+
+  switch (datum_type) {
+    case DatumType::NAME: {
+      fixed_nonce_generator_->set_datum_name(datum);
+      cryptor = &datum_name_cryptor_;
+      break;
+    };
+    case DatumType::VALUE: {
+      cryptor = &datum_value_cryptor_;
+      break;
+    };
+  };
+  // TODO: RETURN_IF_ERROR when one Status rules them all.
+  asylo::Status status =
+      cryptor->Seal(key, additional_authenticated_data, datum, &nonce, &datum_encrypted);
+  if (!status.ok()) {
+    return status;
+  }
+
+  return absl::StrCat(nonce, datum_encrypted);
+}
+
+const asylo::StatusOr<std::string> StorageNode::DecryptDatum(const std::string& input,
+                                                             DatumType datum_type) {
+  asylo::CleansingString input_clean(input.data(), input.size());
+
+  if (input_clean.size() < kAesGcmSivNonceSize) {
+    return asylo::Status(asylo::error::GoogleError::INVALID_ARGUMENT,
+                         absl::StrCat("Input too short: expected at least ", kAesGcmSivNonceSize,
+                                      " bytes, got ", input_clean.size()));
+  }
+
+  // TODO: Replace "foo" with identifier for the encryption key.
+  asylo::CleansingVector<uint8_t> key(GetStorageEncryptionKey("foo"));
+  asylo::CleansingString additional_authenticated_data;
+  asylo::CleansingString nonce = input_clean.substr(0, kAesGcmSivNonceSize);
+  asylo::CleansingString datum_encrypted = input_clean.substr(kAesGcmSivNonceSize);
+  asylo::CleansingString datum_decrypted;
+  asylo::AesGcmSivCryptor* cryptor = nullptr;
+
+  switch (datum_type) {
+    case DatumType::NAME: {
+      cryptor = &datum_name_cryptor_;
+      break;
+    };
+    case DatumType::VALUE: {
+      cryptor = &datum_value_cryptor_;
+      break;
+    };
+  };
+  // TODO: RETURN_IF_ERROR when one Status rules them all.
+  asylo::Status status =
+      cryptor->Open(key, additional_authenticated_data, datum_encrypted, nonce, &datum_decrypted);
+  if (!status.ok()) {
+    return status;
+  }
+
+  return std::string(datum_decrypted.data(), datum_decrypted.size());
+}
+
 void StorageNode::Run() {
   // Borrow pointers to the relevant channel halves.
-  Handle req_handle = FindChannel(kStorageNodeRequestPortName);
-  Handle rsp_handle = FindChannel(kStorageNodeResponsePortName);
-  ChannelHalf* req_half = BorrowChannel(req_handle);
-  ChannelHalf* rsp_half = BorrowChannel(rsp_handle);
-  if (req_half == nullptr || rsp_half == nullptr) {
-    LOG(ERROR) << "Required channel not available; handles: " << req_handle << ", " << rsp_handle;
+  Handle request_handle = FindChannel(kStorageNodeRequestPortName);
+  Handle response_handle = FindChannel(kStorageNodeResponsePortName);
+  ChannelHalf* request_channel = BorrowChannel(request_handle);
+  ChannelHalf* response_channel = BorrowChannel(response_handle);
+  if (request_channel == nullptr || response_channel == nullptr) {
+    LOG(ERROR) << "Required channel not available; handles: " << request_handle << ", "
+               << response_handle;
   }
   std::vector<std::unique_ptr<ChannelStatus>> status;
-  status.push_back(absl::make_unique<ChannelStatus>(req_handle));
+  status.push_back(absl::make_unique<ChannelStatus>(request_handle));
   while (true) {
     if (!WaitOnChannels(&status)) {
       LOG(WARNING) << "Node termination requested";
       return;
     }
 
-    ReadResult result = req_half->Read(INT_MAX);
+    ReadResult result = request_channel->Read(INT_MAX);
     if (result.required_size > 0) {
       LOG(ERROR) << "Message size too large: " << result.required_size;
       return;
@@ -71,12 +153,19 @@ void StorageNode::Run() {
         StorageReadRequest read_request;
         read_request.set_storage_id(GetStorageId(channel_request.storage_name()));
         read_request.set_transaction_id(channel_request.read_request().transaction_id());
-        read_request.set_datum_name(channel_request.read_request().datum_name());
+
+        // TODO: Propagate error status.
+        asylo::StatusOr<std::string> name_or =
+            EncryptDatum(channel_request.read_request().datum_name(), DatumType::NAME);
+        read_request.set_datum_name(name_or.ValueOrDie());
 
         StorageReadResponse read_response;
         status = storage_service_->Read(&context, read_request, &read_response);
         if (status.ok()) {
-          channel_response.mutable_read_response()->set_datum_value(read_response.datum_value());
+          // TODO: Propagate error status.
+          asylo::StatusOr<std::string> value_or =
+              DecryptDatum(read_response.datum_value(), DatumType::VALUE);
+          channel_response.mutable_read_response()->set_datum_value(value_or.ValueOrDie());
         }
         break;
       }
@@ -84,8 +173,16 @@ void StorageNode::Run() {
         StorageWriteRequest write_request;
         write_request.set_storage_id(GetStorageId(channel_request.storage_name()));
         write_request.set_transaction_id(channel_request.write_request().transaction_id());
-        write_request.set_datum_name(channel_request.write_request().datum_name());
-        write_request.set_datum_value(channel_request.write_request().datum_value());
+
+        // TODO: Propagate error status.
+        asylo::StatusOr<std::string> name_or =
+            EncryptDatum(channel_request.write_request().datum_name(), DatumType::NAME);
+        write_request.set_datum_name(name_or.ValueOrDie());
+
+        // TODO: Propagate error status.
+        asylo::StatusOr<std::string> value_or =
+            EncryptDatum(channel_request.write_request().datum_value(), DatumType::VALUE);
+        write_request.set_datum_value(value_or.ValueOrDie());
 
         StorageWriteResponse write_response;
         status = storage_service_->Write(&context, write_request, &write_response);
@@ -95,7 +192,11 @@ void StorageNode::Run() {
         StorageDeleteRequest delete_request;
         delete_request.set_storage_id(GetStorageId(channel_request.storage_name()));
         delete_request.set_transaction_id(channel_request.delete_request().transaction_id());
-        delete_request.set_datum_name(channel_request.delete_request().datum_name());
+
+        // TODO: Propagate error status.
+        asylo::StatusOr<std::string> name_or =
+            EncryptDatum(channel_request.delete_request().datum_name(), DatumType::NAME);
+        delete_request.set_datum_name(name_or.ValueOrDie());
 
         StorageDeleteResponse delete_response;
         status = storage_service_->Delete(&context, delete_request, &delete_response);
@@ -144,11 +245,13 @@ void StorageNode::Run() {
     channel_response.mutable_status()->set_message(status.error_message());
 
     // Serialize the response and write it back to the Node's STORAGE_IN channel
-    std::string rsp_data;
-    channel_response.SerializeToString(&rsp_data);
-    // TODO: figure out a way to avoid the extra copy (into then out of std::string)
-    std::unique_ptr<Message> rsp_msg = absl::make_unique<Message>(rsp_data.begin(), rsp_data.end());
-    rsp_half->Write(std::move(rsp_msg));
+    std::string response_data;
+    channel_response.SerializeToString(&response_data);
+    // TODO: figure out a way to avoid the extra copy (into then out of
+    // std::string)
+    std::unique_ptr<Message> response_message =
+        absl::make_unique<Message>(response_data.begin(), response_data.end());
+    response_channel->Write(std::move(response_message));
   }
 }
 
--- oak/server/storage/storage_node.h
+++ oak/server/storage/storage_node.h
@@ -20,10 +20,13 @@
 #include <memory>
 #include <thread>
 
+#include "asylo/crypto/aes_gcm_siv.h"
+#include "asylo/util/statusor.h"
 #include "oak/common/handles.h"
 #include "oak/proto/storage.grpc.pb.h"
 #include "oak/server/channel.h"
 #include "oak/server/node_thread.h"
+#include "oak/server/storage/fixed_nonce_generator.h"
 
 namespace oak {
 
@@ -32,7 +35,30 @@ class StorageNode final : public NodeThread {
   StorageNode(const std::string& name, const std::string& storage_address);
 
  private:
+  enum class DatumType : int { NAME, VALUE };
+
+  // Encrypts `datum` as either the name or value of a name-value pair.
+  // Datum names are encrypted with datum_name_cryptor_, using a fixed nonce for
+  // deterministic mapping of the name-value pair.
+  // Datum values are encrypted with datum_value_cryptor_, using a random nonce.
+  // Returns the concatenation of the kAesGcmSivNonceSize-byte nonce followed by
+  // the encrypted datum.
+  // TODO: Convert this to a serialized protocol message.
+  const asylo::StatusOr<std::string> EncryptDatum(const std::string& datum, DatumType datum_type);
+
+  // Decrypts `input` which must be a kAesGcmSivNonceSize-byte nonce followed by
+  // the encrypted datum.
+  const asylo::StatusOr<std::string> DecryptDatum(const std::string& input, DatumType datum_type);
+
   void Run() override;
+
+  // Fixed nonce generator owned by datum_name_cryptor_.
+  FixedNonceGenerator* fixed_nonce_generator_;
+
+  // Cryptor that uses a fixed nonce for deterministic encryption of datum names.
+  asylo::AesGcmSivCryptor datum_name_cryptor_;
+  // Cryptor that uses a random nonce for encryption of datum values.
+  asylo::AesGcmSivCryptor datum_value_cryptor_;
 
   std::unique_ptr<oak::Storage::Stub> storage_service_;
 };
