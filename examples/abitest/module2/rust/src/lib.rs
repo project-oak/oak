7b5cb4ce9358bf7c88402736cf46051315f0fc1f
David Drysdale <drysdale@google.com>
2019-10-04 16:33:57 +0100

    Make abitest backend node more general
    Make it re-usable for other, more complex, tests to come.

--- examples/abitest/module2/rust/src/lib.rs
+++ examples/abitest/module2/rust/src/lib.rs
@@ -25,6 +25,12 @@ extern crate serde;
 use abitest_common::InternalMessage;
 use protobuf::ProtobufEnum;
 
+// Backend node for channel testing.  This node listens for read channel handles
+// arriving on the "from_frontend" channel, then in turn listens for JSON-serialized
+// messages arriving on those read handles.  Any such messages are sent back to the
+// frontend via the "to_frontend" channel, but in a slightly convoluted way: the
+// response is written to a fresh channel, and the read handle for that new channel
+// is what's sent back to the frontend.
 #[no_mangle]
 pub extern "C" fn oak_main() -> i32 {
     match std::panic::catch_unwind(|| {
@@ -42,42 +48,66 @@ pub extern "C" fn oak_main() -> i32 {
 
         let in_channel = oak::ReadHandle { handle: in_handle };
         let out_channel = oak::WriteHandle { handle: out_handle };
-        let handles = vec![in_channel];
+        // Wait on 1+N read handles (starting with N=0).
+        let mut wait_handles = vec![in_channel];
         loop {
-            match oak::wait_on_channels(&handles) {
-                Ok(_ready_handles) => (),
+            let ready_status = match oak::wait_on_channels(&wait_handles) {
+                Ok(ready_status) => ready_status,
                 Err(err) => return err.value(),
-            }
-            let mut buf = Vec::<u8>::with_capacity(1024);
-            let mut handles = Vec::with_capacity(1);
-            oak::channel_read(in_channel, &mut buf, &mut handles);
-            if buf.is_empty() {
-                info!("no pending message; poll again");
-                continue;
-            }
-
-            let serialized_req = String::from_utf8(buf).unwrap();
-            let internal_req: InternalMessage = serde_json::from_str(&serialized_req).unwrap();
-            info!("received frontend request: {:?}", internal_req);
-
-            // Create a new channel and write the response into it.
-            let (new_write, new_read) = oak::channel_create().unwrap();
-            let internal_rsp = InternalMessage {
-                msg: internal_req.msg + "xxx",
             };
-            let serialized_rsp = serde_json::to_string(&internal_rsp).unwrap();
-            info!(
-                "send serialized message to new channel {:?}: {}",
-                new_write, serialized_rsp
-            );
-            oak::channel_write(new_write, &serialized_rsp.into_bytes(), &[]);
-            // Drop the write half now it has been written to.
-            oak::channel_close(new_write.handle);
+            // If there is a message on in_channel, it is expected to contain
+            // a collection of read handles for future listening
+            if ready_status[0] == oak::ChannelReadStatus::READ_READY {
+                let mut buf = Vec::<u8>::with_capacity(16);
+                let mut handles = Vec::with_capacity(5);
+                oak::channel_read(in_channel, &mut buf, &mut handles);
+                for handle in handles {
+                    info!("add new handle {} to waiting set", handle);
+                    wait_handles.push(oak::ReadHandle { handle });
+                }
+            }
 
-            // Send a copy of the read half of the new channel back to the frontend,
-            // then close our handle to the read half.
-            oak::channel_write(out_channel, &[], &[new_read.handle]);
-            oak::channel_close(new_read.handle);
+            // All other channels expect to receive Serde-JSON serialized
+            // messages.
+            for i in 1..ready_status.len() {
+                if ready_status[i] != oak::ChannelReadStatus::READ_READY {
+                    continue;
+                }
+                info!(
+                    "message available on waiting handle[{}]={:?}",
+                    i, wait_handles[i]
+                );
+                let mut buf = Vec::<u8>::with_capacity(1024);
+                let mut handles = Vec::with_capacity(1);
+                oak::channel_read(wait_handles[i], &mut buf, &mut handles);
+                if buf.is_empty() {
+                    info!("no pending message; poll again");
+                    continue;
+                }
+
+                let serialized_req = String::from_utf8(buf).unwrap();
+                let internal_req: InternalMessage = serde_json::from_str(&serialized_req).unwrap();
+                info!("received frontend request: {:?}", internal_req);
+
+                // Create a new channel and write the response into it.
+                let (new_write, new_read) = oak::channel_create().unwrap();
+                let internal_rsp = InternalMessage {
+                    msg: internal_req.msg + "xxx",
+                };
+                let serialized_rsp = serde_json::to_string(&internal_rsp).unwrap();
+                info!(
+                    "send serialized message to new channel {:?}: {}",
+                    new_write, serialized_rsp
+                );
+                oak::channel_write(new_write, &serialized_rsp.into_bytes(), &[]);
+                // Drop the write half now it has been written to.
+                oak::channel_close(new_write.handle);
+
+                // Send a copy of the read half of the new channel back to the frontend,
+                // then close our handle to the read half.
+                oak::channel_write(out_channel, &[], &[new_read.handle]);
+                oak::channel_close(new_read.handle);
+            }
         }
     }) {
         Ok(rc) => rc,
