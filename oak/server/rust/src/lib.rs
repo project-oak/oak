afa9727c5f6d72f6f838f63e6cf185fb19d1d88b
Tiziano Santoro <tzn@google.com>
2019-11-19 18:28:28 +0000

    Implement Rust allocator
    This is a thin wrapper around `malloc` / `free` provided by newlib as
    part of Asylo.
    
    Fixes #322

--- oak/server/rust/src/lib.rs
+++ oak/server/rust/src/lib.rs
@@ -3,13 +3,43 @@
 
 #![no_std]
 #![feature(lang_items)]
+#![feature(alloc_prelude)]
+#![feature(alloc_error_handler)]
 
+extern crate alloc;
+
+// TODO: Move to separate crate.
+mod asylo_alloc;
+
+use alloc::prelude::v1::*;
+use core::alloc::Layout;
 use core::panic::PanicInfo;
+
+// TODO: Move to separate crate and expose safe wrappers.
+#[link(name = "sgx_trts")]
+extern "C" {
+    // SGX-enabled abort function that causes an undefined instruction (`UD2`) to be executed, which
+    // terminates the enclave execution.
+    //
+    // The C type of this function is `extern "C" void abort(void) __attribute__(__noreturn__);`
+    //
+    // See https://github.com/intel/linux-sgx/blob/d166ff0c808e2f78d37eebf1ab614d944437eea3/sdk/trts/linux/trts_pic.S#L565.
+    fn abort() -> !;
+}
+
+#[global_allocator]
+static A: asylo_alloc::System = asylo_alloc::System;
+
+// Define what happens in an Out Of Memory (OOM) condition.
+#[alloc_error_handler]
+fn alloc_error(_layout: Layout) -> ! {
+    unsafe { abort() }
+}
 
 // See https://doc.rust-lang.org/nomicon/panic-handler.html.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    unsafe { abort() }
 }
 
 // See https://doc.rust-lang.org/1.2.0/book/no-stdlib.html.
@@ -20,5 +50,7 @@ pub extern "C" fn eh_personality() {}
 /// It just adds "42" to the provided value and returns it to the caller.
 #[no_mangle]
 pub extern "C" fn add_magic_number(x: i32) -> i32 {
-    x + 42
+    // Allocate a bunch of elements on the heap in order to exercise the allocator.
+    let v: Vec<i32> = (0..10).map(|n| n + 40).collect();
+    x + v[2]
 }
