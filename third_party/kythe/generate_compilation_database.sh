1f3aefc3a4427057e7813e4775427dc1dea57ce5
Ivan Petrov <ivanpetrov@google.com>
2019-12-07 20:25:54 +0000

    Fix comments

--- third_party/kythe/generate_compilation_database.sh
+++ third_party/kythe/generate_compilation_database.sh
@@ -3,17 +3,18 @@
 # Generates a compile_commands.json file at $(bazel info execution_root) for
 # your Clang tooling needs.
 
-BAZEL_ROOT="$(bazel info execution_root)"
-pushd "$BAZEL_ROOT" > /dev/null
-echo "[" > compile_commands.json
+readonly FILE_NAME="compile_commands.json"
+readonly BAZEL_ROOT="$(bazel info execution_root)"
+cd "$BAZEL_ROOT"
+echo '[' > "$FILE_NAME"
 COUNT=0
 find . -name '*.compile_command.json' -print0 | while read -r -d '' fname; do
   if ((COUNT++)); then
-    echo ',' >> compile_commands.json
+    echo ',' >> "$FILE_NAME"
   fi
   sed -e "s|@BAZEL_ROOT@|$BAZEL_ROOT|g" < "$fname" | \
   # Clang doesn't recognize '-fno-canonical-system-headers'.
-  sed -e "s|-fno-canonical-system-headers|-no-canonical-prefixes|g" >> compile_commands.json
+  sed -e "s|-fno-canonical-system-headers|-no-canonical-prefixes|g" >> "$FILE_NAME"
 done
-echo "]" >> compile_commands.json
-popd > /dev/null
+echo ']' >> "$FILE_NAME"
+
