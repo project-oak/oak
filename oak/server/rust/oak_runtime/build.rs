f9f5d98ac8f59ee743e1151335ba4ddd13803e52
David Drysdale <drysdale@google.com>
2020-01-09 09:56:11 +0000

    oak_runtime: move out of sdk/ subdir
    The Rust runtime is expected to be used for things other than just
    testing (via oak_tests), so no longer belongs under the sdk/ subdir.

--- oak/server/rust/oak_runtime/build.rs
+++ oak/server/rust/oak_runtime/build.rs
@@ -1,8 +1,8 @@
 fn main() {
     protoc_rust::run(protoc_rust::Args {
         out_dir: "src/proto",
-        input: &["../../../oak/proto/manager.proto"],
-        includes: &["../../../oak/proto"],
+        input: &["../../../../oak/proto/manager.proto"],
+        includes: &["../../../../oak/proto"],
         customize: protoc_rust::Customize::default(),
     })
     .expect("protoc");
