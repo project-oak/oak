b5e1939cd548ecf29a8d2ca2d82e5f20c9cede74
Tiziano Santoro <tzn@google.com>
2020-01-22 16:38:53 +0000

    Improve `runner` script
    - add command-line flag parsing
    - add `dry_run` command-line flag, which does not run commands
    - add `commands` command-line flag, which prints commands
    - add `logs` command-line flag, which prints logs
    - introduce sequences of commands

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -27,19 +27,59 @@ use colored::*;
 use std::io::{Read, Write};
 use std::path::PathBuf;
 use std::process::Command;
+use structopt::StructOpt;
 
-fn main() {
-    let root = Step::root();
+#[derive(StructOpt, Clone)]
+struct Opt {
+    #[structopt(long)]
+    dry_run: bool,
+    #[structopt(long)]
+    commands: bool,
+    #[structopt(long)]
+    logs: bool,
+}
+
+/// Encapsulates all the local state relative to a step, and is propagated to child steps.
+struct Context {
+    opt: Opt,
+    prefix: String,
+}
+
+impl Context {
+    fn root(opt: Opt) -> Self {
+        Context {
+            opt,
+            prefix: "".to_string(),
+        }
+    }
+
+    fn child(&self, prefix: &str) -> Self {
+        Context {
+            opt: self.opt.clone(),
+            prefix: format!("{} ➡ {}", self.prefix, prefix),
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opt = Opt::from_args();
     // TODO: Add support for running individual commands via command line flags.
-    run_buildifier(&root.with_prefix("buildifier"));
-    run_prettier(&root.with_prefix("prettier"));
-    run_embedmd(&root.with_prefix("embedmd"));
-    run_cargo_fmt(&root.with_prefix("cargo fmt"));
-    run_cargo_test(&root.with_prefix("cargo test"));
-    run_cargo_doc_test(&root.with_prefix("cargo doc test"));
-    run_cargo_clippy(&root.with_prefix("cargo clippy"));
-    run_bazel_build(&root.with_prefix("bazel build"));
-    run_bazel_test(&root.with_prefix("bazel test"));
+    let root = Sequence::new(
+        "root",
+        vec![
+            run_buildifier(),
+            run_prettier(),
+            run_embedmd(),
+            run_cargo_fmt(),
+            run_cargo_test(),
+            run_cargo_doc_test(),
+            run_cargo_clippy(),
+            run_bazel_build(),
+            run_bazel_test(),
+        ],
+    );
+    root.run(&Context::root(opt));
+    Ok(())
 }
 
 /// Return whether to ignore the specified path. This is used by the `walker` package to efficiently
@@ -109,104 +149,132 @@ fn is_cargo_workspace_file(path: &PathBuf) -> bool {
     contents.contains("[workspace]")
 }
 
-fn run_buildifier(step: &Step) {
-    for entry in source_files().filter(is_bazel_file) {
-        step.with_prefix(entry.to_str().unwrap()).run_command(
-            "buildifier",
-            &["-lint=warn", "-mode=check", entry.to_str().unwrap()],
-        );
-    }
+fn run_buildifier() -> R {
+    Sequence::new(
+        "buildifier",
+        source_files().filter(is_bazel_file).map(|entry| {
+            Step::new(
+                entry.to_str().unwrap(),
+                "buildifier",
+                &["-lint=warn", "-mode=check", entry.to_str().unwrap()],
+            )
+        }),
+    )
 }
 
-fn run_prettier(step: &Step) {
-    for entry in source_files().filter(is_markdown_file) {
-        step.with_prefix(entry.to_str().unwrap())
-            .run_command("prettier", &["--check", entry.to_str().unwrap()]);
-    }
+fn run_prettier() -> R {
+    Sequence::new(
+        "prettier",
+        source_files().filter(is_markdown_file).map(|entry| {
+            Step::new(
+                entry.to_str().unwrap(),
+                "prettier",
+                &["--check", entry.to_str().unwrap()],
+            )
+        }),
+    )
 }
 
-fn run_embedmd(step: &Step) {
-    for entry in source_files().filter(is_markdown_file) {
-        step.with_prefix(entry.to_str().unwrap()).run_command(
-            &format!("{}/bin/embedmd", std::env::var("GOPATH").unwrap()),
-            &["-d", entry.to_str().unwrap()],
-        );
-    }
+fn run_embedmd() -> R {
+    Sequence::new(
+        "embedmd",
+        source_files().filter(is_markdown_file).map(|entry| {
+            Step::new(
+                entry.to_str().unwrap(),
+                &format!("{}/bin/embedmd", std::env::var("GOPATH").unwrap()),
+                &["-d", entry.to_str().unwrap()],
+            )
+        }),
+    )
 }
 
-fn run_cargo_fmt(step: &Step) {
-    for m in workspace_manifest_files() {
-        let manifest_filename = m.to_str().unwrap();
-        let step = step.with_prefix(manifest_filename);
-        step.run_command(
-            "cargo",
-            &[
-                "fmt",
-                "--all",
-                &format!("--manifest-path={}", manifest_filename),
-                "--",
-                "--check",
-            ],
-        );
-    }
+fn run_cargo_fmt() -> R {
+    Sequence::new(
+        "cargo fmt",
+        workspace_manifest_files().map(|entry| {
+            let manifest_filename = entry.to_str().unwrap();
+            Step::new(
+                manifest_filename,
+                "cargo",
+                &[
+                    "fmt",
+                    "--all",
+                    &format!("--manifest-path={}", manifest_filename),
+                    "--",
+                    "--check",
+                ],
+            )
+        }),
+    )
 }
 
-fn run_cargo_test(step: &Step) {
-    for m in workspace_manifest_files() {
-        let manifest_filename = m.to_str().unwrap();
-        let step = step.with_prefix(manifest_filename);
-        step.run_command(
-            "cargo",
-            &[
-                "test",
-                "--all-targets",
-                &format!("--manifest-path={}", manifest_filename),
-            ],
-        );
-    }
+fn run_cargo_test() -> R {
+    Sequence::new(
+        "cargo test",
+        workspace_manifest_files().map(|entry| {
+            let manifest_filename = entry.to_str().unwrap();
+            Step::new(
+                manifest_filename,
+                "cargo",
+                &[
+                    "test",
+                    "--all-targets",
+                    &format!("--manifest-path={}", manifest_filename),
+                ],
+            )
+        }),
+    )
 }
 
-fn run_cargo_doc_test(step: &Step) {
-    for m in workspace_manifest_files() {
-        let manifest_filename = m.to_str().unwrap();
-        let step = step.with_prefix(manifest_filename);
-        step.run_command(
-            "cargo",
-            &[
-                "test",
-                "--doc",
-                &format!("--manifest-path={}", manifest_filename),
-            ],
-        );
-    }
+fn run_cargo_doc_test() -> R {
+    Sequence::new(
+        "cargo doc test",
+        workspace_manifest_files().map(|entry| {
+            let manifest_filename = entry.to_str().unwrap();
+            Step::new(
+                manifest_filename,
+                "cargo",
+                &[
+                    "test",
+                    "--doc",
+                    &format!("--manifest-path={}", manifest_filename),
+                ],
+            )
+        }),
+    )
 }
 
-fn run_cargo_clippy(step: &Step) {
-    for m in workspace_manifest_files() {
-        let manifest_filename = m.to_str().unwrap();
-        let step = step.with_prefix(manifest_filename);
-        step.run_command(
-            "cargo",
-            &[
-                "clippy",
-                "--all-targets",
-                &format!("--manifest-path={}", manifest_filename),
-                "--",
-                "--deny=warnings",
-            ],
-        );
-    }
+fn run_cargo_clippy() -> R {
+    Sequence::new(
+        "cargo clippy",
+        workspace_manifest_files().map(|entry| {
+            let manifest_filename = entry.to_str().unwrap();
+            Step::new(
+                manifest_filename,
+                "cargo",
+                &[
+                    "clippy",
+                    "--all-targets",
+                    &format!("--manifest-path={}", manifest_filename),
+                    "--",
+                    "--deny=warnings",
+                ],
+            )
+        }),
+    )
 }
 
-fn run_bazel_build(step: &Step) {
-    step.with_prefix("non-Asylo targets").run_command(
+fn run_bazel_build() -> R {
+    Step::new(
+        "non-Asylo targets",
         "bazel",
         &["build", "--", "//oak/...:all", "-//oak/server/asylo:all"],
-    );
+    )
 }
 
-fn run_bazel_test(step: &Step) {
-    step.with_prefix("host targets").run_command(
+fn run_bazel_test() -> R {
+    Step::new(
+        "host targets",
         "bazel",
         // TODO: Extract these targets with `bazel query` at runtime, based on some label or
         // attribute.
@@ -216,61 +284,103 @@ fn run_bazel_test(step: &Step) {
             "//oak/server/storage:host_tests",
             "//oak/common:host_tests",
         ],
-    );
+    )
 }
+
+trait Runnable {
+    fn run(&self, context: &Context);
+}
+
+/// Type alias for a boxed, dynamically dispatched `Runnable` instance.
+type R = Box<dyn Runnable>;
 
 /// A step executor, which pretty prints the current nesting level, and allows executing commands
 /// and reporting their result.
 struct Step {
-    prefixes: Vec<String>,
+    name: String,
+    executable: String,
+    args: Vec<String>,
 }
 
 impl Step {
     /// Create the root step executor, of which there should only be one.
-    fn root() -> Self {
-        Step {
-            prefixes: Vec::new(),
-        }
+    fn new(name: &str, executable: &str, args: &[&str]) -> R {
+        Box::new(Step {
+            name: name.to_string(),
+            executable: executable.to_string(),
+            args: args.iter().cloned().map(|s| s.to_string()).collect(),
+        })
     }
+}
 
-    /// Create a child step executor, with the provided prefix.
-    fn with_prefix(&self, prefix: &str) -> Step {
-        let mut new_prefixes = self.prefixes.clone();
-        new_prefixes.push(prefix.to_string());
-        Step {
-            prefixes: new_prefixes,
-        }
-    }
-
+impl Runnable for Step {
     /// Run the provided command, printing a status message with the current prefix.
     /// TODO: Add ability to run commands in parallel.
     /// TODO: Return one of three results: pass, fail, or internal error (e.g. if the binary to run
     /// was not found).
-    fn run_command(&self, executable: &str, args: &[&str]) {
+    fn run(&self, context: &Context) {
+        let context = context.child(&self.name);
         // TODO: Measure and print elapsed time.
         // TODO: Add dry-run mode that only prints the commands but does not actually run them.
-        eprint!("{} ⊢ ", self.prefixes.join(" ➡ "));
+        eprint!("{} ⊢ ", context.prefix);
         std::io::stderr().flush().expect("could not flush stderr");
-        let mut cmd = Command::new(executable);
-        cmd.args(args);
-        let child = cmd
-            .stdout(std::process::Stdio::piped())
-            .stderr(std::process::Stdio::piped())
-            .spawn()
-            .expect("could not spawn command");
-        let output = child
-            .wait_with_output()
-            .expect("could not wait for command to terminate");
-        if output.status.success() {
-            eprintln!("{}", "OK".bold().bright_green());
-        } else {
-            eprintln!("{} ({})", "ERROR".bold().bright_red(), output.status);
-            // TODO: Add `verbose` flag to always print logs.
-            eprintln!(
-                "⬇⬇⬇⬇\n{}\n----\n{}\n⬆⬆⬆⬆",
-                std::str::from_utf8(&output.stdout).expect("could not parse command output"),
-                std::str::from_utf8(&output.stderr).expect("could not parse command output"),
-            );
+        let mut cmd = Command::new(&self.executable);
+        cmd.args(&self.args);
+        // `dry_run` implies `commands`, otherwise there is not much to see in the output.
+        if context.opt.commands || context.opt.dry_run {
+            eprint!("[{:?}] ", cmd);
         }
+        if context.opt.dry_run {
+            eprintln!("{}", "SKIPPED".bold().bright_yellow());
+        } else {
+            let child = cmd
+                .stdout(std::process::Stdio::piped())
+                .stderr(std::process::Stdio::piped())
+                .spawn()
+                .expect("could not spawn command");
+            let output = child
+                .wait_with_output()
+                .expect("could not wait for command to terminate");
+            if output.status.success() {
+                eprintln!("{}", "OK".bold().bright_green());
+            } else {
+                eprintln!("{} ({})", "ERROR".bold().bright_red(), output.status);
+            }
+            if !output.status.success() || context.opt.logs {
+                eprintln!(
+                    "⬇⬇⬇⬇\n{}\n----\n{}\n⬆⬆⬆⬆",
+                    std::str::from_utf8(&output.stdout).expect("could not parse command output"),
+                    std::str::from_utf8(&output.stderr).expect("could not parse command output"),
+                );
+            }
+        }
+    }
+}
+
+struct Sequence {
+    name: String,
+    entries: Vec<R>,
+}
+
+impl Sequence {
+    fn new<E>(name: &str, entries: E) -> R
+    where
+        E: IntoIterator<Item = R>,
+    {
+        Box::new(Sequence {
+            name: name.to_string(),
+            entries: entries.into_iter().collect(),
+        })
+    }
+}
+
+impl Runnable for Sequence {
+    fn run(&self, context: &Context) {
+        let context = context.child(&self.name);
+        eprintln!("{} {{", context.prefix);
+        for entry in &self.entries {
+            entry.run(&context);
+        }
+        eprintln!("{} }}", context.prefix);
     }
 }
