ca2772ceca5f3ac93105d74a9a0ee09b30ea9fbf
David Drysdale <drysdale@google.com>
2020-01-23 17:46:52 +0000

    sdk: drop unused copy of manager.rs
    Commit 8271ada26a43 ("oak_runtime: separate runtime out from
    oak_tests") split the oak_runtime code out from oak_tests, but it
    made a copy of the generated code for manager.proto rather than
    just moving it to its new location.
    
    Delete the left-over detritus.

--- sdk/rust/oak_tests/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    protoc_rust::run(protoc_rust::Args {
-        out_dir: "src/proto",
-        input: &["../../../oak/proto/manager.proto"],
-        includes: &["../../../oak/proto"],
-        customize: protoc_rust::Customize::default(),
-    })
-    .expect("protoc");
-}
--- sdk/rust/oak_tests/src/proto/mod.rs
+++
@@ -1,5 +0,0 @@
-//! Auto-generated code for processing protocol buffer message definitions and
-//! gRPC service definitions.
-
-#[allow(clippy::all)]
-pub mod manager;
