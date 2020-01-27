5ccf9f5631ea667eec62f95382cb3ab646cb8a93
Ivan Petrov <ivanpetrov@google.com>
2020-01-27 14:33:22 +0000

    Fix lambda

--- oak/server/wasm_node.cc
+++ oak/server/wasm_node.cc
@@ -597,7 +597,7 @@ wabt::interp::HostFunc::Callback WasmNode::OakRandomGet(wabt::interp::Environmen
 }
 
 wabt::interp::HostFunc::Callback WasmNode::WasiPlaceholder(wabt::interp::Environment* env) {
-  return [this, env](const wabt::interp::HostFunc* func, const wabt::interp::FuncSignature*,
+  return [this](const wabt::interp::HostFunc* func, const wabt::interp::FuncSignature*,
                      const wabt::interp::TypedValues& args, wabt::interp::TypedValues& results) {
     LogHostFunctionCall(name_, func, args);
     LOG(QFATAL) << "{" << name_ << "} WASI is not implemented";
