71a4c919e649c7393f3373fc6e5192b37ec9ee62
Ivan Petrov <ivanpetrov@google.com>
2020-01-28 14:31:22 +0000

    Update generated files

--- examples/hello_world/module/rust/build.rs
+++ examples/hello_world/module/rust/build.rs
@@ -2,7 +2,7 @@ fn main() {
     oak_utils::run_protoc_rust_grpc(protoc_rust_grpc::Args {
         out_dir: "src/proto",
         input: &["../../proto/hello_world.proto"],
-        includes: &["../../proto", "../../third_party"],
+        includes: &["../../proto", "../../../../third_party"],
         rust_protobuf: true, // also generate protobuf messages, not just services
         ..Default::default()
     })
