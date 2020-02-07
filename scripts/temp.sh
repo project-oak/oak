project-oak/oak

commit 4ad9891707b514703db37c45bbe532d94689af4c

Add missing license headers

scripts/temp.sh
@@ -1,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Keep this in sync with /scripts/format.
+
+readonly SCRIPTS_DIR="$(dirname "$(readlink -f "$0")")"
+# shellcheck source=scripts/common
+source "$SCRIPTS_DIR/common"
+
+
+find . \
+  \(  \
+    -not \( -path ./bazel-cache -prune \) -and \
+    -not \( -path ./cargo-cache -prune \) -and \
+    -not \( -path ./examples/target -prune \) -and \
+    -not \( -path ./oak/server/rust/target -prune \) -and \
+    -not \( -path ./sdk/rust/target -prune \) -and \
+    -not \( -path ./third_party -prune \) \
+    \( -type f \( -name '*.rs' -o -name '*.cc' \) \) \
+  \) -exec ./scripts/check_license {} +

examples/abitest/module_0/rust/build.rs
@@ -1,3 +1,19 @@
+//
+// Copyright 2019 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 fn main() {
     oak_utils::run_protoc_rust_grpc(protoc_rust_grpc::Args {
         out_dir: "src/proto",

examples/abitest/module_0/rust/src/proto/mod.rs
@@ -1,2 +1,18 @@
+//
+// Copyright 2019 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 pub mod abitest;
 pub mod abitest_grpc;

examples/chat/module/rust/build.rs
@@ -1,3 +1,19 @@
+//
+// Copyright 2019 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 fn main() {
     oak_utils::run_protoc_rust_grpc(protoc_rust_grpc::Args {
         out_dir: "src/proto",

examples/chat/module/rust/src/command.rs
@@ -1,3 +1,19 @@
+//
+// Copyright 2019 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]

examples/chat/module/rust/src/proto/mod.rs
@@ -1,2 +1,18 @@
+//
+// Copyright 2019 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 pub mod chat;
 pub mod chat_grpc;

examples/hello_world/module/rust/build.rs
@@ -1,3 +1,19 @@
+//
+// Copyright 2019 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 fn main() {
     oak_utils::run_protoc_rust_grpc(protoc_rust_grpc::Args {
         out_dir: "src/proto",

examples/hello_world/module/rust/src/proto/mod.rs
@@ -1,2 +1,18 @@
+//
+// Copyright 2019 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 pub mod hello_world;
 pub mod hello_world_grpc;

commit 52851ee5b37a89fd86df1f301b593c7f079bda30

Remove accidentally added script
Commit 4ad9891707b5 ("Add missing license headers") accidentally
included a scripts/temp.sh

scripts/temp.sh
@@ -1,19 +1,0 @@
-#!/usr/bin/env bash
-
-# Keep this in sync with /scripts/format.
-
-readonly SCRIPTS_DIR="$(dirname "$(readlink -f "$0")")"
-# shellcheck source=scripts/common
-source "$SCRIPTS_DIR/common"
-
-
-find . \
-  \(  \
-    -not \( -path ./bazel-cache -prune \) -and \
-    -not \( -path ./cargo-cache -prune \) -and \
-    -not \( -path ./examples/target -prune \) -and \
-    -not \( -path ./oak/server/rust/target -prune \) -and \
-    -not \( -path ./sdk/rust/target -prune \) -and \
-    -not \( -path ./third_party -prune \) \
-    \( -type f \( -name '*.rs' -o -name '*.cc' \) \) \
-  \) -exec ./scripts/check_license {} +