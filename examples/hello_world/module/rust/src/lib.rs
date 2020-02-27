e864a97f5259b871e313e78b781e16a1ce38ff07
David Drysdale <drysdale@google.com>
2020-02-27 16:50:32 +0000

    Revert "examples/hello_world: use external gRPC server"
    This reverts commit c9a2edee0a11433d01138ec11896881199d6208d.
    
    No longer needed now that abitest includes specific tests of gRPC
    client pseudo-Node functionality.

--- examples/hello_world/module/rust/src/lib.rs
+++ examples/hello_world/module/rust/src/lib.rs
@@ -25,16 +25,9 @@ use proto::hello_world_grpc::{Dispatcher, HelloWorld};
 
 oak::entrypoint!(oak_main => {
     oak::logger::init_default();
-
-    // Try to find a translation service, first as an Oak Node and failing that,
-    // as an external gRPC service
-    let mut translator_client = grpc::client::Client::new("translator", "oak_main");
-    if translator_client.is_none() {
-        translator_client = grpc::client::Client::new("translator-grpc", "unused-arg");
-    }
-
     let node = Node {
-        translator: translator_client.map(translator_common::TranslatorClient),
+        translator: grpc::client::Client::new("translator", "oak_main")
+            .map(translator_common::TranslatorClient),
     };
     Dispatcher::new(node)
 });
