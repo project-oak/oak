07c86e13223bfe2a4fe1b558e346fc8d0226b646
David Drysdale <drysdale@google.com>
2020-03-06 14:28:32 +0000

    runtime: add Oak-specific logging wrapper
    This allows logging to be turned off at compile time (by not defining
    OAK_DEBUG), and allows for the possibility of swapping in different
    logging implementations (e.g. gLog, Asylo logging)

---
+++ oak/common/logging.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 The Project Oak Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OAK_COMMON_LOGGING_H_
+#define OAK_COMMON_LOGGING_H_
+
+// Define Oak-specific wrapper macros for logging.
+
+#ifdef OAK_DEBUG
+
+// Forward on to Asylo logging for the time being. Depends on some internal
+// details of Asylo logging (and so may need updating for new versions of
+// Asylo).
+#include "asylo/util/logging.h"
+#define OAK_LOG(severity) COMPACT_ASYLO_LOG_##severity.stream()
+
+#else
+
+// Definition for compiled-out logging.
+#include <ostream>
+namespace oak {
+
+/// Class representing a log message that drops all info.
+class NullMessage {
+ public:
+  NullMessage& stream() { return *this; }
+};
+
+template <typename T>
+inline NullMessage& operator<<(NullMessage& msg, const T&) {
+  return msg;
+}
+inline NullMessage& operator<<(NullMessage& msg, std::ostream& (*)(std::ostream& os)) {
+  return msg;
+}
+
+}  // namespace oak
+
+#define OAK_LOG(severity) ::oak::NullMessage().stream()
+
+#endif
+
+#endif  // OAK_COMMON_LOGGING_H_
