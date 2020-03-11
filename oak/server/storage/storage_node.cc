10510807010256e8abe31a46328426814c3dddfd
David Drysdale <drysdale@google.com>
2020-03-11 14:41:28 +0000

    storage: re-use oak::grpc helpers
    Change the storage interface so that each storage request is expected
    to be in a channel of its own, rather than flowing on the original
    channel.
    
    This makes the storage component's use of channels more directly
    equivalent to how the gRPC server and client pseudo-Nodes use
    channels.
    
    In turn, this allows for various storage-specific bits of code to be
    removed and replaced by use of oak::grpc helper functions.
    
    Along the way, commonize variable naming between the storage code and
    the gRPC client pseudo-Node code.

--- oak/server/storage/storage_node.cc
+++ oak/server/storage/storage_node.cc
@@ -31,137 +31,164 @@ StorageNode::StorageNode(BaseRuntime* runtime, const std::string& name,
                          const std::string& storage_address)
     : NodeThread(runtime, name), storage_processor_(storage_address) {}
 
-void StorageNode::Run(Handle request_handle) {
+void StorageNode::Run(Handle invocation_handle) {
   // Borrow a pointer to the relevant channel half.
-  MessageChannelReadHalf* request_channel = BorrowReadChannel(request_handle);
-  if (request_channel == nullptr) {
-    LOG(ERROR) << "Required channel not available; handle: " << request_handle;
+  MessageChannelReadHalf* invocation_channel = BorrowReadChannel(invocation_handle);
+  if (invocation_channel == nullptr) {
+    LOG(ERROR) << "Required channel not available; handle: " << invocation_handle;
     return;
   }
   std::vector<std::unique_ptr<ChannelStatus>> channel_status;
-  channel_status.push_back(absl::make_unique<ChannelStatus>(request_handle));
+  channel_status.push_back(absl::make_unique<ChannelStatus>(invocation_handle));
   while (true) {
     if (!WaitOnChannels(&channel_status)) {
       LOG(WARNING) << "Node termination requested";
       return;
     }
 
-    ReadResult result = request_channel->Read(INT_MAX, INT_MAX);
-    if (result.required_size > 0) {
-      LOG(ERROR) << "Message size too large: " << result.required_size;
+    // Expect to receive a pair of handles in the invocation message:
+    //  - Handle to the read half of a channel that holds the request, serialized
+    //    as a GrpcRequest.
+    //  - Handle to the write half of a channel to send responses down, each
+    //    serialized as a GrpcResponse.
+    ReadResult invocation = invocation_channel->Read(INT_MAX, INT_MAX);
+    if (invocation.required_size > 0) {
+      LOG(ERROR) << "Message size too large: " << invocation.required_size;
+      return;
+    }
+    if (invocation.msg->data.size() != 0) {
+      LOG(ERROR) << "Unexpectedly received data in invocation";
+      return;
+    }
+    if (invocation.msg->channels.size() != 2) {
+      LOG(ERROR) << "Wrong number of channels " << invocation.msg->channels.size()
+                 << " in invocation";
       return;
     }
 
-    // Expect to receive a request (as serialized data) and the write half of a
-    // channel to send responses on.
-    if (result.msg->channels.size() == 0) {
-      LOG(ERROR) << "No response channel accompanying storage request";
+    std::unique_ptr<ChannelHalf> half0 = std::move(invocation.msg->channels[0]);
+    auto channel0 = absl::get_if<std::unique_ptr<MessageChannelReadHalf>>(half0.get());
+    if (channel0 == nullptr) {
+      LOG(ERROR) << "First channel accompanying invocation is write-direction";
       return;
     }
-    std::unique_ptr<ChannelHalf> half = std::move(result.msg->channels[0]);
-    auto channel = absl::get_if<std::unique_ptr<MessageChannelWriteHalf>>(half.get());
-    if (channel == nullptr) {
-      LOG(ERROR) << "Channel accompanying storage request is read-direction";
-      return;
-    }
-    std::unique_ptr<MessageChannelWriteHalf> response_channel = std::move(*channel);
-    GrpcRequest channel_request;
-    channel_request.ParseFromString(std::string(result.msg->data.data(), result.msg->data.size()));
+    std::unique_ptr<MessageChannelReadHalf> req_channel = std::move(*channel0);
 
-    std::unique_ptr<GrpcResponse> channel_response;
-    asylo::StatusOr<std::unique_ptr<GrpcResponse>> rsp_or = ProcessMethod(&channel_request);
+    std::unique_ptr<ChannelHalf> half1 = std::move(invocation.msg->channels[1]);
+    auto channel1 = absl::get_if<std::unique_ptr<MessageChannelWriteHalf>>(half1.get());
+    if (channel1 == nullptr) {
+      LOG(ERROR) << "Second channel accompanying invocation is read-direction";
+      return;
+    }
+    std::unique_ptr<MessageChannelWriteHalf> rsp_channel = std::move(*channel1);
+
+    // Expect to read a single request out of the request channel.
+    ReadResult req_result = req_channel->Read(INT_MAX, INT_MAX);
+    if (req_result.required_size > 0) {
+      LOG(ERROR) << "Message size too large: " << req_result.required_size;
+      return;
+    }
+    if (req_result.msg->channels.size() != 0) {
+      LOG(ERROR) << "Unexpectedly received channel handles in request channel";
+      return;
+    }
+    GrpcRequest grpc_req;
+    grpc_req.ParseFromString(std::string(req_result.msg->data.data(), req_result.msg->data.size()));
+
+    std::unique_ptr<GrpcResponse> grpc_rsp;
+    asylo::StatusOr<std::unique_ptr<GrpcResponse>> rsp_or = ProcessMethod(&grpc_req);
     if (!rsp_or.ok()) {
-      LOG(ERROR) << "Failed to perform " << channel_request.method_name() << ": "
+      LOG(ERROR) << "Failed to perform " << grpc_req.method_name() << ": "
                  << rsp_or.status().error_code() << ", '" << rsp_or.status().error_message() << "'";
-      channel_response = absl::make_unique<GrpcResponse>();
-      channel_response->mutable_status()->set_code(rsp_or.status().error_code());
-      channel_response->mutable_status()->set_message(std::string(rsp_or.status().error_message()));
+      grpc_rsp = absl::make_unique<GrpcResponse>();
+      grpc_rsp->mutable_status()->set_code(rsp_or.status().error_code());
+      grpc_rsp->mutable_status()->set_message(std::string(rsp_or.status().error_message()));
     } else {
-      channel_response = std::move(rsp_or).ValueOrDie();
+      grpc_rsp = std::move(rsp_or).ValueOrDie();
     }
 
-    std::string response_data;
-    channel_response->SerializeToString(&response_data);
-    // TODO: figure out a way to avoid the extra copy (into then out of
-    // std::string)
-    std::unique_ptr<Message> response_message = absl::make_unique<Message>();
-    response_message->data.insert(response_message->data.end(), response_data.begin(),
-                                  response_data.end());
-    response_channel->Write(std::move(response_message));
+    grpc_rsp->set_last(true);
+    std::unique_ptr<Message> rsp_msg = absl::make_unique<Message>();
+    size_t serialized_size = grpc_rsp->ByteSizeLong();
+    rsp_msg->data.resize(serialized_size);
+    grpc_rsp->SerializeToArray(rsp_msg->data.data(), rsp_msg->data.size());
+    rsp_channel->Write(std::move(rsp_msg));
 
     // The response channel reference is dropped here.
   }
 }
 
-asylo::StatusOr<std::unique_ptr<GrpcResponse>> StorageNode::ProcessMethod(
-    GrpcRequest* channel_request) {
-  auto channel_response = absl::make_unique<GrpcResponse>();
+asylo::StatusOr<std::unique_ptr<GrpcResponse>> StorageNode::ProcessMethod(GrpcRequest* grpc_req) {
+  auto grpc_rsp = absl::make_unique<GrpcResponse>();
   grpc::Status status;
-  std::string method_name = channel_request->method_name();
+  std::string method_name = grpc_req->method_name();
+
   if (method_name == "/oak.StorageNode/Read") {
-    StorageChannelReadRequest channel_read_request;
-    if (!channel_request->req_msg().UnpackTo(&channel_read_request)) {
+    StorageChannelReadRequest read_req;
+    if (!grpc_req->req_msg().UnpackTo(&read_req)) {
       return asylo::Status(asylo::error::GoogleError::INVALID_ARGUMENT, "Failed to unpack request");
     }
-    StorageChannelReadResponse channel_read_response;
+    StorageChannelReadResponse read_rsp;
     std::string value;
-    ASYLO_ASSIGN_OR_RETURN(value, storage_processor_.Read(channel_read_request.storage_name(),
-                                                          channel_read_request.item().name(),
-                                                          channel_read_request.transaction_id()));
-    channel_read_response.mutable_item()->ParseFromString(value);
+    ASYLO_ASSIGN_OR_RETURN(value,
+                           storage_processor_.Read(read_req.storage_name(), read_req.item().name(),
+                                                   read_req.transaction_id()));
+    read_rsp.mutable_item()->ParseFromString(value);
     // TODO(#449): Check security policy for item.
-    channel_response->mutable_rsp_msg()->PackFrom(channel_read_response);
+    grpc_rsp->mutable_rsp_msg()->PackFrom(read_rsp);
+
   } else if (method_name == "/oak.StorageNode/Write") {
-    StorageChannelWriteRequest channel_write_request;
-    if (!channel_request->req_msg().UnpackTo(&channel_write_request)) {
+    StorageChannelWriteRequest write_req;
+    if (!grpc_req->req_msg().UnpackTo(&write_req)) {
       return asylo::Status(asylo::error::GoogleError::INVALID_ARGUMENT, "Failed to unpack request");
     }
     // TODO(#449): Check integrity policy for item.
     std::string item;
-    channel_write_request.item().SerializeToString(&item);
-    ASYLO_RETURN_IF_ERROR(storage_processor_.Write(channel_write_request.storage_name(),
-                                                   channel_write_request.item().name(), item,
-                                                   channel_write_request.transaction_id()));
+    write_req.item().SerializeToString(&item);
+    ASYLO_RETURN_IF_ERROR(storage_processor_.Write(
+        write_req.storage_name(), write_req.item().name(), item, write_req.transaction_id()));
+
   } else if (method_name == "/oak.StorageNode/Delete") {
-    StorageChannelDeleteRequest channel_delete_request;
-    if (!channel_request->req_msg().UnpackTo(&channel_delete_request)) {
+    StorageChannelDeleteRequest delete_req;
+    if (!grpc_req->req_msg().UnpackTo(&delete_req)) {
       return asylo::Status(asylo::error::GoogleError::INVALID_ARGUMENT, "Failed to unpack request");
     }
     // TODO(#449): Check integrity policy for item.
-    ASYLO_RETURN_IF_ERROR(storage_processor_.Delete(channel_delete_request.storage_name(),
-                                                    channel_delete_request.item().name(),
-                                                    channel_delete_request.transaction_id()));
+    ASYLO_RETURN_IF_ERROR(storage_processor_.Delete(
+        delete_req.storage_name(), delete_req.item().name(), delete_req.transaction_id()));
+
   } else if (method_name == "/oak.StorageNode/Begin") {
-    StorageChannelBeginRequest channel_begin_request;
-    if (!channel_request->req_msg().UnpackTo(&channel_begin_request)) {
+    StorageChannelBeginRequest begin_req;
+    if (!grpc_req->req_msg().UnpackTo(&begin_req)) {
       return asylo::Status(asylo::error::GoogleError::INVALID_ARGUMENT, "Failed to unpack request");
     }
-    StorageChannelBeginResponse channel_begin_response;
+    StorageChannelBeginResponse begin_rsp;
     std::string transaction_id;
-    ASYLO_ASSIGN_OR_RETURN(transaction_id,
-                           storage_processor_.Begin(channel_begin_request.storage_name()));
-    channel_begin_response.set_transaction_id(transaction_id);
-    channel_response->mutable_rsp_msg()->PackFrom(channel_begin_response);
+    ASYLO_ASSIGN_OR_RETURN(transaction_id, storage_processor_.Begin(begin_req.storage_name()));
+    begin_rsp.set_transaction_id(transaction_id);
+    grpc_rsp->mutable_rsp_msg()->PackFrom(begin_rsp);
+
   } else if (method_name == "/oak.StorageNode/Commit") {
-    StorageChannelCommitRequest channel_commit_request;
-    if (!channel_request->req_msg().UnpackTo(&channel_commit_request)) {
+    StorageChannelCommitRequest commit_req;
+    if (!grpc_req->req_msg().UnpackTo(&commit_req)) {
       return asylo::Status(asylo::error::GoogleError::INVALID_ARGUMENT, "Failed to unpack request");
     }
-    ASYLO_RETURN_IF_ERROR(storage_processor_.Commit(channel_commit_request.storage_name(),
-                                                    channel_commit_request.transaction_id()));
+    ASYLO_RETURN_IF_ERROR(
+        storage_processor_.Commit(commit_req.storage_name(), commit_req.transaction_id()));
+
   } else if (method_name == "/oak.StorageNode/Rollback") {
-    StorageChannelRollbackRequest channel_rollback_request;
-    if (!channel_request->req_msg().UnpackTo(&channel_rollback_request)) {
+    StorageChannelRollbackRequest rollback_req;
+    if (!grpc_req->req_msg().UnpackTo(&rollback_req)) {
       return asylo::Status(asylo::error::GoogleError::INVALID_ARGUMENT, "Failed to unpack request");
     }
-    ASYLO_RETURN_IF_ERROR(storage_processor_.Rollback(channel_rollback_request.storage_name(),
-                                                      channel_rollback_request.transaction_id()));
+    ASYLO_RETURN_IF_ERROR(
+        storage_processor_.Rollback(rollback_req.storage_name(), rollback_req.transaction_id()));
   } else {
     LOG(ERROR) << "unknown operation " << method_name;
     return asylo::Status(asylo::error::GoogleError::INVALID_ARGUMENT,
                          "Unknown operation request method.");
   }
-  return std::move(channel_response);
+  return std::move(grpc_rsp);
 }
 
 }  // namespace oak
--- sdk/rust/oak/src/storage/mod.rs
+++ sdk/rust/oak/src/storage/mod.rs
@@ -17,14 +17,12 @@
 //! Helper library for accessing Oak storage services.
 
 use crate::grpc;
-use crate::proto::grpc_encap::{GrpcRequest, GrpcResponse};
 use crate::proto::storage_channel::{
     StorageChannelDeleteRequest, StorageChannelDeleteResponse, StorageChannelReadRequest,
     StorageChannelReadResponse, StorageChannelWriteRequest, StorageChannelWriteResponse,
     StorageItem,
 };
-use log::{info, warn};
-use protobuf::Message;
+use log::info;
 
 /// Default name for predefined node config that corresponds to a storage
 /// pseudo-Node.
@@ -32,7 +30,7 @@ pub const DEFAULT_CONFIG_NAME: &str = "storage";
 
 /// Local representation of the connection to an external storage service.
 pub struct Storage {
-    write_channel: crate::WriteHandle,
+    client: crate::grpc::client::Client,
 }
 
 impl Storage {
@@ -45,31 +43,13 @@ impl Storage {
     /// Create a `Storage` instance using the given name identifying storage
     /// node configuration.
     pub fn new(config: &str) -> Option<Storage> {
-        // Create a channel and pass the read half to a fresh storage Node.
-        let (write_channel, read_channel) = match crate::channel_create() {
-            Ok(x) => x,
-            Err(_e) => return None,
-        };
-        if crate::node_create(config, "oak_main", read_channel).is_err() {
-            if let Err(status) = crate::channel_close(write_channel.handle) {
-                warn!("could not close write channel: {:?}", status);
-            };
-            if let Err(status) = crate::channel_close(read_channel.handle) {
-                warn!("could not close read channel: {:?}", status);
-            };
-            return None;
-        }
-
-        if let Err(status) = crate::channel_close(read_channel.handle) {
-            warn!("could not close read channel: {:?}", status);
-        };
-        Some(Storage { write_channel })
+        crate::grpc::client::Client::new(config, "oak_main").map(|client| Storage { client })
     }
 
     fn execute_operation<Req, Res>(
         &mut self,
         grpc_method_name: &str,
-        operation_request: &Req,
+        operation_request: Req,
     ) -> grpc::Result<Res>
     where
         Req: protobuf::Message,
@@ -77,72 +57,16 @@ impl Storage {
     {
         info!(
             "StorageChannelRequest: {}",
-            protobuf::text_format::print_to_string(operation_request)
+            protobuf::text_format::print_to_string(&operation_request)
         );
-
-        let mut request_any = protobuf::well_known_types::Any::new();
-        // TODO: rust-protobuf does not provide pack_from/unpack_to functions.
-        // https://github.com/stepancheg/rust-protobuf/issues/455
-        request_any.set_type_url(
-            String::from("type.googleapis.com/") + operation_request.descriptor().full_name(),
-        );
-        operation_request
-            .write_to_writer(&mut request_any.value)
-            .expect("could not convert operation request to `Any` value");
-        let mut grpc_request = GrpcRequest::new();
-        grpc_request.method_name = grpc_method_name.to_owned();
-        grpc_request.set_req_msg(request_any);
-        let mut grpc_data = Vec::new();
-        grpc_request
-            .write_to_writer(&mut grpc_data)
-            .expect("could not convert gRPC request to bytes");
-
-        // Create an ephemeral channel for the response.
-        let (rsp_out, rsp_in) = match crate::channel_create() {
-            Ok(x) => x,
-            Err(status) => {
-                return Err(grpc::build_status(
-                    grpc::Code::INTERNAL,
-                    &format!("failed to create storage response channel: {:?}", status),
-                ));
-            }
-        };
-
-        crate::channel_write(self.write_channel, &grpc_data, &[rsp_out.handle])
-            .expect("could not write to channel");
-        crate::channel_close(rsp_out.handle).expect("could not close channel");
-
-        // Block until there is a response available.
-        loop {
-            let wait_result = crate::wait_on_channels(&[rsp_in]).unwrap();
-            if wait_result[0] == crate::ChannelReadStatus::ReadReady {
-                break;
-            }
-        }
-
-        let mut buffer = Vec::<u8>::with_capacity(256);
-        let mut handles = Vec::<crate::Handle>::with_capacity(1);
-        crate::channel_read(rsp_in, &mut buffer, &mut handles)
-            .expect("could not read from channel");
-        if !handles.is_empty() {
-            panic!("unexpected handles received alongside storage request")
-        }
-        let mut grpc_response: GrpcResponse = protobuf::parse_from_reader(&mut &buffer[..])
-            .expect("could not parse gRPC response from bytes");
-        info!(
-            "StorageChannelResponse: {}",
-            protobuf::text_format::print_to_string(&grpc_response)
-        );
-        crate::channel_close(rsp_in.handle).expect("could not close channel");
-
-        let status = grpc_response.take_status();
-        if status.code != 0 {
-            Err(status)
-        } else {
-            let response = protobuf::parse_from_bytes(grpc_response.get_rsp_msg().value.as_slice())
-                .expect("could not parse gRPC response message from `Any` value");
-            Ok(response)
-        }
+        let type_url =
+            String::from("type.googleapis.com/") + operation_request.descriptor().full_name();
+        crate::grpc::invoke_grpc_method(
+            grpc_method_name,
+            operation_request,
+            Some(&type_url),
+            &self.client.invocation_sender,
+        )
     }
 
     /// Read the value associated with the given `name` from the storage
@@ -160,7 +84,7 @@ impl Storage {
         // TODO: Automatically generate boilerplate from the proto definition.
         self.execute_operation::<StorageChannelReadRequest, StorageChannelReadResponse>(
             "/oak.StorageNode/Read",
-            &read_request,
+            read_request,
         )
         .map(|r| r.get_item().get_value().to_vec())
     }
@@ -182,7 +106,7 @@ impl Storage {
         // TODO: Automatically generate boilerplate from the proto definition.
         self.execute_operation::<StorageChannelWriteRequest, StorageChannelWriteResponse>(
             "/oak.StorageNode/Write",
-            &write_request,
+            write_request,
         )
         .map(|_| ())
     }
@@ -202,7 +126,7 @@ impl Storage {
         // TODO: Automatically generate boilerplate from the proto definition.
         self.execute_operation::<StorageChannelDeleteRequest, StorageChannelDeleteResponse>(
             "/oak.StorageNode/Delete",
-            &delete_request,
+            delete_request,
         )
         .map(|_| ())
     }
