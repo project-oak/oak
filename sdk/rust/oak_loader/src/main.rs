e4ba151564c9febf83cb1fe3973bc2933267530e
Ivan Petrov <ivanpetrov@google.com>
2020-03-23 15:47:03 +0000

    Format

--- sdk/rust/oak_loader/src/main.rs
+++ sdk/rust/oak_loader/src/main.rs
@@ -22,38 +22,46 @@
 //! cargo run --package=oak_loader -- --application=<APP_CONFIG_PATH>
 //! ```
 
-use clap::{Arg, ArgMatches, App};
-use oak_runtime::proto::ApplicationConfiguration;
+use clap::{App, Arg, ArgMatches};
 use oak_runtime::configure_and_run;
+use oak_runtime::proto::ApplicationConfiguration;
 use prost::Message;
-use std::io::Read;
 use std::fs::File;
+use std::io::Read;
 
 fn get_arguments() -> ArgMatches<'static> {
     App::new("Oak Loader")
         .about("")
-        .arg(Arg::with_name("application")
-            .short("a")
-            .long("application")
-            .required(true)
-            .takes_value(true)
-            .value_name("APP_CONFIG_FILE")
-            .help("Application configuration file"))
-        .arg(Arg::with_name("ca_cert")
-            .long("ca_cert")
-            .takes_value(true)
-            .value_name("CA_CERT")
-            .help("Path to the PEM-encoded CA root certificate"))
-        .arg(Arg::with_name("private_key")
-            .long("private_key")
-            .takes_value(true)
-            .value_name("PRIVATE_KEY")
-            .help("Path to the private key"))
-        .arg(Arg::with_name("cert_chain")
-            .long("cert_chain")
-            .takes_value(true)
-            .value_name("CERT_CHAIN")
-            .help("Path to the PEM-encoded certificate chain"))
+        .arg(
+            Arg::with_name("application")
+                .short("a")
+                .long("application")
+                .required(true)
+                .takes_value(true)
+                .value_name("APP_CONFIG_FILE")
+                .help("Application configuration file"),
+        )
+        .arg(
+            Arg::with_name("ca_cert")
+                .long("ca_cert")
+                .takes_value(true)
+                .value_name("CA_CERT")
+                .help("Path to the PEM-encoded CA root certificate"),
+        )
+        .arg(
+            Arg::with_name("private_key")
+                .long("private_key")
+                .takes_value(true)
+                .value_name("PRIVATE_KEY")
+                .help("Path to the private key"),
+        )
+        .arg(
+            Arg::with_name("cert_chain")
+                .long("cert_chain")
+                .takes_value(true)
+                .value_name("CERT_CHAIN")
+                .help("Path to the PEM-encoded certificate chain"),
+        )
         .get_matches()
 }
 
@@ -64,7 +72,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app_config_path = arguments
         .value_of("application")
         .expect("Application configuration file is not specified");
-    
+
     let app_config = {
         let mut file = File::open(app_config_path)?;
         let mut buffer = Vec::new();
@@ -72,7 +80,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ApplicationConfiguration::decode(&buffer[..])?
     };
 
-    configure_and_run(app_config)?;
+    configure_and_run(app_config).expect("Runtime error");
 
     Ok(())
 }
