6d8f9cefa97aced373a7e544b2edd6f1f0b63775
David Drysdale <drysdale@google.com>
2020-05-22 10:28:25 +0100

    common: use gLog not OAK_LOG

--- oak/common/app_config_serializer.cc
+++ oak/common/app_config_serializer.cc
@@ -22,9 +22,9 @@
 #include "absl/flags/parse.h"
 #include "absl/memory/memory.h"
 #include "absl/strings/str_split.h"
+#include "glog/logging.h"
 #include "google/protobuf/text_format.h"
 #include "oak/common/app_config.h"
-#include "oak/common/logging.h"
 #include "oak/common/utils.h"
 
 ABSL_FLAG(
@@ -40,15 +40,15 @@ int main(int argc, char* argv[]) {
   absl::ParseCommandLine(argc, argv);
   std::string textproto = absl::GetFlag(FLAGS_textproto);
   if (textproto.empty()) {
-    OAK_LOG(FATAL) << "Textproto file is not specified";
+    LOG(FATAL) << "Textproto file is not specified";
   }
   std::vector<std::string> modules = absl::GetFlag(FLAGS_modules);
   if (modules.empty()) {
-    OAK_LOG(FATAL) << "Wasm modules are not specified";
+    LOG(FATAL) << "Wasm modules are not specified";
   }
   std::string output_file = absl::GetFlag(FLAGS_output_file);
   if (output_file.empty()) {
-    OAK_LOG(FATAL) << "Output file is not specified";
+    LOG(FATAL) << "Output file is not specified";
   }
 
   // Parse module names.
@@ -56,7 +56,7 @@ int main(int argc, char* argv[]) {
   for (const std::string& module : absl::GetFlag(FLAGS_modules)) {
     std::vector<std::string> module_info = absl::StrSplit(module, ':');
     if (module_info.size() != 2) {
-      OAK_LOG(FATAL) << "Incorrect module specification: " << module;
+      LOG(FATAL) << "Incorrect module specification: " << module;
     }
     module_map.emplace(module_info.front(), module_info.back());
   }
@@ -74,18 +74,18 @@ int main(int argc, char* argv[]) {
       if (it != module_map.end()) {
         std::string module_bytes = oak::utils::read_file(it->second);
         if (module_bytes.empty()) {
-          OAK_LOG(FATAL) << "Empty Wasm module:" << module_name;
+          LOG(FATAL) << "Empty Wasm module:" << module_name;
         }
         node_config.mutable_wasm_config()->set_module_bytes(module_bytes);
       } else {
-        OAK_LOG(FATAL) << "Module path for " << module_name << " is not specified";
+        LOG(FATAL) << "Module path for " << module_name << " is not specified";
       }
     }
   }
 
   // Check application configuration validity.
   if (!oak::ValidApplicationConfig(*config)) {
-    OAK_LOG(FATAL) << "Application config is not valid";
+    LOG(FATAL) << "Application config is not valid";
   }
 
   oak::WriteConfigToFile(config.get(), output_file);
