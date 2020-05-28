45355db84f8e6c35c4379e8ec0be6c438f8fc79f
Conrad Grobler <grobler@google.com>
2020-05-28 17:44:56 +0100

    Change roughenough to a client library only
    - delete server-only code, including deletion of:
      - server benchmarking code (benches/*)
      - key representations (src/key/*)
      - key management functionality (src/kms/*)
      - stats tracking (src/stats/*)
      - server configuration functionality (src/config)
      - deliberate error injection code (src/grease.rs)
      - the server code itself (src/server.rs)
      - merkle tree building code (src/merkle.rs), keeping tree checking code
      - signing functionilty (src/sign.rs), keeping verification fn
      - server binaries (src/bin/roughenough-server.rs, src/bin/roughenough-kms.rs)
    - convert client binary (src/bin/roughenough-client.rs) into a module
      - move to src/client.rs
      - remove `main`
      - remove socket-related and stress-testing code
      - add some unit tests
    - update dependencies
      - remove dependencies no longer required after removing server code
      - remove specific versions from remaining dependencies
      - update signature verification code to work with `ring` v 0.16
    - fix cargo clippy warnings in remaining files
    - fix formatting errors raised by //scripts/check_formatting
    - remove dead links from code document comments in src/message.rs

--- third_party/roughenough/src/merkle.rs
+++ third_party/roughenough/src/merkle.rs
@@ -16,112 +16,11 @@
 //! Merkle Tree implementation using SHA-512 and the Roughtime leaf and node tweak values.
 //!
 
+use super::{TREE_LEAF_TWEAK, TREE_NODE_TWEAK};
 use ring::digest;
-use super::{HASH_LENGTH, TREE_LEAF_TWEAK, TREE_NODE_TWEAK};
 
 type Data = Vec<u8>;
 type Hash = Data;
-
-///
-/// Merkle Tree implementation using SHA-512 and the Roughtime leaf and node tweak values.
-///
-pub struct MerkleTree {
-    levels: Vec<Vec<Data>>,
-}
-
-impl Default for MerkleTree {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl MerkleTree {
-    ///
-    /// Create a new empty Merkle Tree
-    ///
-    pub fn new() -> MerkleTree {
-        MerkleTree {
-            levels: vec![vec![]],
-        }
-    }
-
-    pub fn push_leaf(&mut self, data: &[u8]) {
-        let hash = self.hash_leaf(data);
-        self.levels[0].push(hash);
-    }
-
-    pub fn get_paths(&self, mut index: usize) -> Vec<u8> {
-        let mut paths = Vec::with_capacity(self.levels.len() * 64);
-        let mut level = 0;
-
-        while !self.levels[level].is_empty() {
-            let sibling = if index % 2 == 0 { index + 1 } else { index - 1 };
-
-            paths.extend(self.levels[level][sibling].clone());
-            level += 1;
-            index /= 2;
-        }
-        paths
-    }
-
-    pub fn compute_root(&mut self) -> Hash {
-        assert!(
-            !self.levels[0].is_empty(),
-            "Must have at least one leaf to hash!"
-        );
-
-        let mut level = 0;
-        let mut node_count = self.levels[0].len();
-
-        while node_count > 1 {
-            level += 1;
-
-            if self.levels.len() < level + 1 {
-                self.levels.push(vec![]);
-            }
-
-            if node_count % 2 != 0 {
-                self.levels[level - 1].push(vec![0; HASH_LENGTH as usize]);
-                node_count += 1;
-            }
-
-            node_count /= 2;
-
-            for i in 0..node_count {
-                let hash = self.hash_nodes(
-                    &self.levels[level - 1][i * 2],
-                    &self.levels[level - 1][(i * 2) + 1],
-                );
-                self.levels[level].push(hash);
-            }
-        }
-
-        assert_eq!(self.levels[level].len(), 1);
-        self.levels[level].pop().unwrap()
-    }
-
-    pub fn reset(&mut self) {
-        for level in &mut self.levels {
-            level.clear();
-        }
-    }
-
-    fn hash_leaf(&self, leaf: &[u8]) -> Data {
-        self.hash(&[TREE_LEAF_TWEAK, leaf])
-    }
-
-    fn hash_nodes(&self, first: &[u8], second: &[u8]) -> Data {
-        self.hash(&[TREE_NODE_TWEAK, first, second])
-    }
-
-    fn hash(&self, to_hash: &[&[u8]]) -> Data {
-        let mut ctx = digest::Context::new(&digest::SHA512);
-        for data in to_hash {
-            ctx.update(data);
-        }
-        Data::from(ctx.finish().as_ref())
-    }
-}
 
 pub fn root_from_paths(mut index: usize, data: &[u8], paths: &[u8]) -> Hash {
     let mut hash = {
@@ -152,41 +51,4 @@ pub fn root_from_paths(mut index: usize, data: &[u8], paths: &[u8]) -> Hash {
     }
 
     hash
-}
-
-#[cfg(test)]
-mod test {
-    use crate::merkle::*;
-
-    fn test_paths_with_num(num: usize) {
-        let mut merkle = MerkleTree::new();
-
-        for i in 0..num {
-            merkle.push_leaf(&[i as u8]);
-        }
-
-        let root = merkle.compute_root();
-
-        for i in 0..num {
-            println!("Testing {:?} {:?}", num, i);
-            let paths: Vec<u8> = merkle.get_paths(i);
-            let computed_root = root_from_paths(i, &[i as u8], &paths);
-
-            assert_eq!(root, computed_root);
-        }
-    }
-
-    #[test]
-    fn power_of_two() {
-        test_paths_with_num(2);
-        test_paths_with_num(4);
-        test_paths_with_num(8);
-        test_paths_with_num(16);
-    }
-
-    #[test]
-    fn not_power_of_two() {
-        test_paths_with_num(1);
-        test_paths_with_num(20);
-    }
 }
--- third_party/roughenough/src/sign.rs
+++ third_party/roughenough/src/sign.rs
@@ -14,31 +14,22 @@
 
 //!
 //! A multi-step (init-update-finish) interface for Ed25519 signing and verification
-//!
 
-use ring::rand;
-use ring::rand::SecureRandom;
 use ring::signature;
-use ring::signature::Ed25519KeyPair;
-
-use untrusted::Input;
-
-use std::fmt;
-use std::fmt::Formatter;
 
 const INITIAL_BUF_SIZE: usize = 1024;
 
 /// A multi-step (init-update-finish) interface for verifying an Ed25519 signature
 #[derive(Debug)]
-pub struct Verifier<'a> {
-    pubkey: Input<'a>,
+pub struct Verifier {
+    pubkey: Vec<u8>,
     buf: Vec<u8>,
 }
 
-impl<'a> Verifier<'a> {
-    pub fn new(pubkey: &'a [u8]) -> Self {
+impl Verifier {
+    pub fn new(pubkey: &[u8]) -> Self {
         Verifier {
-            pubkey: Input::from(pubkey),
+            pubkey: pubkey.to_owned(),
             buf: Vec::with_capacity(INITIAL_BUF_SIZE),
         }
     }
@@ -49,76 +40,11 @@ impl<'a> Verifier<'a> {
     }
 
     pub fn verify(&self, expected_sig: &[u8]) -> bool {
-        let msg = Input::from(&self.buf);
-        let sig = Input::from(expected_sig);
-
-        match signature::verify(&signature::ED25519, self.pubkey, msg, sig) {
+        let public_key = signature::UnparsedPublicKey::new(&signature::ED25519, &self.pubkey);
+        match public_key.verify(&self.buf, expected_sig) {
             Ok(_) => true,
             _ => false,
         }
-    }
-}
-
-/// A multi-step (init-update-finish) interface for creating an Ed25519 signature
-pub struct Signer {
-    key_pair: Ed25519KeyPair,
-    buf: Vec<u8>,
-}
-
-impl Default for Signer {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl Signer {
-    pub fn new() -> Self {
-        let rng = rand::SystemRandom::new();
-        let mut seed = [0u8; 32];
-        rng.fill(&mut seed).unwrap();
-
-        Signer::from_seed(&seed)
-    }
-
-    pub fn from_seed(seed: &[u8]) -> Self {
-        let seed_input = Input::from(seed);
-        Signer {
-            key_pair: Ed25519KeyPair::from_seed_unchecked(seed_input).unwrap(),
-            buf: Vec::with_capacity(INITIAL_BUF_SIZE),
-        }
-    }
-
-    pub fn update(&mut self, data: &[u8]) {
-        self.buf.reserve(data.len());
-        self.buf.extend_from_slice(data);
-    }
-
-    pub fn sign(&mut self) -> Vec<u8> {
-        let signature = self.key_pair.sign(&self.buf).as_ref().to_vec();
-        self.buf.clear();
-
-        signature
-    }
-
-    pub fn public_key_bytes(&self) -> &[u8] {
-        self.key_pair.public_key_bytes()
-    }
-}
-
-impl fmt::Display for Signer {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}", hex::encode(self.public_key_bytes()))
-    }
-}
-
-impl fmt::Debug for Signer {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Signer({}, {:?})",
-            hex::encode(self.public_key_bytes()),
-            self.buf
-        )
     }
 }
 
@@ -157,55 +83,6 @@ mod test {
         let mut v = Verifier::new(&pubkey);
         v.update(&message);
         let result = v.verify(&signature);
-        assert_eq!(result, true);
-    }
-
-    #[test]
-    fn sign_ed25519_empty_message() {
-        let seed = hex::decode("9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60")
-            .unwrap();
-
-        let expected_sig = hex::decode(
-            "e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e065224901555fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b"
-        ).unwrap();
-
-        let mut s = Signer::from_seed(&seed);
-        let sig = s.sign();
-        assert_eq!(sig, expected_sig);
-    }
-
-    #[test]
-    fn sign_ed25519_message() {
-        let seed = hex::decode("0d4a05b07352a5436e180356da0ae6efa0345ff7fb1572575772e8005ed978e9")
-            .unwrap();
-
-        let message = hex::decode("cbc77b").unwrap();
-
-        let expected_sig = hex::decode(
-            "d9868d52c2bebce5f3fa5a79891970f309cb6591e3e1702a70276fa97c24b3a8e58606c38c9758529da50ee31b8219cba45271c689afa60b0ea26c99db19b00c"
-        ).unwrap();
-
-        let mut s = Signer::from_seed(&seed);
-        s.update(&message);
-        let sig = s.sign();
-        assert_eq!(sig, expected_sig);
-    }
-
-    #[test]
-    fn sign_verify_round_trip() {
-        let seed = hex::decode("334a05b07352a5436e180356da0ae6efa0345ff7fb1572575772e8005ed978e9")
-            .unwrap();
-
-        let message = "Hello world".as_bytes();
-
-        let mut signer = Signer::from_seed(&seed);
-        signer.update(&message);
-        let signature = signer.sign();
-
-        let mut v = Verifier::new(signer.public_key_bytes());
-        v.update(&message);
-        let result = v.verify(&signature);
-
         assert_eq!(result, true);
     }
 }
