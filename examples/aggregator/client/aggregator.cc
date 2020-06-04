dbe8f6cde1d89a03e943121e79fbbe315ca03c0d
Tiziano Santoro <tzn@google.com>
2020-06-04 11:11:16 +0100

    More accurate logging of gRPC requests lifecycle (#1080)
    If there is an error when reading a gRPC request from an invocation,
    then attempt to send an error response to the caller rather than leave
    it hanging forever (this happens in case of misplaced labels).

--- examples/aggregator/client/aggregator.cc
+++ examples/aggregator/client/aggregator.cc
@@ -58,7 +58,7 @@ void submit_sample(Aggregator::Stub* stub, std::string& bucket, std::vector<uint
   google::protobuf::Empty response;
   grpc::Status status = stub->SubmitSample(&context, request, &response);
   if (!status.ok()) {
-    LOG(ERROR) << "Could not submit sample: " << status.error_code() << ": "
+    LOG(ERROR) << "Error submitting sample: " << status.error_code() << ": "
                << status.error_message();
   }
 }
