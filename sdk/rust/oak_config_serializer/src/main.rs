3eaa83d418b4233d34c893c5916bb7448d9ab445
Ivan Petrov <ivanpetrov@google.com>
2020-07-03 15:42:42 +0100

    Add Rust application configuration serializer (#1219)
    This change:
    - Adds a Rust version of application configuration serializer
    - Updates examples to use Rust serializer
    
    Fixes #740
    Ref #719

---
+++ sdk/rust/oak_config_serializer/src/main.rs
@@ -0,0 +1,138 @@
+//
+// Copyright 2020 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+//! An utility for serializing an Oak application configuration.
+//!
+//! Invoke with:
+//!
+//! ```shell
+//! cargo run --manifest-path=oak/server/rust/oak_config_serializer/Cargo.toml -- \
+//!     --input-file=<CONFIGURATION_FILE> \
+//!     --output-file=<OUTPUT_FILE>
+//! ```
+
+use anyhow::Context;
+use log::debug;
+use oak_abi::proto::oak::application::{
+    node_configuration::ConfigType, ApplicationConfiguration, NodeConfiguration,
+    WebAssemblyConfiguration,
+};
+use prost::Message;
+use std::{collections::HashMap, fs};
+use structopt::StructOpt;
+
+/// Command line options for the Oak Application Configuration Serializer.
+#[derive(StructOpt, Clone, Debug)]
+#[structopt(about = "Oak Application Configuration Serializer")]
+struct Opt {
+    #[structopt(long, help = "Input application configuration file in TOML format")]
+    input_file: String,
+    #[structopt(long, help = "Output file")]
+    output_file: String,
+}
+
+#[derive(Debug, serde::Deserialize)]
+#[serde(deny_unknown_fields)]
+struct Config {
+    name: String,
+    modules: HashMap<String, Module>,
+    #[serde(default)]
+    initial_node_configuration: InitialNodeConfig,
+}
+
+#[derive(Debug, serde::Deserialize)]
+#[serde(deny_unknown_fields)]
+enum Module {
+    #[serde(rename = "url")]
+    Url(String),
+    #[serde(rename = "path")]
+    Path(String),
+}
+
+#[derive(Debug, serde::Deserialize)]
+#[serde(deny_unknown_fields)]
+struct InitialNodeConfig {
+    wasm_module_name: String,
+    wasm_entrypoint_name: String,
+}
+
+impl Default for InitialNodeConfig {
+    fn default() -> Self {
+        Self {
+            wasm_module_name: "app".to_string(),
+            wasm_entrypoint_name: "oak_main".to_string(),
+        }
+    }
+}
+
+/// Load Wasm module from URL or a file, if URL is not specified.
+fn load_module(module: &Module) -> anyhow::Result<Vec<u8>> {
+    match module {
+        Module::Url(_url) => unimplemented!(),
+        Module::Path(path) => {
+            fs::read(&path).with_context(|| format!("Couldn't read file {}", path))
+        }
+    }
+}
+
+/// Serializes an application configuration from `app_config` and writes it into `filename`.
+pub fn write_config_to_file(
+    app_config: &ApplicationConfiguration,
+    filename: &str,
+) -> anyhow::Result<()> {
+    let mut bytes = Vec::new();
+    app_config
+        .encode(&mut bytes)
+        .context("Couldn't encode application configuration")?;
+    fs::write(filename, &bytes).with_context(|| format!("Couldn't write file {}", filename))?;
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let opt = Opt::from_args();
+    debug!("parsed opts: {:?}", opt);
+
+    let input_file = fs::read_to_string(opt.input_file).context("Couldn't read input file")?;
+    let config: Config = toml::from_str(&input_file).context("Couldn't parse TOML config file")?;
+    debug!("parsed config file: {:?}", config);
+
+    // Load Wasm modules.
+    let mut modules = HashMap::new();
+    for (name, module) in config.modules.iter() {
+        let loaded_module = load_module(&module).context("Couldn't load module")?;
+        modules.insert(name.to_string(), loaded_module);
+    }
+
+    // Create application configuration.
+    let app_config = ApplicationConfiguration {
+        wasm_modules: modules,
+        initial_node_configuration: Some(NodeConfiguration {
+            name: config.name,
+            config_type: Some(ConfigType::WasmConfig(WebAssemblyConfiguration {
+                wasm_module_name: config.initial_node_configuration.wasm_module_name,
+                wasm_entrypoint_name: config.initial_node_configuration.wasm_entrypoint_name,
+            })),
+        }),
+    };
+
+    // Serialize application configuration.
+    write_config_to_file(&app_config, &opt.output_file)
+        .context("Couldn't write serialized config")?;
+
+    Ok(())
+}
