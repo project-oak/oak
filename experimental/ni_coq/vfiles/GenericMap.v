1f701284cd982d1a8462c2dc017426d41fa1abe8
Andrew Ferraiuolo <aferr@google.com>
2020-07-07 17:28:15 +0100

    Generic maps for coq model
    Both total and partial maps are given. Both are modeled as as functions.
    The partial maps have options in the codomain, and are implemented in
    terms of the total maps. Notation is given for both.

---
+++ experimental/ni_coq/vfiles/GenericMap.v
@@ -0,0 +1,37 @@
+(* -- will be needed for theorems, but not yet
+From Coq Require Import Logic.FunctionalExtensionality.
+*)
+
+Class KeyT:= {
+    t: Type;
+    eqb: t -> t -> bool
+    (* TODO, undoubtedly proof obligations *)
+}.
+
+Definition tg_map (KT: KeyT)(VT: Type) := t -> VT.
+Definition tg_empty {KT: KeyT}{VT: Type}(def: VT): tg_map KT VT :=
+    (fun _ => def).
+Definition tg_update {KT: KeyT}{VT: Type}(m: tg_map KT VT)
+    (k: t)(v: VT) :=
+    fun k' => if eqb k k' then v else m k'.
+Notation "'_' '!->' v" := (tg_empty v)
+  (at level 100, right associativity).
+Notation "x '!->' v ';' m" := (tg_update m x v)
+  (at level 100, v at next level, right associativity).
+
+Definition pg_map (KT: KeyT)(VT: Type) :=
+    tg_map KT (option VT).
+Definition pg_empty {KT: KeyT}{VT: Type}: pg_map KT VT :=
+    tg_empty None.
+Definition pg_update {KT: KeyT}{VT: Type}(m: pg_map KT VT)
+    (k: t)(v: VT) :=
+    k !-> (Some v) ; m.
+
+Notation "x '|->' v ';' m" := (pg_update m x v)
+  (at level 100, v at next level, right associativity).
+Notation "x '|->' v" := (pg_update pg_empty x v)
+  (at level 100).
+Notation "m '[' k '-->' v ']'" := (pg_update m k v)
+  (at level 100).
+
+(* undoubtedly theorems *)
