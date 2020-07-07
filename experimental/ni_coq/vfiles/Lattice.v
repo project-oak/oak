cad22ccf4af44388dc45f51b2112a9bce9cf4e1e
Andrew Ferraiuolo <aferr@google.com>
2020-07-07 09:45:50 +0100

    Coq runtime model initial commit / attempt
    As of this stage there is makefile generated by _CoqProject, and:
    - a set of axioms about lattices, used to represent IFC labels
    - a system state consisting of nodes and channels represented by records
    - utility functions for updating the state
    - a functional definition of a state transition function (by contrast to
    a relational definition).
    
    Comments in the code document flaws with the still experimental model.

---
+++ experimental/ni_coq/vfiles/Lattice.v
@@ -0,0 +1,29 @@
+Reserved Infix "⊔" (at level 40, left associativity).
+Reserved Infix "⊓" (at level 40, left associativity).
+Reserved Infix "<<" (at level 50).
+
+Definition assoc {A: Type} (op: A -> A -> A): Prop :=
+    forall x y z, op (op x y) z = op x (op y z).
+Definition comm {A: Type} (op: A -> A -> A): Prop :=
+    forall x y, op x y = op y x.
+Definition idem {A: Type} (op: A -> A -> A): Prop :=
+    forall x, op x x = x.
+
+Class Lattice (A: Type) := {
+    ord: A -> A -> Prop where "x << y" := (ord x y);
+    ord_refl: forall x, x << x;
+    ord_trans: forall x y z, x << y -> y << z -> x << z;
+    ord_anti: forall x y, x << y -> y << x -> x = y;
+
+    join: A -> A -> A where "x ⊔ y" := (join x y);
+    join_assoc: assoc join;
+    join_comm: comm join;
+    join_idem: idem join;
+    join_glb: forall x y, x << x ⊔ y /\ y << x ⊔ y;
+
+    meet: A -> A -> A where "x ⊓ y" := (meet x y);
+    meet_assoc: assoc join;
+    meet_comm: comm join;
+    meet_idem: idem join;
+    meet_lub: forall x y, x ⊓ y << x /\ x ⊓ y << y; 
+}.
