7742a767f4fb7785cf9018dba0a8463ccdf17644
Juliette Pretot <julsh@google.com>
2020-07-16 16:19:34 +0000

    clarify comment

--- oak/server/rust/oak_runtime/src/runtime/introspection_events.rs
+++ oak/server/rust/oak_runtime/src/runtime/introspection_events.rs
@@ -15,41 +15,41 @@
 //
 
 use crate::{
-  proto::oak::introspection_events::{event::EventDetails, Event},
-  runtime::Runtime,
+    proto::oak::introspection_events::{event::EventDetails, Event},
+    runtime::Runtime,
 };
 use log::info;
 use prost_types::Timestamp;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 fn current_timestamp() -> Timestamp {
-  let duration_since_unix_epoch = SystemTime::now()
-    .duration_since(UNIX_EPOCH)
-    .expect("Time went backwards");
+    let duration_since_unix_epoch = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
 
-  Timestamp {
-    seconds: duration_since_unix_epoch.as_secs() as i64,
-    nanos: duration_since_unix_epoch.subsec_nanos() as i32,
-  }
+    Timestamp {
+        seconds: duration_since_unix_epoch.as_secs() as i64,
+        nanos: duration_since_unix_epoch.subsec_nanos() as i32,
+    }
 }
 
 // Introspection event related methods for the Runtime.
 impl Runtime {
-  /// Generates an introspection event recording a modification to the Oak
-  /// application.
-  #[cfg(feature = "oak_debug")]
-  pub fn introspection_event(&self, event_details: EventDetails) {
-    let event = Event {
-      timestamp: Some(current_timestamp()),
-      event_details: Some(event_details),
-    };
+    /// Generates an introspection event recording a modification to the Runtime's
+    /// internal data structures
+    #[cfg(feature = "oak_debug")]
+    pub fn introspection_event(&self, event_details: EventDetails) {
+        let event = Event {
+            timestamp: Some(current_timestamp()),
+            event_details: Some(event_details),
+        };
 
-    // TODO(#913): Push the event over to a channel, to be consumed by the
-    // introspection aux server.
-    info!("Introspection event recorded: {:?}", event);
-  }
+        // TODO(#913): Push the event over to a channel, to be consumed by the
+        // introspection aux server.
+        info!("Introspection event recorded: {:?}", event);
+    }
 
-  /// no-op implementation, introspection events are a debugging feature.
-  #[cfg(not(feature = "oak_debug"))]
-  pub fn introspection_event(&self, event_details: EventDetails) {}
+    /// no-op implementation, introspection events are a debugging feature.
+    #[cfg(not(feature = "oak_debug"))]
+    pub fn introspection_event(&self, event_details: EventDetails) {}
 }
