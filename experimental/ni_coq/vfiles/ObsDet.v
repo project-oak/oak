d88c4c604d0388f0189f0efcd214259bcea187cd
Andrew Ferraiuolo <aferr@google.com>
2020-07-16 16:14:37 +0100

    Proof of single-step noninterference with one call
    This is a proof of a single step of noninterference without downgrades,
    sometimes referred to as "the unwinding condition" in a formal
    verification context, or "single command noninterference" in a
    programming languages context. This means that if we run the state
    transition function for just one step, then noninterference is enforced for
    that one step. At the moment, the model just has a single call that is
    actually properly modeled (the write call), so this is not really a complete
    proof. The definition of noninterference is also still undergoing revision.
    The proof itself is also not terribly automated and almost no effort was
    put into making it elegent yet.

--- experimental/ni_coq/vfiles/ObsDet.v
+++ experimental/ni_coq/vfiles/ObsDet.v
@@ -2,6 +2,7 @@ Require Import OakIFC.RuntimeModel.
 Require Import OakIFC.Parameters.
 Require Import OakIFC.LowEquivalences.
 Require Import Coq.Lists.List.
+Require Import OakIFC.GenericMap.
 Import ListNotations.
 
 Inductive valid_trace: trace -> Prop :=
@@ -19,3 +20,79 @@ Definition observational_determinism: Prop :=
         valid_trace t2 ->
         (state_low_eq ell s1 s2) ->
         (trace_low_eq ell t1 t2).
+
+Theorem chan_upd_preserves_nodes: forall h ch s,
+    (state_upd_chan h ch s).(nodes) = s.(nodes).
+    intros; simpl; reflexivity.
+Qed.
+
+Theorem chan_append_unwind: forall ell m ch1 ch2,
+    (chan_low_eq ell ch1 ch2) ->
+    (chan_low_eq ell (chan_append ch1 m) (chan_append ch2 m)). 
+Proof.
+    intros. inversion H. 
+    constructor. rewrite H0. reflexivity.
+    constructor 2.
+    apply H1. apply H2.
+Qed. 
+
+Theorem leq_ch_upd_preserves_sleq: forall (ell: level)
+    (h:handle)(ch1 ch2: channel)(s1 s2:state),
+    (chan_low_eq ell ch1 ch2) ->
+    (state_low_eq ell s1 s2) ->
+    (state_low_eq ell 
+        (state_upd_chan h ch1 s1)
+        (state_upd_chan h ch2 s2)).
+Proof.
+    intros.
+    inversion H.
+        (* ch1 = ch2 *)
+        - rewrite H1. unfold state_low_eq. split.
+            (* node_state *)
+            rewrite chan_upd_preserves_nodes.
+            rewrite chan_upd_preserves_nodes.
+            apply H0.
+            (* chan_state *)
+            unfold chan_state_low_eq. intros.
+            destruct (dec_eq_h h h0).
+                (* h = h0 *)
+                + rewrite e. unfold state_upd_chan.
+                simpl. rewrite upd_eq. rewrite upd_eq.
+                constructor. reflexivity.
+                (* h <> h0 *)
+                + unfold state_upd_chan. simpl. 
+                rewrite upd_neq. rewrite upd_neq.
+                apply H0. apply n. apply n.
+        (* ~(ch1.l flowsTo ell) /\ ~(ch2.l flowsTo ell) *)
+        - unfold state_low_eq. split.
+            (* node_state *)
+            rewrite chan_upd_preserves_nodes.
+            rewrite chan_upd_preserves_nodes.
+            apply H0.
+            (* chan_state *)
+            unfold chan_state_low_eq. intros. destruct (dec_eq_h h h0).
+                (* h = h0 *)
+                + rewrite e. unfold state_upd_chan. simpl.
+                rewrite upd_eq. rewrite upd_eq.
+                constructor 2. apply H1. apply H2.
+                (* h <> h0 *)
+                + unfold state_upd_chan. simpl. 
+                rewrite upd_neq. rewrite upd_neq.
+                apply H0. apply n. apply n.
+Qed.
+
+Theorem low_eq_step: forall (ell: level)(s1 s2 s1' s2': state)
+        (n: node_id)(c: call),
+    (state_low_eq ell s1 s2) ->
+    (step_call n c s1 s1') ->
+    (step_call n c s2 s2') ->
+    (state_low_eq ell s1' s2').
+Proof.
+    intros.
+    destruct c.
+    - inversion H0. inversion H1.
+    apply leq_ch_upd_preserves_sleq.
+    apply chan_append_unwind.
+    apply H. apply H.
+    - inversion H0. 
+Qed.
