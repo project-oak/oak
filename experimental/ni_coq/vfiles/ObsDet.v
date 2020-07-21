7af4f405fff99d43cd8dab934898f0bc11c4e58c
Andrew Ferraiuolo <aferr@google.com>
2020-07-21 13:16:32 +0100

    Formalize concurrent execution /w two-tier semantics
    The concurrent execution of nodes is now modeled using a two-tiered
    semantics. There are two sets of (related) inference rules: a thread-local
    semantics and a global semantics. The thread-local semantics defines
    execution of a command by a particular node. The global semantics
    non-deterministically selects which node should execute, and updates the
    state by using the thread-local semantics with the chosen node. This
    two-tier semantics is important for the noninterference proof because it
    gives us a way to model the fact that nodes can execute with an
    arbitrary interleaving, and we want to show that there are no
    information leaks regardless of scheduling (aside from perhaps
    secheduling side channels).
    
    The thread-local semantics closely resembles that of an imperative
    programming language. A key difference though is that in order to be
    language-agnostic (we cannot model the code that actually runs inside a
    node aside from execution of the ABI calls) the semantics does not
    contain usual constructs for control flow (like sequential composition
    or loops). Instead, after any command is executed, a new command is
    chosen nondeterministically (though the update to this command happens
    in the global semantics). This models the fact that a node can
    execute arbitrary code aside from the ABI calls in between calls to the
    ABI. The choice of next command to execute is nondeterministic because
    the next command does change, but there are no premises about the new
    command in the inference rule.
    
    The global semantics _nondeterministically_ chooses the thread to execute
    because there are no premises about the thread that is used in the
    thread-local semantics.
    
    This two-tier semantics is inspired by "Secure Information Flow in a
    Multi-threaded Imperative Langiage" by Smith and Volpano '98
    (http://web.cs.ucla.edu/~palsberg/tba/papers/smith-volpano-popl98.pdf).
    This same approach of modeling concurrent execution using a two-tier
    semantics is also taken in "Compositional Non-interference for
    Concurrent Programs via Separation and Framing."

--- experimental/ni_coq/vfiles/ObsDet.v
+++ experimental/ni_coq/vfiles/ObsDet.v
@@ -4,95 +4,26 @@ Require Import OakIFC.LowEquivalences.
 Require Import Coq.Lists.List.
 Require Import OakIFC.GenericMap.
 Import ListNotations.
-
-Inductive valid_trace: trace -> Prop :=
-    | VTOne s: (valid_trace (s :: nil) )
-    | VTAdd s s' t
-            (H0: valid_trace  (s :: t) )
-            (H1: step_system s s'):
-            (valid_trace (s' :: (s :: t))).
+Require Import OakIFC.StepUnwind.
 
 Definition observational_determinism: Prop :=
     forall (ell: level) (s1 s2: state) (t1 t2: trace), 
-        [s1] = (tl t1) ->
-        [s2] = (tl t2) ->
-        valid_trace t1 ->
-        valid_trace t2 ->
+        valid_trace s1 t1 ->
+        valid_trace s2 t2 ->
         (state_low_eq ell s1 s2) ->
-        (trace_low_eq ell t1 t2).
+        (stut_trace_low_eq ell t1 t2).
 
-Theorem chan_upd_preserves_nodes: forall h ch s,
-    (state_upd_chan h ch s).(nodes) = s.(nodes).
-    intros; simpl; reflexivity.
-Qed.
 
-Theorem chan_append_unwind: forall ell m ch1 ch2,
-    (chan_low_eq ell ch1 ch2) ->
-    (chan_low_eq ell (chan_append ch1 m) (chan_append ch2 m)). 
-Proof.
-    intros. inversion H. 
-    constructor. rewrite H0. reflexivity.
-    constructor 2.
-    apply H1. apply H2.
-Qed. 
+Theorem od: observational_determinism.
+Proof. unfold observational_determinism.
+intros.
+inversion H. inversion H0.
+-  (* t1 = [s1], t2 = [s2] *)
+apply SAddBoth. apply SNilEQ. apply H1.
+- 
+(* t1 = [s1], t2 = (s :: t) *)
+(* must be that t2 is taking a "high step", only affecting
+* values visible above ell *)
+inversion H4. induction t2.
+Admitted.
 
-Theorem leq_ch_upd_preserves_sleq: forall (ell: level)
-    (h:handle)(ch1 ch2: channel)(s1 s2:state),
-    (chan_low_eq ell ch1 ch2) ->
-    (state_low_eq ell s1 s2) ->
-    (state_low_eq ell 
-        (state_upd_chan h ch1 s1)
-        (state_upd_chan h ch2 s2)).
-Proof.
-    intros.
-    inversion H.
-        (* ch1 = ch2 *)
-        - rewrite H1. unfold state_low_eq. split.
-            (* node_state *)
-            rewrite chan_upd_preserves_nodes.
-            rewrite chan_upd_preserves_nodes.
-            apply H0.
-            (* chan_state *)
-            unfold chan_state_low_eq. intros.
-            destruct (dec_eq_h h h0).
-                (* h = h0 *)
-                + rewrite e. unfold state_upd_chan.
-                simpl. rewrite upd_eq. rewrite upd_eq.
-                constructor. reflexivity.
-                (* h <> h0 *)
-                + unfold state_upd_chan. simpl. 
-                rewrite upd_neq. rewrite upd_neq.
-                apply H0. apply n. apply n.
-        (* ~(ch1.l flowsTo ell) /\ ~(ch2.l flowsTo ell) *)
-        - unfold state_low_eq. split.
-            (* node_state *)
-            rewrite chan_upd_preserves_nodes.
-            rewrite chan_upd_preserves_nodes.
-            apply H0.
-            (* chan_state *)
-            unfold chan_state_low_eq. intros. destruct (dec_eq_h h h0).
-                (* h = h0 *)
-                + rewrite e. unfold state_upd_chan. simpl.
-                rewrite upd_eq. rewrite upd_eq.
-                constructor 2. apply H1. apply H2.
-                (* h <> h0 *)
-                + unfold state_upd_chan. simpl. 
-                rewrite upd_neq. rewrite upd_neq.
-                apply H0. apply n. apply n.
-Qed.
-
-Theorem low_eq_step: forall (ell: level)(s1 s2 s1' s2': state)
-        (n: node_id)(c: call),
-    (state_low_eq ell s1 s2) ->
-    (step_call n c s1 s1') ->
-    (step_call n c s2 s2') ->
-    (state_low_eq ell s1' s2').
-Proof.
-    intros.
-    destruct c.
-    - inversion H0. inversion H1.
-    apply leq_ch_upd_preserves_sleq.
-    apply chan_append_unwind.
-    apply H. apply H.
-    - inversion H0. 
-Qed.
