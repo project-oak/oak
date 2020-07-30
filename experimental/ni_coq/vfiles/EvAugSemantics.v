89566e54b16dd75d3e101dc99f97934cef2dca9e
Andrew Ferraiuolo <aferr@google.com>
2020-07-30 21:46:09 +0100

    Event-augmented semantics
    This commit introduces "events" that model inputs and outputs to nodes
    (for example, during the channel_read and channel_write calls). At the
    moment an event is a label and either: an input message, an output
    message, or nothing. In the future, events will also be used for modeling
    values that are downgraded.
    
    Events are needed for specifying noninterference in this context. An
    alternative way of writing noninterference (without events) would be to just
    use relations on states. This is not sufficient for the oak runtime model,
    because we cannot model all the state in a node, and so a definition
    that uses states alone and not events would not have any way of tracking
    the read calls.
    
    Events are introduced into traces of system execution by using an
    augmented semantics. The augmented semantics works just like the normal
    one except that it adds these events to the trace. The idea is that
    noninterference proofs will be done on the augmented semantics and there
    will also be proof that the augmented and "normal" semantics are equal.
    (This equality proof should be trivial).

---
+++ experimental/ni_coq/vfiles/EvAugSemantics.v
@@ -0,0 +1,69 @@
+Require Import OakIFC.Lattice.
+Require Import OakIFC.Parameters.
+Require Import OakIFC.GenericMap.
+Require Import List.
+Import ListNotations.
+Require Import RuntimeModel.
+Require Import OakIFC.Events.
+Require Import Coq.Sets.Ensembles.
+
+Definition trace := list (event_l).
+
+Arguments Ensembles.In {U}.
+Arguments Ensembles.Add {U}.
+Arguments Ensembles.Subtract {U}.
+Arguments Ensembles.Singleton {U}.
+
+Definition msg_is_head (ch: channel)(m: message): Prop :=
+    match ch.(ms) with
+        | [] => False
+        | m' :: ms' => m' = m
+    end. 
+
+(* TODO consider whether traces should really also be
+ list (state * event_l)
+ for example so that we can reason about the level of commands
+ of individual steps.
+*)
+
+Inductive step_node_ev: node_id -> call -> state -> trace -> state -> trace -> Prop :=
+    | SWriteChan s id n (han: handle) msg t ev
+        (H0: (s.(nodes) id) = n)
+        (H1: Ensembles.In n.(hans) han)
+        (H2: n.(nlbl) << (s.(chans) han).(clbl))
+        (H3: ev = (EvL (OutEv msg) n.(nlbl))):
+        step_node_ev id (WriteChannel han msg) s t
+            (state_append_msg han msg s) (ev :: t)
+    | SReadChan s id n han chan msg t ev
+        (H0: (s.(nodes) id) = n)
+        (H1: In n.(hans) han)
+        (H2: (s.(chans) han) = chan)
+        (H3: (length chan.(ms)) > 0)
+        (H4: chan.(clbl) << n.(nlbl))
+        (H5: (msg_is_head chan msg))
+        (H6: ev = (EvL (InEv msg) n.(nlbl))):
+        step_node_ev id (ReadChannel han) s t 
+            (state_chan_pop han s) (ev :: t)
+    | SCreateChan s n cid rid wid h lbl t ev
+        (H0: (s.(nodes) cid) = n)
+        (H1: n.(nlbl) << lbl)
+        (H2: handle_fresh s h)
+        (H3: ev = (EvL NilEv n.(nlbl)) ):
+            let s0 := (state_upd_chan h {| ms := []; clbl := lbl; |} s) in
+            let s1 := (state_node_add_han h rid s0) in
+            let s' := (state_node_add_han h wid s1) in
+            step_node_ev cid (CreateChannel lbl rid wid) s t s' (ev :: t)
+    | SCreateNode s n cid nid lbl h t ev
+        (H0: (s.(nodes) cid) = n)
+        (H1: n.(nlbl) << lbl)
+        (H2: (nid_fresh s nid))
+        (H3: ev = (EvL NilEv n.(nlbl))):
+        step_node_ev cid (CreateNode lbl h) s t
+            (state_upd_node nid {| 
+                nlbl := lbl;
+                hans := (Singleton h);
+                ncall := Internal;
+            |} s) (ev :: t)
+    | SInternal s id t ev
+        (H0: ev = (EvL NilEv (s.(nodes) id).(nlbl))) :
+        step_node_ev id Internal s t s (ev :: t).
