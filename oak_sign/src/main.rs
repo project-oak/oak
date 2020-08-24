84ed3ff7cd72b55e0de1f6b96d1d55902511150a
Ivan Petrov <ivanpetrov@google.com>
2020-08-24 19:17:12 +0100

    Change after review

--- oak_sign/src/main.rs
+++ oak_sign/src/main.rs
@@ -14,7 +14,8 @@
 // limitations under the License.
 //
 
-//! An utility binary to sign Oak Wasm modules.
+//! An utility binary to sign files using Ed25519.
+//! https://ed25519.cr.yp.to.
 //!
 //! Invoke with:
 //!
@@ -27,12 +28,12 @@
 //! cargo run --manifest-path=oak_sign/Cargo.toml -- \
 //!     sign \
 //!     --private-key=<PRIVATE_KEY_FILE>.key \
-//!     --module=<WASM_MODULE_FILE>.wasm \
+//!     --input-file=<INPUT_FILE> \
 //!     --signature=<SIGNATURE_FILE>.sign
 //!
 //! cargo run --manifest-path=oak_sign/Cargo.toml -- \
 //!     verify \
-//!     --module=<WASM_MODULE_FILE>.wasm \
+//!     --input-file=<INPUT_FILE> \
 //!     --signature=<SIGNATURE_FILE>.sign
 //! ```
 
@@ -49,7 +50,7 @@ use std::{
 };
 use structopt::StructOpt;
 
-/// Command line options for the Oak sign.
+/// Command line options for `oak_sign`.
 #[derive(StructOpt, Clone)]
 #[structopt(about = "Oak Sign")]
 struct Opt {
@@ -57,7 +58,7 @@ struct Opt {
     cmd: Command,
 }
 
-/// Available commands for the Oak sign.
+/// Available commands for `oak_sign`.
 #[derive(StructOpt, Clone)]
 enum Command {
     Generate(Generate),
@@ -68,30 +69,30 @@ enum Command {
 #[derive(StructOpt, Clone)]
 #[structopt(about = "Generate Ed25519 key pair")]
 struct Generate {
-    #[structopt(long, help = "Output private key file path")]
+    #[structopt(long, help = "Output PEM encoded Ed25519 private key file path")]
     pub private_key: String,
-    #[structopt(long, help = "Output public key file path")]
+    #[structopt(long, help = "Output PEM encoded Ed25519 public key file path")]
     pub public_key: String,
 }
 
 #[derive(StructOpt, Clone)]
-#[structopt(about = "Sign Wasm module")]
+#[structopt(about = "Sign input file")]
 struct Sign {
-    #[structopt(long, help = "Input private key file path")]
+    #[structopt(long, help = "Input PEM encoded Ed25519 private key file path")]
     pub private_key: String,
-    #[structopt(long, help = "Input Wasm module path")]
-    pub module: String,
-    #[structopt(long, help = "Output signature path")]
+    #[structopt(long, help = "Input file path")]
+    pub input_file: String,
+    #[structopt(long, help = "Output PEM encoded Ed25519 signature path")]
     pub signature: String,
 }
 
 #[derive(StructOpt, Clone)]
-#[structopt(about = "Verify Wasm module signature")]
+#[structopt(about = "Verify input file signature")]
 struct Verify {
-    #[structopt(long, help = "Input signature path")]
+    #[structopt(long, help = "Input PEM encoded Ed25519 signature path")]
     pub signature: String,
-    #[structopt(long, help = "Input Wasm module path")]
-    pub module: String,
+    #[structopt(long, help = "Input file path")]
+    pub input_file: String,
 }
 
 // PEM file tags.
@@ -107,7 +108,7 @@ fn create_pem(tag: &str, contents: &[u8]) -> Pem {
     }
 }
 
-/// Main execution point for the Oak sign.
+/// Main execution point for `oak_sign`.
 fn main() -> anyhow::Result<()> {
     env_logger::init();
     let opt = Opt::from_args();
@@ -122,7 +123,7 @@ fn main() -> anyhow::Result<()> {
                 .expect("Couldn't parse generated key pair");
             let public_key_bytes = key_pair.public_key();
 
-            // Encode key pair in PEM format
+            // Encode key pair in PEM format.
             let private_key_pem = create_pem(PRIVATE_KEY_TAG, private_key_bytes.as_ref());
             let encoded_private_key = encode(&private_key_pem);
 
@@ -140,19 +141,19 @@ fn main() -> anyhow::Result<()> {
             // Read input files.
             let private_key_file = read(&opt.private_key)
                 .with_context(|| format!("Couldn't read private key file {}", &opt.private_key))?;
-            let module_bytes = read(&opt.module)
-                .with_context(|| format!("Couldn't Wasm module file {}", &opt.module))?;
+            let input_file_bytes = read(&opt.input_file)
+                .with_context(|| format!("Couldn't read input file {}", &opt.input_file))?;
 
-            // Sign Wasm module
+            // Sign input file.
             let private_key_pem =
                 parse(private_key_file).context("Couldn't parse PEM encoded private key file")?;
             let key_pair =
                 signature::Ed25519KeyPair::from_pkcs8(&private_key_pem.contents.as_ref())
-                    .expect("Couldn't parse PKCS encoded private key");
+                    .expect("Couldn't parse PKCS8 encoded private key");
             let public_key_bytes = key_pair.public_key();
-            let signature_bytes = key_pair.sign(&module_bytes);
+            let signature_bytes = key_pair.sign(&input_file_bytes);
 
-            // Encode signature in PEM format
+            // Encode signature in PEM format.
             let public_key_pem = create_pem(PUBLIC_KEY_TAG, public_key_bytes.as_ref());
             let signature_pem = create_pem(SIGNATURE_TAG, signature_bytes.as_ref());
             let encoded_signature = encode_many(&[public_key_pem, signature_pem]);
@@ -160,14 +161,14 @@ fn main() -> anyhow::Result<()> {
             // Write signature file.
             write(&opt.signature, &encoded_signature)
                 .with_context(|| format!("Couldn't write signature file {}", &opt.signature))?;
-            info!("Wasm module signed successfully");
+            info!("Input file signed successfully");
         }
         Command::Verify(ref opt) => {
             // Read input files.
             let signature_file = read(&opt.signature)
                 .with_context(|| format!("Couldn't read signature file {}", &opt.signature))?;
-            let module_bytes = read(&opt.module)
-                .with_context(|| format!("Couldn't Wasm module file {}", &opt.module))?;
+            let input_file_bytes = read(&opt.input_file)
+                .with_context(|| format!("Couldn't read input file {}", &opt.input_file))?;
 
             // Parse signature file.
             let signature_content =
@@ -184,13 +185,13 @@ fn main() -> anyhow::Result<()> {
                 .get(SIGNATURE_TAG)
                 .context("Signature file doesn't contain signature")?;
 
-            // Verify Wasm module signature.
+            // Verify input file signature.
             let public_key =
                 signature::UnparsedPublicKey::new(&signature::ED25519, public_key_bytes);
             public_key
-                .verify(&module_bytes, &signature_bytes)
-                .expect("Wasm module signature verification failed");
-            info!("Wasm module signature verified successfully");
+                .verify(&input_file_bytes, &signature_bytes)
+                .expect("Input file signature verification failed");
+            info!("Input file signature verified successfully");
         }
     }
     Ok(())
