06aed2936d849c60679ab0602ef2a061f5bc119c
Juliette Pretot <julsh@google.com>
2020-09-03 11:47:20 +0000

    Add basic introspection client

---
+++ oak_runtime/src/introspection_browser_client/index.js
@@ -0,0 +1,46 @@
+//
+// Copyright 2020 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import introspectionEventsProto from './proto/introspection_events_pb';
+
+function loadSerializedEvents() {
+  return new Promise((resolve, reject) => {
+    const eventsRequest = new XMLHttpRequest();
+    eventsRequest.open(
+      'GET',
+      'http://localhost:1909/introspection-events',
+      true
+    );
+    eventsRequest.responseType = 'arraybuffer';
+
+    eventsRequest.addEventListener('load', () =>
+      resolve(eventsRequest.response)
+    );
+    eventsRequest.addEventListener('error', reject);
+    eventsRequest.addEventListener('abort', reject);
+
+    eventsRequest.send(null);
+  });
+}
+
+// TODO(#913): Use these events to create a helpful UI with them
+(async () => {
+  const serializedEvents = await loadSerializedEvents();
+  let events = introspectionEventsProto.Events.deserializeBinary(
+    serializedEvents
+  ).toObject();
+  document.getElementById('app').innerHTML = JSON.stringify(events);
+})();
