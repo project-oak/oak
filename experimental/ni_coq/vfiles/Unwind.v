a5230d6aed9c7a01e41623cfc2c0a623d5af2901
aferr <aferr@google.com>
2020-10-05 18:38:35 +0100

    WriteChannel step_implies_lowproj_steps proof (#1556)
    Prove step_implies_lowproj_steps_leq for WriteChannel case

--- experimental/ni_coq/vfiles/Unwind.v
+++ experimental/ni_coq/vfiles/Unwind.v
@@ -50,7 +50,7 @@ Theorem state_upd_chan_unwind: forall ell han ch1 ch2 s1 s2,
     state_low_eq ell s1 s2 ->
     state_low_eq ell (state_upd_chan han ch1 s1) (state_upd_chan han ch2 s2).
 Proof.
-Admitted. (* WIP *)
+Admitted.
 
 Theorem set_call_unwind: forall ell id c s1 s2,
     state_low_eq ell s1 s2 ->
