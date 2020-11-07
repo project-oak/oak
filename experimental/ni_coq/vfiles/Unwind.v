a296e8a085a60db7fc7fd7085c069e9dfc2020da
Andrew Ferraiuolo <aferr@google.com>
2020-11-07 20:36:01 +0000

    Prove state_upd_unwind
    There's a funny un-productivity story about this one which is this
    abandoned PR from when I thought this wasn't provable:
    [1712](https://github.com/project-oak/oak/pull/1712)

--- experimental/ni_coq/vfiles/Unwind.v
+++ experimental/ni_coq/vfiles/Unwind.v
@@ -36,19 +36,25 @@ Theorem state_upd_node_unwind : forall ell id n s1 s2,
     state_low_eq ell s1 s2 ->
     state_low_eq ell (state_upd_node id n s1) (state_upd_node id n s2).
 Proof.
-  destruct s1, s2; intros *.
-  inversion 1; subst.
-  cbv [state_upd_node state_low_eq state_low_proj set].
-  cbn [RuntimeModel.nodes RuntimeModel.chans].
-  (* this is not provable as stated; because the node-maps are functions, the
-     fact that they return the same result for every input doesn't mean they
-     obey Leibniz equality. The [low_eq] definition should probably say,
-     instead of [low_proj ell x = low_proj ell y], something like:
-
-     (forall id, nodes (low_proj_ell x) id = nodes (low_proj ell y) id)
-     /\ (forall han, chans (low_proj_ell x) han = chans (low_proj ell y) han)
-   *)
-Admitted. (* WIP // TODO *)
+    destruct s1, s2; intros *.
+    inversion 1; subst.
+    cbv [state_upd_node state_low_eq state_low_proj set] in *.
+    cbn [RuntimeModel.nodes RuntimeModel.chans] in *.
+    split; try congruence; intros.
+    destruct (dec_eq_nid id nid).
+    - rewrite e in *. specialize (H0 nid).
+    cbv [node_state_low_proj low_proj fnd RuntimeModel.lbl RuntimeModel.obj] in *.
+    simpl in *. erewrite upd_eq. erewrite upd_eq.
+    destruct (nodes nid), (nodes0 nid).
+    pose proof (top_is_top ell).
+    pose proof (top_is_top lbl).
+    pose proof (top_is_top lbl0).
+    destruct (lbl <<? ell), (lbl0 <<? ell); try congruence.
+    all:
+    inversion H0; subst;
+    pose proof (ord_anti ell top ltac:(eauto) ltac:(eauto));
+    congruence.
+Qed.
 
 Theorem chan_append_unwind: forall ell ch1 ch2 ch1obj ch2obj msg,
     chan_low_eq ell ch1 ch2 ->
