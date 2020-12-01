9b3019c9584a9b0d49d159e0703dfce2daac4c4c
aferr <aferr@google.com>
2020-12-01 17:46:42 +0000

    Finish set_call_unwind (And NI proof!) (#1784)

--- experimental/ni_coq/vfiles/Unwind.v
+++ experimental/ni_coq/vfiles/Unwind.v
@@ -191,12 +191,73 @@ Proof.
         destruct obj; destruct obj0; (rewrite E; auto).
 Qed.
 
+Theorem set_call_none_is_nop: forall s id c,
+    obj ((nodes s) id) = None ->
+    s_set_call s id c = s.
+Proof.
+    autounfold with semutils. unfold fnd. intros.
+    (* why doesn't subst work here? sad/fake_news. *)
+    rewrite H. congruence.
+Qed.
+
+Theorem node_state_proj_index_assoc_form2: forall ell s id,
+    nodes (state_low_proj ell s) id = low_proj ell ((nodes s) id).
+Proof.
+    autounfold with loweq. eauto.
+Qed.
+
 Theorem set_call_unwind: forall ell id c s1 s2,
     state_low_eq ell s1 s2 ->
     state_low_eq ell (s_set_call s1 id c) (s_set_call s2 id c).
 Proof.
-    unfold s_set_call, fnd. 
-Admitted.
+    unfold s_set_call, fnd. intros.
+    pose proof (state_low_eq_implies_node_lookup_eq _ _ _
+        ltac:(eauto) ltac:(eauto)).
+    unfold fnd in *.
+    erewrite !node_state_proj_index_assoc_form2 in *.
+    destruct (lbl (nodes s1 id) <<? ell) eqn:E,
+        (lbl (nodes s2 id) <<? ell) eqn:E1.
+    - destruct (nodes s1 id), (nodes s2 id). simpl in *. 
+    rewrite E in H0. rewrite E1 in H0. inversion H0.
+    destruct obj; eauto using state_upd_node_unwind.
+    - destruct (obj (nodes s2 id)). 
+    remember (n0 <| ncall ::= (fun _ : call => c) |>) as n0'.
+    pose proof (state_upd_node_unobs _ _ _ ltac:(eauto) ltac:(eauto)).
+    destruct (nodes s1 id), (nodes s2 id). simpl in *.
+    rewrite E in H0. rewrite E1 in H0. inversion H0.
+    eapply (state_low_eq_trans _ s1 s2 (state_upd_node id n0' s2)); eauto.
+    destruct (nodes s1 id), (nodes s2 id). simpl in *.
+    rewrite E in H0. rewrite E1 in H0. inversion H0. eauto.
+    - destruct (obj (nodes s1 id)). 
+    remember (n0 <| ncall ::= (fun _ : call => c) |>) as n0'.
+    pose proof (state_upd_node_unobs _ _ _ ltac:(eauto) ltac:(eauto)).
+    destruct (nodes s1 id), (nodes s2 id). simpl in *.
+    rewrite E in H0. rewrite E1 in H0. inversion H0.
+    symmetry in H1.
+    eapply (state_low_eq_trans _ (state_upd_node id n0' s1) s1 s2); eauto.
+    destruct (nodes s1 id), (nodes s2 id). simpl in *.
+    rewrite E in H0. rewrite E1 in H0. inversion H0. eauto.
+    - destruct (obj (nodes s2 id)), (obj (nodes s1 id)).
+    (* both some *)
+    remember (n1 <| ncall ::= (fun _ : call => c) |>) as n1'.
+    remember (n2 <| ncall ::= (fun _ : call => c) |>) as n2'.
+    pose proof (state_upd_node_unobs _ s2 _ n1' ltac:(eauto)).
+    pose proof (state_upd_node_unobs _ s1 _ n2' ltac:(eauto)).
+    symmetry in H2.
+    eapply (state_low_eq_trans ell
+        (state_upd_node id n2' s1) s1 (state_upd_node id n1' s2)); eauto.
+    eapply (state_low_eq_trans ell s1 s2 (state_upd_node id n1' s2)); eauto.
+    (* s2 is some*)
+    remember (n1 <| ncall ::= (fun _ : call => c) |>) as n1'.
+    pose proof (state_upd_node_unobs _ _ _ ltac:(eauto) ltac:(eauto)).
+    eapply (state_low_eq_trans _ s1 s2 (state_upd_node id n1' s2)); eauto.
+    (* s1 is some *)
+    remember (n1 <| ncall ::= (fun _ : call => c) |>) as n1'.
+    pose proof (state_upd_node_unobs _ s1 _ n1' ltac:(eauto)).
+    symmetry in H1.
+    eapply (state_low_eq_trans _ (state_upd_node id n1' s1) s1 s2); eauto.
+    eauto.
+Qed.
 
 Hint Resolve state_upd_chan_unwind chan_append_unwind chan_low_proj_loweq
     chan_low_proj_idempotent state_upd_node_unwind set_call_unwind
