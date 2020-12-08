98f9b1625f914f8569bcaac86668b775f1c5bf54
Ivan Petrov <ivanpetrov@google.com>
2020-12-08 14:51:40 +0000

    Use gRPC in client

--- experimental/treehouse/application/client/src/main.rs
+++ experimental/treehouse/application/client/src/main.rs
@@ -15,15 +15,13 @@
 //
 
 use anyhow::Context;
-use fetch::get_token;
 use log::info;
 use oak_abi::label::Label;
-// use oak_client::{create_tls_channel, interceptors::label::LabelInterceptor};
-use std::{fs, io};
+use oak_client::{create_tls_channel, interceptors::label::LabelInterceptor};
 use structopt::StructOpt;
 use tonic::Request;
 use treehouse_client::proto::{
-    treehouse_client::TreehouseClient, GetCardsResponse, GetCardsRequest,
+    treehouse_client::TreehouseClient, GetCardsRequest,
 };
 
 #[derive(StructOpt, Clone)]
@@ -31,91 +29,46 @@ use treehouse_client::proto::{
 pub struct Opt {
     #[structopt(
         long,
-        help = "Path to the PEM-encoded CA root certificate.",
-        default_value = "../../../examples/certs/local/ca.pem"
+        help = "URI of the Oak application to connect to",
+        default_value = "https://localhost:8080"
     )]
-    ca_cert: String,
-}
-
-async fn send_request(
-    client: &hyper::client::Client<
-        hyper_rustls::HttpsConnector<hyper::client::HttpConnector>,
-        hyper::Body,
-    >,
-    uri: &str,
-    label_bytes: &[u8],
-    signature: String,
-) {
-    let request = hyper::Request::builder()
-        .method(http::Method::GET)
-        .uri(uri)
-        .header(oak_abi::OAK_LABEL_HTTP_JSON_KEY, label_bytes)
-        .header(oak_abi::OAK_SIGNED_CHALLENGE_HTTP_JSON_KEY, signature)
-        .body(hyper::Body::empty())
-        .unwrap();
-
-    let resp = client
-        .request(request)
-        .await
-        .expect("Error while awaiting response");
-
-    assert_eq!(resp.status(), http::StatusCode::OK);
-
-    log::info!("response: {:?}", resp);
-    log::info!(
-        "response body: {:?}",
-        hyper::body::to_bytes(resp.into_body())
-            .await
-            .expect("Couldn't read response body")
-    );
+    uri: String,
+    #[structopt(
+        long,
+        help = "PEM encoded X.509 TLS root certificate file used by gRPC client"
+    )]
+    root_tls_certificate: String,
+    #[structopt(long, help = "Search calendar events on a specified date")]
+    date: String,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
-    // Send a request, and wait for the response
-    let label = oak_abi::label::Label::public_untrusted();
-    let label_bytes = serde_json::to_string(&label)
-        .context("Couldn't serialize public/untrusted label to JSON.")?
-        .into_bytes();
     let opt = Opt::from_args();
 
-    let key_pair = oak_sign::KeyPair::generate()?;
-    let signature =
-        oak_sign::SignatureBundle::create(oak_abi::OAK_CHALLENGE.as_bytes(), &key_pair)?;
+    let uri = opt.uri.parse().context("Error parsing URI")?;
+    let root_tls_certificate =
+        std::fs::read(&opt.root_tls_certificate).context("Couldn't load certificate file")?;
+    let date = opt.date;
 
-    // Signed challenge
-    let signature = oak_abi::proto::oak::identity::SignedChallenge {
-        signed_hash: signature.signed_hash,
-        public_key: key_pair.pkcs8_public_key(),
-    };
+    info!("Connecting to Oak Application: {:?}", uri);
+    let channel = create_tls_channel(&uri, &root_tls_certificate)
+        .await
+        .context("Couldn't create TLS channel")?;
+    let label = Label::public_untrusted();
+    let interceptor =
+        LabelInterceptor::create(&label).context("Couldn't create gRPC interceptor")?;
+    let mut client = TreehouseClient::with_interceptor(channel, interceptor);
 
-    let path = &opt.ca_cert;
-    let ca_file =
-        std::fs::File::open(path).unwrap_or_else(|e| panic!("Couldn't open {}: {}", path, e));
-    let mut ca = std::io::BufReader::new(ca_file);
+    let request = Request::new(GetCardsRequest { date });
+    info!("Sending request: {:?}", request);
 
-    // Build an HTTP connector which supports HTTPS too.
-    let mut http = hyper::client::HttpConnector::new();
-    http.enforce_http(false);
-    // Build a TLS client, using the custom CA store for lookups.
-    let mut tls = rustls::ClientConfig::new();
-    tls.root_store
-        .add_pem_file(&mut ca)
-        .expect("Couldn't load custom CA store");
-    // Join the above part into an HTTPS connector.
-    let https = hyper_rustls::HttpsConnector::from((http, tls));
-
-    let client: hyper::client::Client<_, hyper::Body> =
-        hyper::client::Client::builder().build(https);
-
-    send_request(
-        &client,
-        "https://localhost:8080",
-        &label_bytes,
-        serde_json::to_string(&signature).unwrap(),
-    )
-    .await;
+    let response = client
+        .get_cards(request)
+        .await
+        .context("Couldn't receive response")?;
+    info!("Received response: {:?}", response);
 
     Ok(())
 }
