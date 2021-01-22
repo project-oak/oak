dc49da0b15d98904a56aa53558e5c6dcca500514
Andrew Ferraiuolo <aferr@google.com>
2021-01-22 18:02:42 +0000

    Some fixup.

--- experimental/ni_coq/vfiles/EscapeHatchCondition_def.v
+++ experimental/ni_coq/vfiles/EscapeHatchCondition_def.v
@@ -14,22 +14,23 @@ From OakIFC Require Import
 Definition eh_t := @trace (state * down_l * event_l).
 Definition is_init(t: eh_t) := length t = 1.
 
-Definition d_in_trace_elt d (t_elt: (state * down_l * event_l)) :=
-    match t_elt with
-        | (_, d, _) => True
-        | _ => False
-    end.
+Inductive list_eq {A: Type}: list A -> list A -> Prop :=
+    | LEqBothNil: list_eq [] []
+    | LEqAddBoth e1 e2 l1 l2: 
+            e1 = e2 ->
+            list_eq l1 l2 ->
+            list_eq (e1::l1) (e2::l2).
 
-Inductive traces_corresp: @trace down_l -> eh_t -> Prop :=
-    | TCBothNil: traces_corresp [] []
-    | TCAddHeads d t_elt dt t: 
-        d_in_trace_elt d t_elt ->
-        traces_corresp (d::dt) (t_elt::t).
+Definition traces_corresp (dt: @trace down_l)(t: eh_t) :=
+    list_eq dt (extract_downgrade_trace t).
+
+Definition eh_trace_low_eq: @trace_low_eqT (state * down_l *event_l) := 
+    @trace_low_eq_down state_low_eq down_low_eq event_low_eq.
 
 Definition conjecture_escape_hatch
-    (sem: @trace_semanticsT (state * list event_l)) :=
+    (sem: @trace_semanticsT (state * down_l * event_l)) :=
     forall ell t1_init t2_init dt1 dt2 t1n,
-    (trace_low_eq_down ell t1_init t2_init) /\
+    (eh_trace_low_eq ell t1_init t2_init) /\
     (down_list_low_eq ell dt1 dt2) /\
     (is_init t1_init) /\
     (is_init t2_init) /\
@@ -37,5 +38,5 @@ Definition conjecture_escape_hatch
     (sem t1_init t1n) ->
     (exists t2n,
         (sem t2_init t2n) /\
-        (trace_low_eq_down ell t1n t2n) /\
+        (eh_trace_low_eq ell t1n t2n) /\
         (traces_corresp dt2 t2n)).
