c4f3427177a2e048863c9ef20c8d87f855c67170
Juliette Pretot <julsh@google.com>
2021-02-01 19:03:23 +0000

    Remove dependency on @grpc/proto-loader

--- examples/hello_world/client/nodejs/index.js
+++ examples/hello_world/client/nodejs/index.js
@@ -17,7 +17,6 @@
 const fs = require('fs');
 const protobufjs = require('protobufjs');
 const grpc = require('@grpc/grpc-js');
-const grpcProtoLoader = require('@grpc/proto-loader');
 
 const REPOSITORY_ROOT = `${__dirname}/../../../..`;
 const CERT_PATH = `${REPOSITORY_ROOT}/examples/certs/local/ca.pem`;
@@ -28,20 +27,14 @@ const SERVICE_PROTO_PATH = `${REPOSITORY_ROOT}/examples/hello_world/proto/hello_
 const oakLabelGrpcMetadataKey = 'x-oak-label-bin';
 
 async function main() {
-  const [oaklabelDefinition, helloWorldDefinition] = await Promise.all([
-    // We use two different libs with a similar APIs to load proto files,
-    // which due to the design of the `@grpc/grpc-js` lib.
-    // `@grpc/proto-loader` is a wrapper around `protobufjs` that adds
-    // functionality required for working with `grpc-js`, but omits others.
-    // Hence we use grpcProtoLoader for gRPC services, and protobufjs
-    // for all other protos.
-    protobufjs.load(OAK_LABEL_PROTO_PATH),
-    grpcProtoLoader.load(SERVICE_PROTO_PATH),
+  const protos = await protobufjs.load([
+    OAK_LABEL_PROTO_PATH,
+    SERVICE_PROTO_PATH,
   ]);
 
   function getGrpcMetadata() {
     // TODO(#1097): move this into an SDK package to allow re-use.
-    const OakLabel = oaklabelDefinition.lookupType('oak.label.Label');
+    const OakLabel = protos.lookupType('oak.label.Label');
 
     // TODO(#1066): Use a more restrictive Label.
     const publicUntrustedLabel = OakLabel.create({});
@@ -53,31 +46,34 @@ async function main() {
     return metaData;
   }
 
-  const helloWorldProto = grpc.loadPackageDefinition(helloWorldDefinition).oak
-    .examples.hello_world;
+  const helloWorldService = protos.lookupService(
+    'oak.examples.hello_world.HelloWorld'
+  );
   const credentials = grpc.credentials.createSsl(fs.readFileSync(CERT_PATH));
 
-  const client = new helloWorldProto.HelloWorld('localhost:8080', credentials);
+  const grpcClient = new grpc.Client('localhost:8080', credentials);
 
-  // For documentation on client calls see the `@grpc/grpc-js` documentation:
-  // https://grpc.github.io/grpc/node/grpc.Client.html#~CallProperties
-  client.sayHello(
-    // The arguments passed to the gRPC service. Corresponds to the
-    // `HelloRequest` message type in hello_world.proto file.
-    { greeting: 'Node.js' },
-    // The metadata for this gRPC call.
-    getGrpcMetadata(),
-    // Callback invoked with the response.
-    (error, response) => {
-      if (error) {
-        console.error(error);
-        process.exit(1);
-      } else {
-        console.log(response.reply);
-        process.exit(0);
-      }
+  const HelloWorld = helloWorldService.create(
+    (method, requestData, callback) => {
+      grpcClient.makeUnaryRequest(
+        `/oak.examples.hello_world.HelloWorld/${method.name}`,
+        (arg) => arg,
+        (arg) => arg,
+        requestData,
+        getGrpcMetadata(),
+        callback
+      );
     }
   );
+
+  try {
+    const response = await HelloWorld.sayHello({ greeting: 'Node.js' });
+    console.log(response.reply);
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
 
 main();
