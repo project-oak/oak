5efa408bc3ddf9a7210a2129363870e24f174ccf
Ivan Petrov <ivanpetrov@google.com>
2021-03-29 10:38:45 +0100

    Run examples with no clients (#1928)
    This change restores the ability to run examples with no clients.
    Required for `reproducibility` builds.

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -658,14 +658,7 @@ fn run_example(opt: &RunExamples, example: &Example) -> Step {
     #[allow(clippy::collapsible_if)]
     let run_backend_server_clients: Step = if opt.run_server.unwrap_or(true) {
         let run_server_clients = if example.applications.is_empty() {
-            if opt.build_client.client_variant == NO_CLIENTS {
-                panic!(
-                    "`{}` client variant is not supported when no applications are provided",
-                    NO_CLIENTS
-                );
-            } else {
-                run_clients
-            }
+            run_clients
         } else {
             let application = example
                 .applications
@@ -710,25 +703,14 @@ fn run_example(opt: &RunExamples, example: &Example) -> Step {
                 }
             })
     } else {
-        if opt.build_client.client_variant != NO_CLIENTS {
-            run_clients
-        } else {
-            Step::Multiple {
-                name: "run clients (empty)".to_string(),
-                steps: vec![],
-            }
-        }
+        run_clients
     };
 
     Step::Multiple {
         name: example.name.to_string(),
         steps: vec![
             if example.applications.is_empty() {
-                if opt.build_client.client_variant == NO_CLIENTS {
-                    panic!("`{}` client variant is not supported when no applications are provided", NO_CLIENTS);
-                } else {
-                    vec![]
-                }
+                vec![]
             } else {
                 let application = example
                     .applications
