b509192365ce590baed46e35d08fcf7aa729c6a8
Tiziano Santoro <tiziano88@gmail.com>
2021-04-21 19:16:02 +0100

    Implement Weather Lookup functionality
    This replaces the Hello World example content. We may want to rename it
    to `private_weather` in the future.
    
    Also update lookup data generator to support the appropriate data
    format.

--- oak_functions/lookup_data_generator/src/main.rs
+++ oak_functions/lookup_data_generator/src/main.rs
@@ -19,20 +19,32 @@ use bytes::BytesMut;
 use oak_functions_abi::proto::Entry;
 use prost::Message;
 use rand::Rng;
+use serde::Serialize;
 use std::{fs::File, io::Write};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Clone, Debug)]
 #[structopt(about = "Oak Functions Lookup Data Generator")]
 pub struct Opt {
-    #[structopt(long, default_value = "20")]
-    key_size_bytes: usize,
-    #[structopt(long, default_value = "1000")]
-    value_size_bytes: usize,
-    #[structopt(long, default_value = "100")]
-    entries: usize,
     #[structopt(long)]
     out_file_path: String,
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(StructOpt, Clone, Debug)]
+pub enum Command {
+    #[structopt(about = "Generate random key value pairs")]
+    Random {
+        #[structopt(long, default_value = "20")]
+        key_size_bytes: usize,
+        #[structopt(long, default_value = "1000")]
+        value_size_bytes: usize,
+        #[structopt(long, default_value = "100")]
+        entries: usize,
+    },
+    #[structopt(about = "Generate entries for the weather lookup example")]
+    Weather {},
 }
 
 fn create_bytes<R: Rng>(rng: &mut R, size_bytes: usize) -> Vec<u8> {
@@ -52,11 +64,42 @@ fn main() -> anyhow::Result<()> {
     let opt = Opt::from_args();
     let mut buf = BytesMut::new();
     let mut rng = rand::thread_rng();
-    for _ in 0..opt.entries {
-        let entry = create_entry(&mut rng, opt.key_size_bytes, opt.value_size_bytes);
-        entry
-            .encode_length_delimited(&mut buf)
-            .context("could not encode entry")?;
+    match opt.cmd {
+        Command::Random {
+            key_size_bytes,
+            value_size_bytes,
+            entries,
+        } => {
+            for _ in 0..entries {
+                let entry = create_entry(&mut rng, key_size_bytes, value_size_bytes);
+                entry
+                    .encode_length_delimited(&mut buf)
+                    .context("could not encode entry")?;
+            }
+        }
+        Command::Weather {} => {
+            #[derive(Serialize)]
+            struct WeatherValue {
+                temperature_degrees_celsius: i32,
+            }
+            let dist = rand::distributions::Uniform::new(-30, 40);
+            for lat in -90..=90 {
+                for lon in -180..=180 {
+                    let key = format!("{},{}", lat, lon);
+                    let value = serde_json::to_string(&WeatherValue {
+                        temperature_degrees_celsius: rng.sample(dist),
+                    })
+                    .unwrap();
+                    let entry = Entry {
+                        key: key.as_bytes().to_vec(),
+                        value: value.as_bytes().to_vec(),
+                    };
+                    entry
+                        .encode_length_delimited(&mut buf)
+                        .context("could not encode entry")?;
+                }
+            }
+        }
     }
     let mut file = File::create(opt.out_file_path).context("could not create out file")?;
     file.write_all(&buf).context("could not write to file")?;
