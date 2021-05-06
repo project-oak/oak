d60fa16ab1f2baca2c73f13af18750633755b7f4
Conrad Grobler <grobler@google.com>
2021-05-06 15:02:40 +0000

    Add log macro

--- oak_functions/sdk/oak_functions/src/lib.rs
+++ oak_functions/sdk/oak_functions/src/lib.rs
@@ -83,10 +83,21 @@ pub fn storage_get_item(key: &[u8]) -> Result<Option<Vec<u8>>, OakStatus> {
 /// `oak_unsafe` feature is enabled.
 ///
 /// See [`write_log_message`](https://github.com/project-oak/oak/blob/main/docs/oak_functions_abi.md#write_log_message).
-pub fn log<T: AsRef<str>>(message: T) -> Result<(), OakStatus> {
+pub fn write_log_message<T: AsRef<str>>(message: T) -> Result<(), OakStatus> {
     let buf = message.as_ref().as_bytes();
     let status = unsafe { oak_functions_abi::write_log_message(buf.as_ptr(), buf.len()) };
     result_from_status(status as i32, ())
+}
+
+/// Logs a debug message.
+///
+/// These log messages are considered sensitive, so will only be logged by the runtime if the
+/// `oak_unsafe` feature is enabled.
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt)+) => {
+        $crate::write_log_message(format!($($arg)+))
+    }
 }
 
 /// Convert a status returned from a host function call to a `Result`.
