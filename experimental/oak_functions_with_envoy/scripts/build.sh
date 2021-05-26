b1beece5a788197b80e7c36a0e4886e4acf1fb7b
Razieh Behjati <razieh@google.com>
2021-05-26 15:43:26 +0100

    QC

--- experimental/oak_functions_with_envoy/scripts/build.sh
+++ experimental/oak_functions_with_envoy/scripts/build.sh
@@ -4,14 +4,11 @@ readonly EXPERIMENTAL_SCRIPTS_DIR="$(dirname "$0")"
 # shellcheck source=experimental/oak_functions_with_envoy/scripts/common.sh
 source "$EXPERIMENTAL_SCRIPTS_DIR/common.sh"
 
-# Build Oak Functions server binary
-./scripts/docker_run ./scripts/runner build-functions-server --server-variant=base
-
-# Build the `weather_lookup` example application built on Oak Functions.
-cargo -Zunstable-options build --release \
-  --target=wasm32-unknown-unknown \
-  --manifest-path=./oak_functions/examples/weather_lookup/module/Cargo.toml \
-  --out-dir=./oak_functions/examples/weather_lookup/bin
+# Build Oak Functions server binary, and the `weather_lookup` example application built on Oak Functions.
+./scripts/docker_run ./scripts/runner run-functions-examples \
+  --example-name=weather_lookup \
+  --run-server=false \
+  --client-variant=none
 
 # Copy binaries to `experimental` directory, because global `.dockerignore` ignores all the files.
 readonly EXPERIMENTAL_BIN=./experimental/oak_functions_with_envoy/bin
