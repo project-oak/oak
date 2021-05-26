3b74a8797570b056e53785149c62ea80a295e494
rbehjati <razieh@google.com>
2021-05-26 17:47:56 +0100

    Change deploy script for Oak Functions to run outside docker (#2139)

--- experimental/oak_functions_with_envoy/scripts/build.sh
+++ experimental/oak_functions_with_envoy/scripts/build.sh
@@ -4,16 +4,11 @@ readonly EXPERIMENTAL_SCRIPTS_DIR="$(dirname "$0")"
 # shellcheck source=experimental/oak_functions_with_envoy/scripts/common.sh
 source "$EXPERIMENTAL_SCRIPTS_DIR/common.sh"
 
-# TODO(#1943): Remove when #1943 is submitted.
-cargo build --target=x86_64-unknown-linux-musl \
-  --manifest-path=./oak_functions/loader/Cargo.toml \
-  --release
-
-# Build the `weather_lookup` example application built on Oak Functions.
-cargo -Zunstable-options build --release \
-  --target=wasm32-unknown-unknown \
-  --manifest-path=./oak_functions/examples/weather_lookup/module/Cargo.toml \
-  --out-dir=./oak_functions/examples/weather_lookup/bin
+# Build Oak Functions server binary, and the `weather_lookup` example application built on Oak Functions.
+./scripts/docker_run ./scripts/runner run-functions-examples \
+  --example-name=weather_lookup \
+  --run-server=false \
+  --client-variant=none
 
 # Copy binaries to `experimental` directory, because global `.dockerignore` ignores all the files.
 readonly EXPERIMENTAL_BIN=./experimental/oak_functions_with_envoy/bin
