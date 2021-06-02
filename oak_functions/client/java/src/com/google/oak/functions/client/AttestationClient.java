5cabb2d8c5085e6e6bbca3982c24045f83fa7d1c
rbehjati <razieh@google.com>
2021-06-02 15:47:26 +0100

    Format AttestationCleint.java (#2160)

--- oak_functions/client/java/src/com/google/oak/functions/client/AttestationClient.java
+++ oak_functions/client/java/src/com/google/oak/functions/client/AttestationClient.java
@@ -1,15 +1,12 @@
 package com.google.oak.functions.client;
 
 import com.google.common.base.VerifyException;
-import com.google.oak.functions.client.AeadEncryptor;
-import com.google.oak.functions.client.KeyNegotiator;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
-import java.lang.RuntimeException;
 import java.security.GeneralSecurityException;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -24,6 +21,10 @@ import oak.functions.server.RemoteAttestationGrpc;
 import oak.functions.server.RemoteAttestationGrpc.RemoteAttestationStub;
 
 // TODO(#2121): Implement a protocol independent state machine.
+/**
+ * Client with remote attestation support for sending requests to an Oak Functions loader
+ * application.
+ */
 public class AttestationClient {
     private static final Logger logger = Logger.getLogger(AttestationClient.class.getName());
     private final ManagedChannel channel;
@@ -38,29 +39,29 @@ public class AttestationClient {
 
         // Create server response handler.
         messageQueue = new ArrayBlockingQueue<>(1);
-        StreamObserver<AttestedInvokeResponse> responseObserver = new StreamObserver<AttestedInvokeResponse>() {
-            @Override
-            public void onNext(AttestedInvokeResponse response) {
-                try {
-                    messageQueue.put(response);
-                } catch (Exception e) {
-                    if (e instanceof InterruptedException) {
-                        Thread.currentThread().interrupt();
+        StreamObserver<AttestedInvokeResponse> responseObserver =
+                new StreamObserver<AttestedInvokeResponse>() {
+                    @Override
+                    public void onNext(AttestedInvokeResponse response) {
+                        try {
+                            messageQueue.put(response);
+                        } catch (Exception e) {
+                            if (e instanceof InterruptedException) {
+                                Thread.currentThread().interrupt();
+                            }
+                            logger.log(Level.WARNING, "Couldn't send server response to the message queue: " + e);
+                        }
                     }
-                    logger.log(Level.WARNING, "Couldn't send server response to the message queue: " + e);
-                }
-            }
 
-            @Override
-            public void onError(Throwable t) {
-                Status status = Status.fromThrowable(t);
-                logger.log(Level.WARNING, "Couldn't receive response: " + status);
-            }
+                    @Override
+                    public void onError(Throwable t) {
+                        Status status = Status.fromThrowable(t);
+                        logger.log(Level.WARNING, "Couldn't receive response: " + status);
+                    }
 
-            @Override
-            public void onCompleted() {
-            }
-        };
+                    @Override
+                    public void onCompleted() {}
+                };
         requestObserver = stub.attestedInvoke(responseObserver);
 
         // Generate client private/public key pair.
@@ -69,8 +70,10 @@ public class AttestationClient {
         ByteString publicKeyBytes = ByteString.copyFrom(publicKey);
 
         // Send client public key to the server.
-        AttestedInvokeRequest request = AttestedInvokeRequest.newBuilder()
-                .setClientIdentity(ClientIdentity.newBuilder().setPublicKey(publicKeyBytes).build()).build();
+        AttestedInvokeRequest request =
+                AttestedInvokeRequest.newBuilder()
+                        .setClientIdentity(ClientIdentity.newBuilder().setPublicKey(publicKeyBytes).build())
+                        .build();
         requestObserver.onNext(request);
         AttestedInvokeResponse response = messageQueue.take();
 
@@ -96,22 +99,23 @@ public class AttestationClient {
         channel.shutdown();
     }
 
-    /**
-     * Encrypts and sends `message` via an attested gRPC channel to the server.
-     */
+    /** Encrypts and sends `message` via an attested gRPC channel to the server. */
+    @SuppressWarnings("ProtoParseWithRegistry")
     public Response send(Request request)
             throws GeneralSecurityException, InterruptedException, InvalidProtocolBufferException {
         byte[] encryptedMessage = encryptor.encrypt(request.getBody().toByteArray());
-        oak.functions.server.Request serverRequest = oak.functions.server.Request.newBuilder()
-                .setEncryptedPayload(ByteString.copyFrom(encryptedMessage)).build();
-        AttestedInvokeRequest attestedRequest = AttestedInvokeRequest.newBuilder().setRequest(serverRequest).build();
+        oak.functions.server.Request serverRequest =
+                oak.functions.server.Request.newBuilder()
+                        .setEncryptedPayload(ByteString.copyFrom(encryptedMessage))
+                        .build();
+        AttestedInvokeRequest attestedRequest =
+                AttestedInvokeRequest.newBuilder().setRequest(serverRequest).build();
 
         requestObserver.onNext(attestedRequest);
         AttestedInvokeResponse attestedResponse = messageQueue.take();
 
         byte[] responsePayload = attestedResponse.getEncryptedPayload().toByteArray();
         byte[] decryptedResponse = encryptor.decrypt(responsePayload);
-        Response response = Response.parseFrom(decryptedResponse);
-        return response;
+        return Response.parseFrom(decryptedResponse);
     }
 }
