063487b664086a6e70ef6663428101c3b8229378
Ivan Petrov <ivanpetrov@google.com>
2021-10-25 19:25:56 +0100

    Remove Math.toIntExact from Java Remote Attestation

--- remote_attestation/java/src/com/google/oak/remote_attestation/Message.java
+++ remote_attestation/java/src/com/google/oak/remote_attestation/Message.java
@@ -16,8 +16,6 @@
 
 package com.google.oak.remote_attestation;
 
-import static java.lang.Math.toIntExact;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInput;
@@ -410,7 +408,7 @@ public class Message {
     byte[] sizeBytes =
         readFixedSizeArray(inputStream, ARRAY_SERIALIZATION_PREFIX_LENGTH, name + " size");
     long size = ByteBuffer.wrap(sizeBytes).order(ByteOrder.LITTLE_ENDIAN).getLong();
-    return readFixedSizeArray(inputStream, toIntExact(size), name);
+    return readFixedSizeArray(inputStream, (int) size, name);
   }
 
   private static void writeFixedSizeArray(DataOutputStream outputStream, byte[] value, int size,
