149621fdd9e7048fcac08bfeb8ec748c92f0df9c
maria-a-schett <mariaschett@users.noreply.github.com>
2021-12-01 19:29:35 +0000

    Refactor example/abitest as integration test of SDK (#2413)
    Add module as oak_functions/examples/abitest/module to integration test and remove oak_functions/examples/abitest

---
+++ oak_functions/sdk/oak_functions/tests/integration_test.rs
@@ -0,0 +1,123 @@
+//
+// Copyright 2021 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+use lazy_static::lazy_static;
+use maplit::hashmap;
+use oak_functions_abi::proto::{Request, Response};
+use oak_functions_loader::{logger::Logger, lookup::LookupData, server::WasmHandler};
+use std::sync::Arc;
+use tokio;
+
+lazy_static! {
+    static ref WASM_MODULE_BYTES: Vec<u8> = {
+        let mut manifest_path = std::env::current_dir().unwrap();
+        manifest_path.push("tests");
+        manifest_path.push("module");
+        manifest_path.push("Cargo.toml");
+
+        test_utils::compile_rust_wasm(manifest_path.to_str().unwrap(), false)
+            .expect("Could not read Wasm module")
+    };
+}
+
+#[tokio::test]
+async fn test_read_write() {
+    let wasm_handler = WasmHandler::create(
+        &WASM_MODULE_BYTES,
+        Arc::new(LookupData::for_test(hashmap! {})),
+        vec![],
+        Logger::for_test(),
+    )
+    .expect("Could not instantiate WasmHandler.");
+
+    let request = Request {
+        body: b"ReadWrite".to_vec(),
+    };
+    let response: Response = wasm_handler.handle_invoke(request).await.unwrap();
+    assert_eq!(response.body().unwrap(), b"ReadWriteResponse");
+}
+
+#[tokio::test]
+async fn test_double_read() {
+    let wasm_handler = WasmHandler::create(
+        &WASM_MODULE_BYTES,
+        Arc::new(LookupData::for_test(hashmap! {})),
+        vec![],
+        Logger::for_test(),
+    )
+    .expect("Could not instantiate WasmHandler.");
+
+    let request = Request {
+        body: b"DoubleRead".to_vec(),
+    };
+    let response: Response = wasm_handler.handle_invoke(request).await.unwrap();
+    assert_eq!(response.body().unwrap(), b"DoubleReadResponse");
+}
+
+#[tokio::test]
+async fn test_double_write() {
+    let wasm_handler = WasmHandler::create(
+        &WASM_MODULE_BYTES,
+        Arc::new(LookupData::for_test(hashmap! {})),
+        vec![],
+        Logger::for_test(),
+    )
+    .expect("Could not instantiate WasmHandler.");
+
+    let request = Request {
+        body: b"DoubleWrite".to_vec(),
+    };
+    let response: Response = wasm_handler.handle_invoke(request).await.unwrap();
+    assert_eq!(response.body().unwrap(), b"DoubleWriteResponse");
+}
+
+#[tokio::test]
+async fn test_write_log() {
+    let wasm_handler = WasmHandler::create(
+        &WASM_MODULE_BYTES,
+        Arc::new(LookupData::for_test(hashmap! {})),
+        vec![],
+        Logger::for_test(),
+    )
+    .expect("Could not instantiate WasmHandler.");
+
+    let request = Request {
+        body: b"WriteLog".to_vec(),
+    };
+    let response: Response = wasm_handler.handle_invoke(request).await.unwrap();
+    assert_eq!(response.body().unwrap(), b"WriteLogResponse");
+}
+
+#[tokio::test]
+async fn test_storage_get_item() {
+    let entries = hashmap! {
+       b"StorageGet".to_vec() => b"StorageGetResponse".to_vec(),
+    };
+
+    let wasm_handler = WasmHandler::create(
+        &WASM_MODULE_BYTES,
+        Arc::new(LookupData::for_test(entries)),
+        vec![],
+        Logger::for_test(),
+    )
+    .expect("Could not instantiate WasmHandler.");
+
+    let request = Request {
+        body: b"StorageGet".to_vec(),
+    };
+    let response: Response = wasm_handler.handle_invoke(request).await.unwrap();
+    assert_eq!(response.body().unwrap(), b"StorageGetResponse");
+}
---
+++ oak_functions/sdk/oak_functions/tests/module/src/lib.rs
@@ -0,0 +1,109 @@
+//
+// Copyright 2021 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+//! Oak Functions ABI test.
+
+use anyhow::{anyhow, Context};
+use assert_matches::assert_matches;
+use maplit::hashmap;
+use std::collections::HashMap;
+
+type TestFn = fn(&str) -> ();
+
+struct TestManager<'a> {
+    tests: HashMap<&'a str, TestFn>,
+}
+
+impl TestManager<'static> {
+    fn new() -> Self {
+        Self {
+            tests: hashmap! [
+                "ReadWrite" => Self::test_read_write as TestFn,
+                "DoubleRead" => Self::test_double_read as TestFn,
+                "DoubleWrite" => Self::test_double_write as TestFn,
+                "WriteLog" => Self::test_write_log as TestFn,
+                "StorageGet" => Self::test_storage_get as TestFn,
+            ],
+        }
+    }
+
+    fn run_test(&self) -> anyhow::Result<()> {
+        let request = oak_functions::read_request()
+            .map_err(|error| anyhow!("Couldn't read request: {:?}", error))?;
+        let test_name = String::from_utf8(request).context("Couldn't parse request")?;
+        let test = self
+            .tests
+            .get(&test_name as &str)
+            .ok_or(anyhow!("Couldn't find test: {}", &test_name))?;
+        test(&test_name);
+        Ok(())
+    }
+
+    fn test_read_write(_request: &str) {
+        let result = oak_functions::write_response(b"ReadWriteResponse");
+        assert_matches!(result, Ok(_));
+    }
+
+    /// Tests that a second call to [`oak_functions_abi::read_request`] returns the same value.
+    fn test_double_read(first_request_body: &str) {
+        let second_read_request =
+            oak_functions::read_request().expect("Failed to read second request.");
+        assert_eq!(second_read_request, first_request_body.as_bytes());
+        // If assert_eq fails then run_test will return with an empty response body.
+        oak_functions::write_response(b"DoubleReadResponse").expect("Failed to write response.")
+    }
+
+    /// Tests that multiple calls to [`oak_functions_abi::write_response`] will replace the earlier
+    /// responses. The response body has to be checked in the integration test.
+    fn test_double_write(_request: &str) {
+        // First response contains a different value.
+        oak_functions::write_response(b"FirstResponseInDoubleWrite")
+            .expect("Failed to write first response.");
+        oak_functions::write_response(b"DoubleWriteResponse")
+            .expect("Failed to write second response.");
+    }
+
+    // TODO(#2417): Test logging of `write_log_message`
+    fn test_write_log(request: &str) {
+        let result = oak_functions::write_log_message(request);
+        assert_matches!(result, Ok(_));
+        let result = oak_functions::write_response(b"WriteLogResponse");
+        assert_matches!(result, Ok(_));
+    }
+
+    /// Tests [`oak_functions_abi::storage_get_item`] when the key in the lookup data. The lookup
+    /// data is set in the integration test. The value has to be checked in the integration
+    /// test.
+    // TODO(#2414): Add test for ERR_STORAGE_ITEM_NOT_FOUND
+    fn test_storage_get(key: &str) {
+        let value = oak_functions::storage_get_item(key.as_bytes());
+        assert_matches!(value, Ok(_));
+        let value = value.unwrap();
+        assert_matches!(value, Some(_));
+
+        oak_functions::write_response(&value.unwrap()).expect("Failed to write response.");
+    }
+
+    // TODO(#2415): Add tests for report_metric.
+
+    // TODO(#2416): Add tests for tf_model_infer.
+}
+
+#[cfg_attr(not(test), no_mangle)]
+pub extern "C" fn main() {
+    let test_manager = TestManager::new();
+    test_manager.run_test().expect("Couldn't run test");
+}
