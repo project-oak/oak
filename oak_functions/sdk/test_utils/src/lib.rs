85b5005feeb0e2f67471474bf736c8f4e28f4387
Maria Schett <mschett@google.com>
2022-02-21 11:14:51 +0000

    Rename and add comments

--- oak_functions/sdk/test_utils/src/lib.rs
+++ oak_functions/sdk/test_utils/src/lib.rs
@@ -263,7 +263,9 @@ fn create_wasm_module_bytes_from_example(
     release: bool,
 ) -> Vec<u8> {
     let mut manifest_path = std::path::PathBuf::new();
-    manifest_path.push(env!("OAK_FUNCTIONS_WORKSPACE"));
+    // WORKSPACE_ROOT is set in .cargo/config.toml.
+    manifest_path.push(env!("WORKSPACE_ROOT"));
+    manifest_path.push("oak_functions");
     manifest_path.push("examples");
     manifest_path.push(manifest_path_from_examples);
     compile_rust_wasm(manifest_path.to_str().expect("Invalid target dir"), release)
