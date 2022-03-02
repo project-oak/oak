c7dfa5a7c1ea57ea4962c8661435b5fb06697fa9
Juliette Pretot <julsh@google.com>
2022-03-02 13:45:21 +0000

    Document why EKEP messages use custom serialization logic

--- remote_attestation/rust/src/message.rs
+++ remote_attestation/rust/src/message.rs
@@ -14,6 +14,12 @@
 // limitations under the License.
 //
 
+// Messages used to implement the Enclave Key Exchange Protocol (EKEP).
+//
+// Unlike other RPC messages these are expressed as Rust structs and serialized
+// with custom logic. The primary reason for is maintaining binary-compatibility
+// with other implementations of the Enclave Key Exchange Protocol (EKEP).
+
 use crate::crypto::{
     KEY_AGREEMENT_ALGORITHM_KEY_LENGTH, NONCE_LENGTH, SIGNATURE_LENGTH,
     SIGNING_ALGORITHM_KEY_LENGTH,
