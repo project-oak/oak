57569cdea14c9a8bec00b58482dd10b80fef0361
Andri Saar <andrisaar@google.com>
2022-03-30 16:30:07 +0100

    Add a second serial port to the system. (#2643)
    The first serial port is used for UEFI console/stdio, the second one will be for our communication. For now, it just echos all bytes back.

--- experimental/uefi/src/main.rs
+++ experimental/uefi/src/main.rs
@@ -17,13 +17,24 @@
 #![no_main]
 #![no_std]
 #![feature(abi_efiapi)]
+#![feature(never_type)]
 #![feature(custom_test_frameworks)]
 // As we're in a `no_std` environment, testing requires special handling. This
 // approach was inspired by https://os.phil-opp.com/testing/.
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use uefi::{prelude::*, table::runtime::ResetType, ResultExt};
+#[macro_use]
+extern crate log;
+
+use uefi::{
+    prelude::*,
+    proto::console::serial::Serial,
+    table::{
+        boot::{OpenProtocolAttributes, OpenProtocolParams},
+        runtime::ResetType,
+    },
+};
 
 // The main entry point of the UEFI application.
 //
@@ -32,7 +43,7 @@ use uefi::{prelude::*, table::runtime::ResetType, ResultExt};
 // dependency graph.
 #[entry]
 fn _start(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
-    uefi_services::init(&mut system_table).unwrap_success();
+    uefi_services::init(&mut system_table).unwrap();
 
     // As we're not relying on the normal Rust test harness, we need to call
     // the tests ourselves if necessary.
@@ -51,14 +62,59 @@ fn _start(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
         .reset(ResetType::Shutdown, status, None);
 }
 
-fn main(_handle: Handle, system_table: &mut SystemTable<Boot>) -> Status {
+fn main(handle: Handle, system_table: &mut SystemTable<Boot>) -> Status {
     use core::fmt::Write;
 
-    let status = writeln!(system_table.stdout(), "Hello World!");
+    writeln!(system_table.stdout(), "Hello World!").unwrap();
 
-    status
-        .map(|_| Status::SUCCESS)
-        .unwrap_or(Status::DEVICE_ERROR)
+    serial_echo(handle, system_table.boot_services()).unwrap();
+}
+
+fn echo_loop(serial: &mut Serial) -> Result<!, uefi::Error<()>> {
+    let mut buf: [u8; 1024] = [0; 1024];
+
+    loop {
+        // read() returns Ok if it managed to fill the whole buffer, or the error will contain
+        // the number of bytes read. The only error we're fine with is TIMEOUT, as we can simply
+        // retry that (and we'll keep getting TIMEOUTs when nobody is talking to us). In case of
+        // any other error, bail out.
+        let len = serial.read(&mut buf).map(|_| buf.len()).or_else(|err| {
+            if err.status() == Status::TIMEOUT {
+                Ok(*err.data())
+            } else {
+                Err(err.status())
+            }
+        })?;
+
+        // Write out what we read; if we get any errors, propagate them.
+        if len > 0 {
+            info!("Read data: {:?}", &buf[..len]);
+            serial.write(&buf[..len]).discard_errdata()?;
+        }
+    }
+}
+
+fn serial_echo(handle: Handle, bt: &BootServices) -> Result<!, uefi::Error<()>> {
+    // Expect (at least) two serial ports on the system; the first will be used
+    // for stdio, and we can use the second one for our echo example. If we don't
+    // seem to have a second serial port, err out with the (arbitrarily chosen)
+    // NO_MAPPING error.
+    let serial_handles = bt.find_handles::<Serial>()?;
+    let serial_handle = serial_handles.get(1).ok_or(Status::NO_MAPPING)?;
+    let serial = bt.open_protocol::<Serial>(
+        OpenProtocolParams {
+            handle: *serial_handle,
+            agent: handle,
+            controller: None,
+        },
+        OpenProtocolAttributes::Exclusive,
+    )?;
+    // Dereference the raw pointer (*mut Serial) we get to the serial interface.
+    // This is safe as according to the UEFI spec for the OpenProtocol call to succeed the
+    // interface must not be null (see Section 7.3 in the UEFI Specification, Version 2.9).
+    let serial = unsafe { &mut *serial.interface.get() };
+
+    echo_loop(serial)
 }
 
 #[cfg(test)]
