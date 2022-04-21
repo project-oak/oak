c07f9c0d00ee57ae5e758acd6c63dce921da9aa4
Maria Schett <mschett@google.com>
2022-04-21 09:46:27 +0000

    Fix tests for find extension

--- oak_functions/loader/src/server.rs
+++ oak_functions/loader/src/server.rs
@@ -840,8 +840,10 @@ mod tests {
     #[tokio::test]
     async fn test_find_extension_not_available() {
         let mut wasm_state = create_test_wasm_state();
-        // Assumes we have add no TF extension to our test wasm_state.
-        let extension = wasm_state.find_extension(ExtensionHandle::TfHandle);
+        // Assumes we have add no TF extension to our test wasm_state. The remaining arguments don't
+        // matter, hence they are 0.
+        let extension =
+            wasm_state.invoke_extension_with_handle(ExtensionHandle::TfHandle, 0, 0, 0, 0);
         assert!(extension.is_err());
         assert_eq!(OakStatus::ErrInvalidHandle, extension.unwrap_err())
     }
@@ -849,8 +851,10 @@ mod tests {
     #[tokio::test]
     async fn test_find_extension() {
         let mut wasm_state = create_test_wasm_state();
-        // Assumes we have a Testing extension in our test wasm_state.
-        let extension = wasm_state.find_extension(ExtensionHandle::TestingHandle);
+        // Assumes we have a Testing extension in our test wasm_state. The remaining arguments don't
+        // matter, hence they are 0.
+        let extension =
+            wasm_state.invoke_extension_with_handle(ExtensionHandle::TestingHandle, 0, 0, 0, 0);
         assert!(extension.is_ok());
 
         assert_eq!(
