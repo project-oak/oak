72a511bd290410a637ef0d74fcc842c587c383e5
Conrad Grobler <grobler@google.com>
2022-04-21 18:57:29 +0000

    Improve code comments

--- experimental/offline_attestation/server/src/main.rs
+++ experimental/offline_attestation/server/src/main.rs
@@ -47,24 +47,32 @@ async fn main() -> anyhow::Result<()> {
     let opt = Opt::parse();
     let address = SocketAddr::from((Ipv6Addr::UNSPECIFIED, opt.port));
 
+    // Generate a private key for the lifetime of the server at startup.
+    // In a production implementation there would typically be some form of key provisioning
+    // mechanism that will ensure all servers that run the right code in an appropriate enclave
+    // receive the same key.
     debug!("Generating private key");
     let private_key_handle = Arc::new(generate_private_key()?);
     let public_key_handle = private_key_handle
         .public()
         .map_err(|error| anyhow!("Couldn't get public key: {}", error))?;
 
+    // Combine the public key with an attesation report in preparation for publishing it.
     let public_key_info = Arc::new(PublicKeyInfo::new(&public_key_handle)?);
     debug!(
         "Constructed public key info: {}",
         serde_json::to_string(public_key_info.as_ref())?
     );
 
+    // Set up the path filters and appropriate handlers.
     let root_filer = create_root_filter(private_key_handle, public_key_info);
 
+    // Start serving requests.
     warp::serve(root_filer).run(address).await;
     Ok(())
 }
 
+/// Creates the root filter that combines the two path filters into a single one.
 fn create_root_filter(
     private_key_handle: Arc<Handle>,
     public_key_info: Arc<PublicKeyInfo>,
@@ -72,16 +80,19 @@ fn create_root_filter(
     get_public_key_info(public_key_info).or(encrypted_echo(private_key_handle))
 }
 
+/// Path filter for "GET /public_key_info".
+///
+/// Returns the JSON-encoded public key info.
 fn get_public_key_info(
     public_key_info: Arc<PublicKeyInfo>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    // Path filer for "GET /public_key_info".
     warp::get()
         .and(warp::path(PUBLIC_KEY_INFO_PATH))
         .and(warp::path::end())
         .map(move || warp::reply::json(public_key_info.clone().as_ref()))
 }
 
+/// Path filter for "POST /encrypted_echo" with a JSON body.
 fn encrypted_echo(
     private_key_handle: Arc<Handle>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -93,6 +104,7 @@ fn encrypted_echo(
         .map(move |request: EncryptedRequest| handle(request, private_key_handle.clone()))
 }
 
+/// Handles an encrypted request.
 fn handle(encrypted_request: EncryptedRequest, private_key_handle: Arc<Handle>) -> Response {
     let result: anyhow::Result<EncryptedResponse> = try {
         debug!(
@@ -100,8 +112,11 @@ fn handle(encrypted_request: EncryptedRequest, private_key_handle: Arc<Handle>) 
             serde_json::to_string(&encrypted_request)?
         );
 
+        // Decrypt the ciphertext.
         let clear_text = decrypt(private_key_handle.as_ref(), &encrypted_request.ciphertext)?;
         info!("Received cleartext: {:?}", clear_text);
+        // For now we just echo it back, so encrypt the same clear text with the client's public
+        // key.
         let ciphertext = encrypt(&encrypted_request.get_public_key_handle()?, &clear_text)?;
         let response = EncryptedResponse { ciphertext };
 
