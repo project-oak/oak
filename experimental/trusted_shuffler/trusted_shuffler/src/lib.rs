10261e38d8b84262a0512f791c44be8c0eafdfff
Ivan Petrov <ivanpetrov@google.com>
2022-04-28 16:19:04 +0100

    Add a Trusted Shuffler initial implementation (#2680)
    This PR adds an initial implementation of the Trusted Shuffler struct.
    
    Ref #2614

--- experimental/trusted_shuffler/trusted_shuffler/src/lib.rs
+++ experimental/trusted_shuffler/trusted_shuffler/src/lib.rs
@@ -16,4 +16,144 @@
 
 //! Trusted Shuffler implementation.
 
-// TODO(#2614): Implement Trusted Shuffler logic.
+#[cfg(test)]
+mod tests;
+
+use anyhow::{anyhow, Context};
+use async_trait::async_trait;
+use futures::{stream::FuturesOrdered, StreamExt};
+use std::{
+    marker::{Send, Sync},
+    mem::take,
+    ops::DerefMut,
+    sync::{Arc, Mutex},
+};
+use tokio::sync::oneshot;
+
+type Request = Vec<u8>;
+type Response = Vec<u8>;
+
+struct Message {
+    // Determines the original order in which messages arrived.
+    // Index is used to send requests back to the client in the order of arrival.
+    index: usize,
+    // Arbitrary message data.
+    data: Vec<u8>,
+    // Channel for sending responses back to the client async tasks.
+    response_sender: oneshot::Sender<Response>,
+}
+
+#[async_trait]
+pub trait RequestHandler: Send + Sync {
+    async fn handle(&self, request: Vec<u8>) -> Vec<u8>;
+}
+
+// Trusted Shuffler implementation.
+pub struct TrustedShuffler {
+    // Value k that represents k-anonymity.
+    k: usize,
+
+    // Current batch of requests to be shuffled.
+    // Mutex is used because messages are collected in different async tasks.
+    requests_to_shuffle: Arc<Mutex<Vec<Message>>>,
+
+    // Async function that sends a request to the backend and returns a response.
+    request_handler: Arc<dyn RequestHandler>,
+}
+
+impl TrustedShuffler {
+    pub fn new(k: usize, request_handler: Arc<dyn RequestHandler>) -> Self {
+        Self {
+            k,
+            requests_to_shuffle: Arc::new(Mutex::new(vec![])),
+            request_handler,
+        }
+    }
+
+    // Asynchronously handles an incoming request.
+    pub async fn invoke(&self, request: Request) -> anyhow::Result<Response> {
+        let (response_sender, response_receiver) = oneshot::channel();
+
+        // Check if the request batch is filled and create a separate task for shuffling requests.
+        {
+            let mut requests_to_shuffle = self
+                .requests_to_shuffle
+                .lock()
+                .map_err(|error| anyhow!("Couldn't lock current messages mutex: {:?}", error))?;
+
+            let request = Message {
+                index: requests_to_shuffle.len(),
+                data: request,
+                response_sender,
+            };
+
+            requests_to_shuffle.push(request);
+
+            if requests_to_shuffle.len() >= self.k {
+                // Replace current requests with an empty vector.
+                let requests = take(requests_to_shuffle.deref_mut());
+
+                // Shuffle requests and spawn async tasks for sending them to the backend.
+                let request_handler = self.request_handler.clone();
+                tokio::spawn(async move {
+                    if let Err(error) =
+                        TrustedShuffler::shuffle_requests(requests, request_handler).await
+                    {
+                        log::error!("Shuffling error: {:?}", error);
+                    }
+                });
+            }
+        }
+
+        response_receiver.await.context("Couldn't receive response")
+    }
+
+    // Lexicographically sorts requests and sends them to the backend using the
+    // [`TrustedShuffler::request_handler`].
+    async fn shuffle_requests(
+        mut requests: Vec<Message>,
+        request_handler: Arc<dyn RequestHandler>,
+    ) -> anyhow::Result<()> {
+        requests.sort_by(|first, second| first.data.cmp(&second.data));
+
+        let response_futures: FuturesOrdered<_> = requests
+            .into_iter()
+            .map(|request| {
+                let request_handler_clone = request_handler.clone();
+                async move {
+                    let response = request_handler_clone.handle(request.data).await;
+                    Message {
+                        index: request.index,
+                        data: response,
+                        response_sender: request.response_sender,
+                    }
+                }
+            })
+            .collect();
+        let responses = response_futures.collect::<Vec<_>>().await;
+
+        Self::shuffle_responses(responses)
+    }
+
+    // Restores the original order in which messages arrived and sends them back to the client async
+    // tasks using corresponding channels defined in the [`Message::response_sender`].
+    fn shuffle_responses(mut responses: Vec<Message>) -> anyhow::Result<()> {
+        responses.sort_by_key(|response| response.index);
+
+        responses
+            .into_iter()
+            .map(
+                |Message {
+                     index: _,
+                     data,
+                     response_sender,
+                 }| {
+                    response_sender
+                        .send(data)
+                        .map_err(|_data| anyhow!("Couldn't send response"))
+                },
+            )
+            .collect::<Result<Vec<_>, _>>()
+            .map(|_: Vec<()>| ())
+    }
+}
---
+++ experimental/trusted_shuffler/trusted_shuffler/src/tests.rs
@@ -0,0 +1,132 @@
+//
+// Copyright 2022 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+use crate::{RequestHandler, TrustedShuffler};
+use async_trait::async_trait;
+use futures::future::join_all;
+use std::{sync::Arc, time::Duration};
+use tokio::time::timeout;
+
+const TEST_TIMEOUT_MILLISECONDS: u64 = 100;
+
+struct TestRequestHandler;
+
+#[async_trait]
+impl RequestHandler for TestRequestHandler {
+    // A test function that processes requests.
+    // In the real use-case, this function should send requests to the backend and
+    // return responses.
+    async fn handle(&self, request: Vec<u8>) -> Vec<u8> {
+        generate_response(request)
+    }
+}
+
+// Non-async version of the `handle_request` function used to create expected responses.
+fn generate_response(request: Vec<u8>) -> Vec<u8> {
+    let parsed_request = String::from_utf8(request).expect("Couldn't parse request");
+    format!("Response for: {}", parsed_request).into_bytes()
+}
+
+// Generates a request and a corresponding response from a string.
+fn generate_request_and_expected_response(data: &str) -> (Vec<u8>, Vec<u8>) {
+    let request = format!("Request: {}", data).into_bytes();
+    let expected_response = generate_response(request.clone());
+    (request, expected_response)
+}
+
+#[tokio::test]
+async fn anonymity_value_2_test() {
+    let anonymity_value = 2;
+    let trusted_shuffler = Arc::new(TrustedShuffler::new(
+        anonymity_value,
+        Arc::new(TestRequestHandler {}),
+    ));
+
+    let (request, expected_response) = generate_request_and_expected_response("Test");
+    let (background_request, expected_background_response) =
+        generate_request_and_expected_response("Background test");
+
+    let trusted_shuffler_clone = trusted_shuffler.clone();
+    let background_result =
+        tokio::spawn(async move { trusted_shuffler_clone.invoke(background_request).await });
+
+    let response = trusted_shuffler.invoke(request.clone()).await;
+    assert!(response.is_ok());
+    assert_eq!(expected_response, response.unwrap());
+
+    let background_result = background_result.await;
+    assert!(background_result.is_ok());
+    let background_response = background_result.unwrap();
+    assert!(background_response.is_ok());
+    assert_eq!(expected_background_response, background_response.unwrap());
+}
+
+#[tokio::test]
+async fn anonymity_value_10_test() {
+    let anonymity_value = 10;
+    let trusted_shuffler = Arc::new(TrustedShuffler::new(
+        anonymity_value,
+        Arc::new(TestRequestHandler {}),
+    ));
+
+    let (requests, expected_responses): (Vec<Vec<u8>>, Vec<Vec<u8>>) = (0..anonymity_value)
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|k| generate_request_and_expected_response(&format!("Test {}", k)))
+        .unzip();
+
+    let mut result_futures = vec![];
+    for request in requests.iter() {
+        let trusted_shuffler_clone = trusted_shuffler.clone();
+        let request_clone = request.clone();
+        let result_future =
+            tokio::spawn(async move { trusted_shuffler_clone.invoke(request_clone).await });
+        result_futures.push(result_future);
+    }
+    let results = join_all(result_futures).await;
+
+    for (expected_response, result) in expected_responses.iter().zip(results.iter()) {
+        assert!(result.is_ok());
+        let response = result.as_ref().unwrap();
+        assert!(response.is_ok());
+        assert_eq!(*expected_response, *response.as_ref().unwrap());
+    }
+}
+
+// Test that the Trusted Shuffler with the anonymity value equal to 3 waits for the 3rd client and
+// doesn't process requests from only 2 clients.
+#[tokio::test]
+async fn waiting_for_enough_requests_test() {
+    let anonymity_value = 3;
+    let trusted_shuffler = Arc::new(TrustedShuffler::new(
+        anonymity_value,
+        Arc::new(TestRequestHandler {}),
+    ));
+
+    let (request_1, _) = generate_request_and_expected_response("Test 1");
+    let (request_2, _) = generate_request_and_expected_response("Test 2");
+
+    let trusted_shuffler_clone = trusted_shuffler.clone();
+    tokio::spawn(async move { trusted_shuffler_clone.invoke(request_1).await });
+
+    let response_future = trusted_shuffler.invoke(request_2);
+    let result = timeout(
+        Duration::from_millis(TEST_TIMEOUT_MILLISECONDS),
+        response_future,
+    )
+    .await;
+    assert!(result.is_err());
+}
