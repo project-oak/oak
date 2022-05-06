d3c5f60e6da3c05d79aa73a8b4c43c99bc0a97c9
Andri Saar <andrisaar@google.com>
2022-05-06 16:20:40 +0000

    muck around with /dev/kvm permissions

--- kokoro/presubmit.sh
+++ kokoro/presubmit.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 ./scripts/docker_pull
+
+ls -l /dev/kvm
+sudo chwon "$USER" /dev/kvm
+ls -l /dev/kvm
+./scripts/docker_run ls -l /dev/kvm
+
 ./scripts/docker_run ./scripts/xtask build-oak-functions-server-variants
 ./scripts/docker_run ./scripts/xtask run-vm-test
