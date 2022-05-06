2087321a9f553eca81b48b0f6d51b73181a475c8
Andri Saar <andrisaar@google.com>
2022-05-06 16:41:29 +0000

    Apparently I can't spell

--- kokoro/presubmit.sh
+++ kokoro/presubmit.sh
@@ -3,7 +3,9 @@
 ./scripts/docker_pull
 
 ls -l /dev/kvm
-sudo chwon "$USER" /dev/kvm
+# Fix the permissions of /dev/kvm.
+readonly KVM_GID="$(getent group kvm | cut -d: -f3)"
+sudo chown "$USER:$KVM_GID" /dev/kvm
 ls -l /dev/kvm
 ./scripts/docker_run ls -l /dev/kvm
 
