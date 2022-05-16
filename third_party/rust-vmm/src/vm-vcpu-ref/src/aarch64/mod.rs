13db427ab4152735ec09e5009f3086e23e975ed8
Andri Saar <andrisaar@google.com>
2022-05-16 14:07:05 +0000

    Remove rust-vmm code from third_party

--- third_party/rust-vmm/src/vm-vcpu-ref/src/aarch64/mod.rs
+++
@@ -1,4 +0,0 @@
-#![cfg(target_arch = "aarch64")]
-
-/// Helpers for setting up the interrupt controller.
-pub mod interrupts;
--- third_party/rust-vmm/src/vm-vcpu/src/lib.rs
+++
@@ -1,4 +0,0 @@
-// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
-pub mod vcpu;
-pub mod vm;
