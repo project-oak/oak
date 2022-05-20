ccdc3f857dfb1fc2976df45125c52f9f4c5fbdee
Andri Saar <andrisaar@google.com>
2022-05-20 16:38:49 +0000

    Add a crosvm-compatible bootloader.

--- third_party/rust-hypervisor-firmware-subset/src/lib.rs
+++ third_party/rust-hypervisor-firmware-subset/src/lib.rs
@@ -12,7 +12,7 @@ use core::ffi::c_void;
 
 mod asm;
 pub mod boot;
-mod common;
+pub mod common;
 pub mod device;
 mod gdt;
 pub mod mem;
@@ -32,28 +32,22 @@ extern "C" {
     static STACK_START: c_void;
 }
 
-const PAGE_SIZE: u64 = 4096;
-
 pub fn ram_min() -> u64 {
     let ram_min = unsafe { &RAM_MIN as *const _ as u64 };
-    assert!(ram_min % PAGE_SIZE == 0);
     ram_min
 }
 
 pub fn text_start() -> u64 {
     let text_start = unsafe { &TEXT_START as *const _ as u64 };
-    assert!(text_start % PAGE_SIZE == 0);
     text_start
 }
 
 pub fn text_end() -> u64 {
     let text_end = unsafe { &TEXT_END as *const _ as u64 };
-    assert!(text_end % PAGE_SIZE == 0);
     text_end
 }
 
 pub fn stack_start() -> u64 {
     let stack_start = unsafe { &STACK_START as *const _ as u64 };
-    assert!(stack_start % PAGE_SIZE == 0);
     stack_start
 }
