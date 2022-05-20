c54314c9fc519280ff7a6807b2ade8afaa7d3fd8
Conrad Grobler <grobler@google.com>
2022-05-20 12:33:29 +0000

    Add tests

--- experimental/virtio/src/queue.rs
+++ experimental/virtio/src/queue.rs
@@ -296,11 +296,19 @@ impl<const QUEUE_SIZE: usize, const BUFFER_SIZE: usize> Queue<QUEUE_SIZE, BUFFER
             self.queue_type != QueueType::DriverWriteOnly,
             "Cannot read from a driver-write-only queue."
         );
-        // TODO(#2876): Avoid copying the buffer slice if possible.
-        self.get_next_used_element().map(|used_element| {
-            self.get_mut_slice_from_index(used_element.id as usize, used_element.len as usize)
-                .to_vec()
-        })
+        if let Some(UsedElem { id, len }) = self.get_next_used_element() {
+            // TODO(#2876): Avoid copying the buffer slice if possible.
+            let result = self
+                .get_mut_slice_from_index(id as usize, len as usize)
+                .to_vec();
+
+            // Recycle used element to so the buffer is available again.
+            self.add_available_descriptor(id as u16);
+
+            Some(result)
+        } else {
+            None
+        }
     }
 
     /// Writes the data to a buffer and adds its descriptor to the available ring if possible and
@@ -325,7 +333,7 @@ impl<const QUEUE_SIZE: usize, const BUFFER_SIZE: usize> Queue<QUEUE_SIZE, BUFFER
         if let Some(id) = self.free_ids.pop_back() {
             // TODO(#2876): Avoid copying the buffer slice if possible.
             self.get_mut_slice_from_index(id as usize, len)
-                .copy_from_slice(data);
+                .copy_from_slice(&data[..len]);
 
             // Update the length of the descriptor.
             let mut desc = &mut self.virt_queue.desc[id as usize];
@@ -353,8 +361,8 @@ impl<const QUEUE_SIZE: usize, const BUFFER_SIZE: usize> Queue<QUEUE_SIZE, BUFFER
     fn get_next_used_element(&mut self) -> Option<UsedElem> {
         // Memory fence so that we read a fresh value from the device-owned section.
         core::sync::atomic::fence(core::sync::atomic::Ordering::Acquire);
-        let next_used = self.virt_queue.used.idx;
-        if next_used == self.last_used_idx {
+        let next_used = self.last_used_idx;
+        if next_used == self.virt_queue.used.idx {
             None
         } else {
             self.last_used_idx += 1;
@@ -367,6 +375,232 @@ impl<const QUEUE_SIZE: usize, const BUFFER_SIZE: usize> Queue<QUEUE_SIZE, BUFFER
         // Add the descriptor index to the available ring at the next location.
         self.virt_queue.avail.ring[self.virt_queue.avail.idx as usize % QUEUE_SIZE] = index;
         // Increment the available ring index to use next time.
-        self.virt_queue.avail.idx += 1;
+        let next = self.virt_queue.avail.idx + 1;
+        let idx = &mut self.virt_queue.avail.idx;
+        // Memory fence to ensure the device will not see the index update before the available ring
+        // entry update.
+        core::sync::atomic::fence(core::sync::atomic::Ordering::Release);
+        *idx = next;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const QUEUE_SIZE: usize = 4;
+    const BUFFER_SIZE: usize = 4;
+
+    #[test]
+    fn test_read_empty_queue() {
+        let mut queue = Queue::<QUEUE_SIZE, BUFFER_SIZE>::new_device_write_only();
+        let data = queue.read_next_used_buffer();
+        assert!(data.is_none());
+        assert_eq!(queue.last_used_idx, 0);
+        assert_eq!(queue.virt_queue.used.idx, 0);
+        assert_eq!(queue.virt_queue.avail.idx as usize, QUEUE_SIZE);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_read_from_driver_write_only_queue() {
+        Queue::<QUEUE_SIZE, BUFFER_SIZE>::new_driver_write_only().read_next_used_buffer();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_write_to_device_write_only_queue() {
+        Queue::<QUEUE_SIZE, BUFFER_SIZE>::new_device_write_only().write_buffer(&vec![0, 1]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_queue_size() {
+        // `QUEUE_SIZE` must be a power of 2.
+        let _ = Queue::<3, BUFFER_SIZE>::new_device_write_only();
+    }
+
+    #[test]
+    fn test_read_once() {
+        let data = vec![0, 1, 2];
+        let mut queue = Queue::<QUEUE_SIZE, BUFFER_SIZE>::new_device_write_only();
+
+        let result = device_write(queue.virt_queue.as_mut(), &data);
+        assert_eq!(Some(3), result);
+        assert_eq!(queue.last_used_idx, 0);
+        assert_eq!(queue.virt_queue.used.idx, 1);
+        assert_eq!(queue.virt_queue.used.ring[0].len, 3);
+        assert_eq!(queue.virt_queue.avail.idx as usize, QUEUE_SIZE);
+
+        let read = queue.read_next_used_buffer();
+        assert_eq!(queue.last_used_idx, 1);
+        assert_eq!(Some(data), read);
+        assert_eq!(queue.virt_queue.avail.idx as usize, QUEUE_SIZE + 1);
+    }
+
+    #[test]
+    fn test_write_once() {
+        let data = vec![0, 1, 2];
+        let mut queue = Queue::<QUEUE_SIZE, BUFFER_SIZE>::new_driver_write_only();
+
+        let result = queue.write_buffer(&data);
+        assert_eq!(Some(3), result);
+        assert_eq!(queue.virt_queue.avail.idx as usize, 1);
+
+        let read = device_read_once(queue.virt_queue.as_mut());
+        assert_eq!(queue.virt_queue.used.idx, 1);
+        assert_eq!(Some(data), read);
+    }
+
+    #[test]
+    fn test_write_too_long() {
+        let data = vec![0, 1, 2, 3, 4];
+        let mut queue = Queue::<QUEUE_SIZE, BUFFER_SIZE>::new_driver_write_only();
+
+        let result = queue.write_buffer(&data);
+        assert_eq!(Some(4), result);
+    }
+
+    #[test]
+    fn test_device_queue_exhaustion() {
+        let mut queue = Queue::<QUEUE_SIZE, BUFFER_SIZE>::new_device_write_only();
+        let data = Some(vec![0]);
+        // We can write 4 times.
+        let result = device_write(queue.virt_queue.as_mut(), data.as_ref().unwrap());
+        assert_eq!(Some(1), result);
+        let result = device_write(queue.virt_queue.as_mut(), data.as_ref().unwrap());
+        assert_eq!(Some(1), result);
+        let result = device_write(queue.virt_queue.as_mut(), data.as_ref().unwrap());
+        assert_eq!(Some(1), result);
+        let result = device_write(queue.virt_queue.as_mut(), data.as_ref().unwrap());
+        assert_eq!(Some(1), result);
+        // The 5th time should not write.
+        let result = device_write(queue.virt_queue.as_mut(), data.as_ref().unwrap());
+        assert_eq!(None, result);
+        // Read twice to make space.
+        let result = queue.read_next_used_buffer();
+        assert_eq!(data, result);
+        let result = queue.read_next_used_buffer();
+        assert_eq!(data, result);
+        // Now we can write twice.
+        let result = device_write(queue.virt_queue.as_mut(), data.as_ref().unwrap());
+        assert_eq!(Some(1), result);
+        let result = device_write(queue.virt_queue.as_mut(), data.as_ref().unwrap());
+        assert_eq!(Some(1), result);
+        // But the 3rd time should be blocked again.
+        let result = device_write(queue.virt_queue.as_mut(), data.as_ref().unwrap());
+        assert_eq!(None, result);
+    }
+
+    #[test]
+    fn test_driver_queue_exhaustion() {
+        let mut queue = Queue::<QUEUE_SIZE, BUFFER_SIZE>::new_driver_write_only();
+        let data = Some(vec![0]);
+        // We should be able to write 4 times.
+        let result = queue.write_buffer(data.as_ref().unwrap());
+        assert_eq!(Some(1), result);
+        let result = queue.write_buffer(data.as_ref().unwrap());
+        assert_eq!(Some(1), result);
+        let result = queue.write_buffer(data.as_ref().unwrap());
+        assert_eq!(Some(1), result);
+        let result = queue.write_buffer(data.as_ref().unwrap());
+        assert_eq!(Some(1), result);
+        // The 5th time should not be able to write.
+        let result = queue.write_buffer(data.as_ref().unwrap());
+        assert_eq!(None, result);
+        // Read twice from the device.
+        let result = device_read_once(queue.virt_queue.as_mut());
+        assert_eq!(data, result);
+        let result = device_read_once(queue.virt_queue.as_mut());
+        assert_eq!(data, result);
+        // Now we can write twice again.
+        let result = queue.write_buffer(data.as_ref().unwrap());
+        assert_eq!(Some(1), result);
+        let result = queue.write_buffer(data.as_ref().unwrap());
+        assert_eq!(Some(1), result);
+        // But the 3rd time should be blocked again.
+        let result = queue.write_buffer(data.as_ref().unwrap());
+        assert_eq!(None, result);
+    }
+
+    #[test]
+    fn test_many_echos() {
+        let data_1 = Some(vec![0]);
+        let data_2 = Some(vec![1, 2, 3]);
+        let data_3 = Some(vec![4, 5]);
+        let mut tx = Queue::<QUEUE_SIZE, BUFFER_SIZE>::new_driver_write_only();
+        let mut rx = Queue::<QUEUE_SIZE, BUFFER_SIZE>::new_device_write_only();
+        for _ in 0..100 {
+            // Run batches of 3 echos or different data lengths repeatedly.
+            tx.write_buffer(data_1.as_ref().unwrap()).unwrap();
+            tx.write_buffer(data_2.as_ref().unwrap()).unwrap();
+            tx.write_buffer(data_3.as_ref().unwrap()).unwrap();
+
+            for _ in 0..3 {
+                let tmp = device_read_once(tx.virt_queue.as_mut()).unwrap();
+                device_write(rx.virt_queue.as_mut(), &tmp).unwrap();
+            }
+
+            let result = rx.read_next_used_buffer();
+            assert_eq!(data_1, result);
+            let result = rx.read_next_used_buffer();
+            assert_eq!(data_2, result);
+            let result = rx.read_next_used_buffer();
+            assert_eq!(data_3, result);
+        }
+    }
+
+    fn device_read_once<const QUEUE_SIZE: usize>(
+        virt_queue: &mut VirtQueue<QUEUE_SIZE>,
+    ) -> Option<Vec<u8>> {
+        if virt_queue.avail.idx == virt_queue.used.idx {
+            return None;
+        }
+
+        let ring_index = virt_queue.used.idx as usize % QUEUE_SIZE;
+        let index = virt_queue.avail.ring[ring_index];
+        let desc = &virt_queue.desc[index as usize];
+        assert!(!desc.flags.contains(DescFlags::VIRTQ_DESC_F_WRITE));
+        let used_elem = &mut virt_queue.used.ring[ring_index];
+        used_elem.id = index as u32;
+        used_elem.len = 0;
+        virt_queue.used.idx += 1;
+        // Safety: we purposely use unsafe code to simulate the way the the device/VMM will interact
+        // with the memory. We treat the contents of the slice as data only and ensure we only pass
+        // valid addresses and sizes from the tests.
+        let buffer = unsafe {
+            let ptr = desc.addr as usize as *const u8;
+            let len = desc.length as usize;
+            alloc::slice::from_raw_parts(ptr, len)
+        };
+        Some(buffer.to_vec())
+    }
+
+    fn device_write<const QUEUE_SIZE: usize>(
+        virt_queue: &mut VirtQueue<QUEUE_SIZE>,
+        data: &[u8],
+    ) -> Option<usize> {
+        if virt_queue.avail.idx == virt_queue.used.idx {
+            return None;
+        }
+
+        let len = core::cmp::min(data.len(), BUFFER_SIZE);
+        let ring_index = virt_queue.used.idx as usize % QUEUE_SIZE;
+        let index = virt_queue.avail.ring[ring_index];
+        let desc = &virt_queue.desc[index as usize];
+        assert!(desc.flags.contains(DescFlags::VIRTQ_DESC_F_WRITE));
+        let used_elem = &mut virt_queue.used.ring[ring_index];
+        used_elem.id = index as u32;
+        used_elem.len = len as u32;
+        virt_queue.used.idx += 1;
+        // Safety: we purposely use unsafe code to simulate the way the the device/VMM will interact
+        // with the memory. We treat the contents of the slice as data only and ensure we only pass
+        // valid addresses and lengths from the tests.
+        let buffer = unsafe {
+            let ptr = desc.addr as usize as *mut u8;
+            alloc::slice::from_raw_parts_mut(ptr, len)
+        };
+        buffer.copy_from_slice(data);
+        Some(len)
     }
 }
