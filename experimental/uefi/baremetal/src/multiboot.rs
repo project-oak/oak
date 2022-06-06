13339ea1e853f45744b085eafb2ed93d3905a6d9
Andri Saar <andrisaar@google.com>
2022-06-06 10:02:37 +0000

    Build the Multiboot1 header in pure Rust

--- experimental/uefi/baremetal/src/multiboot.rs
+++ experimental/uefi/baremetal/src/multiboot.rs
@@ -17,6 +17,7 @@
 // Required as Bindgen-generated code doesn't follow idiomatic Rust style.
 #![allow(non_camel_case_types)]
 
+use bitflags::bitflags;
 use kernel::boot::{BootInfo, E820Entry, E820EntryType};
 
 include!(concat!(env!("OUT_DIR"), "/multiboot.rs"));
@@ -55,3 +56,57 @@ impl BootInfo<multiboot_mmap_entry> for multiboot_info {
         }
     }
 }
+
+/// Magic number identifying the Multiboot header.
+#[repr(u32)]
+enum Magic {
+    MULTIBOOT1 = 0x1BADB002,
+}
+
+bitflags! {
+    /// Flags specify features requested or required from an boot loader.
+    ///
+    /// See <https://www.gnu.org/software/grub/manual/multiboot/multiboot.html#Header-magic-fields>
+    /// for more details.
+    struct HeaderFlags: u32 {
+        /// Align all boot modules on i386 page (4KB) boundaries.
+        const MULTIBOOT_PAGE_ALIGN = 0x00000001;
+
+        /// Must pass memory information to OS.
+        const MULTIBOOT_MEMORY_INFO = 0x00000002;
+
+        /// Must pass video information to OS.
+        const MULTIBOOT_VIDEO_MODE = 0x00000004;
+
+        /// This flag indicates the use of the address fields in the header.
+        const MULTIBOOT_AOUT_KLUDGE = 0x00010000;
+    }
+}
+
+/// Multiboot header.
+///
+/// The Multiboot header must be contained completely within the first 8192 bytes of the OS image,
+/// and must be longword (32-bit) aligned. In general, it should come as early as possible, and may
+/// be embedded in the beginning of the text segment after the real executable header.
+///
+/// See <https://www.gnu.org/software/grub/manual/multiboot/multiboot.html#OS-image-format> for
+/// more details.
+#[repr(C, packed(4))]
+pub struct Header {
+    magic: Magic,
+    flags: HeaderFlags,
+    checksum: u32,
+}
+
+/// Constructs a Multiboot1 header, with the correct checksum, and the provided flags.
+const fn build_multboot_header(flags: HeaderFlags) -> Header {
+    Header {
+        magic: Magic::MULTIBOOT1,
+        flags,
+        checksum: (0x100000000u64 - (Magic::MULTIBOOT1 as u64 + flags.bits() as u64)) as u32,
+    }
+}
+
+#[link_section = ".multiboot_header"]
+#[used]
+pub static MULTIBOOT_HEADER: Header = build_multboot_header(HeaderFlags::MULTIBOOT_MEMORY_INFO);
