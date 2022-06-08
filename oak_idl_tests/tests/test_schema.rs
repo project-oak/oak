ab499abd906e6a05a86b762766b211009c421afd
Juliette Pretot <julsh@google.com>
2022-06-08 12:37:32 +0000

    oak_idl: Improve error model
    Aligns the oak_idl error model a bit closer to what gRPC does. This means:
    - Error codes represent a numeric ID, with a few missing ones added
    - Added an error message for debugging
    
    Ideally we imo should fully align the IDL error model with the gRPC status model: https://github.com/grpc/grpc/blob/master/src/proto/grpc/status/status.proto
    
    Specifically this would mean replacing our error codes, and returning a status object for both successful responses and errors.
    
    Doing so would require larger changes to the IDL, and imo is not a priority right now.

--- oak_idl_tests/tests/test_schema.rs
+++ oak_idl_tests/tests/test_schema.rs
@@ -43,7 +43,10 @@ impl test_schema::TestService for TestServiceImpl {
             &mut b,
             &test_schema::LookupDataResponseArgs { value },
         );
-        let b = b.finish(m).map_err(|_| oak_idl::Error::InternalError)?;
+        let b = b.finish(m).map_err(|_| oak_idl::Error {
+            code: oak_idl::ErrorCode::InternalError,
+            message: None,
+        })?;
         Ok(b)
     }
 
@@ -54,7 +57,10 @@ impl test_schema::TestService for TestServiceImpl {
         eprintln!("log: {}", request.entry().unwrap());
         let mut b = oak_idl::utils::MessageBuilder::default();
         let m = test_schema::LogResponse::create(&mut b, &test_schema::LogResponseArgs {});
-        let b = b.finish(m).map_err(|_| oak_idl::Error::InternalError)?;
+        let b = b.finish(m).map_err(|_| oak_idl::Error {
+            code: oak_idl::ErrorCode::InternalError,
+            message: None,
+        })?;
         Ok(b)
     }
 }
