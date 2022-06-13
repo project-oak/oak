c6c6af61494f545ab008710817e95938863686cf
jul-sh <julsh@google.com>
2022-06-13 18:11:20 +0100

    remove UEFI app (#2956)

---
+++ experimental/oak_baremetal_app_crosvm/src/main.rs
@@ -0,0 +1,63 @@
+//
+// Copyright 2022 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#![no_std]
+#![no_main]
+#![feature(lang_items)]
+#![feature(alloc_error_handler)]
+#![feature(custom_test_frameworks)]
+// As we're in a `no_std` environment, testing requires special handling. This
+// approach was inspired by https://os.phil-opp.com/testing/.
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+mod asm;
+mod bootparam;
+
+#[no_mangle]
+#[cfg(test)]
+pub extern "C" fn rust64_start(_rdi: u64, _rsi: &bootparam::boot_params) -> ! {
+    test_main();
+    oak_baremetal_kernel::i8042::shutdown();
+}
+
+#[no_mangle]
+#[cfg(not(test))]
+pub extern "C" fn rust64_start(_rdi: u64, rsi: &bootparam::boot_params) -> ! {
+    oak_baremetal_kernel::start_kernel(rsi);
+}
+
+#[alloc_error_handler]
+fn out_of_memory(layout: ::core::alloc::Layout) -> ! {
+    panic!("Error allocating memory: {:#?}", layout);
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    oak_baremetal_kernel::panic(info);
+}
+
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    for test in tests {
+        test();
+    }
+}
---
+++ experimental/oak_baremetal_kernel/src/lib.rs
@@ -0,0 +1,96 @@
+//
+// Copyright 2022 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+//! Main 'kernel' for bare-metal Oak Functions.
+//!
+//! This code takes care of initializing the x86-64 machine properly and
+//! handing the reins over to the oak_baremetal_runtime. It is in a separate crate so that we
+//! could support different boot protocols (eg Linux boot protocol or PVH) that
+//! require different targets, linker scripts and/or provide machine
+//! information in different data structures.
+//!
+//! Bootloaders (and VMMs under them) have to adhere to the following protocol:
+//!   * Enter 64-bit long mode, and set up basic paging -- enough to load the code, as we will set
+//!     up a full page table in `start_kernel`.
+//!   * Implement a `#[panic_handler]` that delegates to `panic()` in this crate.
+//!   * Call `start_kernel` from the entry point of the bootloader.
+
+#![no_std]
+#![feature(abi_x86_interrupt)]
+
+mod avx;
+pub mod boot;
+pub mod i8042;
+mod interrupts;
+mod libm;
+mod logging;
+mod memory;
+
+use core::panic::PanicInfo;
+use log::{error, info};
+use oak_remote_attestation::handshaker::{
+    AttestationBehavior, EmptyAttestationGenerator, EmptyAttestationVerifier,
+};
+use rust_hypervisor_firmware_boot::paging;
+use rust_hypervisor_firmware_virtio::pci::VirtioPciTransport;
+
+#[cfg(feature = "vsock_channel")]
+// The virtio vsock port on which to listen.
+const VSOCK_PORT: u32 = 1024;
+
+/// Main entry point for the kernel, to be called from bootloader.
+pub fn start_kernel<E: boot::E820Entry, B: boot::BootInfo<E>>(info: &B) -> ! {
+    avx::enable_avx();
+    logging::init_logging();
+    interrupts::init_idt();
+    paging::setup();
+    // If we don't find memory for heap, it's ok to panic.
+    memory::init_allocator(info.e820_table()).unwrap();
+    main(info);
+}
+
+fn main<E: boot::E820Entry, B: boot::BootInfo<E>>(info: &B) -> ! {
+    info!("In main! Boot protocol:  {}", info.protocol());
+    let attestation_behavior =
+        AttestationBehavior::create(EmptyAttestationGenerator, EmptyAttestationVerifier);
+    oak_baremetal_runtime::framing::handle_frames(get_channel(), attestation_behavior).unwrap();
+}
+
+// Use a virtio console device for the communications channel if we don't support virtio vsock.
+#[cfg(not(feature = "vsock_channel"))]
+fn get_channel() -> virtio::console::Console<VirtioPciTransport> {
+    let console = virtio::console::Console::find_and_configure_device()
+        .expect("Couldn't configure PCI virtio console device.");
+    info!("Console device status: {}", console.get_status());
+    console
+}
+
+// Use virtio vsock for the communications channel.
+#[cfg(feature = "vsock_channel")]
+fn get_channel() -> virtio::vsock::socket::Socket<VirtioPciTransport> {
+    let vsock = virtio::vsock::VSock::find_and_configure_device()
+        .expect("Couldn't configure PCI virtio vsock device.");
+    info!("Socket device status: {}", vsock.get_status());
+    let listener = virtio::vsock::socket::SocketListener::new(vsock, VSOCK_PORT);
+    listener.accept().expect("Couldn't accept connection.")
+}
+
+/// Common panic routine for the kernel. This needs to be wrrapped in a
+/// panic_handler function in individual bootloader crates.
+pub fn panic(info: &PanicInfo) -> ! {
+    error!("PANIC: {}", info);
+    i8042::shutdown();
+}
