9c0ecdaab30c3292e9489a83c4d2cd15f436e94c
Maria Schett <mschett@google.com>
2022-07-18 08:17:43 +0000

    Extract call to http client

--- experimental/trusted_shuffler/client/src/main.rs
+++ experimental/trusted_shuffler/client/src/main.rs
@@ -62,7 +62,7 @@ async fn main() -> anyhow::Result<()> {
 
     let start_time = Instant::now();
 
-    for (n, p) in Sieve::new()
+    for (n, prime) in Sieve::new()
         .iter()
         .take((target_qps * seconds) as usize)
         .enumerate()
@@ -72,28 +72,7 @@ async fn main() -> anyhow::Result<()> {
         // We currently spawn a new client for every new request. Alternatively we could re-use the
         // client for every round.
         clients.push(tokio::spawn(async move {
-            let request = format!("{}", p);
-            let request_sent = start_time.elapsed();
-            log::info!("Client Request {}", p);
-
-            let url = format!("{}/request", server_url);
-            let response = send_request(&url, Method::POST, request.as_bytes())
-                .await
-                .context("Couldn't receive response");
-
-            let response_received = start_time.elapsed();
-
-            let parsed_response =
-                String::from_utf8(response.unwrap()).context("Couldn't decode response body");
-
-            assert_eq!(parsed_response.unwrap(), p.to_string());
-
-            log::info!(
-                "Client Response {}: {:?}",
-                p,
-                response_received - request_sent,
-            );
-            response_received - request_sent
+            http_request(prime, server_url, start_time).await
         }));
 
         // Compute current round from queries already sent.
@@ -156,4 +135,33 @@ async fn main() -> anyhow::Result<()> {
     );
 
     Ok(())
+}
+
+// Send a HTTP request of `prime_to_send` to `server_url` taking time measurement from
+// `start_time` and return duration until response is received.
+async fn http_request(prime_to_send: u64, server_url: String, start_time: Instant) -> Duration {
+    {
+        let request = format!("{}", prime_to_send);
+        let request_sent = start_time.elapsed();
+        log::info!("Client Request {}", prime_to_send);
+
+        let url = format!("{}/request", server_url);
+        let response = send_request(&url, Method::POST, request.as_bytes())
+            .await
+            .context("Couldn't receive response");
+
+        let response_received = start_time.elapsed();
+
+        let parsed_response =
+            String::from_utf8(response.unwrap()).context("Couldn't decode response body");
+
+        assert_eq!(parsed_response.unwrap(), prime_to_send.to_string());
+
+        log::info!(
+            "Client Response {}: {:?}",
+            prime_to_send,
+            response_received - request_sent,
+        );
+        response_received - request_sent
+    }
 }
