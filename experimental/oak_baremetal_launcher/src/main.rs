c5a5f731bfef913e7158f6d79c9cd1c880fe2ac8
jul-sh <julsh@google.com>
2022-08-04 14:21:01 +0100

    Launcher extract communication logic into module (#3121)

--- experimental/oak_baremetal_launcher/src/main.rs
+++ experimental/oak_baremetal_launcher/src/main.rs
@@ -19,7 +19,6 @@
 use anyhow::Context;
 use clap::Parser;
 use instance::{crosvm, native, LaunchedInstance};
-use oak_baremetal_communication_channel::client::{ClientChannelHandle, RequestEncoder};
 use std::{
     fs,
     io::{BufRead, BufReader},
@@ -40,6 +39,7 @@ pub mod schema {
     ));
 }
 
+mod channel;
 mod instance;
 mod lookup;
 mod server;
@@ -84,70 +84,6 @@ fn path_exists(s: &str) -> Result<(), String> {
     }
 }
 
-/// Client implementation that communicates with the communication
-/// task via a bmrng channel. Used by various parts of the loader to communicate
-/// with the trusted runtime.
-pub struct Client {
-    request_dispatcher: bmrng::unbounded::UnboundedRequestSender<
-        oak_idl::Request,
-        Result<Vec<u8>, oak_idl::Status>,
-    >,
-}
-
-#[async_trait::async_trait]
-impl oak_idl::AsyncHandler for Client {
-    async fn invoke(&mut self, request: oak_idl::Request) -> Result<Vec<u8>, oak_idl::Status> {
-        self.request_dispatcher
-            .send_receive(request)
-            .await
-            .map_err(|err| {
-                oak_idl::Status::new_with_message(
-                    oak_idl::StatusCode::Internal,
-                    format!("failed when invoking the request_dispatcher: {}", err),
-                )
-            })?
-    }
-}
-
-/// Singleton responsible for sending requests, and receiving responses over the underlying
-/// communication channel with the baremetal runtime.
-pub struct BaremetalCommunicationChannel {
-    inner: ClientChannelHandle,
-    request_encoder: RequestEncoder,
-}
-
-impl BaremetalCommunicationChannel {
-    pub fn new(inner: Box<dyn oak_baremetal_communication_channel::Channel>) -> Self {
-        Self {
-            inner: ClientChannelHandle::new(inner),
-            request_encoder: RequestEncoder::default(),
-        }
-    }
-
-    fn invoke(&mut self, request: oak_idl::Request) -> Result<Vec<u8>, oak_idl::Status> {
-        let request_message = self.request_encoder.encode_request(request);
-        let request_message_invocation_id = request_message.invocation_id;
-        self.inner
-            .write_request(request_message)
-            .map_err(|_| oak_idl::Status::new(oak_idl::StatusCode::Internal))?;
-
-        let response_message = self
-            .inner
-            .read_response()
-            .map_err(|_| oak_idl::Status::new(oak_idl::StatusCode::Internal))?;
-
-        // For now all messages are sent in sequence, hence we expect that the
-        // id of the next response matches the preceeding request.
-        // TODO(#2848): Allow messages to be sent and received out of order.
-        assert_eq!(
-            request_message_invocation_id,
-            response_message.invocation_id
-        );
-
-        response_message.into()
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Args::parse();
@@ -179,52 +115,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     let comms = launched_instance.create_comms_channel().await?;
+    let connector_handle = channel::Connector::spawn(comms);
 
-    // A message based communication channel that permits other parts of the
-    // untrusted launcher to send requests to the task that handles communicating
-    // with the runtime and receive responses.
-    let (request_dispatcher, mut request_receiver) =
-        bmrng::unbounded_channel::<oak_idl::Request, Result<Vec<u8>, oak_idl::Status>>();
-
-    // Spawn task to handle communicating with the runtime and receiving responses.
-    tokio::spawn(async move {
-        let mut communication_channel = BaremetalCommunicationChannel::new(comms);
-        while let Ok((request, response_dispatcher)) = request_receiver.recv().await {
-            // At the moment requests are sent sequentially, and in FIFO order. The next request
-            // is sent only once a response to the previous message has been implemented.
-            // TODO(#2848): Implement message prioritization, and non sequential invocations.
-            let response = communication_channel.invoke(request);
-            response_dispatcher.respond(response).unwrap();
-        }
-    });
-
-    let mut lookup_data_client = schema::TrustedRuntimeAsyncClient::new(Client {
-        request_dispatcher: request_dispatcher.clone(),
-    });
     // Spawn task to load & periodically refresh lookup data.
-    tokio::spawn(async move {
-        let mut interval = tokio::time::interval(std::time::Duration::from_millis(1000 * 60 * 10));
+    {
+        let mut runtime_client = schema::TrustedRuntimeAsyncClient::new(connector_handle.clone());
+        tokio::spawn(async move {
+            let mut interval =
+                tokio::time::interval(std::time::Duration::from_millis(1000 * 60 * 10));
+            loop {
+                let lookup_data =
+                    lookup::load_lookup_data(&cli.lookup_data).expect("failed to load lookup data");
+                let encoded_lookup_data =
+                    lookup::encode_lookup_data(lookup_data).expect("failed to encode lookup data");
 
-        loop {
-            let lookup_data =
-                lookup::load_lookup_data(&cli.lookup_data).expect("failed to load lookup data");
-            let encoded_lookup_data =
-                lookup::encode_lookup_data(lookup_data).expect("failed to encode lookup data");
+                if let Err(err) = runtime_client
+                    .update_lookup_data(encoded_lookup_data.into_vec())
+                    .await
+                {
+                    panic!("failed to send lookup data: {:?}", err)
+                }
 
-            if let Err(err) = lookup_data_client
-                .update_lookup_data(encoded_lookup_data.into_vec())
-                .await
-            {
-                panic!("failed to send lookup data: {:?}", err)
+                interval.tick().await;
             }
-
-            interval.tick().await;
-        }
-    });
-
-    let mut client = schema::TrustedRuntimeAsyncClient::new(Client {
-        request_dispatcher: request_dispatcher.clone(),
-    });
+        });
+    }
 
     let wasm_bytes = fs::read(&cli.wasm)
         .with_context(|| format!("Couldn't read Wasm file {}", &cli.wasm.display()))
@@ -243,14 +158,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .finish(initialization_flatbuffer)
             .expect("errored when creating initialization message")
     };
-    if let Err(err) = client
+    if let Err(err) = schema::TrustedRuntimeAsyncClient::new(connector_handle.clone())
         .initialize(owned_initialization_flatbuffer.into_vec())
         .await
     {
         panic!("failed to initialize the runtime: {:?}", err)
     }
 
-    let server_future = server::server("127.0.0.1:8080".parse()?, request_dispatcher);
+    let server_future = server::server("127.0.0.1:8080".parse()?, connector_handle);
 
     // Wait until something dies or we get a signal to terminate.
     tokio::select! {
