47907efff67aa4f019250d27cc20ba5b889ff794
Andri Saar <andrisaar@google.com>
2022-08-16 08:02:20 +0000

    Create a simple hello world kernel for future SEV-SNP enlightenment

---
+++ testing/sev_snp_hello_world_kernel/src/main.rs
@@ -0,0 +1,44 @@
+//
+// Copyright 2022 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use x86_64::instructions::{hlt, port::PortWriteOnly};
+
+mod asm;
+mod serial;
+
+#[no_mangle]
+pub extern "C" fn rust64_start() -> ! {
+    serial::init_logging();
+    log::info!("Hello World!");
+    panic!();
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    // Shut down the machine via the i8042 controller.
+    unsafe {
+        let mut port = PortWriteOnly::new(0x64);
+        port.write(0xFE_u8);
+    }
+
+    loop {
+        hlt();
+    }
+}
