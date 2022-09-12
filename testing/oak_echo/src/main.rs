9e21131148c18e323a18700387c0075abe4c10ff
Ivan Petrov <ivanpetrov@google.com>
2022-09-12 12:58:59 +0100

    Add Echo Bare-Metal Runtime (#3194)
    This PR adds a simple Echo baremetal runtime that can be used for tests.

---
+++ testing/oak_echo/src/main.rs
@@ -0,0 +1,55 @@
+//
+// Copyright 2022 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#![no_std]
+#![no_main]
+#![feature(alloc_error_handler)]
+#![feature(core_c_str)]
+#![feature(core_ffi_c)]
+
+extern crate alloc;
+
+use alloc::boxed::Box;
+use core::panic::PanicInfo;
+use log::info;
+use oak_baremetal_communication_channel::Channel;
+
+mod asm;
+mod bootparam;
+
+#[no_mangle]
+pub extern "C" fn rust64_start(_rdi: u64, rsi: &bootparam::BootParams) -> ! {
+    let channel = oak_baremetal_kernel::start_kernel(rsi);
+    main(channel)
+}
+
+fn main(channel: Box<dyn Channel>) -> ! {
+    info!("In main!");
+    let runtime = oak_echo_runtime::RuntimeImplementation::new();
+    let service = oak_echo_runtime::schema::EchoRuntime::serve(runtime);
+    oak_baremetal_communication_channel::server::start_blocking_server(channel, service)
+        .expect("Runtime encountered an unrecoverable error");
+}
+
+#[alloc_error_handler]
+fn out_of_memory(layout: ::core::alloc::Layout) -> ! {
+    panic!("Error allocating memory: {:#?}", layout);
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    oak_baremetal_kernel::panic(info);
+}
---
+++ testing/oak_echo_runtime/src/lib.rs
@@ -0,0 +1,62 @@
+//
+// Copyright 2022 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#![no_std]
+#![feature(never_type)]
+
+extern crate alloc;
+
+pub mod schema {
+    #![allow(clippy::derivable_impls, clippy::needless_borrow)]
+    #![allow(dead_code, unused_imports)]
+
+    include!(concat!(env!("OUT_DIR"), "/schema_generated.rs"));
+    include!(concat!(env!("OUT_DIR"), "/schema_services_servers.rs"));
+}
+
+#[derive(Default)]
+pub struct RuntimeImplementation {}
+
+impl RuntimeImplementation {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl schema::EchoRuntime for RuntimeImplementation {
+    fn echo(
+        &mut self,
+        request_message: &schema::EchoRequest,
+    ) -> Result<oak_idl::utils::OwnedFlatbuffer<schema::EchoResponse>, oak_idl::Status> {
+        let request_body: &[u8] = request_message
+            .body()
+            .ok_or_else(|| oak_idl::Status::new(oak_idl::StatusCode::InvalidArgument))?;
+        let response_body = request_body;
+
+        let response_message = {
+            let mut builder = oak_idl::utils::OwnedFlatbufferBuilder::default();
+            let body = builder.create_vector::<u8>(response_body);
+            let user_request_response = schema::EchoResponse::create(
+                &mut builder,
+                &schema::EchoResponseArgs { body: Some(body) },
+            );
+            builder
+                .finish(user_request_response)
+                .map_err(|_err| oak_idl::Status::new(oak_idl::StatusCode::Internal))?
+        };
+        Ok(response_message)
+    }
+}
