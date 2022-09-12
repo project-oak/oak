d85ff8ebe5e539ef0f7762562e386940753e3100
Ivan Petrov <ivanpetrov@google.com>
2022-09-12 10:51:55 +0100

    Update

--- testing/echo/build.rs
+++ testing/echo/build.rs
@@ -15,7 +15,6 @@
 //
 
 fn main() {
-    println!("cargo:rerun-if-changed=target.json");
     println!("cargo:rerun-if-changed=layout.ld");
     println!("cargo:rustc-link-arg=--script=layout.ld");
 }
