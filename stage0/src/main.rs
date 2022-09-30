0c04174b4345008b981e082bba2d683a3b43df9e
Andri Saar <andrisaar@google.com>
2022-09-30 15:15:34 +0100

    Populate locations of SEV secrets/cpuid pages in zero page. (#3246)
    * Populate locations of SEV secrets/cpuid pages in zero page.
    
    * Mark the whole boot section as unmeasured

--- stage0/src/main.rs
+++ stage0/src/main.rs
@@ -17,7 +17,13 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::asm, ffi::c_void, mem::MaybeUninit, panic::PanicInfo};
+use core::{
+    arch::asm,
+    ffi::c_void,
+    mem::{size_of, zeroed, MaybeUninit},
+    panic::PanicInfo,
+};
+use oak_linux_boot_params::BootParams;
 use sev_guest::instructions::{pvalidate, InstructionError, PageSize as SevPageSize, Validation};
 use x86_64::{
     instructions::{hlt, interrupts::int3, segmentation::Segment},
@@ -42,21 +48,24 @@ mod asm;
 static mut BOOT_GDT: MaybeUninit<GlobalDescriptorTable> = MaybeUninit::uninit();
 #[link_section = ".boot.idt"]
 static mut BOOT_IDT: MaybeUninit<InterruptDescriptorTable> = MaybeUninit::uninit();
+#[link_section = ".boot.zero_page"]
+static mut BOOT_ZERO_PAGE: MaybeUninit<BootParams> = MaybeUninit::uninit();
 #[link_section = ".boot.pml4"]
 static mut BOOT_PML4: MaybeUninit<PageTable> = MaybeUninit::uninit();
 #[link_section = ".boot.pdpt"]
 static mut BOOT_PDPT: MaybeUninit<PageTable> = MaybeUninit::uninit();
 #[link_section = ".boot.pd"]
 static mut BOOT_PD: MaybeUninit<PageTable> = MaybeUninit::uninit();
-#[link_section = ".boot.unmeasured"]
-#[used]
-static mut SEV_UNMEASURED: MaybeUninit<[u8; 4096]> = MaybeUninit::uninit();
 #[link_section = ".boot.secrets"]
-#[used]
 static mut SEV_SECRETS: MaybeUninit<sev_guest::secrets::SecretsPage> = MaybeUninit::uninit();
 #[link_section = ".boot.cpuid"]
-#[used]
 static mut SEV_CPUID: MaybeUninit<sev_guest::cpuid::CpuidPage> = MaybeUninit::uninit();
+
+#[link_section = ".boot"]
+static mut CC_BLOB_SEV_INFO: MaybeUninit<oak_linux_boot_params::CCBlobSevInfo> =
+    MaybeUninit::uninit();
+#[link_section = ".boot"]
+static mut CC_SETUP_DATA: MaybeUninit<oak_linux_boot_params::CCSetupData> = MaybeUninit::uninit();
 
 extern "C" {
     #[link_name = "pd_addr"]
@@ -111,11 +120,12 @@ pub unsafe fn jump_to_kernel(entry_point: VirtAddr) -> ! {
         // Boot stack pointer
         "mov {1}, %rsp",
         // Zero page address
-        "mov $0x7000, %rsi",
+        "mov {2}, %rsi",
         // ...and away we go!
         "jmp *{0}",
         in(reg) entry_point.as_u64(),
         in(reg) &BOOT_STACK_POINTER as *const _ as u64,
+        in(reg) BOOT_ZERO_PAGE.as_ptr() as u64,
         options(noreturn, att_syntax)
     );
 }
@@ -283,6 +293,32 @@ pub extern "C" fn rust64_start(encrypted: u64) -> ! {
         );
     }
     /* TODO(#3198): interrogate qemu for memory areas and set up the zero page. */
+
+    // Safety: We assume the VMM has filled in the basic zero page for us. This is not true with
+    // qemu. In the future, construction of the full zero page should be in here, and instead of
+    // `assume_init_mut` we will use the safe `write` to zero it out and get a mutable reference.
+    let zero_page = unsafe { BOOT_ZERO_PAGE.assume_init_mut() };
+
+    if snp {
+        let setup_data = unsafe { CC_SETUP_DATA.write(zeroed()) };
+        let cc_blob = unsafe { CC_BLOB_SEV_INFO.write(zeroed()) };
+
+        setup_data.header.type_ = oak_linux_boot_params::SetupDataType::CCBlob;
+        setup_data.header.len = (size_of::<oak_linux_boot_params::CCSetupData>()
+            - size_of::<oak_linux_boot_params::SetupData>()) as u32;
+        setup_data.cc_blob_address = cc_blob as *const _ as u32;
+
+        cc_blob.magic = oak_linux_boot_params::CC_BLOB_SEV_INFO_MAGIC;
+        cc_blob.version = 1;
+        cc_blob.secrets_phys = unsafe { SEV_SECRETS.as_ptr() } as usize;
+        cc_blob.secrets_len = size_of::<sev_guest::secrets::SecretsPage>() as u32;
+        cc_blob.cpuid_phys = unsafe { SEV_CPUID.as_ptr() } as usize;
+        cc_blob.cpuid_len = size_of::<sev_guest::cpuid::CpuidPage>() as u32;
+
+        // Put our header as the first element in the linked list.
+        setup_data.header.next = zero_page.hdr.setup_data;
+        zero_page.hdr.setup_data = &setup_data.header as *const oak_linux_boot_params::SetupData;
+    }
 
     // Temporary hack: PVALIDATE [2..32] MiB of physical memory, ignoring any failures to update
     // the status for now. In the future, after we have determined the physical memory
