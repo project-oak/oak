626a859b684a564d9ba97e344e9f1f719f00852e
Andri Saar <andrisaar@google.com>
2022-10-06 16:31:11 +0000

    Delete the runt rust-hypervisor-firmware-boot crate.

--- third_party/rust-hypervisor-firmware-boot/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//
-// Copyright © 2019 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-//
-// Copyright 2022 The Project Oak Authors
-// SPDX-License-Identifier: Apache-2.0
-//
-
-#![no_std]
