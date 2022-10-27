48d0b0be6ef04aec0697be96c59b7c44ba99fb34
Tiziano Santoro <tzn@google.com>
2022-10-27 22:38:00 +0100

    Serialize requests and responses via IDL
    This allows serializing the combination of method id and request body
    into an IDL message (so meta), which is then sent over the underlying
    transport, which now only needs to know about raw bytes and nothing
    about the serialization mechanism.
    
    As a response, also the errors from the service are serialized back via
    the IDL to the client, so that we can have something much more similar
    to how RPC / IPC mechanisms usually work.
    
    This adds an additional layer of encoding / decoding, but since it's the
    same as the messages in the inner layer, it seems acceptable after all.
    
    Eventually the oak_protobuf_idl will replace the oak_idl crate, so the
    duplication will go away.
    
    Ref #3385

--- oak_protobuf_idl_tests/tests/test_schema.rs
+++ oak_protobuf_idl_tests/tests/test_schema.rs
@@ -16,13 +16,27 @@
 //! This crate contains tests for the `oak_idl_gen_structs` and `oak_idl_gen_services` crates. It
 //! needs to be separate from them in order to be able to invoke them at build time.
 
+#![feature(never_type)]
+#![feature(unwrap_infallible)]
+
 extern crate alloc;
 
-use oak_idl::Handler;
+use oak_protobuf_idl::Transport;
 
 mod test_schema {
     #![allow(dead_code)]
     include!(concat!(env!("OUT_DIR"), "/oak.protobuf_idl.tests.rs"));
+}
+
+/// Test implementation of a fallible transport that always returns the same error.
+struct FailingTransport;
+
+impl Transport for FailingTransport {
+    type Error = u32;
+
+    fn invoke(&mut self, _request_bytes: &[u8]) -> Result<Vec<u8>, Self::Error> {
+        Err(42)
+    }
 }
 
 struct TestServiceImpl;
@@ -32,33 +46,46 @@ impl test_schema::TestService for TestServiceImpl {
     fn lookup_data(
         &mut self,
         request: &test_schema::LookupDataRequest,
-    ) -> Result<test_schema::LookupDataResponse, oak_idl::Status> {
+    ) -> Result<test_schema::LookupDataResponse, oak_protobuf_idl::Status> {
         let h = maplit::hashmap! {
             vec![14, 12] => vec![19, 88]
         };
         h.get(&request.key)
             .map(|v| test_schema::LookupDataResponse { value: v.clone() })
-            .ok_or_else(|| oak_idl::Status::new(oak_idl::StatusCode::NotFound))
+            .ok_or_else(|| {
+                oak_protobuf_idl::Status::new_with_message(
+                    oak_protobuf_idl::StatusCode::NotFound,
+                    "not found",
+                )
+            })
     }
 
     fn log(
         &mut self,
         request: &test_schema::LogRequest,
-    ) -> Result<test_schema::LogResponse, oak_idl::Status> {
+    ) -> Result<test_schema::LogResponse, oak_protobuf_idl::Status> {
         eprintln!("log: {}", request.entry);
         Ok(test_schema::LogResponse {})
     }
 }
 
 #[test]
+fn test_failing_transport() {
+    let mut client = test_schema::TestServiceClient::new(FailingTransport);
+    let request = test_schema::LookupDataRequest { key: vec![14, 12] };
+    let transport_response = client.lookup_data(&request);
+    assert_eq!(Err(42), transport_response);
+}
+
+#[test]
 fn test_lookup_data() {
     let service = TestServiceImpl;
     use test_schema::TestService;
-    let handler = service.serve();
-    let mut client = test_schema::TestServiceClient::new(handler);
+    let transport = service.serve();
+    let mut client = test_schema::TestServiceClient::new(transport);
     {
         let request = test_schema::LookupDataRequest { key: vec![14, 12] };
-        let response = client.lookup_data(&request);
+        let response = client.lookup_data(&request).into_ok();
         assert_eq!(
             Ok(test_schema::LookupDataResponse {
                 value: vec![19, 88]
@@ -68,29 +95,29 @@ fn test_lookup_data() {
     }
     {
         let request = test_schema::LookupDataRequest { key: vec![10, 00] };
-        let response = client.lookup_data(&request);
+        let response = client.lookup_data(&request).into_ok();
         assert_eq!(
-            Err(oak_idl::Status::new(oak_idl::StatusCode::NotFound)),
+            Err(oak_protobuf_idl::Status::new_with_message(
+                oak_protobuf_idl::StatusCode::NotFound,
+                "not found"
+            )),
             response
         );
     }
 }
 
 /// Simple async wrapper around the synchronous server.
-/// Used to test async clients that expect an async handler.
+/// Used to test async clients that expect an async transport.
 pub struct AsyncTestServiceServer<S: test_schema::TestService> {
     inner: test_schema::TestServiceServer<S>,
 }
 
 #[async_trait::async_trait]
-impl<S: test_schema::TestService + std::marker::Send + std::marker::Sync> oak_idl::AsyncHandler
-    for AsyncTestServiceServer<S>
+impl<S: test_schema::TestService + std::marker::Send + std::marker::Sync>
+    oak_protobuf_idl::AsyncTransport for AsyncTestServiceServer<S>
 {
-    async fn invoke(
-        &mut self,
-        request: oak_idl::Request,
-    ) -> Result<alloc::vec::Vec<u8>, oak_idl::Status> {
-        self.inner.invoke(request)
+    async fn invoke(&mut self, request_bytes: &[u8]) -> Result<alloc::vec::Vec<u8>, !> {
+        self.inner.invoke(request_bytes)
     }
 }
 
@@ -99,13 +126,13 @@ async fn test_async_lookup_data() {
     let service = TestServiceImpl;
     use test_schema::TestService;
     let service_impl = service.serve();
-    let async_handler = AsyncTestServiceServer {
+    let async_transport = AsyncTestServiceServer {
         inner: service_impl,
     };
-    let mut client = test_schema::TestServiceAsyncClient::new(async_handler);
+    let mut client = test_schema::TestServiceAsyncClient::new(async_transport);
     {
         let request = test_schema::LookupDataRequest { key: vec![14, 12] };
-        let response = client.lookup_data(&request).await;
+        let response = client.lookup_data(&request).await.into_ok();
         assert_eq!(
             Ok(test_schema::LookupDataResponse {
                 value: vec![19, 88]
@@ -115,9 +142,12 @@ async fn test_async_lookup_data() {
     }
     {
         let request = test_schema::LookupDataRequest { key: vec![10, 00] };
-        let response = client.lookup_data(&request).await;
+        let response = client.lookup_data(&request).await.into_ok();
         assert_eq!(
-            Err(oak_idl::Status::new(oak_idl::StatusCode::NotFound)),
+            Err(oak_protobuf_idl::Status::new_with_message(
+                oak_protobuf_idl::StatusCode::NotFound,
+                "not found"
+            )),
             response
         );
     }
