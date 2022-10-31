3cd21acdcb4a7938ffec14b5d546de608e2764cd
Ian Chang <ianchang@google.com>
2022-10-31 23:58:47 +0000

    Add update_tflm script

--- cc/tflite_micro/tools/update_tlfm.sh
+++ cc/tflite_micro/tools/update_tlfm.sh
@@ -6,13 +6,18 @@ if [[ $# -ne 1 ]]; then
     exit 2
 fi
 
-pushd "$(dirname "$0")" >/dev/null
+TFLM_SOURCE_DIR="$(readlink -f "$@")"
+TFLM_GENERATED_DIR="/tmp/tflm_generated"
+SCRIPT_DIR="$(dirname $(readlink -f "$0"))"
+WORKSPACE_ROOT_DIR="${SCRIPT_DIR}/../../.."
+
+pushd "${TFLM_SOURCE_DIR}" >/dev/null
 trap "popd > /dev/null" EXIT
 
-SCRIPT_DIR="$(dirname "$0")"
-WORKSPACE_ROOT_DIR="${SCRIPT_DIR}/../../.."
-TFLM_SOURCE_DIR="$@"
-TFLM_GENERATED_DIR="/tmp/tflm_generated"
+echo ${SCRIPT_DIR}
+echo ${WORKSPACE_ROOT_DIR}
+echo ${TFLM_SOURCE_DIR}
+echo ${TFLM_GENERATED_DIR}
 
 # Prune previously generated source tree if any.
 rm -rf ${TFLM_GENERATED_DIR} 2>&1 >/dev/null
@@ -30,7 +35,7 @@ rm -rf ${WORKSPACE_ROOT_DIR}/third_party/gemmlowp/internal
 rm -rf ${WORKSPACE_ROOT_DIR}/third_party/gemmlowp/LICENSE
 rm -rf ${WORKSPACE_ROOT_DIR}/third_party/ruy/ruy
 
-# Copy over generated sources to corresponding directories.
+# # Copy over generated sources to corresponding directories.
 cp -rf ${TFLM_GENERATED_DIR}/tensorflow ${WORKSPACE_ROOT_DIR}/third_party/tflite-micro/
 cp -rf ${TFLM_GENERATED_DIR}/LICENSE ${WORKSPACE_ROOT_DIR}/third_party/tflite-micro/
 cp -rf ${TFLM_GENERATED_DIR}/third_party/flatbuffers ${WORKSPACE_ROOT_DIR}/third_party/
