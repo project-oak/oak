4d2c84c54adaa1620f36e423a94bc9b57493ca5d
Ian Chang <ianchang@google.com>
2022-11-01 00:21:12 +0000

    Update script and fix style

--- cc/tflite_micro/tools/update_tflm.sh
+++ cc/tflite_micro/tools/update_tflm.sh
@@ -8,36 +8,31 @@ fi
 
 TFLM_SOURCE_DIR="$(readlink -f "$@")"
 TFLM_GENERATED_DIR="/tmp/tflm_generated"
-SCRIPT_DIR="$(dirname $(readlink -f "$0"))"
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 WORKSPACE_ROOT_DIR="${SCRIPT_DIR}/../../.."
 
 pushd "${TFLM_SOURCE_DIR}" >/dev/null
 trap "popd > /dev/null" EXIT
 
-echo ${SCRIPT_DIR}
-echo ${WORKSPACE_ROOT_DIR}
-echo ${TFLM_SOURCE_DIR}
-echo ${TFLM_GENERATED_DIR}
-
 # Prune previously generated source tree if any.
-rm -rf ${TFLM_GENERATED_DIR} 2>&1 >/dev/null
+rm -rf "${TFLM_GENERATED_DIR}" >/dev/null 2>&1
 
 # Generate clean set of tflm source tree.
-python3 ${TFLM_SOURCE_DIR}/tensorflow/lite/micro/tools/project_generation/create_tflm_tree.py ${TFLM_GENERATED_DIR}
+python3 "${TFLM_SOURCE_DIR}/tensorflow/lite/micro/tools/project_generation/create_tflm_tree.py" "${TFLM_GENERATED_DIR}"
 
 # Prune old tflm sources cleanly.
-rm -rf ${WORKSPACE_ROOT_DIR}/third_party/tflite-micro/tensorflow
-rm -rf ${WORKSPACE_ROOT_DIR}/third_party/tflite-micro/LICENSE
-rm -rf ${WORKSPACE_ROOT_DIR}/third_party/flatbuffers/include
-rm -rf ${WORKSPACE_ROOT_DIR}/third_party/flatbuffers/LICENSE.txt
-rm -rf ${WORKSPACE_ROOT_DIR}/third_party/gemmlowp/fixedpoint
-rm -rf ${WORKSPACE_ROOT_DIR}/third_party/gemmlowp/internal
-rm -rf ${WORKSPACE_ROOT_DIR}/third_party/gemmlowp/LICENSE
-rm -rf ${WORKSPACE_ROOT_DIR}/third_party/ruy/ruy
+rm -rf "${WORKSPACE_ROOT_DIR}/third_party/tflite-micro/tensorflow"
+rm -rf "${WORKSPACE_ROOT_DIR}/third_party/tflite-micro/LICENSE"
+rm -rf "${WORKSPACE_ROOT_DIR}/third_party/flatbuffers/include"
+rm -rf "${WORKSPACE_ROOT_DIR}/third_party/flatbuffers/LICENSE.txt"
+rm -rf "${WORKSPACE_ROOT_DIR}/third_party/gemmlowp/fixedpoint"
+rm -rf "${WORKSPACE_ROOT_DIR}/third_party/gemmlowp/internal"
+rm -rf "${WORKSPACE_ROOT_DIR}/third_party/gemmlowp/LICENSE"
+rm -rf "${WORKSPACE_ROOT_DIR}/third_party/ruy/ruy"
 
 # # Copy over generated sources to corresponding directories.
-cp -rf ${TFLM_GENERATED_DIR}/tensorflow ${WORKSPACE_ROOT_DIR}/third_party/tflite-micro/
-cp -rf ${TFLM_GENERATED_DIR}/LICENSE ${WORKSPACE_ROOT_DIR}/third_party/tflite-micro/
-cp -rf ${TFLM_GENERATED_DIR}/third_party/flatbuffers ${WORKSPACE_ROOT_DIR}/third_party/
-cp -rf ${TFLM_GENERATED_DIR}/third_party/ruy ${WORKSPACE_ROOT_DIR}/third_party/
-cp -rf ${TFLM_GENERATED_DIR}/third_party/gemmlowp ${WORKSPACE_ROOT_DIR}/third_party/
+cp -rf "${TFLM_GENERATED_DIR}/tensorflow" "${WORKSPACE_ROOT_DIR}/third_party/tflite-micro/"
+cp -rf "${TFLM_GENERATED_DIR}/LICENSE" "${WORKSPACE_ROOT_DIR}/third_party/tflite-micro/"
+cp -rf "${TFLM_GENERATED_DIR}/third_party/flatbuffers" "${WORKSPACE_ROOT_DIR}/third_party/"
+cp -rf "${TFLM_GENERATED_DIR}/third_party/ruy" "${WORKSPACE_ROOT_DIR}/third_party/"
+cp -rf "${TFLM_GENERATED_DIR}/third_party/gemmlowp" "${WORKSPACE_ROOT_DIR}/third_party/"
