7ab87aaa541e0dd9d8a2160e4752f71e287ca030
Conrad Grobler <grobler@google.com>
2022-11-08 16:21:56 +0000

    Implement a synchronized no_std OnceCell

--- oak_functions_utils/src/sync.rs
+++ oak_functions_utils/src/sync.rs
@@ -15,10 +15,70 @@
 //
 
 use core::{
+    cell::UnsafeCell,
     hint::spin_loop,
+    mem::MaybeUninit,
     sync::atomic::{AtomicBool, Ordering},
 };
 use lock_api::{GuardSend, RawMutex};
+use spinning_top::RawSpinlock;
+
+/// A synchronised implementation of a cell that can be initialized only once.
+///
+/// This achieves the same goal as `std::sync::OnceLock`, but uses a spinlock to be compatible with
+/// `no_std`.
+pub struct OnceCell<T> {
+    initialized: AtomicBool,
+    lock: RawSpinlock,
+    value: UnsafeCell<MaybeUninit<T>>,
+}
+
+impl<T> OnceCell<T> {
+    pub const fn new() -> Self {
+        Self {
+            initialized: AtomicBool::new(false),
+            lock: RawSpinlock::INIT,
+            value: UnsafeCell::new(MaybeUninit::uninit()),
+        }
+    }
+
+    pub fn get(&self) -> Option<&T> {
+        if !self.initialized.load(Ordering::Acquire) {
+            return None;
+        }
+        // Safety: there are no mutable references to the inner value if `initialized` is set to
+        // `true`, and the value has been initialized, so it is safe to create a new shared
+        // reference and assume it is initialized.
+        Some(unsafe { (*self.value.get()).assume_init_ref() })
+    }
+
+    pub fn set(&self, value: T) -> Result<(), T> {
+        let mut result = Ok(());
+        // Do an initial check to see whether the value has been initialized.
+        if !self.initialized.load(Ordering::Acquire) {
+            // Lock to make sure we have exclusive access.
+            self.lock.lock();
+            // Double check that someone else didn't initialize while we were waiting to get the
+            // lock. Relaxed ordering is sufficient since the lock acts as a memory barrier and also
+            // ensures no concurrent access to the code that stores the value.
+            if !self.initialized.load(Ordering::Relaxed) {
+                // Safety: the combination of the lock and the fact that `initialized` is `false`
+                // ensures that we have exclusive access to the inner value, so taking a mutable
+                // reference to it is safe.
+                unsafe { &mut *self.value.get() }.write(value);
+                self.initialized.store(true, Ordering::Release);
+            } else {
+                result = Err(value);
+            }
+            // Safety: we acquired the lock in the same context, so unlocking here is safe.
+            unsafe { self.lock.unlock() };
+        } else {
+            result = Err(value);
+        }
+
+        result
+    }
+}
 
 pub struct SpinLock(AtomicBool);
 
@@ -55,3 +115,30 @@ unsafe impl RawMutex for SpinLock {
 
 pub type Mutex<T> = lock_api::Mutex<SpinLock, T>;
 pub type MutexGuard<'a, T> = lock_api::MutexGuard<'a, SpinLock, T>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_once_cell_get_uninitialized() {
+        let cell = OnceCell::<u64>::new();
+        assert!(cell.get().is_none());
+    }
+
+    #[test]
+    fn test_once_cell_set_and_get() {
+        let cell = OnceCell::<u64>::new();
+        assert!(cell.get().is_none());
+        assert!(cell.set(4).is_ok());
+        assert_eq!(cell.get(), Some(&4));
+    }
+
+    #[test]
+    fn test_once_cell_set_twice() {
+        let cell = OnceCell::<u64>::new();
+        assert!(cell.set(4).is_ok());
+        assert_eq!(cell.set(5), Err(5));
+        assert_eq!(cell.get(), Some(&4));
+    }
+}
