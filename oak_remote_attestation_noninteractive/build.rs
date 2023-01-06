991739ab2389640a2d2424cc87250ae210ba45fb
Patrick McGrath <patarmcgrath17@gmail.com>
2023-01-06 03:04:26 +0000

    Fixing build dependencies

--- oak_remote_attestation_noninteractive/build.rs
+++ oak_remote_attestation_noninteractive/build.rs
@@ -18,8 +18,8 @@ use oak_grpc_utils::{generate_grpc_code, CodegenOptions};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     generate_grpc_code(
-        "./proto",
-        &["oak_session_noninteractive_v1.proto"],
+        "./proto/v1",
+        &["service_streaming.proto"],
         CodegenOptions {
             build_server: true,
             build_client: true,
