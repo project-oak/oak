dbb269c4ce9f244c5b8b57ba9d198eb453e94233
Patrick McGrath <patarmcgrath17@gmail.com>
2023-01-09 23:16:22 +0000

    fix protoc issue

--- oak_remote_attestation_noninteractive/build.rs
+++ oak_remote_attestation_noninteractive/build.rs
@@ -18,8 +18,9 @@ use oak_grpc_utils::{generate_grpc_code, CodegenOptions};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     generate_grpc_code(
-        "./proto/v1",
-        &["service_streaming.proto", "messages.proto"],
+        "../",
+        &["oak_remote_attestation_noninteractive/proto/v1/messages.proto",
+        "oak_remote_attestation_noninteractive/proto/v1/service_streaming.proto"],
         CodegenOptions {
             build_server: true,
             build_client: true,
