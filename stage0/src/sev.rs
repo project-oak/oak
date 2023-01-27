c6ed50470727b23e7bbd72c289d46dcf53bede79
Conrad Grobler <grobler@google.com>
2023-01-27 14:52:19 +0000

    PVALIDATE page when unsharing

--- stage0/src/sev.rs
+++ stage0/src/sev.rs
@@ -115,6 +115,12 @@ pub fn unshare_page(page: Page<Size4KiB>, snp: bool, encrypted: u64) {
         let request = SnpPageStateChangeRequest::new(page_start as usize, PageAssignment::Private)
             .expect("invalid address for page location");
         change_snp_page_state(request).expect("couldn't change SNP state for page");
+        pvalidate(
+            page_start as usize,
+            SevPageSize::Page4KiB,
+            Validation::Validated,
+        )
+        .unwrap();
     }
 
     let PageTables { pdpt: _, pd } = get_page_tables(encrypted);
