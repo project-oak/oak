214100e47dc040f24792081b13861e3231e95daa
Tiziano Santoro <tzn@google.com>
2023-02-15 13:19:55 +0000

    Use nix flakes for local development

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  description = "oak";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    gomod2nix.url = "github:nix-community/gomod2nix";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = { self, nixpkgs, flake-utils, gomod2nix, rust-overlay }:
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              gomod2nix.overlays.default
+              rust-overlay.overlays.default
+            ];
+            config = {
+              android_sdk.accept_license = true; # accept all of the sdk licenses
+              allowUnfree = true; # needed to get android stuff to compile
+            };
+          };
+        in
+        {
+          packages = { };
+          devShells = rec {
+            rust = with pkgs; mkShell {
+              packages = [
+                (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+                  extensions = [
+                    "clippy"
+                    "llvm-tools-preview"
+                    "rust-src"
+                    "rustfmt"
+                  ];
+                  targets = [
+                    "wasm32-unknown-unknown"
+                    "x86_64-unknown-linux-musl"
+                    "x86_64-unknown-none"
+                  ];
+                }))
+                cargo-deadlinks
+                cargo-binutils
+                cargo-deny
+                cargo-udeps
+                protobuf
+                qemu_kvm
+              ];
+            };
+            lint = with pkgs; mkShell {
+              packages = [
+                hadolint
+                cargo-deadlinks
+                nodePackages.prettier
+                nodePackages.prettier-plugin-toml
+                nixpkgs-fmt
+              ];
+            };
+            bazel = with pkgs; mkShell {
+              shellHook = ''
+                export A=$(dirname $(dirname $(which android)))
+                export ANDROID_HOME=$A/libexec/android-sdk
+                export ANDROID_NDK_HOME=$A/libexec/android-sdk/ndk/24.0.8215888
+              '';
+              packages = [
+                jdk11
+                (androidenv.composeAndroidPackages {
+                  includeNDK = true;
+                  platformVersions = [ "30" "32" ];
+                  buildToolsVersions = [ "30.0.0" ];
+                }).androidsdk
+              ];
+            };
+            default = with pkgs; mkShell {
+              inputsFrom = [
+                rust
+                bazel
+                lint
+              ];
+            };
+          };
+        }));
+}
