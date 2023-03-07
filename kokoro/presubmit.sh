d3fc5e69beff99f809278550e875131d8639aba4
Tiziano Santoro <tzn@google.com>
2023-03-07 10:31:42 +0000

    Run all tests in Kokoro

--- kokoro/presubmit.sh
+++ kokoro/presubmit.sh
@@ -21,9 +21,7 @@ export RUST_BACKTRACE=1
 export RUST_LOG=debug
 
 ./scripts/docker_pull
-./scripts/docker_run cargo nextest run --hide-progress-bar \
-  --package=oak_functions_launcher \
-  --package=quirk_echo_launcher
+./scripts/docker_run cargo nextest run --hide-progress-bar
 
 cp ./target/nextest/default/junit.xml "$KOKORO_ARTIFACTS_DIR/"
 ls -als "$KOKORO_ARTIFACTS_DIR"
