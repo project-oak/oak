bc1c28b6b6cf51d4672f75a21029fd4110f16e13
Andri Saar <andrisaar@google.com>
2023-03-24 16:03:41 +0000

    Remove KVM and Docker-related things that Kokoro no longer needs.

--- kokoro/presubmit.sh
+++ kokoro/presubmit.sh
@@ -5,16 +5,6 @@ set -o nounset
 set -o xtrace
 set -o pipefail
 
-# On Kokoro instances /dev/kvm, /dev/vhost-vsock and /dev/vsock are owned by root:root.
-# Set the permissions so that these are owned by the `kvm` group as our scripts expect it to be.
-readonly KVM_GID="$(getent group kvm | cut -d: -f3)"
-sudo chown "$USER:$KVM_GID" /dev/kvm
-
-sudo /etc/init.d/docker stop
-sudo mv /var/lib/docker /tmpfs/
-sudo ln -s /tmpfs/docker /var/lib/docker
-sudo /etc/init.d/docker start
-
 export CI=kokoro
 
 export RUST_BACKTRACE=1
