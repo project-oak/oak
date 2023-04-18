3a51ea2f6a666655f278696a6b7d49f3c84a363a
Ivan Petrov <ivanpetrov@google.com>
2023-04-18 16:26:49 +0100

    Update

---
+++ java/src/test/java/com/google/oak/transport/GrpcStreamingTransportTest.java
@@ -0,0 +1,44 @@
+//
+// Copyright 2023 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package com.google.oak.transport;
+
+import com.google.oak.transport.GrpcStreamingTransport;
+import com.google.oak.util.Result;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class GrpcStreamingTransportTest {
+  @Test
+  public void testGetEvidence() throws Exception {
+    // TODO(#3644): Implement a unit test for gRPC streaming.
+  }
+
+  @Test
+  public void testInvoke() throws Exception {
+    // TODO(#3644): Implement a unit test for gRPC streaming.
+  }
+
+  @Test
+  public void testStream() throws Exception {
+    // TODO(#3644): Implement a unit test for gRPC streaming.
+  }
+}
