3989f0d8c2f9e5c9266fcc7c39ca20d3e634e644
Ivan Petrov <ivanpetrov@google.com>
2023-04-18 15:09:26 +0100

    Use Oak Crypto proto in Java encryptor

--- java/src/test/java/com/google/oak/crypto/ClientEncryptorTest.java
+++ java/src/test/java/com/google/oak/crypto/ClientEncryptorTest.java
@@ -16,10 +16,15 @@
 
 package com.google.oak.crypto;
 
+import com.google.oak.crypto.AeadEncryptor;
 import com.google.oak.crypto.ClientEncryptor;
 import com.google.oak.util.Result;
+import com.google.protobuf.ByteString;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
+import oak.crypto.AeadEncryptedMessage;
+import oak.crypto.EncryptedRequest;
+import oak.crypto.EncryptedResponse;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,21 +34,45 @@ import org.junit.runners.JUnit4;
 public class ClientEncryptorTest {
   private static final byte[] TEST_SERIALIZED_SERVER_PUBLIC_KEY = new byte[0];
   private static final byte[] TEST_PLAINTEXT = new byte[0];
+  private static final byte[] TEST_CIPHERTEXT = new byte[0];
   private static final byte[] TEST_ASSOCIATED_DATA = new byte[0];
-  private static final byte[] TEST_ENCRYPTED_MESSAGE = new byte[0];
 
   @Test
-  public void testEncryptDecrypt() throws Exception {
+  public void testEncrypt() throws Exception {
     // TODO(#3644): Implement and test Java hybrid encryption.
     ClientEncryptor encryptor = new ClientEncryptor(TEST_SERIALIZED_SERVER_PUBLIC_KEY);
 
     Result<byte[], Exception> encrypt_result =
         encryptor.encrypt(TEST_PLAINTEXT, TEST_ASSOCIATED_DATA);
     Assert.assertTrue(encrypt_result.isSuccess());
-    Assert.assertArrayEquals(encrypt_result.success().get(), TEST_ENCRYPTED_MESSAGE);
+    byte[] serializedEncryptedRequest = encrypt_result.success().get();
 
-    Result<ClientEncryptor.DecryptionResult, Exception> decrypt_result =
-        encryptor.decrypt(TEST_ENCRYPTED_MESSAGE);
+    EncryptedRequest encryptedRequest = EncryptedRequest.parseFrom(serializedEncryptedRequest);
+    // TODO(#3644): Once we implement Java hybrid encryption, we need to add a test for checking
+    // that the message was actually encrypted.
+    // Assert.assertFalse(
+    //     assertArrayEquals(encryptedRequest.getEncryptedMessage().getCiphertext().toByteArray(),
+    //         TEST_PLAINTEXT));
+    Assert.assertArrayEquals(
+        encryptedRequest.getEncryptedMessage().getAssociatedData().toByteArray(),
+        TEST_ASSOCIATED_DATA);
+  }
+
+  @Test
+  public void testDecrypt() throws Exception {
+    // TODO(#3644): Implement and test Java hybrid encryption.
+    ClientEncryptor encryptor = new ClientEncryptor(TEST_SERIALIZED_SERVER_PUBLIC_KEY);
+
+    EncryptedResponse encryptedResponse =
+        EncryptedResponse.newBuilder()
+            .setEncryptedMessage(AeadEncryptedMessage.newBuilder()
+                                     .setCiphertext(ByteString.copyFrom(TEST_CIPHERTEXT))
+                                     .setAssociatedData(ByteString.copyFrom(TEST_ASSOCIATED_DATA))
+                                     .build())
+            .build();
+
+    Result<AeadEncryptor.DecryptionResult, Exception> decrypt_result =
+        encryptor.decrypt(encryptedResponse.toByteArray());
     Assert.assertTrue(decrypt_result.isSuccess());
     Assert.assertArrayEquals(decrypt_result.success().get().plaintext, TEST_PLAINTEXT);
     Assert.assertArrayEquals(decrypt_result.success().get().associatedData, TEST_ASSOCIATED_DATA);
