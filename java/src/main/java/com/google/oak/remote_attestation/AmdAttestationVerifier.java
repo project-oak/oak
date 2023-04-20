65696fd6bc4752058129e0678bef081076ab1ab0
Ivan Petrov <ivanpetrov@google.com>
2023-04-20 15:34:09 +0100

    Add Java Attestation Verifier template (#3880)
    This PR adds a template for the remote attestation verifier in Java.
    
    Ref https://github.com/project-oak/oak/issues/3644
    
    This PR is a split from https://github.com/project-oak/oak/pull/3854

---
+++ java/src/main/java/com/google/oak/remote_attestation/AmdAttestationVerifier.java
@@ -0,0 +1,47 @@
+//
+// Copyright 2023 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package com.google.oak.remote_attestation;
+
+import com.google.oak.util.Result;
+import oak.session.noninteractive.v1.AttestationEndorsement;
+import oak.session.noninteractive.v1.AttestationEvidence;
+
+/**
+ * Attestation verifier implementation for AMD SEV-SNP.
+ * <https://www.amd.com/system/files/TechDocs/SEV-SNP-strengthening-vm-isolation-with-integrity-protection-and-more.pdf>
+ */
+public class AmdAttestationVerifier implements AttestationVerifier {
+  /**
+   * Verify that the provided evidence was endorsed and contains specified reference values.
+   *
+   * @param evidence contains claims about the Trusted Execution Environment
+   * <https://www.rfc-editor.org/rfc/rfc9334.html#name-evidence>
+   * @param endorsement contains statements that the endorsers vouch for the integrity of claims
+   * provided in the evidence
+   * <https://www.rfc-editor.org/rfc/rfc9334.html#name-endorsements>
+   * @param reference_value contains values used to verify the evidence
+   * <https://www.rfc-editor.org/rfc/rfc9334.html#name-reference-values>
+   * @return boolean corresponding to the sussess of the verification wrapped in a {@code Result}
+   */
+  // TODO(#3641): Rewrite java-doc to represent actual AMD attestation verification.
+  @Override
+  public final Result<Boolean, Exception> verify(final AttestationEvidence evidence,
+      final AttestationEndorsement endorsement, byte[] reference_value) {
+    // TODO(#3641): Implement AMD SEV-SNP attestation verification.
+    return Result.success(true);
+  }
+}
---
+++ java/src/test/java/com/google/oak/remote_attestation/AmdAttestationVerifierTest.java
@@ -0,0 +1,37 @@
+//
+// Copyright 2023 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package com.google.oak.remote_attestation;
+
+import com.google.oak.remote_attestation.AmdAttestationVerifier;
+import com.google.oak.util.Result;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class AmdAttestationVerifierTest {
+  @Test
+  public void testVerify() throws Exception {
+    // TODO(#3641): Implement AMD SEV-SNP attestation verification.
+    AmdAttestationVerifier verifier = new AmdAttestationVerifier();
+    Result<Boolean, Exception> verifyResult = verifier.verify(null, null, new byte[0]);
+    Assert.assertTrue(verifyResult.isSuccess());
+  }
+}
