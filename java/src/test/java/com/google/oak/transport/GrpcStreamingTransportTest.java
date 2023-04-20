115dde037a4597dade479e437488377719f94fb5
Ivan Petrov <ivanpetrov@google.com>
2023-04-20 14:04:54 +0100

    Update

--- java/src/test/java/com/google/oak/transport/GrpcStreamingTransportTest.java
+++ java/src/test/java/com/google/oak/transport/GrpcStreamingTransportTest.java
@@ -16,29 +16,98 @@
 
 package com.google.oak.transport;
 
+import static org.mockito.AdditionalAnswers.delegatesTo;
+import static org.mockito.Mockito.mock;
+
 import com.google.oak.transport.GrpcStreamingTransport;
 import com.google.oak.util.Result;
+import io.grpc.ManagedChannel;
+import io.grpc.stub.StreamObserver;
+import io.grpc.testing.GrpcCleanupRule;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
 import java.nio.charset.StandardCharsets;
+import java.util.function.Function;
 import java.util.Optional;
+import oak.session.noninteractive.v1.RequestWrapper;
+import oak.session.noninteractive.v1.ResponseWrapper;
+import oak.session.noninteractive.v1.StreamingSessionGrpc;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 @RunWith(JUnit4.class)
 public class GrpcStreamingTransportTest {
+  @Rule
+  public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
+
+  private final StreamingSessionGrpc.StreamingSessionImplBase serviceImpl =
+      mock(StreamingSessionGrpc.StreamingSessionImplBase.class, delegatesTo(
+          new StreamingSessionGrpc.StreamingSessionImplBase() {
+            @Override
+            public StreamObserver<RequestWrapper> stream(StreamObserver<ResponseWrapper> responseObserver) {
+              StreamObserver<RequestWrapper> requestObserver = new StreamObserver<RequestWrapper>() {
+                @Override
+                public void onNext(RequestWrapper request) {
+                  responseObserver.onNext(ResponseWrapper.getDefaultInstance());
+                }
+
+                @Override
+                public void onError(Throwable t) {}
+
+                @Override
+                public void onCompleted() {
+                  responseObserver.onCompleted();
+                }
+              };
+              return requestObserver;
+            }
+          }));
+
+  private StreamingSessionGrpc.StreamingSessionStub client;
+
+  @Before
+  public void setUp() throws Exception {
+    // Generate a unique in-process server name.
+    String serverName = InProcessServerBuilder.generateName();
+
+    // Create a server, add service, start, and register for automatic graceful shutdown.
+    grpcCleanup.register(InProcessServerBuilder
+        .forName(serverName).directExecutor().addService(serviceImpl).build().start());
+
+    // Create a client channel and register for automatic graceful shutdown.
+    ManagedChannel channel = grpcCleanup.register(
+        InProcessChannelBuilder.forName(serverName).directExecutor().build());
+
+    // Create a client using the in-process channel;
+    StreamingSessionGrpc.StreamingSessionStub client = StreamingSessionGrpc.newStub(channel);
+  }
+
   @Test
   public void testGetEvidence() throws Exception {
     // TODO(#3644): Implement a unit test for gRPC streaming.
-  }
+    Assert.assertNotNull(client);
+    GrpcStreamingTransport transport = new GrpcStreamingTransport(client::stream);
 
-  @Test
-  public void testInvoke() throws Exception {
-    // TODO(#3644): Implement a unit test for gRPC streaming.
-  }
+    // Function<StreamObserver<ResponseWrapper>, StreamObserver<RequestWrapper>> stream =
+    //     client::stream;
 
-  @Test
-  public void testStream() throws Exception {
-    // TODO(#3644): Implement a unit test for gRPC streaming.
+    // StreamObserver<ResponseWrapper> responseObserver = new StreamObserver<ResponseWrapper>() {
+    //   @Override
+    //   public void onNext(ResponseWrapper response) {}
+
+    //   @Override
+    //   public void onError(Throwable t) {}
+
+    //   @Override
+    //   public void onCompleted() {}
+    // };
+    // StreamObserver<RequestWrapper> requestObserver = client.stream(responseObserver);
   }
 }
