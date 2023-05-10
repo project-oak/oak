2398fb856b9dd4f6edf96797a0f9d312a0c830e8
Tiziano Santoro <tzn@google.com>
2023-05-10 21:27:59 +0100

    Fix benches and add them to CI (#3933)
    There is more room for improvement in sharing logic between benches, tests and xtask, but I'll leave that for a separate PR.
    
    Ref #3931 #3738

--- kokoro/presubmit.sh
+++ kokoro/presubmit.sh
@@ -11,7 +11,8 @@ export RUST_BACKTRACE=1
 export RUST_LOG=debug
 
 ./scripts/docker_pull
-./scripts/docker_run cargo nextest run --hide-progress-bar
+# --all-targets is needed to also run tests for examples and benches.
+./scripts/docker_run cargo nextest run --all-targets --hide-progress-bar
 
 mkdir "$KOKORO_ARTIFACTS_DIR/test_logs/"
 cp ./target/nextest/default/*.xml "$KOKORO_ARTIFACTS_DIR/test_logs/"
