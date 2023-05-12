61265b513931f27f9f3a028a8888e0372c7b93ad
Gogul Balakrishnan <bgogul@google.com>
2023-05-12 08:37:26 -0700

    Update prepare_docker_initramfs.sh script to package docker as a qcow2 image (#3944)

--- oak_docker_linux_init/prepare_docker_launcher_initramfs.sh
+++ oak_docker_linux_init/prepare_docker_launcher_initramfs.sh
@@ -17,6 +17,7 @@ print_usage_and_exit() {
   ${0} [-h] \\
        -k <output-linux-kernel> \\
        -o <output-initramfs>    \\
+       -q <qcow2-file>          \\
        -d <docker-image>
 
 Creates an initramfs with minirootfs+networking support as docker launcher.
@@ -24,12 +25,13 @@ Creates an initramfs with minirootfs+networking support as docker launcher.
 Options:
   -k      Location for the extracted Linux kernel.
   -d      Docker image that should be launched.
+  -q      Extract docker image as a qcow2 image instead of packing in initramfs.
   -o      Output initramfs file.
   -h      Print this help message and exit."
   exit 0
 }
 
-while getopts "hk:d:o:" opt; do
+while getopts "hk:d:o:q:" opt; do
   case $opt in
     h)
       print_usage_and_exit;;
@@ -39,6 +41,8 @@ while getopts "hk:d:o:" opt; do
       readonly DOCKER_IMAGE="${OPTARG}";;
     o)
       readonly OUTPUT_INITRAMFS="${OPTARG}";;
+    q)
+      readonly DOCKER_QCOW2_IMAGE="${OPTARG}";;
     *)
       echo "Invalid argument: ${OPTARG}"
       exit 1;;
@@ -74,14 +78,30 @@ mv "${RAMDIR}/vmlinux" "${LINUX_KERNEL}"
 if [ -z "${DOCKER_IMAGE}" ]; then
   echo "[WARN] No docker image specified. initramfs will launch a shell."
   echo "       Use -d option to specify docker image if needed."
-  LAUNCH_CMD="exec /sbin/getty -n -l /bin/sh 115200 /dev/console"
+  readonly LAUNCH_CMD="exec /sbin/getty -n -l /bin/sh 115200 /dev/console"
 else
   echo "[INFO] Preparing docker image as a rootfs."
-  readonly DOCKER_ROOTFS_DIR="${RAMDIR}/docker_rootfs"
+  if [ -z "${DOCKER_QCOW2_IMAGE}" ]; then
+    readonly DOCKER_ROOTFS_DIR="${RAMDIR}/docker_rootfs"
+  else
+    readonly DOCKER_ROOTFS_DIR="${SCRATCH_DIR}/docker_rootfs"
+  fi
   mkdir -p "${DOCKER_ROOTFS_DIR}"
-  sh "$SCRIPT_DIR/docker_to_rootfs.sh" \
+  sh "${SCRIPT_DIR}/docker_to_rootfs.sh" \
     -d "${DOCKER_IMAGE}" -r "${DOCKER_ROOTFS_DIR}"
-  LAUNCH_CMD="
+  if [ -z "${DOCKER_QCOW2_IMAGE}" ]; then
+    echo "[INFO] Embedding Docker filesystem into initramfs."
+  else
+    echo "[INFO] Creating QEMU qcow2 image at ${DOCKER_QCOW2_IMAGE}"
+    virt-make-fs --format=qcow2 --size=+200M \
+      "${DOCKER_ROOTFS_DIR}" "${DOCKER_QCOW2_IMAGE}"
+    MOUNT_DOCKER_ROOTFS_CMD="
+    # Assumes that the virtual block drive is at /dev/vda.
+    mkdir /docker_rootfs
+    mount /dev/vda /docker_rootfs
+"
+  fi
+  readonly LAUNCH_CMD="
 # copy the resolv.conf file to the chroot.
 cp -f /etc/resolv.conf /docker_rootfs/etc/resolv.conf
 exec /usr/sbin/chroot /docker_rootfs /init
