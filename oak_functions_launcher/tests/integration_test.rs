459356d0ddeda77cfdb57c1b3185a3f5f1c81681
Juliette Pretot <julsh@google.com>
2023-05-15 16:13:51 +0000

    rm unused var

--- oak_functions_launcher/tests/integration_test.rs
+++ oak_functions_launcher/tests/integration_test.rs
@@ -93,9 +93,6 @@ async fn test_launcher_weather_lookup_virtual() {
         return;
     }
 
-    let wasm_path = oak_functions_test_utils::build_rust_crate_wasm("weather_lookup")
-        .expect("Failed to build Wasm module");
-
     let (_background, port) = xtask::launcher::run_oak_functions_example_in_background(
         "weather_lookup",
         workspace_path(&[
