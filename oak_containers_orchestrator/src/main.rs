3df71f0ee7e02b6a6d70325ed8b07a65d18c6d2a
Juliette Pretot <julsh@google.com>
2023-06-16 21:39:49 +0000

    Split orchestrator client into its own crate

--- oak_containers_orchestrator/src/main.rs
+++ oak_containers_orchestrator/src/main.rs
@@ -12,11 +12,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-mod client;
 
 use anyhow::anyhow;
 use clap::Parser;
-use client::LauncherClient;
+use oak_containers_orchestrator_client::LauncherClient;
 
 #[derive(Parser, Debug)]
 struct Args {
