e52c199c3bd84b0a92776dc77f3d57ac87d8b86d
Ivan Petrov <ivanpetrov@google.com>
2023-06-21 20:04:06 +0100

    Add C++ Client Encryptor implementation (#4043)
    This PR adds an implementation for the Client Encryptor as part of C++ Hybrid Encryption implementation.
    
    Ref https://github.com/project-oak/oak/issues/4026

--- cc/crypto/client_encryptor.cc
+++ cc/crypto/client_encryptor.cc
@@ -17,7 +17,70 @@
 #include "cc/crypto/client_encryptor.h"
 
 #include "absl/status/statusor.h"
+#include "cc/crypto/common.h"
 #include "cc/crypto/hpke/sender_context.h"
 #include "oak_crypto/proto/v1/crypto.pb.h"
 
-namespace oak::crypto {}  // namespace oak::crypto
+namespace oak::crypto {
+
+namespace {
+using oak::crypto::v1::EncryptedRequest;
+using oak::crypto::v1::EncryptedResponse;
+}  // namespace
+
+absl::StatusOr<std::unique_ptr<ClientEncryptor>> ClientEncryptor::Create(
+    absl::string_view serialized_server_public_key) {
+  absl::StatusOr<SenderContext> sender_context =
+      SetupBaseSender(serialized_server_public_key, OAK_HPKE_INFO);
+  if (!sender_context.ok()) {
+    return sender_context.status();
+  }
+  return std::make_unique<ClientEncryptor>(ClientEncryptor(*sender_context));
+}
+
+absl::StatusOr<std::string> ClientEncryptor::Encrypt(absl::string_view plaintext,
+                                                     absl::string_view associated_data) {
+  // Encrypt request.
+  absl::StatusOr<std::string> ciphertext =
+      sender_request_context_->Seal(plaintext, associated_data);
+  if (!ciphertext.ok()) {
+    return ciphertext.status();
+  }
+
+  // Create request message.
+  EncryptedRequest request;
+  *request.mutable_encrypted_message()->mutable_ciphertext() = *ciphertext;
+  *request.mutable_encrypted_message()->mutable_associated_data() = associated_data;
+
+  // Encapsulated public key is only sent in the initial request message of the session.
+  if (!serialized_encapsulated_public_key_has_been_sent_) {
+    *request.mutable_serialized_encapsulated_public_key() = serialized_encapsulated_public_key_;
+    serialized_encapsulated_public_key_has_been_sent_ = true;
+  }
+
+  // Serialize request.
+  std::string serialized_request;
+  if (!request.SerializeToString(&serialized_request)) {
+    return absl::InternalError("couldn't serialize request");
+  }
+  return serialized_request;
+}
+
+absl::StatusOr<DecryptionResult> ClientEncryptor::Decrypt(absl::string_view encrypted_response) {
+  // Deserialize response.
+  EncryptedResponse response;
+  if (!response.ParseFromString(encrypted_response)) {
+    return absl::InvalidArgumentError("couldn't deserialize response");
+  }
+
+  // Decrypt response.
+  absl::StatusOr<std::string> plaintext = sender_response_context_->Open(
+      response.encrypted_message().ciphertext(), response.encrypted_message().associated_data());
+  if (!plaintext.ok()) {
+    return plaintext.status();
+  }
+
+  return DecryptionResult{*plaintext, response.encrypted_message().associated_data()};
+}
+
+}  // namespace oak::crypto
