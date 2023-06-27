6472266d5817a81da2e0c0e86216557aa8429eee
Razieh Behjati <razieh@google.com>
2023-06-27 16:53:21 +0100

    Fixes after review

--- kokoro/build_binaries.sh
+++ kokoro/build_binaries.sh
@@ -18,8 +18,6 @@ readonly SCRIPTS_DIR="$(dirname "$0")"
 # shellcheck source=scripts/common
 source "$SCRIPTS_DIR/common"
 
-readonly COMMIT_HASH="$(git rev-parse HEAD)"
-
 # Download the builder tool
 curl --location https://github.com/slsa-framework/slsa-github-generator/releases/download/v1.7.0/slsa-builder-docker-linux-amd64
 chmod +x slsa-builder-docker-linux-amd64
@@ -28,12 +26,12 @@ chmod +x slsa-builder-docker-linux-amd64
 ./slsa-builder-docker-linux-amd64 build \
  --build-config-path buildconfigs/oak_restricted_kernel_simple_io_bin.toml \
  --builder-image "$DOCKER_IMAGE_REPO_DIGEST" \
- --git-commit-digest sha1:"$COMMIT_HASH" \
+ --git-commit-digest sha1:"$KOKORO_GITHUB_COMMIT" \
  --source-repo git+https://github.com/project-oak/oak@refs/heads/main \
  --subjects-path subjects.json \
  --output-folder /tmp/build-outputs \
  --verbose
 
 # Copy the generated binary to placer
-cp /tmp/build-outputs/oak_restricted_kernel_simple_io_bin "$KOKORO_ARTIFACTS_DIR/latest"
+cp /tmp/build-outputs/oak_restricted_kernel_simple_io_bin "$KOKORO_ARTIFACTS_DIR/latest/oak_restricted_kernel_simple_io_bin/$KOKORO_GITHUB_COMMIT"
 ls -als "$KOKORO_ARTIFACTS_DIR"
