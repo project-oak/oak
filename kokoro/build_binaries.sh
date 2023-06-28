43bc40f5da7cb357bb6548216ac511ad9a51a624
Razieh Behjati <razieh@google.com>
2023-06-28 09:40:42 +0100

    Build the binary directly

--- kokoro/build_binaries.sh
+++ kokoro/build_binaries.sh
@@ -14,26 +14,8 @@ export RUST_BACKTRACE=1
 export RUST_LOG=debug
 export XDG_RUNTIME_DIR=/var/run
 
-readonly ROOT_DIR="$(dirname "$0" | cut -d/ -f1)"
-# shellcheck source=scripts/common
-source "$ROOT_DIR/scripts/common"
-
-# readonly COMMIT_HASH="$(git rev-parse HEAD)"
-
-# Download the builder tool
-curl --location https://github.com/slsa-framework/slsa-github-generator/releases/download/v1.7.0/slsa-builder-docker-linux-amd64 --output builder
-chmod +x builder
-
-# Build the binary. Should be made parametric.
-./builder build \
- --build-config-path buildconfigs/oak_restricted_kernel_simple_io_bin.toml \
- --builder-image "$DOCKER_IMAGE_REPO_DIGEST" \
- --git-commit-digest "sha1:$KOKORO_GIT_COMMIT_oak" \
- --source-repo git+https://github.com/project-oak/oak@refs/heads/main \
- --subjects-path subjects.json \
- --output-folder "/tmp/build-outputs-$KOKORO_GIT_COMMIT_oak" \
- --verbose
+./scripts/docker_run  nix develop .#rust --command env --chdir=oak_restricted_kernel_bin cargo build --release
 
 # Copy the generated binary to placer
-cp /tmp/build-outputs/oak_restricted_kernel_simple_io_bin "$KOKORO_ARTIFACTS_DIR/latest/oak_restricted_kernel_simple_io_bin/$KOKORO_GIT_COMMIT_oak"
+cp ./oak_restricted_kernel_bin/target/x86_64-unknown-none/release/oak_restricted_kernel_bin "$KOKORO_ARTIFACTS_DIR/latest/oak_restricted_kernel_bin/$KOKORO_GIT_COMMIT_oak"
 ls -als "$KOKORO_ARTIFACTS_DIR"
