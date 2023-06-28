fb2f20646354c3caa6c3bfd452f96cf91b56d10f
Razieh Behjati <razieh@google.com>
2023-06-28 09:14:49 +0100

    Use COMMIT_HASH instead of KOKORO_GITHUB_COMMIT

--- kokoro/build_binaries.sh
+++ kokoro/build_binaries.sh
@@ -18,6 +18,8 @@ readonly ROOT_DIR="$(dirname "$0" | cut -d/ -f1)"
 # shellcheck source=scripts/common
 source "$ROOT_DIR/scripts/common"
 
+readonly COMMIT_HASH="$(git rev-parse HEAD)"
+
 # Download the builder tool
 curl --location https://github.com/slsa-framework/slsa-github-generator/releases/download/v1.7.0/slsa-builder-docker-linux-amd64 --output builder
 chmod +x builder
@@ -26,12 +28,12 @@ chmod +x builder
 ./builder build \
  --build-config-path buildconfigs/oak_restricted_kernel_simple_io_bin.toml \
  --builder-image "$DOCKER_IMAGE_REPO_DIGEST" \
- --git-commit-digest "sha1:$KOKORO_GITHUB_COMMIT" \
+ --git-commit-digest "sha1:$COMMIT_HASH" \
  --source-repo git+https://github.com/project-oak/oak@refs/heads/main \
  --subjects-path subjects.json \
- --output-folder "/tmp/build-outputs-$KOKORO_GITHUB_COMMIT" \
+ --output-folder "/tmp/build-outputs-$COMMIT_HASH" \
  --verbose
 
 # Copy the generated binary to placer
-cp /tmp/build-outputs/oak_restricted_kernel_simple_io_bin "$KOKORO_ARTIFACTS_DIR/latest/oak_restricted_kernel_simple_io_bin/$KOKORO_GITHUB_COMMIT"
+cp /tmp/build-outputs/oak_restricted_kernel_simple_io_bin "$KOKORO_ARTIFACTS_DIR/latest/oak_restricted_kernel_simple_io_bin/$COMMIT_HASH"
 ls -als "$KOKORO_ARTIFACTS_DIR"
