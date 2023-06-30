1b02f39d75b37f703f69b39b2e4dcd6529d80b57
Andri Saar <andrisaar@google.com>
2023-06-30 18:06:48 +0000

    Move stage1 to a separate Kokoro presubmit

---
+++ kokoro/oak_containers_presubmit.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o xtrace
+set -o pipefail
+
+export CI=kokoro
+
+export RUST_BACKTRACE=1
+export RUST_LOG=debug
+export XDG_RUNTIME_DIR=/var/run
+
+./scripts/docker_pull
+./scripts/docker_run nix develop .#containers --command just kokoro_oak_containers
+
+mkdir -p "$KOKORO_ARTIFACTS_DIR/test_logs/"
+cp ./target/nextest/default/*.xml "$KOKORO_ARTIFACTS_DIR/test_logs/" || true
+
+mkdir -p "$KOKORO_ARTIFACTS_DIR/binaries/"
+
+# Store the git commit hash in a file.
+echo "${KOKORO_GIT_COMMIT_oak:?}" > "$KOKORO_ARTIFACTS_DIR/binaries/git_commit"
+
+# Copy the generated binaries to Placer.
+export GENERATED_BINARIES=(
+    ./target/stage1.cpio
+)
+cp "${GENERATED_BINARIES[@]}" "$KOKORO_ARTIFACTS_DIR/binaries/"
+
+ls -alsR "$KOKORO_ARTIFACTS_DIR/binaries"
