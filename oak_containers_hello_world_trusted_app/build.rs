56542300b58ce980d0a607e28be1b03eb61ab364
Juliette Pretot <julsh@google.com>
2023-07-05 17:18:47 +0000

    add missing proto includes

--- oak_containers_hello_world_trusted_app/build.rs
+++ oak_containers_hello_world_trusted_app/build.rs
@@ -27,7 +27,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     generate_grpc_code(
         "../",
-        &["oak_containers/proto/interfaces.proto"],
+        &[
+            "oak_containers/proto/interfaces.proto",
+            "oak_remote_attestation/proto/v1/messages.proto",
+        ],
         CodegenOptions {
             build_client: true,
             ..Default::default()
