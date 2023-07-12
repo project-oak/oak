b25da5436345fb7e1539730d0a55e0d7b2a43768
Ivan Petrov <ivanpetrov@google.com>
2023-07-12 17:14:06 +0100

    Implement gRPC streaming transport (#4161)
    Ref https://github.com/project-oak/oak/issues/4069

---
+++ cc/client/grpc_client_cli.cc
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2023 The Project Oak Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <grpc/grpc.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/security/credentials.h>
+
+#include <memory>
+
+#include "cc/client/client.h"
+#include "cc/remote_attestation/insecure_attestation_verifier.h"
+#include "cc/transport/grpc_streaming_transport.h"
+#include "oak_remote_attestation/proto/v1/service_streaming.grpc.pb.h"
+
+using ::grpc::Channel;
+using ::grpc::ClientContext;
+using ::grpc::ClientReaderWriter;
+using ::grpc::CreateChannel;
+using ::grpc::InsecureChannelCredentials;
+using ::oak::client::OakClient;
+using ::oak::remote_attestation::InsecureAttestationVerifier;
+using ::oak::session::v1::RequestWrapper;
+using ::oak::session::v1::ResponseWrapper;
+using ::oak::session::v1::StreamingSession;
+using ::oak::transport::GrpcStreamingTransport;
+
+// TODO(#4069): Finish CLI implementation.
+int main(int argc, char* argv[]) {
+  // Create gRPC client stub.
+  std::shared_ptr<Channel> channel = CreateChannel("", InsecureChannelCredentials());
+  std::shared_ptr<oak::session::v1::StreamingSession::Stub> stub =
+      StreamingSession::NewStub(channel);
+  ClientContext context;
+  std::unique_ptr<ClientReaderWriter<RequestWrapper, ResponseWrapper>> channel_reader_writer =
+      stub->Stream(&context);
+
+  // Create Oak Client.
+  std::unique_ptr<GrpcStreamingTransport> transport = std::make_unique<GrpcStreamingTransport>(
+      GrpcStreamingTransport(std::move(channel_reader_writer)));
+  InsecureAttestationVerifier verifier = InsecureAttestationVerifier();
+  absl::StatusOr<std::unique_ptr<OakClient>> oak_client =
+      OakClient::Create(std::move(transport), verifier);
+  if (!oak_client.ok()) {
+    // TODO(#4069): Log errors.
+    return 1;
+  }
+}
--- cc/transport/grpc_streaming_transport.h
+++ cc/transport/grpc_streaming_transport.h
@@ -17,17 +17,100 @@
 #ifndef CC_TRANSPORT_GRPC_STREAMING_TRANSPORT_H_
 #define CC_TRANSPORT_GRPC_STREAMING_TRANSPORT_H_
 
+#include <grpc/grpc.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/grpcpp.h>
+
+#include <memory>
+
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "cc/transport/transport.h"
 #include "oak_remote_attestation/proto/v1/messages.pb.h"
+#include "oak_remote_attestation/proto/v1/service_streaming.grpc.pb.h"
+#include "oak_remote_attestation/proto/v1/service_streaming.pb.h"
 
 namespace oak::transport {
 
 class GrpcStreamingTransport : public TransportWrapper {
  public:
-  absl::StatusOr<::oak::session::v1::AttestationBundle> GetEvidence() override;
-  absl::StatusOr<std::string> Invoke(absl::string_view request_bytes) override;
+  explicit GrpcStreamingTransport(
+      std::unique_ptr<::grpc::ClientReaderWriter<::oak::session::v1::RequestWrapper,
+                                                 ::oak::session::v1::ResponseWrapper>>
+          channel_reader_writer)
+      : channel_reader_writer_(std::move(channel_reader_writer)) {}
+
+  absl::StatusOr<::oak::session::v1::AttestationBundle> GetEvidence() override {
+    // Create request.
+    ::oak::session::v1::RequestWrapper request;
+    ::oak::session::v1::GetPublicKeyRequest get_public_key_request;
+    *request.mutable_get_public_key_request() = get_public_key_request;
+
+    // Send request.
+    auto response = Send(request);
+    if (!response.ok()) {
+      return response.status();
+    }
+
+    // Process response.
+    switch (response->response_case()) {
+      case ::oak::session::v1::ResponseWrapper::kGetPublicKeyResponseFieldNumber:
+        return response->get_public_key_response().attestation_bundle();
+      case ::oak::session::v1::ResponseWrapper::kInvokeResponseFieldNumber:
+        return absl::InternalError("received InvokeResponse instead of GetPublicKeyResponse");
+      case ::oak::session::v1::ResponseWrapper::RESPONSE_NOT_SET:
+      default:
+        return absl::InternalError("received unsupported response: " + response->DebugString());
+    }
+  }
+
+  absl::StatusOr<std::string> Invoke(absl::string_view request_bytes) override {
+    // Create request.
+    ::oak::session::v1::RequestWrapper request;
+    ::oak::session::v1::InvokeRequest* invoke_request = request.mutable_invoke_request();
+    invoke_request->set_encrypted_body(request_bytes);
+
+    // Send request.
+    auto response = Send(request);
+    if (!response.ok()) {
+      return response.status();
+    }
+
+    // Process response.
+    switch (response->response_case()) {
+      case ::oak::session::v1::ResponseWrapper::kGetPublicKeyResponseFieldNumber:
+        return absl::InternalError("received GetPublicKeyResponse instead of InvokeResponse");
+      case ::oak::session::v1::ResponseWrapper::kInvokeResponseFieldNumber:
+        return response->invoke_response().encrypted_body();
+      case ::oak::session::v1::ResponseWrapper::RESPONSE_NOT_SET:
+      default:
+        return absl::InternalError("received unsupported response: " + response->DebugString());
+    }
+  }
+
+ private:
+  std::unique_ptr<::grpc::ClientReaderWriter<::oak::session::v1::RequestWrapper,
+                                             ::oak::session::v1::ResponseWrapper>>
+      channel_reader_writer_;
+
+  absl::StatusOr<::oak::session::v1::ResponseWrapper> Send(
+      const ::oak::session::v1::RequestWrapper& request) {
+    // Send a request.
+    channel_reader_writer_->Write(request);
+    channel_reader_writer_->WritesDone();
+
+    // Receive a response.
+    ::oak::session::v1::ResponseWrapper response;
+    channel_reader_writer_->Read(&response);
+    ::grpc::Status status = channel_reader_writer_->Finish();
+    if (status.ok()) {
+      return response;
+    } else {
+      return absl::InternalError("couldn't send request: " + status.error_message());
+    }
+  }
 };
 
 }  // namespace oak::transport
