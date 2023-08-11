f86dc2a26462dbafe3861b8635e40b67ad4b0fe6
Razieh Behjati <razieh@google.com>
2023-08-11 13:13:02 +0100

    Update rekor verification testdata

--- oak_remote_attestation_verification/src/tests.rs
+++ oak_remote_attestation_verification/src/tests.rs
@@ -19,24 +19,27 @@ use std::fs;
 
 #[test]
 fn test_verify_rekor_log_entry() {
-    // Example endorsement file corresponding to the provenance file in
-    // https://github.com/project-oak/transparent-release/blob/main/testdata/provenances/15dc16c42a4ac9ed77f337a4a3065a63e444c29c18c8cf69d6a6b4ae678dca5c.json
+    // Example endorsement file generated with the following provenance as the evidence:
+    // https://ent-server-62sa4xcfia-ew.a.run.app/raw/sha256:b28696a8341443e3ba433373c60fe1eba8d96f28c8aff6c5ee03d752dd3b399b
     let endorsement_path = "testdata/endorsement.json";
 
     // LogEntry downloaded from
-    // https://rekor.sigstore.dev/api/v1/log/entries/bb05be1bd813f8afb7b77b2d9f7be5ae25b396d111c7a26a04b785c48c277372.
-    // This LogEntry was created by sining the example endorsement file above,
-    // and publishing that to Rekor (https://rekor.sigstore.dev), as described
+    // https://rekor.sigstore.dev/api/v1/log/entries/24296fb24b8ad77a51d549703a3a1c2dd2639ba49617fc563854031cb93e6d354e7b005065c334a8.
+    // This LogEntry was created by signing the example endorsement file above, using google-cloud
+    // KMS, and uploading it to Rekor (https://rekor.sigstore.dev) using `rekor-cli`, as described
     // in https://github.com/sigstore/rekor/blob/main/types.md#pkixx509.
     // The resulting entry in Rekor has UUID
-    // `bb05be1bd813f8afb7b77b2d9f7be5ae25b396d111c7a26a04b785c48c277372` and logIndex 1323526.
-    // The body of LogEntry can be fetched using `rekor-cli get --log-index 1323526`.
+    // `24296fb24b8ad77a51d549703a3a1c2dd2639ba49617fc563854031cb93e6d354e7b005065c334a8` and
+    // logIndex 30891523. The body of LogEntry can be fetched using `rekor-cli get --log-index
+    // 30891523`.
     let log_entry_path = "testdata/logentry.json";
 
-    // Test public key, representing the product team that signed the endorsement file.
-    let pubkey_path = "testdata/ec_public.pem";
+    // Public key fetched from Google Cloud KMS, associated with the signing key that was used for
+    // signing the endorsement statement.
+    let pubkey_path = "testdata/oak-development.pem";
 
-    // Public key of the Rekor instance hosted by sigstore.dev. It is downloaded from https://rekor.sigstore.dev/api/v1/log/publicKey.
+    // Public key of the Rekor instance hosted by sigstore.dev. It is downloaded from
+    // https://rekor.sigstore.dev/api/v1/log/publicKey.
     let rekor_pubkey_path = "testdata/rekor_public_key.pem";
 
     let endorsement_bytes = fs::read(endorsement_path).expect("couldn't read endorsement file");
