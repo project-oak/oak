cad2ca06b26b3a411ad7a285a28166d9a127c54b
Ivan Petrov <ivanpetrov@google.com>
2023-08-14 17:48:11 +0100

    Update

--- oak_containers_hello_world_untrusted_app/src/app_client.rs
+++ oak_containers_hello_world_untrusted_app/src/app_client.rs
@@ -21,8 +21,6 @@ use anyhow::Context;
 use tokio::time::Duration;
 use tonic::transport::Endpoint;
 
-const EMPTY_ASSOCIATED_DATA: &[u8] = b"";
-
 /// Utility struct used to interface with the launcher
 pub struct TrustedApplicationClient {
     inner: GrpcTrustedApplicationClient<tonic::transport::channel::Channel>,
