a9293c41cfe24e758a7b5042fd937d6230df65f5
Andri Saar <andrisaar@google.com>
2023-09-14 14:43:54 +0000

    Start the container via `systemd-nspawn`

--- oak_containers_orchestrator/src/container_runtime.rs
+++ oak_containers_orchestrator/src/container_runtime.rs
@@ -17,8 +17,6 @@
 //! containers in the form of an OCI filesystem bundle. However the runtime
 //! itself is not OCI spec compliant.
 
-use anyhow::Context;
-use std::path::PathBuf;
 use tokio::sync::oneshot::Sender;
 
 /// Representation of a minimal OCI filesystem bundle config, including just the required fields.
@@ -30,6 +28,7 @@ struct OciFilesystemBundleConfig {
 }
 
 #[derive(serde::Deserialize)]
+#[allow(unused)]
 struct OciFilesystemBundleConfigProcess {
     args: Vec<String>,
     #[serde(default)]
@@ -40,6 +39,7 @@ struct OciFilesystemBundleConfigProcess {
 }
 
 #[derive(serde::Deserialize)]
+#[allow(unused)]
 struct OciFilesystemBundleConfigProcessUser {
     uid: u32,
     gid: u32,
@@ -52,27 +52,6 @@ struct OciFilesystemBundleConfigRoot {
 
 // Directory at which the container OCI filesystem bundle will be unpacked.
 const CONTAINER_DIR: &str = "/oak_container";
-
-async fn rmount_dir(source: PathBuf, target: PathBuf) -> Result<(), anyhow::Error> {
-    if target.exists() {
-        tokio::fs::remove_dir_all(&target)
-            .await
-            .context("failed to removing existing directories at the target")?
-    }
-    tokio::fs::create_dir_all(&target)
-        .await
-        .context("failed to create a directory at the target")?;
-
-    tokio::process::Command::new("mount")
-        .current_dir(CONTAINER_DIR)
-        .arg("--rbind")
-        .arg(&source)
-        .arg(&target)
-        .status()
-        .await?;
-
-    Ok(())
-}
 
 pub async fn run(
     container_bundle: &[u8],
@@ -101,84 +80,47 @@ pub async fn run(
         base
     };
 
-    // mount the utility dir (includes the ipc socket) into the container
-    let container_dev_path = {
-        let mut base = container_rootfs_path.clone();
-        base.push(crate::UTIL_DIR);
-        base
-    };
-    rmount_dir(
-        std::path::Path::new("/").join(crate::UTIL_DIR),
-        container_dev_path,
-    )
-    .await?;
-
-    // mount host /dev into the container
-    let container_dev_path = {
-        let mut base = container_rootfs_path.clone();
-        base.push("dev");
-        base
-    };
-    rmount_dir(std::path::PathBuf::from("/dev"), container_dev_path).await?;
-
-    // mount host /sys into the container
-    let container_sys_path = {
-        let mut base = container_rootfs_path.clone();
-        base.push("sys");
-        base
-    };
-    rmount_dir(std::path::PathBuf::from("/sys"), container_sys_path).await?;
-
-    // mount host /proc into the container
-    let container_proc_path = {
-        let mut base = container_rootfs_path.clone();
-        base.push("proc");
-        base
-    };
-    rmount_dir(std::path::PathBuf::from("/proc"), container_proc_path).await?;
-
     log::debug!(
         "Startup command: {:?}",
         oci_filesystem_bundle_config.process.args
     );
+
+    // systemd-nspawn claims to support OCI bundles, but unfortunately it doesn't support bundles
+    // in the 1.0.2-dev format. See https://github.com/systemd/systemd/issues/22148
     let mut start_trusted_app_cmd = {
-        let mut cmd =
-            tokio::process::Command::new(oci_filesystem_bundle_config.process.args[0].clone());
-        cmd.args(oci_filesystem_bundle_config.process.args.as_slice()[1..].to_vec())
-            .uid(oci_filesystem_bundle_config.process.user.uid)
-            .gid(oci_filesystem_bundle_config.process.user.gid);
-        for variable in oci_filesystem_bundle_config.process.env.clone() {
-            if let Some((key, value)) = variable.split_once('=') {
-                log::debug!("Setting environment variable: {key}={value}");
-                cmd.env(key, value);
-            }
+        let mut nspawn = tokio::process::Command::new("/usr/bin/systemd-nspawn");
+        // See https://www.freedesktop.org/software/systemd/man/systemd-nspawn.html for the list of args.
+        let mut args = vec![
+            "--ephemeral".to_string(),
+            format!(
+                "--directory={}",
+                container_rootfs_path
+                    .into_os_string()
+                    .into_string()
+                    .unwrap()
+                    .as_str()
+            ),
+            "--as-pid2".to_string(),
+            format!("--user={}", oci_filesystem_bundle_config.process.user.uid),
+            format!("--bind=/{}", crate::UTIL_DIR),
+            "--console=read-only".to_string(),
+            "--no-pager".to_string(),
+        ];
+
+        for env in &oci_filesystem_bundle_config.process.env {
+            args.push(format!("--setenv={}", env));
         }
-        cmd
+
+        nspawn.args(
+            args.iter()
+                .chain(oci_filesystem_bundle_config.process.args.iter()),
+        );
+        nspawn
     };
 
-    log::debug!(
-        "Setting working directory: {:?}",
-        oci_filesystem_bundle_config.process.cwd
-    );
+    log::info!("nspawn: {:?}", start_trusted_app_cmd);
 
-    let prep_trusted_app_process = move || {
-        // Run the trusted app in a chroot environment of the container.
-        std::os::unix::fs::chroot(container_rootfs_path.clone())?;
-        let cwd = oci_filesystem_bundle_config.process.cwd.clone();
-        if cwd.has_root() {
-            std::env::set_current_dir(cwd)?;
-        }
-        Ok(())
-    };
-    // Safety: this unsafe block exists solely we can call the unsafe `pre_exec`
-    // method, allowing us to use a closure to prep the newly forked child
-    // process. That closure runs in a special environment so it can behave a bit
-    // unexpectedly. For our case that's fine though, since we just use it to
-    // make chdir & chroot syscalls.
-    // Ref: https://docs.rs/tokio/latest/tokio/process/struct.Command.html#safety
-    let status = unsafe { start_trusted_app_cmd.pre_exec(prep_trusted_app_process) }
-        .status()
-        .await?;
+    let status = start_trusted_app_cmd.status().await?;
     log::info!("Container exited with status {status:?}");
 
     let _ = exit_notification_sender.send(());
