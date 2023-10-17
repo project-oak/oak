7db46254e6d98d24b1bb0cff7c52f5fdefcdc139
Andri Saar <andrisaar@google.com>
2023-10-17 18:37:48 +0000

    Opentelemetrification, vol 2: use the opentelemetry log APIs.

--- oak_containers_syslogd/src/log_relay.rs
+++ oak_containers_syslogd/src/log_relay.rs
@@ -17,13 +17,14 @@
 use crate::systemd_journal::{Journal, JournalOpenFlags};
 use anyhow::{Context, Result};
 use oak_containers_orchestrator_client::LauncherClient;
+use opentelemetry_api::logs::{AnyValue, LogRecord, Logger, Severity};
+use std::time::{Duration, SystemTime};
 use tokio::sync::mpsc;
-use tokio_stream::wrappers::UnboundedReceiverStream;
 
 pub async fn run(launcher_client: LauncherClient) -> Result<()> {
     // Journal is not Send, because the underlying systemd journal can't be shared between threads
     // (even with locking). Thus, let's wrap things in a channel.
-    let (send, recv) = mpsc::unbounded_channel();
+    let (send, mut recv) = mpsc::unbounded_channel();
 
     let reader = async move {
         // Iterating over the journal can block (synchronously), so we need to wrap this in a
@@ -52,11 +53,53 @@ pub async fn run(launcher_client: LauncherClient) -> Result<()> {
         });
         x.await?
     };
+    let logger = opentelemetry_otlp::new_pipeline()
+        .logging()
+        .with_exporter(launcher_client.openmetrics_builder())
+        .install_batch(opentelemetry_sdk::runtime::Tokio)
+        .context("could not create OTLP logger")?;
     let sender = async {
-        launcher_client
-            .log(UnboundedReceiverStream::new(recv))
-            .await
-            .map_err(|err| anyhow::anyhow!("error writing logs to launcher: {}", err))
+        while let Some(mut msg) = recv.recv().await {
+            let mut builder = LogRecord::builder();
+            // PRIORITY
+            if let Some(val) = msg.remove("PRIORITY") {
+                builder = builder.with_severity_number(match val.parse()? {
+                    // EMERGENCY
+                    0 => Severity::Error4,
+                    // ALERT
+                    1 => Severity::Error3,
+                    // CRITICAL
+                    2 => Severity::Error2,
+                    // ERROR
+                    3 => Severity::Error,
+                    // WARNING
+                    4 => Severity::Warn,
+                    // NOTICE
+                    5 => Severity::Info2,
+                    // INFORMATIONAL
+                    6 => Severity::Info,
+                    // DEBUG
+                    7 => Severity::Debug,
+                    // lower priorities
+                    _ => Severity::Trace,
+                });
+            }
+            if let Some(val) = msg.remove("_SOURCE_REALTIME_TIMESTAMP") {
+                builder = builder
+                    .with_timestamp(SystemTime::UNIX_EPOCH + Duration::from_secs(val.parse()?));
+            }
+            if let Some(val) = msg.remove("MESSAGE") {
+                builder = builder.with_body(AnyValue::String(val.into()));
+            }
+            builder = builder.with_attributes(
+                msg.into_iter()
+                    .map(|(k, v)| (k.into(), AnyValue::String(v.into())))
+                    .collect(),
+            );
+            logger.emit(builder.build());
+        }
+        Ok(())
     };
+
     tokio::try_join!(reader, sender).map(|((), ())| ())
 }
