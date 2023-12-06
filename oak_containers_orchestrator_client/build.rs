39341c7819938460c0d82a72ce83ccb0c93c5a16
Ivan Petrov <ivanpetrov@google.com>
2023-12-06 22:31:08 +0000

    Add Hostlib method for requesting group keys (#4530)
    This PR adds a Hostlib method for requesting the group keys by the Orchestrator.
    This method is currently not called by the Orchestrator, because we first need to provide it internally.
    
    Ref https://github.com/project-oak/oak/issues/4442

--- oak_containers_orchestrator_client/build.rs
+++ oak_containers_orchestrator_client/build.rs
@@ -23,6 +23,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             "oak_containers/proto/interfaces.proto",
             "oak_crypto/proto/v1/crypto.proto",
             "oak_remote_attestation/proto/v1/messages.proto",
+            "proto/containers/hostlib_key_provisioning.proto",
         ],
         CodegenOptions {
             build_client: true,
