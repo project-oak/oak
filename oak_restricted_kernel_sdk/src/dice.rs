5d356359371a457d458e0d5e5414a6ab47f453fa
jul-sh <julsh@google.com>
2023-12-15 16:08:27 -0500

    Oak SDK Implement remaining structs and handles (#4560)
    * Implement EncryptionKeyHandle in oak sdk
    
    * derive copy, clone for SDK structs
    
    * Accept a key origin parameter when creating handles
    
    * expose EncryptionKeyHandle and KeyOrigin
    
    * make clippy happy
    
    * Implement Attester to retrieve evidence
    
    * don't derive copy for dice structs

--- oak_restricted_kernel_sdk/src/dice.rs
+++ oak_restricted_kernel_sdk/src/dice.rs
@@ -15,7 +15,7 @@
 //
 
 use anyhow::Ok;
-use oak_crypto::encryptor::EncryptionKeyProvider;
+use oak_crypto::encryptor::{EncryptionKeyProvider, RecipientContextGenerator};
 use oak_dice::evidence::{Evidence, RestrictedKernelDiceData, P256_PRIVATE_KEY_SIZE};
 use oak_restricted_kernel_interface::{syscall::read, DICE_DATA_FD};
 use p256::ecdsa::SigningKey;
@@ -56,22 +56,94 @@ fn get_restricted_kernel_dice_data() -> anyhow::Result<RestrictedKernelDiceData>
     Ok(result)
 }
 
+/// Defines the origin of the key that should be used.
+pub enum KeyOrigin {
+    /// Describes the key originating in the hardware of the current TEE.
+    Instance,
+    /// Use a key that is shared across enclaves executing the same task.
+    /// Not yet supported on the restricted kernel.
+    Group,
+}
+
+#[derive(core::marker::Copy, Clone)]
 pub struct Signer {
     key: &'static SigningKey,
 }
 
 impl Signer {
+    pub fn create(key_origin: KeyOrigin) -> anyhow::Result<Self> {
+        match key_origin {
+            KeyOrigin::Instance => {
+                DICE_WRAPPER
+                    .as_ref()
+                    .map_err(anyhow::Error::msg)
+                    .and_then(|d| {
+                        Ok(Signer {
+                            key: &d.signing_key,
+                        })
+                    })
+            }
+            KeyOrigin::Group => Err(anyhow::Error::msg(
+                "Group keys are not yet implemented for the restricted kernel.",
+            )),
+        }
+    }
+    pub fn sign(&self, message: &[u8]) -> oak_crypto::signer::Signature {
+        <SigningKey as oak_crypto::signer::Signer>::sign(self.key, message)
+    }
+}
+
+#[derive(core::marker::Copy, Clone)]
+pub struct EncryptionKeyHandle {
+    key: &'static EncryptionKeyProvider,
+}
+
+impl EncryptionKeyHandle {
+    pub fn create(key_origin: KeyOrigin) -> anyhow::Result<Self> {
+        match key_origin {
+            KeyOrigin::Instance => {
+                DICE_WRAPPER
+                    .as_ref()
+                    .map_err(anyhow::Error::msg)
+                    .and_then(|d| {
+                        Ok(EncryptionKeyHandle {
+                            key: &d.encryption_key,
+                        })
+                    })
+            }
+            KeyOrigin::Group => Err(anyhow::Error::msg(
+                "Group keys are not yet implemented for the restricted kernel.",
+            )),
+        }
+    }
+}
+
+impl RecipientContextGenerator for EncryptionKeyHandle {
+    fn generate_recipient_context(
+        &self,
+        encapsulated_public_key: &[u8],
+    ) -> anyhow::Result<oak_crypto::hpke::RecipientContext> {
+        self.key.generate_recipient_context(encapsulated_public_key)
+    }
+}
+
+pub struct Attester {
+    evidence: &'static Evidence,
+}
+
+impl Attester {
     pub fn create() -> anyhow::Result<Self> {
         DICE_WRAPPER
             .as_ref()
             .map_err(anyhow::Error::msg)
             .and_then(|d| {
-                Ok(Signer {
-                    key: &d.signing_key,
+                Ok(Attester {
+                    evidence: &d.evidence,
                 })
             })
     }
-    pub fn sign(&self, message: &[u8]) -> oak_crypto::signer::Signature {
-        <SigningKey as oak_crypto::signer::Signer>::sign(self.key, message)
+    /// Get the attestation evidence of the current enclave.
+    pub fn get_evidence(&self) -> &Evidence {
+        self.evidence
     }
 }
