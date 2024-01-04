65e0dfdfa1faba0cc0a01cb9615b6a97cde39936
Ivan Petrov <ivanpetrov@google.com>
2024-01-04 12:40:11 +0000

    Remove commented code from JNI (#4585)

--- cc/crypto/hpke/jni/jni_helper.cc
+++ cc/crypto/hpke/jni/jni_helper.cc
@@ -20,13 +20,6 @@
 #include <string>
 #include <vector>
 
-// std::unique_ptr<char[]> convert_jbytearray_to_array(JNIEnv* env, jbyteArray arr) {
-//   int len = env->GetArrayLength(arr);
-//   char* buf = new char[len];
-//   std::unique_ptr<char[]> buf_uptr(buf);
-//   return buf_uptr;
-// }
-
 std::string convert_jbytearray_to_string(JNIEnv* env, jbyteArray arr) {
   int len = env->GetArrayLength(arr);
   char* buf = new char[len];
@@ -35,12 +28,3 @@ std::string convert_jbytearray_to_string(JNIEnv* env, jbyteArray arr) {
   std::string result(buf, len);
   return result;
 }
-
-// std::vector<uint8_t> convert_jbytearray_to_vector(JNIEnv* env, jbyteArray arr) {
-//   int len = env->GetArrayLength(arr);
-//   char* buf = new char[len];
-//   std::unique_ptr<char[]> buf_uptr(buf);
-//   env->GetByteArrayRegion(arr, 0, len, reinterpret_cast<jbyte*>(buf));
-//   std::vector<uint8_t> result{buf, len};
-//   return result;
-// }
