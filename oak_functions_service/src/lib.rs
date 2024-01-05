ce881f3dd584eb7605f5e85eab32e93975c73dee
Juliette Pretot <julsh@google.com>
2024-01-05 17:55:35 +0000

    use try_into instead try_from

--- oak_functions_service/src/lib.rs
+++ oak_functions_service/src/lib.rs
@@ -142,10 +142,11 @@ impl<
                         format!("failed to get encryption public key: {err}"),
                     )
                 })?;
-                let evidence =
-                    oak_remote_attestation::proto::oak::attestation::v1::Evidence::try_from(
-                        self.evidencer.get_evidence().clone(),
-                    )
+                let evidence: oak_remote_attestation::proto::oak::attestation::v1::Evidence = self
+                    .evidencer
+                    .get_evidence()
+                    .clone()
+                    .try_into()
                     .map_err(|err| {
                         micro_rpc::Status::new_with_message(
                             micro_rpc::StatusCode::Internal,
