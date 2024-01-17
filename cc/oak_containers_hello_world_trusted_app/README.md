5f236f0edc6fbe3172e349df65d8f2e5dfc49158
Brett McLarnon <bmclarnon@google.com>
2024-01-16 17:03:16 -0800

    C++ Hello World Trusted App implementation.
    This matches the Rust implementation and is useful for (a) demonstrating
    the C++ Oak Containers APIs and (b) demonstrating in a follow-up change
    how to build OCI runtime bundles with bazel.

---
+++ cc/oak_containers_hello_world_trusted_app/README.md
@@ -0,0 +1,12 @@
+<!-- Oak Logo Start -->
+<!-- An HTML element is intentionally used since GitHub recommends this approach to handle different images in dark/light modes. Ref: https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax#specifying-the-theme-an-image-is-shown-to -->
+<!-- markdownlint-disable-next-line MD033 -->
+<h1><picture><source media="(prefers-color-scheme: dark)" srcset="/docs/oak-logo/svgs/oak-containers-negative-colour.svg?sanitize=true"><source media="(prefers-color-scheme: light)" srcset="/docs/oak-logo/svgs/oak-containers.svg?sanitize=true"><img alt="Project Oak Containers Logo" src="/docs/oak-logo/svgs/oak-containers.svg?sanitize=true"></picture></h1>
+<!-- Oak Logo End -->
+
+# C++ Hello World Trusted App
+
+C++ implementation of the trusted part (inside the TEE) of the Oak Containers
+Hello World example application. This implementation parallels the Rust
+implementation in
+[`/oak_containers_hello_world_trusted_app`](../../oak_containers_hello_world_trusted_app).
---
+++ cc/oak_containers_hello_world_trusted_app/app_service.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2024 The Project Oak Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef CC_OAK_CONTAINERS_HELLO_WORLD_TRUSTED_APP_APP_SERVICE_H_
+#define CC_OAK_CONTAINERS_HELLO_WORLD_TRUSTED_APP_APP_SERVICE_H_
+
+#include <string>
+
+#include "absl/log/die_if_null.h"
+#include "absl/strings/string_view.h"
+#include "cc/oak_containers_hello_world_trusted_app/orchestrator_client.h"
+#include "grpcpp/server_context.h"
+#include "grpcpp/support/status.h"
+#include "oak_containers_hello_world_trusted_app/proto/interface.grpc.pb.h"
+#include "oak_containers_hello_world_trusted_app/proto/interface.pb.h"
+
+namespace oak::oak_containers_hello_world_trusted_app {
+
+class TrustedApplicationImpl : public containers::example::TrustedApplication::Service {
+ public:
+  TrustedApplicationImpl(OrchestratorClient* orchestrator_client,
+                         absl::string_view application_config)
+      : orchestrator_client_(*ABSL_DIE_IF_NULL(orchestrator_client)),
+        application_config_(application_config) {}
+
+  grpc::Status Hello(grpc::ServerContext* context, const containers::example::HelloRequest* request,
+                     containers::example::HelloResponse* response) override;
+
+ private:
+  OrchestratorClient& orchestrator_client_;
+  const std::string application_config_;
+};
+
+}  // namespace oak::oak_containers_hello_world_trusted_app
+
+#endif  // CC_OAK_CONTAINERS_HELLO_WORLD_TRUSTED_APP_APP_SERVICE_H_
