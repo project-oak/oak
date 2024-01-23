8688b873200af60cc9678e233e1b705ae156d384
Juliette Pretot <julsh@google.com>
2024-01-23 18:50:07 +0000

    Validate function type signature

--- oak_restricted_kernel_sdk_proc_macro/src/lib.rs
+++ oak_restricted_kernel_sdk_proc_macro/src/lib.rs
@@ -1,37 +1,50 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Item, Result};
+use syn::{parse_macro_input, Item, ItemFn, Result};
 
 #[proc_macro_attribute]
-pub fn entrypoint(_attr: TokenStream, input: TokenStream) -> TokenStream {
-    let input_item = parse_macro_input!(input as syn::Item);
-
-    match process_oak_main(input_item) {
-        Ok(output) => output,
+pub fn entrypoint(_attr: TokenStream, entry: TokenStream) -> TokenStream {
+    let entry_item = parse_macro_input!(entry as syn::Item);
+    match validate_type_signature(entry_item) {
+        Ok(entry_fn) => process_entry_fn(entry_fn),
         Err(error) => error.to_compile_error().into(),
     }
 }
 
-fn process_oak_main(input_item: Item) -> Result<TokenStream> {
-    let input_fn = match input_item {
-        syn::Item::Fn(fn_item) => Ok(fn_item),
+fn validate_type_signature(entry_item: Item) -> Result<ItemFn> {
+    let entry_fn: ItemFn = match entry_item {
+        syn::Item::Fn(entry_fn) => Ok(entry_fn),
         _ => Err(syn::Error::new(
-            syn::spanned::Spanned::span(&input_item),
+            syn::spanned::Spanned::span(&entry_item),
             "the entrypoint macro can only be applied to functions",
         )),
     }?;
 
-    let input_fn_name = &input_fn.sig.ident;
+    let return_type_error = syn::Error::new(
+        syn::spanned::Spanned::span(&entry_fn.sig.output),
+        "the entrypoint function should have return type !",
+    );
+    match &entry_fn.sig.output {
+        syn::ReturnType::Default => Err(return_type_error),
+        syn::ReturnType::Type(_, return_type) => match **return_type {
+            syn::Type::Never(_) => Ok(entry_fn),
+            _ => Err(return_type_error),
+        },
+    }
+}
+
+fn process_entry_fn(entry_fn: ItemFn) -> TokenStream {
+    let entry_fn_name = &entry_fn.sig.ident;
 
     let generated = quote! {
-        #input_fn
+        #entry_fn
 
         static LOGGER: oak_restricted_kernel_sdk::StderrLogger = oak_restricted_kernel_sdk::StderrLogger {};
 
         #[no_mangle]
         fn _start() -> ! {
             oak_restricted_kernel_sdk::init(log::LevelFilter::Debug);
-            #input_fn_name();
+            #entry_fn_name();
         }
 
         #[alloc_error_handler]
@@ -44,6 +57,5 @@ fn process_oak_main(input_item: Item) -> Result<TokenStream> {
             oak_restricted_kernel_sdk::panic_handler(info);
         }
     };
-
-    Ok(TokenStream::from(generated))
+    TokenStream::from(generated)
 }
