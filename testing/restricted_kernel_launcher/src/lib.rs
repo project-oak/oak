19ddc96f6bc95e3345985bf0e1bb5ea396cda8d8
Juliette Pretot <julsh@google.com>
2024-01-24 20:58:57 +0000

    Add simple testing launcher for enclave apps
    Currently we only have a very specific launcher used for oak functions. The idea is to use this simpler launcher to aid in the development of the restricted kernel orchestrator.

---
+++ testing/restricted_kernel_launcher/src/lib.rs
@@ -0,0 +1,32 @@
+//
+// Copyright 2022 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#![feature(never_type)]
+#![feature(result_flattening)]
+#![feature(array_chunks)]
+
+use oak_launcher_utils::{
+    channel::{self},
+    launcher,
+};
+
+pub async fn create(
+    params: launcher::Params,
+) -> Result<(Box<dyn launcher::GuestInstance>, channel::ConnectorHandle), Box<dyn std::error::Error>>
+{
+    log::info!("creating guest instance");
+    launcher::launch(params).await
+}
