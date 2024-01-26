1d4e8a540be3eb30d0c3941dbd5b489a5b282ce4
Ivan Petrov <ivanpetrov@google.com>
2024-01-26 12:35:34 +0000

    Implement GetGroupKeys for Key Provisioning

--- oak_containers_orchestrator/src/key_provisioning.rs
+++ oak_containers_orchestrator/src/key_provisioning.rs
@@ -13,9 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::sync::{Arc, OnceLock};
-
-use tonic::{Request, Response};
 
 use crate::{
     crypto::KeyStore,
@@ -23,27 +20,89 @@ use crate::{
         key_provisioning_server::KeyProvisioning, GetGroupKeysRequest, GetGroupKeysResponse,
     },
 };
-
-#[allow(dead_code)]
-/// Dependant is a Key Provisioning role that requests group keys from the
-/// Key Provisioning Leader.
-struct KeyProvisioningDependant {
-    _key_store: OnceLock<Arc<KeyStore>>,
-}
+use oak_attestation_verification::verifier::verify;
+use oak_crypto::encryptor::{EncryptionKeyProvider, ServerEncryptor};
+use oak_proto_rust::oak::attestation::v1::ReferenceValues;
+use std::sync::Arc;
+use tonic::{Request, Response};
 
 struct KeyProvisioningService {
-    _key_store: Arc<KeyStore>,
+    key_store: Arc<KeyStore>,
+    reference_values: ReferenceValues,
+}
+
+impl KeyProvisioningService {
+    pub fn new(key_store: Arc<KeyStore>) -> Self {
+        Self {
+            key_store,
+            // TODO(#4442): Provide reference values by the hostlib.
+            reference_values: ReferenceValues{},
+        }
+    }
 }
 
 #[tonic::async_trait]
 impl KeyProvisioning for KeyProvisioningService {
     async fn get_group_keys(
         &self,
-        _request: Request<GetGroupKeysRequest>,
+        request: Request<GetGroupKeysRequest>,
     ) -> Result<Response<GetGroupKeysResponse>, tonic::Status> {
-        // TODO(#4442): Implement generating group encryption key.
-        Err(tonic::Status::unimplemented(
-            "Key Provisioning is not implemented",
-        ))
+        let request = request.into_inner();
+
+        // Verify attestation evidence and get encryption public key, which will be used to encrypt
+        // group keys.
+        let evidence = request
+            .evidence
+            .context("request message doesn't contain evidence")?;
+        let endorsements = request
+            .endorsements
+            .context("request message doesn't contain endorsements")?;
+        let attestation_results = verify(&evidence, &endorsements, &self.reference_values)
+            .context("couldn't verify endorsed evidence")?;
+
+        // Create server encryptor for decrypting the group keys received from the leader enclave.
+        let encrypted_encryption_private_key =
+            request
+                .encrypted_encryption_private_key
+                .ok_or(tonic::Status::invalid_argument(
+                    "encrypted encryption key wasn't provided",
+                ))?;
+        let encapsulated_public_key = encrypted_encryption_private_key
+            .serialized_encapsulated_public_key
+            .as_ref()
+            .ok_or(tonic::Status::invalid_argument(
+                "encapsulated public key wasn't provided",
+            ))?;
+        let mut server_encryptor = ServerEncryptor::create(
+            encapsulated_public_key,
+            self.key_store.instance_encryption_key(),
+        )
+        .map_err(|err| {
+            tonic::Status::internal(format!("couldn't create server encryptor: {err}"))
+        })?;
+
+        // Decrypt group keys.
+        let (decrypted_encryption_private_key, _) = server_encryptor
+            .decrypt(&encrypted_encryption_private_key)
+            .map_err(|err| {
+                tonic::Status::internal(format!(
+                    "couldn't decrypt the encryption private key: {err}"
+                ))
+            })?;
+        let group_encryption_key =
+            EncryptionKeyProvider::from_private_key(decrypted_encryption_private_key).map_err(
+                |err| tonic::Status::internal(format!("couldn't deserialize private key: {err}")),
+            )?;
+
+        // Add group keys to the Orchestrator key store.
+        self.key_store
+            .mutable_group_encryption_key()
+            .set(group_encryption_key)
+            .map_err(|_| {
+                tonic::Status::internal("group encryption key was already initialized".to_string())
+            })?;
+
+        self.cancellation_token.cancel();
+        Ok(tonic::Response::new(()))
     }
 }
