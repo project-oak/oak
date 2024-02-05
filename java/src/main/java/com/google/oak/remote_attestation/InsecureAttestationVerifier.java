9d96b4b03fe70dfeb5c3fae07b635901818e7e46
k-naliuka <katsiaryna@google.com>
2024-02-05 12:31:10 +0100

    Removed debugging artifacts

--- java/src/main/java/com/google/oak/remote_attestation/InsecureAttestationVerifier.java
+++ java/src/main/java/com/google/oak/remote_attestation/InsecureAttestationVerifier.java
@@ -37,17 +37,12 @@ import com.google.oak.attestation.v1.Endorsements;
 import com.google.oak.attestation.v1.Evidence;
 import com.google.oak.attestation.v1.ReferenceValues;
 import com.google.oak.util.Result;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * Verifier implementation that doesn't verify attestation evidence and is used for testing.
  */
 public class InsecureAttestationVerifier implements AttestationVerifier {
-  private static final Logger logger =
-      Logger.getLogger(InsecureAttestationVerifier.class.getName());
 
   private static final DataItem SUBJECT_PUBLIC_KEY_ID = new NegativeInteger(-4670552);
 
@@ -65,7 +60,6 @@ public class InsecureAttestationVerifier implements AttestationVerifier {
   public final Result<AttestationResults, Exception> verify(long nowUtcMillis,
       final Evidence evidence, final Endorsements endorsements,
       final ReferenceValues referenceValues) {
-    logger.severe(evidence.getApplicationKeys().getEncryptionPublicKeyCertificate().toString());
 
     return Result.success(
         AttestationResults.newBuilder()
@@ -90,26 +84,26 @@ public class InsecureAttestationVerifier implements AttestationVerifier {
           "Failed to parse the encryption public key certificate payload", e);
     }
     for (DataItem di : dataItems) {
-      logger.severe(di.getMajorType() + ": " + di.toString());
       if (di.getMajorType() == MajorType.MAP) {
         Map cwtMap = (Map) di;
         if (!cwtMap.getKeys().contains(SUBJECT_PUBLIC_KEY_ID)) {
           continue;
         }
         byte[] serializedPublicKey = ((ByteString) cwtMap.get(SUBJECT_PUBLIC_KEY_ID)).getBytes();
-        logger.severe(Base64.getEncoder().encodeToString(serializedPublicKey));
         try {
-          DataItem publicKeyCbor = CborDecoder.decode(serializedPublicKey).get(0);
-          logger.severe(publicKeyCbor.toString());
+          List<DataItem> publicKeyDataItems = CborDecoder.decode(serializedPublicKey);
+          if (publicKeyDataItems.size() != 1) {
+            throw new IllegalArgumentException("Serialized COSE key couldn't be parsed");
+          }
+          DataItem publicKeyCbor = publicKeyDataItems.get(0);
 
           OkpKeyAgreementKey publicKey = OkpKeyAgreementKey.decode(publicKeyCbor);
-          logger.severe(Base64.getEncoder().encodeToString(publicKey.serialize()));
           return publicKey.getPublicKeyBytes();
         } catch (CborException | CoseException e) {
           throw new IllegalArgumentException("Failed to parse the public key", e);
         }
       }
     }
-    throw new IllegalArgumentException("Certificate payload doesn';'t contain a public key");
+    throw new IllegalArgumentException("Certificate payload doesn't contain a public key");
   }
 }
