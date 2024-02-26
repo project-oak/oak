efcf68a0a1309467e316609df42a8b71c9dd065a
Ivan Petrov <ivanpetrov@google.com>
2024-02-26 19:00:25 +0000

    Fix clang tidy errors for C++ attestation verification (#4841)

--- cc/attestation/verification/utils.cc
+++ cc/attestation/verification/utils.cc
@@ -16,7 +16,6 @@
 
 #include "cc/attestation/verification/utils.h"
 
-#include <chrono>
 #include <string>
 
 #include "absl/status/statusor.h"
@@ -40,13 +39,11 @@ absl::StatusOr<std::string> ExtractPublicKey(absl::string_view certificate) {
   return std::string(public_key.begin(), public_key.end());
 }
 
-absl::StatusOr<std::string> ExtractEncryptionPublicKey(
-    const ::oak::attestation::v1::Evidence& evidence) {
+absl::StatusOr<std::string> ExtractEncryptionPublicKey(const Evidence& evidence) {
   return ExtractPublicKey(evidence.application_keys().encryption_public_key_certificate());
 }
 
-absl::StatusOr<std::string> ExtractSigningPublicKey(
-    const ::oak::attestation::v1::Evidence& evidence) {
+absl::StatusOr<std::string> ExtractSigningPublicKey(const Evidence& evidence) {
   return ExtractPublicKey(evidence.application_keys().signing_public_key_certificate());
 }
 
