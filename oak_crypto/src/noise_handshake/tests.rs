e90c372ddc9da8a9d6185195f02233af9d1898a9
Bill <waywardgeek@google.com>
2024-03-21 15:59:05 -0700

    Rebuilding noise_handshake code to fix broken fork on github. (#4933)

--- oak_crypto/src/noise_handshake/tests.rs
+++ oak_crypto/src/noise_handshake/tests.rs
@@ -15,16 +15,43 @@
 #[cfg(test)]
 use alloc::vec;
 
-use crate::noise_handshake::{client::HandshakeInitiator, respond, P256Scalar};
+use crate::noise_handshake::{client::HandshakeInitiator, respond_nk, respond_nn, P256Scalar};
 
 #[test]
-fn process_handshake() {
+fn process_nk_handshake() {
     let test_messages = vec![vec![1u8, 2u8, 3u8, 4u8], vec![4u8, 3u8, 2u8, 1u8], vec![]];
     let identity_priv = P256Scalar::generate();
     let identity_pub_bytes = identity_priv.compute_public_key();
-    let mut initiator = HandshakeInitiator::new(&identity_pub_bytes);
+    let mut initiator = HandshakeInitiator::new_nk(&identity_pub_bytes);
     let message = initiator.build_initial_message();
-    let handshake_response = respond(identity_priv.bytes().as_slice(), &message).unwrap();
+    let handshake_response = respond_nk(identity_priv.bytes().as_slice(), &message).unwrap();
+    let mut enclave_crypter = handshake_response.crypter;
+
+    let (client_hash, mut client_crypter) =
+        initiator.process_response(&handshake_response.response);
+    assert_eq!(&client_hash, &handshake_response.handshake_hash);
+
+    // Client -> Enclave encrypt+decrypt
+    for message in &test_messages {
+        let ciphertext = client_crypter.encrypt(message).unwrap();
+        let plaintext = enclave_crypter.decrypt(&ciphertext).unwrap();
+        assert_eq!(message, &plaintext);
+    }
+
+    // Enclave -> Client encrypt+decrypt
+    for message in &test_messages {
+        let ciphertext = enclave_crypter.encrypt(message).unwrap();
+        let plaintext = client_crypter.decrypt(&ciphertext).unwrap();
+        assert_eq!(message, &plaintext);
+    }
+}
+
+#[test]
+fn process_nn_handshake() {
+    let test_messages = vec![vec![1u8, 2u8, 3u8, 4u8], vec![4u8, 3u8, 2u8, 1u8], vec![]];
+    let mut initiator = HandshakeInitiator::new_nn();
+    let message = initiator.build_initial_message();
+    let handshake_response = respond_nn(&message).unwrap();
     let mut enclave_crypter = handshake_response.crypter;
 
     let (client_hash, mut client_crypter) =
