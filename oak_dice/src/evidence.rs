fdb24b38f05ddc5b86ae800a7b97a7a932f6c185
Conrad Grobler <grobler@google.com>
2024-03-22 19:09:10 +0000

    Fix sizes

--- oak_dice/src/evidence.rs
+++ oak_dice/src/evidence.rs
@@ -53,8 +53,11 @@ pub const X25519_PRIVATE_KEY_SIZE: usize = 32;
 /// public key.
 pub const PUBLIC_KEY_SIZE: usize = 256;
 
-/// The maximum size of a serialized CWT certificate.
-pub const CERTIFICATE_SIZE: usize = 1536;
+/// The maximum size of a larger serialized CWT certificate.
+pub const LARGE_CERTIFICATE_SIZE: usize = 1536;
+
+/// The maximum size of a standard serialized CWT certificate.
+pub const CERTIFICATE_SIZE: usize = 1024;
 
 /// The name of the kernel command-line parameter that is used to send the
 /// physical address of the Stage 0 DICE data struct.
@@ -139,7 +142,7 @@ pub struct LayerEvidence {
     /// Serialized CWT certificate for the ECA private key owned by the
     /// corresponding layer. The certificate must include measurements of
     /// the layer that owns the private key.
-    pub eca_certificate: [u8; CERTIFICATE_SIZE],
+    pub eca_certificate: [u8; LARGE_CERTIFICATE_SIZE],
 }
 
 impl LayerEvidence {
@@ -150,7 +153,7 @@ impl LayerEvidence {
     }
 }
 
-static_assertions::assert_eq_size!([u8; CERTIFICATE_SIZE], LayerEvidence);
+static_assertions::assert_eq_size!([u8; LARGE_CERTIFICATE_SIZE], LayerEvidence);
 
 /// Private key that can be used by a layer to sign a certificate for the next
 /// layer.
@@ -248,7 +251,7 @@ pub struct Evidence {
     pub application_keys: ApplicationKeys,
 }
 
-static_assertions::assert_eq_size!([u8; 5392], Evidence);
+static_assertions::assert_eq_size!([u8; 5904], Evidence);
 
 /// Wrapper for passing the attestation evidence and private keys from the
 /// Restricted Kernel to the application.
@@ -259,4 +262,4 @@ pub struct RestrictedKernelDiceData {
     pub application_private_keys: ApplicationPrivateKeys,
 }
 
-static_assertions::assert_eq_size!([u8; 5520], RestrictedKernelDiceData);
+static_assertions::assert_eq_size!([u8; 6032], RestrictedKernelDiceData);
