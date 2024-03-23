e6f986d79d5ed0fab7a188913749ac9f566b9b69
Tiziano Santoro <tiziano88@gmail.com>
2024-03-23 00:19:54 +0000

    Make linux kernel build reproducible
    b/330744888

--- flake.nix
+++ flake.nix
@@ -35,6 +35,10 @@
           # Note that building a package via nix is not by itself a guarantee of
           # reproducibility; see https://reproducible.nixos.org.
           linux_kernel = pkgs.linuxManualConfig {
+            # To allow reproducibility, the following options need to be configured:
+            # - CONFIG_MODULE_SIG is not set
+            # - CONFIG_MODULE_SIG_ALL is not set
+            # - CONFIG_DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT is not set
             configfile = ./oak_containers_kernel/configs/6.1.33/minimal.config;
             version = linux_kernel_version;
             src = linux_kernel_src;
