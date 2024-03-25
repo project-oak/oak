a5471950ae3ac993e102bfc094265c9014f887d9
Ernesto Ocampo <ocampoernesto@gmail.com>
2024-03-25 15:53:29 -0300

    Revert "Add micro_rpc method_id option to service protos. (#4948)"
    This reverts commit 62b11da983051ddbfbacc704f6232aeb929bbf68.

--- micro_rpc_tests/build.rs
+++ micro_rpc_tests/build.rs
@@ -18,10 +18,7 @@ fn main() {
     println!("cargo:rerun-if-env-changed=WORKSPACE_ROOT");
     micro_rpc_build::compile(
         &[format!("{}micro_rpc_tests/proto/test_schema.proto", env!("WORKSPACE_ROOT"))],
-        &[
-            format!("{}micro_rpc_tests/proto", env!("WORKSPACE_ROOT")),
-            env!("WORKSPACE_ROOT").to_string(),
-        ],
+        &[format!("{}micro_rpc_tests/proto", env!("WORKSPACE_ROOT"))],
         Default::default(),
     );
 }
