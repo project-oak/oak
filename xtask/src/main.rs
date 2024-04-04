af9ecc6412382e749edb42c43011aab69feddff7
Ernesto Ocampo <ernoc@google.com>
2024-04-04 16:44:57 -0300

    Bazel test all as part of CI (#4988)
    Change-Id: Ic9817fa4ffa21ee57ba150f7c1200b1df8622a25

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -636,26 +636,13 @@ fn run_cargo_clean() -> Step {
 fn run_bazel_build() -> Step {
     Step::Single {
         name: "bazel build".to_string(),
-        command: Cmd::new(
-            "bazel",
-            [
-                "build",
-                "--",
-                "//java/...:all",
-                "//cc/...:all",
-                "//proto/...:all",
-                "//oak_proto_rust",
-            ],
-        ),
+        command: Cmd::new("bazel", ["build", "--", "//...:all"]),
     }
 }
 
 fn run_bazel_test() -> Step {
     Step::Single {
         name: "bazel test".to_string(),
-        command: Cmd::new(
-            "bazel",
-            ["test", "--", "//java/...:all", "//cc/...:all", "//proto/...:all", "//oak_proto_rust"],
-        ),
+        command: Cmd::new("bazel", ["test", "--", "//...:all"]),
     }
 }
