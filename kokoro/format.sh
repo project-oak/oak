ef7bd22491e3f2c462bd2b0c944010ed2c009a2d
Jason LeBrun <jibbl@google.com>
2024-05-24 17:43:03 +0000

    Remove kokoro/format.sh script
    We've removed the corresponding kokoro job.
    
    This is a subset of check_format, with the --fix flags instead of --check
    flags. It doesn't make sense to run this as part of presubmit or CI.
    
    Bug: 341167730
    Change-Id: I492c943bd81c366a19caefb9307ba13e66d19ad0

--- kokoro/format.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck source=./kokoro/common.sh
-source "$(dirname "$0")/common.sh"
-
-./scripts/docker_pull
-./scripts/docker_run nix develop .#ci --command ./scripts/xtask format
