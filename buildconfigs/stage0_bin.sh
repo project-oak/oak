34ce7334a886d6606d643d4a8012c147563c76fb
Tom Binder <tbinder@google.com>
2024-05-29 09:31:33 +0000

    Enable generation of multiple provenances for stage0.
    This is the last step to make the previous workflows (provenance and full_provenance) obsolete. They can be removed after verifying that everything works as desired.
    
    cl/638220281 contains adjustments for the TR import.
    
    Bug: 333745447
    Bug: 339774247
    
    Change-Id: I80ebdcbdd9a3e191277838ba8e91f1684077d8d8

--- buildconfigs/stage0_bin.sh
+++ buildconfigs/stage0_bin.sh
@@ -9,16 +9,18 @@ export BUILD_COMMAND=(
   develop
   .#rust
   --command
-  env
-  --chdir=stage0_bin
-  cargo
-  objcopy
-  --release
-  --
-  --output-target=binary
-  target/x86_64-unknown-none/release/stage0_bin
+  just
+  stage0_provenance_subjects
 )
 
+# The first element must be the Transparent Release binary (the main binary).
 export SUBJECT_PATHS=(
   stage0_bin/target/x86_64-unknown-none/release/stage0_bin
+  stage0_bin/bin/subjects/sha2_384_measurement_of_initial_memory_with_stage0_and_01_vcpu
+  stage0_bin/bin/subjects/sha2_384_measurement_of_initial_memory_with_stage0_and_02_vcpu
+  stage0_bin/bin/subjects/sha2_384_measurement_of_initial_memory_with_stage0_and_04_vcpu
+  stage0_bin/bin/subjects/sha2_384_measurement_of_initial_memory_with_stage0_and_08_vcpu
+  stage0_bin/bin/subjects/sha2_384_measurement_of_initial_memory_with_stage0_and_16_vcpu
+  stage0_bin/bin/subjects/sha2_384_measurement_of_initial_memory_with_stage0_and_32_vcpu
+  stage0_bin/bin/subjects/sha2_384_measurement_of_initial_memory_with_stage0_and_64_vcpu
 )
