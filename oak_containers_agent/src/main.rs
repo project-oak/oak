5cf3c5f5d20c7e8d95fa7999eda28c4098647fb3
msilezin <msilezin@google.com>
2024-06-11 00:44:01 +0000

    expand metrics library and add base metrics
    Change-Id: Ifea81e9a80aea4dff2ad00a614e8fc3bffb5ba55

--- oak_containers_agent/src/main.rs
+++ oak_containers_agent/src/main.rs
@@ -17,9 +17,10 @@
 #![feature(c_size_t)]
 
 use clap::Parser;
+use oak_containers_agent::metrics::{MetricsConfig, OakObserver};
 use opentelemetry::{
     global::set_error_handler,
-    metrics::{AsyncInstrument, Meter, MeterProvider, ObservableCounter, ObservableGauge, Unit},
+    metrics::{AsyncInstrument, Unit},
     KeyValue,
 };
 use procfs::{Current, CurrentSI};
@@ -31,239 +32,248 @@ struct Args {
     launcher_addr: String,
 }
 
-// It's not dead, it's just asynchronous.
-#[allow(dead_code)]
-pub struct SystemMetrics {
-    cpu_seconds_total: ObservableCounter<u64>,
-    context_switches_total: ObservableCounter<u64>,
-    forks_total: ObservableCounter<u64>,
-    procs_blocked: ObservableGauge<u64>,
-    procs_running: ObservableGauge<u64>,
-
-    // Network-related metrics
-    net_recv_bytes: ObservableCounter<u64>,
-    net_recv_packets: ObservableCounter<u64>,
-    net_recv_errs: ObservableCounter<u64>,
-    net_sent_bytes: ObservableCounter<u64>,
-    net_sent_packets: ObservableCounter<u64>,
-    net_sent_errs: ObservableCounter<u64>,
-
-    // Memory-related metrics
-    mem_total: ObservableGauge<u64>,
-    mem_free: ObservableGauge<u64>,
-    mem_available: ObservableGauge<u64>,
-    mem_buffers: ObservableGauge<u64>,
-    mem_cached: ObservableGauge<u64>,
-    mem_slab: ObservableGauge<u64>,
+fn register_system_metrics(oak_observer: &mut OakObserver) -> Result<(), anyhow::Error> {
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_counter("cpu_seconds_total")
+            .with_unit(Unit::new("seconds"))
+            .with_callback(cpu_seconds_total)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_counter("context_switches_total")
+            .with_callback(context_switches_total)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_counter("forks_total")
+            .with_callback(forks_total)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_gauge("procs_blocked")
+            .with_callback(procs_blocked)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_gauge("procs_running")
+            .with_callback(procs_running)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_counter("network_receive_bytes")
+            .with_unit(Unit::new("bytes"))
+            .with_callback(net_recv_bytes)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_counter("network_receive_packets")
+            .with_callback(net_recv_packets)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_counter("network_receive_errors")
+            .with_callback(net_recv_errs)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_counter("network_transmit_bytes")
+            .with_unit(Unit::new("bytes"))
+            .with_callback(net_sent_bytes)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_counter("network_transmit_packets")
+            .with_callback(net_sent_packets)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_counter("network_transmit_errors")
+            .with_callback(net_sent_errs)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer.meter.u64_observable_gauge("mem_total").with_callback(mem_total).try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer.meter.u64_observable_gauge("mem_free").with_callback(mem_free).try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_gauge("mem_available")
+            .with_callback(mem_available)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_gauge("mem_buffers")
+            .with_callback(mem_buffers)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer
+            .meter
+            .u64_observable_gauge("mem_cached")
+            .with_callback(mem_cached)
+            .try_init()?,
+    );
+    oak_observer.register_metric(
+        oak_observer.meter.u64_observable_gauge("mem_slab").with_callback(mem_slab).try_init()?,
+    );
+    Ok(())
 }
 
-#[allow(dead_code)]
-impl SystemMetrics {
-    fn new(meter: Meter) -> Result<Self, anyhow::Error> {
-        Ok(Self {
-            cpu_seconds_total: meter
-                .u64_observable_counter("cpu_seconds_total")
-                .with_unit(Unit::new("seconds"))
-                .with_callback(Self::cpu_seconds_total)
-                .try_init()?,
-            context_switches_total: meter
-                .u64_observable_counter("context_switches_total")
-                .with_callback(Self::context_switches_total)
-                .try_init()?,
-            forks_total: meter
-                .u64_observable_counter("forks_total")
-                .with_callback(Self::forks_total)
-                .try_init()?,
-            procs_blocked: meter
-                .u64_observable_gauge("procs_blocked")
-                .with_callback(Self::procs_blocked)
-                .try_init()?,
-            procs_running: meter
-                .u64_observable_gauge("procs_running")
-                .with_callback(Self::procs_running)
-                .try_init()?,
-            net_recv_bytes: meter
-                .u64_observable_counter("network_receive_bytes")
-                .with_unit(Unit::new("bytes"))
-                .with_callback(Self::net_recv_bytes)
-                .try_init()?,
-            net_recv_packets: meter
-                .u64_observable_counter("network_receive_packets")
-                .with_callback(Self::net_recv_packets)
-                .try_init()?,
-            net_recv_errs: meter
-                .u64_observable_counter("network_receive_errors")
-                .with_callback(Self::net_recv_errs)
-                .try_init()?,
-            net_sent_bytes: meter
-                .u64_observable_counter("network_transmit_bytes")
-                .with_unit(Unit::new("bytes"))
-                .with_callback(Self::net_sent_bytes)
-                .try_init()?,
-            net_sent_packets: meter
-                .u64_observable_counter("network_transmit_packets")
-                .with_callback(Self::net_sent_packets)
-                .try_init()?,
-            net_sent_errs: meter
-                .u64_observable_counter("network_transmit_errors")
-                .with_callback(Self::net_sent_errs)
-                .try_init()?,
-            mem_total: meter
-                .u64_observable_gauge("mem_total")
-                .with_callback(Self::mem_total)
-                .try_init()?,
-            mem_free: meter
-                .u64_observable_gauge("mem_free")
-                .with_callback(Self::mem_free)
-                .try_init()?,
-            mem_available: meter
-                .u64_observable_gauge("mem_available")
-                .with_callback(Self::mem_available)
-                .try_init()?,
-            mem_buffers: meter
-                .u64_observable_gauge("mem_buffers")
-                .with_callback(Self::mem_buffers)
-                .try_init()?,
-            mem_cached: meter
-                .u64_observable_gauge("mem_cached")
-                .with_callback(Self::mem_cached)
-                .try_init()?,
-            mem_slab: meter
-                .u64_observable_gauge("mem_slab")
-                .with_callback(Self::mem_slab)
-                .try_init()?,
-        })
-    }
+fn cpu_seconds_total(counter: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::KernelStats::current().unwrap();
 
-    fn cpu_seconds_total(counter: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::KernelStats::current().unwrap();
+    for (cpu, stats) in stats.cpu_time.iter().enumerate() {
+        let attributes =
+            |mode| [KeyValue::new("cpu", cpu.to_string()), KeyValue::new("mode", mode)];
 
-        for (cpu, stats) in stats.cpu_time.iter().enumerate() {
-            let attributes =
-                |mode| [KeyValue::new("cpu", cpu.to_string()), KeyValue::new("mode", mode)];
-
-            counter.observe(stats.user, &attributes("user"));
-            counter.observe(stats.idle, &attributes("idle"));
-            counter.observe(stats.nice, &attributes("nice"));
-            counter.observe(stats.system, &attributes("system"));
-            if let Some(iowait) = stats.iowait {
-                counter.observe(iowait, &attributes("iowait"));
-            }
-            if let Some(irq) = stats.irq {
-                counter.observe(irq, &attributes("irq"));
-            }
-            if let Some(softirq) = stats.softirq {
-                counter.observe(softirq, &attributes("softirq"));
-            }
-            if let Some(steal) = stats.steal {
-                counter.observe(steal, &attributes("steal"));
-            }
+        counter.observe(stats.user, &attributes("user"));
+        counter.observe(stats.idle, &attributes("idle"));
+        counter.observe(stats.nice, &attributes("nice"));
+        counter.observe(stats.system, &attributes("system"));
+        if let Some(iowait) = stats.iowait {
+            counter.observe(iowait, &attributes("iowait"));
+        }
+        if let Some(irq) = stats.irq {
+            counter.observe(irq, &attributes("irq"));
+        }
+        if let Some(softirq) = stats.softirq {
+            counter.observe(softirq, &attributes("softirq"));
+        }
+        if let Some(steal) = stats.steal {
+            counter.observe(steal, &attributes("steal"));
         }
     }
+}
 
-    fn context_switches_total(counter: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::KernelStats::current().unwrap();
-        counter.observe(stats.ctxt, &[]);
+fn context_switches_total(counter: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::KernelStats::current().unwrap();
+    counter.observe(stats.ctxt, &[]);
+}
+
+fn forks_total(counter: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::KernelStats::current().unwrap();
+    counter.observe(stats.processes, &[]);
+}
+
+fn procs_blocked(gauge: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::KernelStats::current().unwrap();
+    if let Some(procs_blocked) = stats.procs_blocked {
+        gauge.observe(procs_blocked.into(), &[]);
     }
+}
 
-    fn forks_total(counter: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::KernelStats::current().unwrap();
-        counter.observe(stats.processes, &[]);
+fn procs_running(gauge: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::KernelStats::current().unwrap();
+    if let Some(procs_running) = stats.procs_running {
+        gauge.observe(procs_running.into(), &[]);
     }
+}
 
-    fn procs_blocked(gauge: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::KernelStats::current().unwrap();
-        if let Some(procs_blocked) = stats.procs_blocked {
-            gauge.observe(procs_blocked.into(), &[]);
-        }
+fn net_recv_bytes(counter: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::net::dev_status().unwrap();
+
+    for (interface, stats) in stats {
+        counter.observe(stats.recv_bytes, &[KeyValue::new("device", interface)]);
     }
+}
 
-    fn procs_running(gauge: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::KernelStats::current().unwrap();
-        if let Some(procs_running) = stats.procs_running {
-            gauge.observe(procs_running.into(), &[]);
-        }
+fn net_recv_packets(counter: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::net::dev_status().unwrap();
+
+    for (interface, stats) in stats {
+        counter.observe(stats.recv_packets, &[KeyValue::new("device", interface)]);
     }
+}
 
-    fn net_recv_bytes(counter: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::net::dev_status().unwrap();
+fn net_recv_errs(counter: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::net::dev_status().unwrap();
 
-        for (interface, stats) in stats {
-            counter.observe(stats.recv_bytes, &[KeyValue::new("device", interface)]);
-        }
+    for (interface, stats) in stats {
+        counter.observe(stats.recv_errs, &[KeyValue::new("device", interface)]);
     }
+}
 
-    fn net_recv_packets(counter: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::net::dev_status().unwrap();
+fn net_sent_bytes(counter: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::net::dev_status().unwrap();
 
-        for (interface, stats) in stats {
-            counter.observe(stats.recv_packets, &[KeyValue::new("device", interface)]);
-        }
+    for (interface, stats) in stats {
+        counter.observe(stats.sent_bytes, &[KeyValue::new("device", interface)]);
     }
+}
 
-    fn net_recv_errs(counter: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::net::dev_status().unwrap();
+fn net_sent_packets(counter: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::net::dev_status().unwrap();
 
-        for (interface, stats) in stats {
-            counter.observe(stats.recv_errs, &[KeyValue::new("device", interface)]);
-        }
+    for (interface, stats) in stats {
+        counter.observe(stats.sent_packets, &[KeyValue::new("device", interface)]);
     }
+}
 
-    fn net_sent_bytes(counter: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::net::dev_status().unwrap();
+fn net_sent_errs(counter: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::net::dev_status().unwrap();
 
-        for (interface, stats) in stats {
-            counter.observe(stats.sent_bytes, &[KeyValue::new("device", interface)]);
-        }
+    for (interface, stats) in stats {
+        counter.observe(stats.sent_errs, &[KeyValue::new("device", interface)]);
     }
+}
 
-    fn net_sent_packets(counter: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::net::dev_status().unwrap();
+fn mem_total(gauge: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::Meminfo::current().unwrap();
+    gauge.observe(stats.mem_total, &[]);
+}
 
-        for (interface, stats) in stats {
-            counter.observe(stats.sent_packets, &[KeyValue::new("device", interface)]);
-        }
+fn mem_free(gauge: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::Meminfo::current().unwrap();
+    gauge.observe(stats.mem_free, &[]);
+}
+
+fn mem_available(gauge: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::Meminfo::current().unwrap();
+    if let Some(mem_available) = stats.mem_available {
+        gauge.observe(mem_available, &[]);
     }
+}
 
-    fn net_sent_errs(counter: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::net::dev_status().unwrap();
+fn mem_buffers(gauge: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::Meminfo::current().unwrap();
+    gauge.observe(stats.buffers, &[]);
+}
 
-        for (interface, stats) in stats {
-            counter.observe(stats.sent_errs, &[KeyValue::new("device", interface)]);
-        }
-    }
+fn mem_cached(gauge: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::Meminfo::current().unwrap();
+    gauge.observe(stats.cached, &[]);
+}
 
-    fn mem_total(gauge: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::Meminfo::current().unwrap();
-        gauge.observe(stats.mem_total, &[]);
-    }
-
-    fn mem_free(gauge: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::Meminfo::current().unwrap();
-        gauge.observe(stats.mem_free, &[]);
-    }
-
-    fn mem_available(gauge: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::Meminfo::current().unwrap();
-        if let Some(mem_available) = stats.mem_available {
-            gauge.observe(mem_available, &[]);
-        }
-    }
-
-    fn mem_buffers(gauge: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::Meminfo::current().unwrap();
-        gauge.observe(stats.buffers, &[]);
-    }
-
-    fn mem_cached(gauge: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::Meminfo::current().unwrap();
-        gauge.observe(stats.cached, &[]);
-    }
-
-    fn mem_slab(gauge: &dyn AsyncInstrument<u64>) {
-        let stats = procfs::Meminfo::current().unwrap();
-        gauge.observe(stats.slab, &[]);
-    }
+fn mem_slab(gauge: &dyn AsyncInstrument<u64>) {
+    let stats = procfs::Meminfo::current().unwrap();
+    gauge.observe(stats.slab, &[]);
 }
 
 #[tokio::main]
@@ -271,9 +281,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     set_error_handler(|err| eprintln!("oak-agent: OTLP error: {}", err))?;
 
-    let meter_provider = oak_containers_agent::metrics::init_metrics(args.launcher_addr).unwrap();
-    let meter = meter_provider.meter("oak_agent");
-    let _system_metrics = SystemMetrics::new(meter);
+    let metrics_config = MetricsConfig {
+        launcher_addr: args.launcher_addr,
+        scope: String::from("oak_agent"),
+        excluded_metrics: None,
+    };
+
+    let mut oak_observer = oak_containers_agent::metrics::init_metrics(metrics_config);
+    if let Err(e) = register_system_metrics(&mut oak_observer) {
+        eprintln!("Error registering system metrics: {:?}", e);
+    }
 
     // keep alive loop
     loop {
--- oak_containers_agent/src/metrics.rs
+++ oak_containers_agent/src/metrics.rs
@@ -13,17 +13,267 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use opentelemetry::metrics::MetricsError;
+use std::time::Duration;
+
+use opentelemetry::{
+    global,
+    metrics::{
+        Counter, Histogram, Meter, MeterProvider, MetricsError, ObservableCounter, ObservableGauge,
+        ObservableUpDownCounter, UpDownCounter,
+    },
+    KeyValue,
+};
 use opentelemetry_otlp::{ExportConfig, WithExportConfig};
-use opentelemetry_sdk::metrics::SdkMeterProvider;
+use opentelemetry_sdk::{
+    metrics::{
+        reader::{DefaultAggregationSelector, DefaultTemporalitySelector},
+        Aggregation, Instrument, PeriodicReader, SdkMeterProvider, Stream,
+    },
+    runtime,
+};
+use tokio::runtime::Handle;
 
-pub fn init_metrics(launcher_addr: String) -> Result<SdkMeterProvider, MetricsError> {
-    let export_config = ExportConfig { endpoint: launcher_addr, ..ExportConfig::default() };
+const EXPORT_PERIOD: u64 = 60;
 
-    let provider = opentelemetry_otlp::new_pipeline()
-        .metrics(opentelemetry_sdk::runtime::Tokio)
-        .with_exporter(opentelemetry_otlp::new_exporter().tonic().with_export_config(export_config))
-        .build()?;
+pub enum MeterInstrument {
+    U64Counter(Counter<u64>),
+    F64Counter(Counter<f64>),
 
-    Ok(provider)
+    F64UpDownCounter(UpDownCounter<f64>),
+    I64UpDownCounter(UpDownCounter<i64>),
+
+    U64Histogram(Histogram<u64>),
+    F64Histogram(Histogram<f64>),
+
+    U64ObservableCounter(ObservableCounter<u64>),
+    F64ObservableCounter(ObservableCounter<f64>),
+
+    F64ObservableUpDownCounter(ObservableUpDownCounter<f64>),
+    I64ObservableUpDownCounter(ObservableUpDownCounter<i64>),
+
+    U64ObservableGauge(ObservableGauge<u64>),
+    F64ObservableGauge(ObservableGauge<f64>),
+    I64ObservableGauge(ObservableGauge<i64>),
+}
+
+pub struct OakObserver {
+    pub meter: Meter,
+    pub metric_registry: Vec<MeterInstrument>,
+}
+
+impl OakObserver {
+    fn create(
+        launcher_addr: String,
+        scope: String,
+        excluded_metrics: Vec<String>,
+    ) -> Result<Self, MetricsError> {
+        let export_config = ExportConfig { endpoint: launcher_addr, ..ExportConfig::default() };
+
+        let exporter = opentelemetry_otlp::new_exporter()
+            .tonic()
+            .with_export_config(export_config)
+            .build_metrics_exporter(
+                Box::new(DefaultAggregationSelector::new()),
+                Box::new(DefaultTemporalitySelector::new()),
+            )?;
+
+        let reader = PeriodicReader::builder(exporter, runtime::Tokio)
+            .with_interval(Duration::from_secs(EXPORT_PERIOD))
+            .build();
+        let mut provider = SdkMeterProvider::builder().with_reader(reader);
+
+        // drop [base] metrics marked excluded
+        for metric in excluded_metrics {
+            provider = provider.with_view(move |i: &Instrument| {
+                if i.name == metric {
+                    Some(Stream::new().aggregation(Aggregation::Drop))
+                } else {
+                    None
+                }
+            });
+        }
+
+        let provider = provider.build();
+        global::set_meter_provider(provider.clone());
+        let meter = provider.meter(scope);
+
+        Ok(Self { meter, metric_registry: Vec::new() })
+    }
+
+    pub fn register_metric<T: Into<MeterInstrument>>(&mut self, i: T) {
+        self.metric_registry.push(i.into());
+    }
+}
+
+pub struct MetricsConfig {
+    pub launcher_addr: String,
+    pub scope: String,
+    pub excluded_metrics: Option<Vec<String>>,
+}
+
+pub fn init_metrics(metrics_config: MetricsConfig) -> OakObserver {
+    let observer = OakObserver::create(
+        metrics_config.launcher_addr,
+        metrics_config.scope,
+        metrics_config.excluded_metrics.unwrap_or_default(),
+    );
+    let mut observer = observer.unwrap();
+    if let Err(e) = add_base_metrics(&mut observer) {
+        eprintln!("Error adding base metrics: {:?}", e);
+    }
+    observer
+}
+
+fn add_base_metrics(observer: &mut OakObserver) -> Result<(), MetricsError> {
+    observer.register_metric(
+        observer
+            .meter
+            .u64_observable_counter("tokio_workers_count")
+            .with_description("Number of worker threads used by the runtime")
+            .with_callback(|counter| {
+                if let Ok(num_workers) = Handle::current().metrics().num_workers().try_into() {
+                    counter.observe(num_workers, &[]);
+                }
+            })
+            .try_init()?,
+    );
+
+    observer.register_metric(
+        observer
+            .meter
+            .u64_observable_counter("tokio_blocking_threads_count")
+            .with_description("Number of additional threads used by the runtime")
+            .with_callback(|counter| {
+                if let Ok(num_blocking_threads) =
+                    Handle::current().metrics().num_blocking_threads().try_into()
+                {
+                    counter.observe(num_blocking_threads, &[]);
+                }
+            })
+            .try_init()?,
+    );
+    observer.register_metric(
+        observer
+            .meter
+            .u64_observable_counter("tokio_active_tasks")
+            .with_description("Number of active tasks in the runtime")
+            .with_callback(|counter| {
+                if let Ok(active_tasks_count) =
+                    Handle::current().metrics().active_tasks_count().try_into()
+                {
+                    counter.observe(active_tasks_count, &[]);
+                }
+            })
+            .try_init()?,
+    );
+    observer.register_metric(
+        observer
+            .meter
+            .u64_observable_counter("tokio_injection_queue_depth")
+            .with_description("Number of tasks currently in the runtime's injection queue")
+            .with_callback(|counter| {
+                if let Ok(injection_queue_depth) =
+                    Handle::current().metrics().injection_queue_depth().try_into()
+                {
+                    counter.observe(injection_queue_depth, &[]);
+                }
+            })
+            .try_init()?,
+    );
+    observer.register_metric(
+        observer
+            .meter
+            .u64_observable_counter("tokio_worker_local_queue_depth")
+            .with_description("Number of tasks currently scheduled in the workers' local queue")
+            .with_callback(|counter| {
+                let metrics = Handle::current().metrics();
+                for worker in 0..metrics.num_workers() {
+                    if let (Ok(depth), Ok(worker)) =
+                        (metrics.worker_local_queue_depth(worker).try_into(), worker.try_into())
+                    {
+                        counter.observe(depth, &[KeyValue::new::<&str, i64>("worker", worker)])
+                    }
+                }
+            })
+            .try_init()?,
+    );
+    Ok(())
+}
+
+impl From<Counter<u64>> for MeterInstrument {
+    fn from(val: Counter<u64>) -> Self {
+        MeterInstrument::U64Counter(val)
+    }
+}
+
+impl From<Counter<f64>> for MeterInstrument {
+    fn from(val: Counter<f64>) -> Self {
+        MeterInstrument::F64Counter(val)
+    }
+}
+
+impl From<UpDownCounter<f64>> for MeterInstrument {
+    fn from(val: UpDownCounter<f64>) -> Self {
+        MeterInstrument::F64UpDownCounter(val)
+    }
+}
+
+impl From<UpDownCounter<i64>> for MeterInstrument {
+    fn from(val: UpDownCounter<i64>) -> Self {
+        MeterInstrument::I64UpDownCounter(val)
+    }
+}
+
+impl From<Histogram<u64>> for MeterInstrument {
+    fn from(val: Histogram<u64>) -> Self {
+        MeterInstrument::U64Histogram(val)
+    }
+}
+
+impl From<Histogram<f64>> for MeterInstrument {
+    fn from(val: Histogram<f64>) -> Self {
+        MeterInstrument::F64Histogram(val)
+    }
+}
+
+impl From<ObservableCounter<u64>> for MeterInstrument {
+    fn from(val: ObservableCounter<u64>) -> Self {
+        MeterInstrument::U64ObservableCounter(val)
+    }
+}
+
+impl From<ObservableCounter<f64>> for MeterInstrument {
+    fn from(val: ObservableCounter<f64>) -> Self {
+        MeterInstrument::F64ObservableCounter(val)
+    }
+}
+
+impl From<ObservableUpDownCounter<f64>> for MeterInstrument {
+    fn from(val: ObservableUpDownCounter<f64>) -> Self {
+        MeterInstrument::F64ObservableUpDownCounter(val)
+    }
+}
+
+impl From<ObservableUpDownCounter<i64>> for MeterInstrument {
+    fn from(val: ObservableUpDownCounter<i64>) -> Self {
+        MeterInstrument::I64ObservableUpDownCounter(val)
+    }
+}
+
+impl From<ObservableGauge<u64>> for MeterInstrument {
+    fn from(val: ObservableGauge<u64>) -> Self {
+        MeterInstrument::U64ObservableGauge(val)
+    }
+}
+
+impl From<ObservableGauge<f64>> for MeterInstrument {
+    fn from(val: ObservableGauge<f64>) -> Self {
+        MeterInstrument::F64ObservableGauge(val)
+    }
+}
+
+impl From<ObservableGauge<i64>> for MeterInstrument {
+    fn from(val: ObservableGauge<i64>) -> Self {
+        MeterInstrument::I64ObservableGauge(val)
+    }
 }
