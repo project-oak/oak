3713b3c071984362f532eb97d2e7c558bcafac86
Jason LeBrun <jibbl@google.com>
2024-07-11 20:36:44 +0000

    Update nixpkgs (and so, just) and add just timestamps
    Change-Id: I776c4e9b4daa52bb90cdb21375e42540f8d28960

--- kokoro/common.sh
+++ kokoro/common.sh
@@ -9,6 +9,8 @@ export CI=kokoro
 export RUST_BACKTRACE=1
 export RUST_LOG=debug
 export XDG_RUNTIME_DIR=/var/run
+export JUST_TIMESTAMP=true
+export JUST_TIMESTAMP_FORMAT='JUST:%H:%M:%S%.3f'
 
 # Make sure we're in the root of the repository.
 cd "$(dirname "$0")/.."
