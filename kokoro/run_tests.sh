136358d057216b2ba2f3c16204ec8ba9beabc339
Ernesto Ocampo <ernoc@google.com>
2024-07-24 13:42:14 +0000

    Stop CI running tests with Cargo when they're already run with Bazel.
    Also:
    - Add missing unit test targets
    - Rename `just kokoro_run_tests` to `just kokoro_run_cargo_tests` for clarity.
    - Remove some commented out code.
    
    Change-Id: I3f0482d947d82bf29b8aa3655204cb6faf3ced5a

--- kokoro/run_tests.sh
+++ kokoro/run_tests.sh
@@ -4,7 +4,7 @@
 source "$(dirname "$0")/common.sh"
 
 ./scripts/docker_pull
-./scripts/docker_run nix develop .#default --command just kokoro_run_tests
+./scripts/docker_run nix develop .#default --command just kokoro_run_cargo_tests
 ./scripts/git_check_diff
 
 mkdir -p "${KOKORO_ARTIFACTS_DIR}/test_logs/"
