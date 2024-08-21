aa1840dab73b0ee4f8c3750982c85938e3f7c4c1
Tom Binder <tbinder@google.com>
2024-08-21 12:25:58 +0000

    Fix GitHub build of Oak restricted kernel.
    Bug: 361272078
    
    Change-Id: Ib12b1daeb467409ae178ca73e9e85d6868ac5d05

--- buildconfigs/oak_restricted_kernel_simple_io_init_rd_wrapper_bin.sh
+++ buildconfigs/oak_restricted_kernel_simple_io_init_rd_wrapper_bin.sh
@@ -2,6 +2,9 @@
 #
 # Build configuration for oak_restricted_kernel_simple_io_init_rd_wrapper_bin.
 #
+# Verify changes to this file by running:
+#   scripts/test_buildconfigs \
+#   buildconfigs/oak_restricted_kernel_simple_io_init_rd_wrapper_bin.sh
 export PACKAGE_NAME=oak_restricted_kernel_simple_io_init_rd_wrapper_bin
 
 export BUILD_COMMAND=(
@@ -16,6 +19,6 @@ export BUILD_COMMAND=(
 # The first element must be the Transparent Release binary (the main binary).
 export SUBJECT_PATHS=(
   oak_restricted_kernel_wrapper/bin/wrapper_bzimage_simple_io_channel
-  oak_restricted_kernel_wrapper/bin/wrapper_simple_io_channel_subjects/oak_restricted_kernel_simple_io_channel_image
-  oak_restricted_kernel_wrapper/bin/wrapper_simple_io_channel_subjects/oak_restricted_kernel_simple_io_channel_setup_data
+  oak_restricted_kernel_wrapper/bin/wrapper_simple_io_channel_subjects/oak_restricted_kernel_wrapper_simple_io_channel_measurement_image
+  oak_restricted_kernel_wrapper/bin/wrapper_simple_io_channel_subjects/oak_restricted_kernel_wrapper_simple_io_channel_measurement_setup_data
 )
