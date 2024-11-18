72f0d627aed1930f324e507020d4bfd8c64b3aca
Ernesto Ocampo <ernoc@google.com>
2024-11-18 17:39:39 +0000

    Bazelify micro_rpc import test.
    BUG: 330178457
    Change-Id: Id42a46e91a27b6d27b7dbb5b4072c0a30592dcf4

---
+++ bazel/test_workspace/test_import_microrpc_protos/README.md
@@ -0,0 +1,4 @@
+# Micro RPC Workspace Test
+
+This directory contains a test to check whether the `micro_rpc` generated code
+crate can be depended on from another workspace.
--- bazel/test_workspace/test_import_microrpc_protos/build.rs
+++ bazel/test_workspace/test_import_microrpc_protos/build.rs
@@ -18,8 +18,11 @@ use oak_grpc_utils::{generate_grpc_code, CodegenOptions, ExternPath};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     generate_grpc_code(
-        &["test.proto", "stubs.proto"],
-        &["proto"],
+        &[
+            "../test_import_microrpc_protos/proto/test.proto",
+            "../test_import_microrpc_protos/proto/stubs.proto",
+        ],
+        &[".."],
         CodegenOptions {
             extern_paths: vec![ExternPath::new(
                 ".oak.crypto.v1",
