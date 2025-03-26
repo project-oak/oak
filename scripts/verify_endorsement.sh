3ae8ff1ab5a46f9bcb5e3179529e0f1d82bbd03a
Tom Binder <tbinder@google.com>
2025-03-26 12:26:52 +0000

    Add list mode to verify_endorsement script. Change the existing verify mode to work by endorsement hash.
    The list mode parses the endorsements and prints information of interest: subject name, subject hash (which is the same as the binary hash), and the number of remaining days the endorsement is valid. So the caller gets a bit of support to select the desired endorsement.
    
    Verify mode now requires the hash of an endorsement (and not the hash of the binary, as before). It downloads the matching verifying key from discoverability storage. Removed the hard-wired dependency on the oak development key.
    
    Bug: 403524037
    Change-Id: If4643a65113f3dd9f36de35e07a0504739dfc977

--- scripts/verify_endorsement.sh
+++ scripts/verify_endorsement.sh
@@ -15,21 +15,17 @@
 # limitations under the License.
 #
 
-# Reads a binary package from discoverability storage and runs endorsement
-# verification on the package.
+# Lists and retrieves package metdata from discoverability storage and runs
+# endorsement verification on selected packages. This operates in terms of
+# endorsements only - actual binaries are not involved, but can still be
+# downloaded from the files bucket.
 #
-# Example:
-#
-# scripts/verify_endorsement.sh \
-#    fe25525afee52590d82b953743dd5a25c3439322f018c8a4966a2d3220fe66ef
+# Requires: curl, python3, sha256sum
 set -e
 set -o pipefail
 
 readonly FILES_PREFIX=https://storage.googleapis.com/oak-files
 readonly INDEX_PREFIX=https://storage.googleapis.com/oak-index
-
-# ID of index to retrieve endorsement hashes by binary hashes.
-readonly ENDORSEMENT_FOR_BINARY=13
 
 # ID of index to retrieve signature hashes by endorsement hashes.
 readonly SIGNATURE_FOR_ENDORSEMENT=14
@@ -37,8 +33,21 @@ readonly SIGNATURE_FOR_ENDORSEMENT=14
 # ID of index to retrieve log entry hashes by endorsement hashes.
 readonly LOGENTRY_FOR_ENDORSEMENT=15
 
+# ID of index to retrieve the public key hash from the signature hash.
+readonly PK_FOR_SIGNATURE=16
+
+# ID of index to retrieve endorsement hashes by public key hashes.
+readonly ENDORSEMENTS_FOR_PK=21
+
+readonly NANOS_PER_DAY=$((24 * 3600 * 1000 * 1000 * 1000))
+
 usage_and_exit() {
-  >&2 echo "Usage: $0 <binary_hash>"
+  >&2 echo "Usage:"
+  >&2 echo "  $0 --key_file $HOME/public_key.pem"
+  >&2 echo "  $0 --endorsement_hash sha2-256:0cb3e0457eae486ecbc42e553f1ec8327691ae836692e080a1aa191d29b2a121"
+  >&2 echo "These modes are mutually exclusive. The first mode lists endorsement"
+  >&2 echo "hashes that can go as input to the second mode."
+
   exit 1
 }
 
@@ -54,21 +63,56 @@ fetch_file() {
   fi
 }
 
+readonly JSON_PARSER="import sys, json
+e = json.load(sys.stdin)
+print(
+  e['subject'][0]['name'],
+  e['subject'][0]['digest']['sha256'],
+  e['predicate']['validity']['notAfter'])"
+
+timestamp() {
+  date "+%s%N" --date="$1"
+}
+
+now_timestamp() {
+  date "+%s%N"
+}
+
+list_all_endorsements() {
+  local keyfile="$1"
+  local pk_hash="sha2-256:$(sha256sum "${keyfile}" | cut -d " " -f 1)"
+  local endorsement_hashes=$(curl --fail --silent \
+    "${INDEX_PREFIX}/${ENDORSEMENTS_FOR_PK}/${pk_hash}")
+  local now=$(now_timestamp)
+  local tmp_path=$(mktemp)
+  for endorsement_hash in ${endorsement_hashes}; do
+    echo "${endorsement_hash}"
+    fetch_file "${endorsement_hash}" "${tmp_path}"
+    local endorsement=$(cat "${tmp_path}")
+    read -r subject_name subject_hash not_after <<< \
+        "$(echo "${endorsement}" | python3 -c "${JSON_PARSER}")"
+    local days=$(( ($(timestamp "${not_after}") - now) / "${NANOS_PER_DAY}" ))
+    echo "    Subject name:   ${subject_name}"
+    echo "    Subject hash:   sha2-256:${subject_hash}"
+    echo "    Days remaining: ${days}"
+  done
+}
+
 download() {
-  local binary_hash="$1"
+  local endorsement_hash="$1"
   local dir="$2"
 
-  endorsement_hash=$(curl --fail --silent \
-      "${INDEX_PREFIX}/${ENDORSEMENT_FOR_BINARY}/${binary_hash}" | tail -1)
   signature_hash=$(curl --fail --silent \
       "${INDEX_PREFIX}/${SIGNATURE_FOR_ENDORSEMENT}/${endorsement_hash}")
+  pk_hash=$(curl --fail --silent \
+      "${INDEX_PREFIX}/${PK_FOR_SIGNATURE}/${signature_hash}")
   # The log entry may not exist, in which case we set it to empty.
   logentry_hash=$(curl --fail --silent \
       "${INDEX_PREFIX}/${LOGENTRY_FOR_ENDORSEMENT}/${endorsement_hash}" || echo "")
 
-  fetch_file "${binary_hash}" "${dir}/binary"
   fetch_file "${endorsement_hash}" "${dir}/endorsement.json"
   fetch_file "${signature_hash}" "${dir}/endorsement.json.sig"
+  fetch_file "${pk_hash}" "${dir}/endorser_public_key.pem"
   if [[ -n "${logentry_hash}" ]]; then
     fetch_file "${logentry_hash}" "${dir}/logentry.json"
   fi
@@ -79,7 +123,7 @@ verify() {
   local opts=(
     --endorsement="${dir}/endorsement.json"
     --signature="${dir}/endorsement.json.sig"
-    --endorser-public-key=oak_attestation_verification/testdata/endorser_public_key.pem
+    --endorser-public-key="${dir}/endorser_public_key.pem"
   )
   if [[ -f "${dir}/logentry.json" ]]; then
     opts+=(
@@ -90,12 +134,33 @@ verify() {
   bazel run //oak_attestation_verification:verify_endorsement -- "${opts[@]}"
 }
 
-if [[ $# != 1 ]]; then
-  usage_and_exit
-fi
+download_verify() {
+  local endorsement_hash="$1"
 
-dir=$(mktemp -d)
->&2 echo "ls -la ${dir}"
-download "sha2-256:$1" "${dir}"
-ls -la "${dir}" 1>&2
-verify "${dir}"
+  local dir=$(mktemp -d)
+  >&2 echo "ls -la ${dir}"
+  download "${endorsement_hash}" "${dir}"
+  ls -la "${dir}" 1>&2
+  verify "${dir}"
+}
+
+trap usage_and_exit ERR # When shift fails
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --key_file)
+      shift
+      keyfile="$1"
+      shift
+      list_all_endorsements "${keyfile}"
+      ;;
+    --endorsement_hash)
+      shift
+      endorsement_hash="$1"
+      shift
+      download_verify "${endorsement_hash}"
+      ;;
+    *)
+      usage_and_exit
+      ;;
+  esac
+done
