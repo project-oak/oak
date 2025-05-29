c34b791f330587e855230408b5627de4f7b76b4a
Jason LeBrun <jibbl@google.com>
2025-05-29 03:14:26 +0000

    Add a file that got missed in go/oak-cr/20705
    The presubmit didn't break beacuse only GitHub CI will fail due to the
    missing file.
    
    Change-Id: I3ddcfb66d6171f504606da89fc2ae025e4934eae

---
+++ buildconfigs/oak_restricted_kernel_simple_io_channel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Build configuration for oak_restricted_kernel_simple_io_init_rd_wrapper_bin.
+#
+# Verify changes to this file by running:
+#   scripts/test_buildconfigs \
+#   buildconfigs/oak_restricted_kernel_simple_io_init_rd_wrapper_bin.sh
+export PACKAGE_NAME=oak_restricted_kernel_simple_io_init_rd_wrapper_bin
+
+export BUILD_COMMAND=(
+  nix
+  develop
+  .#default
+  --command
+  just
+  github-oak_restricted_kernel_wrapper_simple_io_channel
+)
+
+# The first element must be the Transparent Release binary (the main binary).
+export SUBJECT_PATHS=(
+  artifacts/binaries/oak_restricted_kernel_wrapper_simple_io_channel_bin
+  artifacts/binaries/oak_restricted_kernel_wrapper_simple_io_channel_measurement_image
+  artifacts/binaries/oak_restricted_kernel_wrapper_simple_io_channel_measurement_setup_data
+)
