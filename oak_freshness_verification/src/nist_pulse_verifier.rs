3eefb4bf175da24e660288a6950e06b89f3a7de8
Patrick McGrath <mcgrathp@google.com>
2025-07-30 14:14:49 +0000

    Introduce new crate for freshness verification.
    This introduces a new crate that relies on std to verify the
    `ProofOfFreshness` proto. Currently, it only verifies that the
    outputValues (between the Nist website and the proto itself) line up.
    
    A new crate is introduced here because this must be linked into the
    oak_crypto crate, which is `no-std`.
    
    Bug: 424736845
    
    Change-Id: Ife08f17f3ea068e665abc4b4663d2515490791cb

---
+++ oak_freshness_verification/src/nist_pulse_verifier.rs
@@ -0,0 +1,168 @@
+//
+// Copyright 2025 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+use alloc::{boxed::Box, format, string::String};
+use core::error::Error;
+
+use anyhow::Context;
+use hex::FromHexError;
+#[cfg(test)]
+use mockall::automock;
+use oak_proto_rust::oak::crypto::v1::ProofOfFreshness;
+use serde::{Deserialize, Serialize};
+
+const NIST_WEB_PREFIX: &str = "https://beacon.nist.gov";
+// TODO: b/424736845 - Add support for version 2.1
+const NIST_VERSION: &str = "2.0";
+
+#[derive(thiserror::Error, Debug)]
+pub enum ProofOfFreshnessVerificationError {
+    #[error("OutputValue not found; expected: {expected}, actual: {actual}")]
+    OutputValueNotFound { expected: String, actual: String },
+    #[error("Invalid hexadecimal: {0}")]
+    InvalidHexadecimal(#[from] FromHexError),
+    #[error("API call failed: {0}")]
+    ApiCallFailed(#[from] Box<dyn Error>),
+    #[error("JSON parsing failed: {0}")]
+    JsonParsingFailed(#[from] serde_json::Error),
+}
+
+/// Represents a single value within the `listValues` array.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ListValue {
+    pub uri: String,
+    #[serde(rename = "type")] // Use rename since "type" is a Rust keyword
+    pub value_type: String,
+    pub value: String,
+}
+
+/// Represents the `external` object within the `pulse` data.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct External {
+    #[serde(rename = "sourceId")]
+    pub source_id: String,
+    #[serde(rename = "statusCode")]
+    pub status_code: i64,
+    pub value: String,
+}
+
+/// Represents the main `pulse` object.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Pulse {
+    pub uri: String,
+    pub version: String,
+    #[serde(rename = "cipherSuite")]
+    pub cipher_suite: i64,
+    pub period: i64,
+    #[serde(rename = "certificateId")]
+    pub certificate_id: String,
+    #[serde(rename = "chainIndex")]
+    pub chain_index: i64,
+    #[serde(rename = "pulseIndex")]
+    pub pulse_index: i64,
+    #[serde(rename = "timeStamp")]
+    pub time_stamp: String,
+    #[serde(rename = "localRandomValue")]
+    pub local_random_value: String,
+    pub external: External,
+    #[serde(rename = "listValues")]
+    pub list_values: Vec<ListValue>,
+    #[serde(rename = "precommitmentValue")]
+    pub precommitment_value: String,
+    #[serde(rename = "statusCode")]
+    pub status_code: i64,
+    #[serde(rename = "signatureValue")]
+    pub signature_value: String,
+    #[serde(rename = "outputValue")]
+    pub output_value: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct PulseResponse {
+    pulse: Pulse,
+}
+
+// Simple API get request trait that can be mocked in tests.
+#[cfg_attr(test, automock)]
+pub trait ApiGetter {
+    fn get(&self, url: &str) -> Result<String, Box<dyn Error>>;
+}
+
+// Fetches the contents of a URL using the `ureq` crate.
+pub struct UreqGetter {}
+
+impl ApiGetter for UreqGetter {
+    fn get(&self, url: &str) -> Result<String, Box<dyn Error>> {
+        let res: ureq::Response =
+            ureq::get(url).call().with_context(|| format!("fetching URL {url}"))?;
+        let body_string =
+            res.into_string().with_context(|| format!("reading response body from URL {url}"))?;
+        Ok(body_string)
+    }
+}
+
+// Formats the expected nist pulse URI from the ProofOfFreshness proto.
+fn create_nist_pulse_uri(proof_of_freshness: &ProofOfFreshness) -> String {
+    format!(
+        "{}/beacon/{}/chain/{}/pulse/{}",
+        NIST_WEB_PREFIX,
+        NIST_VERSION,
+        proof_of_freshness.nist_chain_index,
+        proof_of_freshness.nist_pulse_index
+    )
+}
+
+// Ensures that the ProofOfFreshness output value is equal to the passed in
+// output_value.
+fn verify_output_value(
+    proof_of_freshness: &ProofOfFreshness,
+    output_value: &str,
+) -> Result<(), ProofOfFreshnessVerificationError> {
+    let output_value_bytes: Vec<u8> = hex::decode(output_value)?;
+    match output_value_bytes == proof_of_freshness.nist_pulse_output_value {
+        true => Ok(()),
+        false => Err(ProofOfFreshnessVerificationError::OutputValueNotFound {
+            expected: hex::encode(proof_of_freshness.nist_pulse_output_value.clone())
+                .to_ascii_uppercase(),
+            actual: output_value.to_string().to_ascii_uppercase(),
+        }),
+    }
+}
+
+// Verifies that a ProofOfFreshness entry is based on NIST's Interoperable
+// Randomness Beacon project: https://csrc.nist.gov/projects/interoperable-randomness-beacons/beacon-20.
+pub struct NistPulseVerifier {
+    data_fetcher: Box<dyn ApiGetter>,
+}
+
+impl NistPulseVerifier {
+    pub fn new(data_fetcher: Box<dyn ApiGetter>) -> Self {
+        Self { data_fetcher }
+    }
+
+    // TODO: b/424736845 - Verify signature of pulse and CA certificate.
+    pub fn verify(
+        &self,
+        proof_of_freshness: ProofOfFreshness,
+    ) -> Result<(), ProofOfFreshnessVerificationError> {
+        let nist_pulse_uri = create_nist_pulse_uri(&proof_of_freshness);
+        let result_str = self.data_fetcher.get(&nist_pulse_uri)?;
+        let pulse_response: PulseResponse = serde_json::from_str(&result_str)?;
+
+        verify_output_value(&proof_of_freshness, &pulse_response.pulse.output_value)?;
+        Ok(())
+    }
+}
---
+++ oak_freshness_verification/src/tests.rs
@@ -0,0 +1,156 @@
+//
+// Copyright 2025 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+use alloc::string::ToString;
+use core::assert_matches::assert_matches;
+
+use mockall::*;
+use nist_pulse_verifier::__mock_MockApiGetter_ApiGetter::__get::Expectation;
+use oak_proto_rust::oak::crypto::v1::ProofOfFreshness;
+
+use crate::nist_pulse_verifier::{
+    self, MockApiGetter, NistPulseVerifier, ProofOfFreshnessVerificationError,
+};
+
+const MOCK_JSON_RESPONSE_STRING: &str = r#"{
+        "pulse": {
+            "uri": "https://beacon.nist.gov/beacon/2.0/chain/2/pulse/1352280",
+            "version": "Version 2.0",
+            "cipherSuite": 0,
+            "period": 60000,
+            "certificateId": "some-cert-id",
+            "chainIndex": 2,
+            "pulseIndex": 1352280,
+            "timeStamp": "2025-07-30T00:00:00.000Z",
+            "localRandomValue": "some-random-value",
+            "external": {
+                "sourceId": "some-source-id",
+                "statusCode": 0,
+                "value": "some-value"
+            },
+            "listValues": [],
+            "precommitmentValue": "some-precommitment-value",
+            "statusCode": 0,
+            "signatureValue": "some-signature",
+            "outputValue": "B097474508DA9EEFEA3DC10F882F9262C6F2D3D9F428FCF981BB67271DA57606226E1E138EDD481712F6DBAF6BCA1B3E0E55FB3F2011EC4FFFBAD5A5635E722E"
+        }
+    }"#;
+
+// Modifies the mock api getter to expect a single get call to `https://beacon.nist.gov/beacon/2.0/chain/2/pulse/1352280`.
+fn expect_get_once(mock_getter: &mut MockApiGetter) -> &mut Expectation {
+    mock_getter
+        .expect_get()
+        .with(predicate::eq("https://beacon.nist.gov/beacon/2.0/chain/2/pulse/1352280"))
+        .once()
+}
+
+#[test]
+fn verify_nist_pulse_successful() {
+    let mut mock_getter = MockApiGetter::new();
+    expect_get_once(&mut mock_getter).returning(move |_| Ok(MOCK_JSON_RESPONSE_STRING.to_string()));
+
+    let verifier = NistPulseVerifier::new(Box::new(mock_getter));
+    let nist_pulse = ProofOfFreshness {
+        nist_chain_index: 2,
+        nist_pulse_index: 1352280,
+        nist_pulse_output_value: hex::decode("B097474508DA9EEFEA3DC10F882F9262C6F2D3D9F428FCF981BB67271DA57606226E1E138EDD481712F6DBAF6BCA1B3E0E55FB3F2011EC4FFFBAD5A5635E722E")
+            .unwrap(),
+    };
+    assert_matches!(verifier.verify(nist_pulse), Ok(()));
+}
+
+#[test]
+fn verify_nist_pulse_mismatch_output_value() {
+    let mut mock_getter = MockApiGetter::new();
+    expect_get_once(&mut mock_getter).returning(move |_| Ok(MOCK_JSON_RESPONSE_STRING.to_string()));
+
+    let verifier = NistPulseVerifier::new(Box::new(mock_getter));
+    let incorrect_pulse_output_value = "7435D447C495ECB94CDF749F87EFFC758FEFF18020E4DBACC5D5BB5A6D0AD8338D049A04B3B51B9D5CE9E9E7454427210AB3252D5AF38142B5A374E6BDC8E616";
+    let invalid_nist_pulse = ProofOfFreshness {
+        nist_chain_index: 2,
+        nist_pulse_index: 1352280,
+        nist_pulse_output_value: hex::decode(incorrect_pulse_output_value).unwrap(),
+    };
+
+    let result = verifier.verify(invalid_nist_pulse);
+    assert_matches!(result, Err(ProofOfFreshnessVerificationError::OutputValueNotFound { .. }));
+
+    // Ensure OutputValueNotFound error is structured as expected. Since
+    // ProofOfFreshnessVerificationError does not derive PartialEq (this stems from
+    // the arguement errors such as serde_json::Error not deriving PartialEq), we
+    // compare the strings.
+    let actual_pulse_output_value = "B097474508DA9EEFEA3DC10F882F9262C6F2D3D9F428FCF981BB67271DA57606226E1E138EDD481712F6DBAF6BCA1B3E0E55FB3F2011EC4FFFBAD5A5635E722E";
+    let expected_error = ProofOfFreshnessVerificationError::OutputValueNotFound {
+        expected: incorrect_pulse_output_value.to_string(),
+        actual: actual_pulse_output_value.to_string(),
+    };
+    assert_eq!(result.unwrap_err().to_string(), expected_error.to_string());
+}
+
+#[test]
+fn verify_nist_pulse_invalid_hexadecimal() {
+    let mut mock_getter = MockApiGetter::new();
+    let invalid_hex_response = MOCK_JSON_RESPONSE_STRING.replace(
+        "B097474508DA9EEFEA3DC10F882F9262C6F2D3D9F428FCF981BB67271DA57606226E1E138EDD481712F6DBAF6BCA1B3E0E55FB3F2011EC4FFFBAD5A5635E722E",
+        "invalid-hex",
+    );
+    expect_get_once(&mut mock_getter).returning(move |_| Ok(invalid_hex_response.clone()));
+
+    let verifier = NistPulseVerifier::new(Box::new(mock_getter));
+    let nist_pulse_with_invalid_hex = ProofOfFreshness {
+        nist_chain_index: 2,
+        nist_pulse_index: 1352280,
+        nist_pulse_output_value: b"some-output-value".to_vec(),
+    };
+    assert_matches!(
+        verifier.verify(nist_pulse_with_invalid_hex),
+        Err(ProofOfFreshnessVerificationError::InvalidHexadecimal(_))
+    );
+}
+
+#[test]
+fn verify_nist_pulse_api_call_failed() {
+    let mut mock_getter = MockApiGetter::new();
+    expect_get_once(&mut mock_getter).returning(|_| Err("API call failed".into()));
+
+    let verifier = NistPulseVerifier::new(Box::new(mock_getter));
+    let nist_pulse = ProofOfFreshness {
+        nist_chain_index: 2,
+        nist_pulse_index: 1352280,
+        nist_pulse_output_value: b"some-output-value".to_vec(),
+    };
+    assert_matches!(
+        verifier.verify(nist_pulse),
+        Err(ProofOfFreshnessVerificationError::ApiCallFailed(_))
+    );
+}
+
+#[test]
+fn verify_nist_pulse_json_parsing_failed() {
+    let mut mock_getter = MockApiGetter::new();
+    expect_get_once(&mut mock_getter).returning(|_| Ok("invalid-json".to_string()));
+
+    let verifier = NistPulseVerifier::new(Box::new(mock_getter));
+    let nist_pulse = ProofOfFreshness {
+        nist_chain_index: 2,
+        nist_pulse_index: 1352280,
+        nist_pulse_output_value: b"some-output-value".to_vec(),
+    };
+    assert_matches!(
+        verifier.verify(nist_pulse),
+        Err(ProofOfFreshnessVerificationError::JsonParsingFailed(_))
+    );
+}
