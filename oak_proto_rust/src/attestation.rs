24d6f2071e9999e50d6ec23b34337ea01ef94619
andrisaar <andrisaar@google.com>
2025-11-05 16:14:15 +0000

    Create `CStr` versions of the attestation type constants
    This makes them usable across C FFI boundaries, as Rust strings and C
    strings are implemented differently.
    
    Ideally I'd leave just the `CStr` constants around, but as always,
    migration is hard so at least temporarily I've also left code in that
    translates the `CStr`-s into Rust `str`-s.
    
    Unfortunately this does mean I have to touch unsafe code, as you can't
    unwrap a `Result` in a const context; thus, we have to rely on
    `from_utf8_unchecked` -- but the constants _are_ valid UTF-8, so this is
    not a safety issue.
    
    Change-Id: I71c9e9b96afa4aa78ed96a505d2c91406a6a6964

--- oak_proto_rust/src/attestation.rs
+++ oak_proto_rust/src/attestation.rs
@@ -14,15 +14,26 @@
 // limitations under the License.
 
 // Random IDs for commonly used attestation types.
+use core::ffi::CStr;
 
 /// Attestation type where the session binding key is signed by a key known to
 /// the client.
+pub const CERTIFICATE_BASED_ATTESTATION_ID_CSTR: &CStr = c"95553023-358f-4f8c-b75c-e6e185cc05ca";
+
+// Safety: the UUID is valid UTF-8, and we can't use `Result` in a const
+// context.
 #[allow(dead_code)]
-pub const CERTIFICATE_BASED_ATTESTATION_ID: &str = "95553023-358f-4f8c-b75c-e6e185cc05ca";
+pub const CERTIFICATE_BASED_ATTESTATION_ID: &str =
+    unsafe { str::from_utf8_unchecked(CERTIFICATE_BASED_ATTESTATION_ID_CSTR.to_bytes()) };
 
 /// Attestation type where the session binding key is included in a JWT which
 /// itself is signed by a certificate chain whose root is GCP Confidential
 /// Space's root certificate (see
 /// https://cloud.google.com/confidential-computing/confidential-space/docs/reference/token-claims).
+pub const CONFIDENTIAL_SPACE_ATTESTATION_ID_CSTR: &CStr = c"d4d3e98d-334f-4f6d-9e84-d0291f9dbcdd";
+
+// Safety: the UUID is valid UTF-8, and we can't use `Result` in a const
+// context.
 #[allow(dead_code)]
-pub const CONFIDENTIAL_SPACE_ATTESTATION_ID: &str = "d4d3e98d-334f-4f6d-9e84-d0291f9dbcdd";
+pub const CONFIDENTIAL_SPACE_ATTESTATION_ID: &str =
+    unsafe { str::from_utf8_unchecked(CONFIDENTIAL_SPACE_ATTESTATION_ID_CSTR.to_bytes()) };
