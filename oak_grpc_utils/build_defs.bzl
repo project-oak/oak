8d3f991861fcde0edb4f0c30dc7d278adfcb2eeb
Ben Birt <benbirt@google.com>
2025-11-18 13:37:12 +0000

    Implement a `rust_tonic_grpc_service` Bazel BUILD rule. Use it to simplify generation of tonic gRPC stubs for the Oak Containers Helloworld example.
    Change-Id: Iaf93a1dc7e71c3b90480ad21cc48e5587f666693

---
+++ oak_grpc_utils/build_defs.bzl
@@ -0,0 +1,102 @@
+# Copyright 2025 The Project Oak Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Bazel build definitions for tonic gRPC."""
+
+load("@com_google_protobuf//bazel/common:proto_info.bzl", "ProtoInfo")
+load("@rules_rust//rust:defs.bzl", "rust_library")
+
+def _generate_service_lib_impl(ctx):
+    if len(ctx.attr.srcs) != 1:
+        fail("The 'srcs' attribute must contain exactly one proto_library target.")
+
+    main_target = ctx.attr.srcs[0]
+    proto_info = main_target[ProtoInfo]
+
+    output_file = ctx.actions.declare_file(ctx.attr.out_filename)
+    outputs_dir = ctx.actions.declare_directory(ctx.attr.name + ".out_dir")
+
+    args = ctx.actions.args()
+    args.add_joined("--files-to-compile", proto_info.check_deps_sources, join_with = ",")
+    args.add_joined("--proto-source-roots", proto_info.transitive_proto_path, join_with = ",")
+    args.add("--out-filename", output_file)
+    args.add("--out-dir", outputs_dir.path)
+    args.add("--extern-paths", json.encode(ctx.attr.extern_paths))
+
+    ctx.actions.run(
+        executable = ctx.executable._generator,
+        inputs = proto_info.transitive_sources,
+        outputs = [outputs_dir, output_file],
+        arguments = [args],
+        env = {
+            "PROTOC": ctx.file._protoc.path,
+            "OUT_DIR": outputs_dir.path,
+        },
+        tools = [ctx.file._protoc],
+    )
+
+    return [DefaultInfo(files = depset([outputs_dir, output_file]))]
+
+_generate_service_lib = rule(
+    implementation = _generate_service_lib_impl,
+    attrs = {
+        "srcs": attr.label_list(providers = [ProtoInfo], mandatory = True, allow_files = False),
+        "out_filename": attr.string(mandatory = True),
+        "extern_paths": attr.string_dict(mandatory = True),
+        "_generator": attr.label(
+            default = Label("//oak_grpc_utils:generator"),
+            executable = True,
+            cfg = "exec",
+        ),
+        "_protoc": attr.label(
+            default = Label("@com_google_protobuf//:protoc"),
+            allow_single_file = True,
+            cfg = "exec",
+        ),
+    },
+)
+
+def rust_tonic_grpc_service(
+        name,
+        srcs = [],
+        deps = [],
+        extern_paths = {},
+        **kwargs):
+    """A macro that generates a Rust tonic gRPC service library.
+
+    Args:
+        name: The name of the tonic gRPC service library.
+        srcs: Proto files to generate Rust tonic gRPC service code from.
+        deps: Dependencies on external Rust libraries required by the generated service library.
+        extern_paths: A mapping of (fully-qualified) proto package names to external crate paths.
+            Also see https://docs.rs/tonic-build-codec/latest/tonic_build_codec/struct.Builder.html#method.extern_path.
+        **kwargs: Any other arguments to rust_library.
+    """
+
+    generate_service_lib_name = name + "_gen"
+    _generate_service_lib(
+        name = generate_service_lib_name,
+        srcs = srcs,
+        out_filename = name + ".rs",
+        extern_paths = extern_paths,
+    )
+
+    rust_library(
+        name = name,
+        srcs = [":" + generate_service_lib_name],
+        deps = [
+            Label("@oak_crates_index//:tonic"),
+        ] + deps,
+        **kwargs
+    )
---
+++ oak_grpc_utils/main.rs
@@ -0,0 +1,179 @@
+//
+// Copyright 2025 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+use std::{collections::HashMap, fs, path::PathBuf};
+
+use anyhow::anyhow;
+use clap::Parser;
+use serde_json::Value;
+
+#[derive(Parser)]
+struct Flags {
+    #[arg(long, value_delimiter = ',', required = true)]
+    files_to_compile: Vec<PathBuf>,
+
+    #[arg(long, value_delimiter = ',', required = true)]
+    proto_source_roots: Vec<PathBuf>,
+
+    #[arg(long, required = true)]
+    out_filename: PathBuf,
+
+    #[arg(long, required = true)]
+    out_dir: PathBuf,
+
+    #[arg(long, required = true)]
+    extern_paths: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let Flags { files_to_compile, proto_source_roots, out_filename, out_dir, extern_paths } =
+        Flags::parse();
+
+    // Generate a file per proto package included in `files_to_compile`.
+    oak_grpc_utils::generate_grpc_code(
+        &files_to_compile,
+        &proto_source_roots,
+        oak_grpc_utils::CodegenOptions {
+            build_server: true,
+            build_client: true,
+            extern_paths: parse_extern_paths(&extern_paths)?,
+        },
+    )?;
+
+    let output_files = fs::read_dir(&out_dir)?.collect::<Result<Vec<_>, _>>()?;
+
+    // Generate a single "lib" file in which the per-proto-package files will be
+    // included.
+    let mut generated_file = File { mods: HashMap::new() };
+    for output_file in output_files {
+        let filename = output_file
+            .file_name()
+            .into_string()
+            .map_err(|_| anyhow!("failed to map OsString filename to String"))?;
+        let mut filename_parts = filename.split(".");
+
+        // Get a handle on the top-level Rust `mod` implied by this filename.
+        let mut current_mod = generated_file.get_mod(
+            filename_parts.next().ok_or(anyhow!("filename appears to be empty"))?.to_string(),
+        );
+
+        // Recurse down the `mod` tree until we find the `mod` that should include this
+        // filename.
+        let filename_parts: Vec<_> = filename_parts.collect();
+        // Skip the last filename_part because it is (expected to be) "rs".
+        for filename_part in filename_parts.clone().into_iter().take(filename_parts.len() - 1) {
+            current_mod = current_mod.get_mod(filename_part.to_string());
+        }
+
+        // Update the `mod` that should include this filename. There can be only one
+        // such included filename per `mod`.
+        current_mod.included_file = Some(
+            output_file
+                .file_name()
+                .into_string()
+                .map_err(|_| anyhow!("failed to map OsString filename to String"))?,
+        );
+    }
+
+    // Finally, write out the resulting file (containing `mod` and `include!`
+    // statements).
+    let out_dir_basename = out_dir
+        .file_name()
+        .ok_or(anyhow!("out_dir does not have a filename"))?
+        .to_str()
+        .ok_or(anyhow!("failed to map OsString filename to String"))?;
+    fs::write(out_filename, generated_file.as_code(&out_dir_basename.to_string()).as_bytes())?;
+
+    Ok(())
+}
+
+fn parse_extern_paths(extern_paths: &str) -> anyhow::Result<Vec<oak_grpc_utils::ExternPath>> {
+    Ok(match serde_json::from_str(extern_paths)? {
+        Value::Object(extern_paths_map) => {
+            let mut extern_paths = Vec::new();
+            for (key, value) in extern_paths_map {
+                extern_paths.push(oak_grpc_utils::ExternPath::new(
+                    &key,
+                    value.as_str().ok_or(anyhow!("unexpected extern_paths type"))?,
+                ));
+            }
+            extern_paths
+        }
+        _ => anyhow::bail!("unexpected extern_paths type"),
+    })
+}
+
+#[derive(Debug)]
+struct File {
+    mods: HashMap<String, Mod>,
+}
+
+impl File {
+    fn get_mod(&mut self, name: String) -> &mut Mod {
+        self.mods.entry(name.clone()).or_insert(Mod::new(name))
+    }
+
+    fn as_code(&self, include_prefix: &String) -> String {
+        as_code(&mut self.mods.values(), include_prefix)
+    }
+}
+
+#[derive(Debug)]
+struct Mod {
+    name: String,
+    included_file: Option<String>,
+    mods: HashMap<String, Mod>,
+}
+
+impl Mod {
+    fn new(name: String) -> Self {
+        Mod { name, included_file: None, mods: HashMap::new() }
+    }
+
+    fn get_mod(&mut self, name: String) -> &mut Mod {
+        self.mods.entry(name.clone()).or_insert(Self::new(name))
+    }
+
+    fn as_code(&self, include_prefix: &String) -> String {
+        let mod_name = &self.name;
+        let inner_code = indent(as_code(&mut self.mods.values(), include_prefix));
+        match &self.included_file {
+            None => {
+                format!(
+                    "pub mod {mod_name} {{
+{inner_code}
+}}"
+                )
+            }
+            Some(included_file) => {
+                format!(
+                    "pub mod {mod_name} {{
+    include!(\"{include_prefix}/{included_file}\");
+{inner_code}
+}}"
+                )
+            }
+        }
+    }
+}
+
+fn as_code(mods: &mut dyn Iterator<Item = &Mod>, include_prefix: &String) -> String {
+    mods.map(|inner_mod| inner_mod.as_code(include_prefix)).collect::<Vec<_>>().join("\n")
+}
+
+fn indent(str: String) -> String {
+    str.split("\n").map(|line| "    ".to_owned() + line).collect::<Vec<_>>().join("\n")
+}
