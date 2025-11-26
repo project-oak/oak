4658faf89809d8ca08ddc0060ffd6a02ab043c2d
Ben Birt <benbirt@google.com>
2025-11-26 17:05:08 +0000

    Extract some common proto library generator code to a shared location. (Used by `rust_micro_rpc_library` and `rust_tonic_grpc_library`.)
    Change-Id: Iba2ac585638f1f0f611c06d30a26f15725ae1a6f

--- micro_rpc_build/main.rs
+++ micro_rpc_build/main.rs
@@ -14,7 +14,7 @@
 // limitations under the License.
 //
 
-use std::{collections::HashMap, fs, path::PathBuf};
+use std::{fs, path::PathBuf};
 
 use anyhow::anyhow;
 use clap::Parser;
@@ -56,40 +56,8 @@ fn main() -> anyhow::Result<()> {
             ..Default::default()
         },
     );
-    let output_files = fs::read_dir(&out_dir)?.collect::<Result<Vec<_>, _>>()?;
 
-    // Generate a single "lib" file in which the per-proto-package files will be
-    // included.
-    let mut generated_file = File { mods: HashMap::new() };
-    for output_file in output_files {
-        let filename = output_file
-            .file_name()
-            .into_string()
-            .map_err(|_| anyhow!("failed to map OsString filename to String"))?;
-        let mut filename_parts = filename.split(".");
-
-        // Get a handle on the top-level Rust `mod` implied by this filename.
-        let mut current_mod = generated_file.get_mod(
-            filename_parts.next().ok_or(anyhow!("filename appears to be empty"))?.to_string(),
-        );
-
-        // Recurse down the `mod` tree until we find the `mod` that should include this
-        // filename.
-        let filename_parts: Vec<_> = filename_parts.collect();
-        // Skip the last filename_part because it is (expected to be) "rs".
-        for filename_part in filename_parts.clone().into_iter().take(filename_parts.len() - 1) {
-            current_mod = current_mod.get_mod(filename_part.to_string());
-        }
-
-        // Update the `mod` that should include this filename. There can be only one
-        // such included filename per `mod`.
-        current_mod.included_file = Some(
-            output_file
-                .file_name()
-                .into_string()
-                .map_err(|_| anyhow!("failed to map OsString filename to String"))?,
-        );
-    }
+    let generated_file = proto_gen::ProtoLib::from_files_in_dir(&out_dir)?;
 
     // Finally, write out the resulting file (containing `mod` and `include!`
     // statements).
@@ -98,7 +66,10 @@ fn main() -> anyhow::Result<()> {
         .ok_or(anyhow!("out_dir does not have a filename"))?
         .to_str()
         .ok_or(anyhow!("failed to map OsString filename to String"))?;
-    fs::write(out_filename, generated_file.as_code(&out_dir_basename.to_string()).as_bytes())?;
+    fs::write(
+        out_filename,
+        proto_lib_as_code(&generated_file, &out_dir_basename.to_string()).as_bytes(),
+    )?;
 
     Ok(())
 }
@@ -119,72 +90,47 @@ fn parse_extern_paths(extern_paths: &str) -> anyhow::Result<Vec<micro_rpc_build:
     })
 }
 
-#[derive(Debug)]
-struct File {
-    mods: HashMap<String, Mod>,
-}
-
-impl File {
-    fn get_mod(&mut self, name: String) -> &mut Mod {
-        self.mods.entry(name.clone()).or_insert(Mod::new(name))
-    }
-
-    fn as_code(&self, include_prefix: &String) -> String {
-        let inner_code = as_code(&mut self.mods.values(), include_prefix);
-        format!(
-            "
+fn proto_lib_as_code(file: &proto_gen::ProtoLib, include_prefix: &String) -> String {
+    let inner_code = mods_as_code(file.mods.values(), include_prefix);
+    format!(
+        "
 #![no_std]
 #![feature(never_type)]
 #[allow(unused_imports)]
 {inner_code}
 "
-        )
-    }
+    )
 }
 
-#[derive(Debug)]
-struct Mod {
-    name: String,
-    included_file: Option<String>,
-    mods: HashMap<String, Mod>,
-}
-
-impl Mod {
-    fn new(name: String) -> Self {
-        Mod { name, included_file: None, mods: HashMap::new() }
-    }
-
-    fn get_mod(&mut self, name: String) -> &mut Mod {
-        self.mods.entry(name.clone()).or_insert(Self::new(name))
-    }
-
-    fn as_code(&self, include_prefix: &String) -> String {
-        let mod_name = &self.name;
-        let inner_code = indent(as_code(&mut self.mods.values(), include_prefix));
-        match &self.included_file {
-            None => {
-                format!(
-                    "pub mod {mod_name} {{
+fn mod_as_code(module: &proto_gen::Mod, include_prefix: &String) -> String {
+    let mod_name = &module.name;
+    let inner_code = indent(mods_as_code(module.mods.values(), include_prefix));
+    match &module.included_filename {
+        None => {
+            format!(
+                "pub mod {mod_name} {{
 {inner_code}
 }}"
-                )
-            }
-            Some(included_file) => {
-                format!(
-                    "#[allow(clippy::let_unit_value)]
+            )
+        }
+        Some(included_file) => {
+            format!(
+                "#[allow(clippy::let_unit_value)]
 pub mod {mod_name} {{
     use prost::Message;
     include!(\"{include_prefix}/{included_file}\");
 {inner_code}
 }}"
-                )
-            }
+            )
         }
     }
 }
 
-fn as_code(mods: &mut dyn Iterator<Item = &Mod>, include_prefix: &String) -> String {
-    mods.map(|inner_mod| inner_mod.as_code(include_prefix)).collect::<Vec<_>>().join("\n")
+fn mods_as_code<'a>(
+    mods: impl Iterator<Item = &'a proto_gen::Mod>,
+    include_prefix: &String,
+) -> String {
+    mods.map(|inner_mod| mod_as_code(inner_mod, include_prefix)).collect::<Vec<_>>().join("\n")
 }
 
 fn indent(str: String) -> String {
