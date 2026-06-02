c4f8c58a1912a782b520742f441886dd0dabedec
grobler <grobler@google.com>
2026-06-02 13:28:06 +0000

    Update the Oak Containers kernel
    The latest release of the 6.12 LTS version is 6.12.90.
    
    Change-Id: Iaa12ea7db11242cd18b0e09ee81768986a6a6964

--- oak_containers/kernel/kernel-common.nix
+++ oak_containers/kernel/kernel-common.nix
@@ -5,7 +5,7 @@ with { linux_version = pkgs.lib.removeSuffix "\n" untrimmed_version; };
   version = linux_version;
   src = builtins.fetchurl {
     url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${linux_version}.tar.xz";
-    sha256 = "e35ac999f40a6874493d8d60f33f1150d7a89ae5841c428da82257fbcd070aed";
+    sha256 = "1f1a1ff057468b8a6284d3ce1d181278259beb5670ea16724c46dbf8820fa2a8";
   };
   # To allow reproducibility, the following options need to be configured:
   # - CONFIG_MODULE_SIG is not set
