fbe7133a3c7f387cd6c233da8502d9a7022d8640
grobler <grobler@google.com>
2026-09-21 10:48:37 +0000

    Update the Containers Linux Kernel version
    Change-Id: I4631edd146829b31f08131f5171c6e436a6a6964

--- oak_containers/kernel/kernel-common.nix
+++ oak_containers/kernel/kernel-common.nix
@@ -5,7 +5,7 @@ with { linux_version = pkgs.lib.removeSuffix "\n" untrimmed_version; };
   version = linux_version;
   src = builtins.fetchurl {
     url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${linux_version}.tar.xz";
-    sha256 = "f143aaade8877ba5616e788b4482576db28481bcf557ef537f4fcc3938fc3176";
+    sha256 = "8cee19e1839bb6ff4d5254d761933ae6ab670492d5ed030e09a80538320d5c4c";
   };
   # To allow reproducibility, the following options need to be configured:
   # - CONFIG_MODULE_SIG is not set
